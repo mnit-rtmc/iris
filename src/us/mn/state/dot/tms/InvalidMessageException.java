4b5ce6d3d806af3d9cab0051c7c380388df7f722
Douglas Lau <doug.lau@state.mn.us>
2007-11-06 09:34:02 -0600

    Cleaned up InvalidMessagesException.

--- src/us/mn/state/dot/tms/InvalidMessageException.java
+++ src/us/mn/state/dot/tms/InvalidMessageException.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000,2001  Minnesota Department of Transportation
+ * Copyright (C) 2000-2007  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -11,15 +11,11 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 package us.mn.state.dot.tms;
 
 /**
- * InvalidMessageException indicates that a drum sign does not contain the
+ * InvalidMessageException indicates that a sign is not able to display the
  * specified message.
  *
  * @author Douglas Lau
