da4cbea077a4cdb11e099b9a5ccd4f65445da16f
Douglas Lau <doug.lau@state.mn.us>
2008-01-09 14:29:10 -0600

    Added a missing hashCode definition (FindBugs).

--- src/us/mn/state/dot/tms/client/security/IrisPermission.java
+++ src/us/mn/state/dot/tms/client/security/IrisPermission.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2007  Minnesota Department of Transportation
+ * Copyright (C) 2000-2008  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -37,6 +37,10 @@ public class IrisPermission implements Permission {
 	public boolean equals(Object obj) {
 		return (obj instanceof IrisPermission) &&
 			(toString().equals(obj.toString()));
+	}
+
+	public int hashCode() {
+		return toString().hashCode();
 	}
 
 	static public final IrisPermission DMS_TAB =
