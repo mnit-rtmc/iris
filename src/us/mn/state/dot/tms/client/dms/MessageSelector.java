8f45bda0d9a58b595dd7bc30fe3db861d28b25b4
Douglas Lau <doug.lau@state.mn.us>
2008-01-09 11:13:38 -0600

    Minor cleanup found by FindBugs.

--- src/us/mn/state/dot/tms/client/dms/MessageSelector.java
+++ src/us/mn/state/dot/tms/client/dms/MessageSelector.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2007  Minnesota Department of Transportation
+ * Copyright (C) 2000-2008  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -156,7 +156,7 @@ public class MessageSelector extends JPanel {
 		if(message == null) {
 			if(lastMessage != null) {
 				clearSelections();
-				lastMessage = message;
+				lastMessage = null;
 			}
 			return;
 		} else if(message.equals(lastMessage))
