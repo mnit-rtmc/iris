8240cff1e20e11de1c85b31791a91566aafce0d8
Douglas Lau <doug.lau@state.mn.us>
2008-01-09 13:24:05 -0600

    Added SignMessage.hashCode() (found by FindBugs).

--- src/us/mn/state/dot/tms/SignMessage.java
+++ src/us/mn/state/dot/tms/SignMessage.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2006  Minnesota Department of Transportation
+ * Copyright (C) 2000-2008  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -75,6 +75,11 @@ public class SignMessage implements Serializable {
 	/** Check if a string matches the message */
 	public boolean equals(String s) {
 		return multi.equals(s);
+	}
+
+	/** Calculate a hash code for the message */
+	public int hashCode() {
+		return multi.hashCode();
 	}
 
 	/** Is the message blank? */
