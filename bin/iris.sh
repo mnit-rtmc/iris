c54349b3bf6e1babd2ffb043974d4beebfc3e4b7
Douglas Lau <doug.lau@state.mn.us>
2008-03-31 14:18:49 -0500

    Added debug agent to client iris.sh script.

--- bin/iris.sh
+++ bin/iris.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xms64m -Xmx256m -jar lib/iris-client-@@VERSION@@.jar iris-client.properties
+java -Xms64m -Xmx256m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n -jar lib/iris-client-@@VERSION@@.jar iris-client.properties
