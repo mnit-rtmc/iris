b1a92ac3d558d9737cfb51b03cdc205171c9da9c
Douglas Lau <doug.lau@state.mn.us>
2008-04-25 10:47:34 -0500

    Minor whitespace cleanup.

--- src/us/mn/state/dot/tms/client/dms/DMSTab.java
+++ src/us/mn/state/dot/tms/client/dms/DMSTab.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2007  Minnesota Department of Transportation
+ * Copyright (C) 2000-2008  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -44,10 +44,10 @@ public class DMSTab extends MapTab {
 
 	/** Create a new DMS tab */
 	public DMSTab(List<LayerState> lstates, ViewLayer vlayer,
-		DMSHandler handler,final SonarState st)
+		DMSHandler handler, final SonarState st)
 	{
 		super("DMS", "Operate Dynamic Message Signs");
-		dispatcher = new DMSDispatcher(handler,st);
+		dispatcher = new DMSDispatcher(handler, st);
 		chooser = new DmsStatusSummary(handler);
 		map.addLayers(lstates);
 		tabPanel = createSideBar();
