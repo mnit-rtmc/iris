70ee3c87dede5dec62c4441087bed06a16edbf84
Douglas Lau <doug.lau@state.mn.us>
2008-09-24 11:45:23 -0500

    Updated SQL scripts for system attributes and traffic device attributes.

--- sql/tms-template.sql
+++ sql/tms-template.sql
@@ -406,6 +406,19 @@ CREATE RULE warning_sign_update AS ON UPDATE TO iris.warning_sign DO INSTEAD
 CREATE RULE warning_sign_delete AS ON DELETE TO iris.warning_sign DO INSTEAD
 	DELETE FROM iris._device_io WHERE name = OLD.name;
 
+CREATE TABLE traffic_device_attribute (
+	name VARCHAR(32) PRIMARY KEY,
+	id text NOT NULL,
+	aname VARCHAR(32) NOT NULL,
+	avalue VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE system_attribute (
+	name VARCHAR(32) PRIMARY KEY,
+	value VARCHAR(64) NOT NULL
+);
+
+INSERT INTO system_attribute VALUES('database_version', '3.78.0');
 
 
 CREATE TABLE vault_object (
