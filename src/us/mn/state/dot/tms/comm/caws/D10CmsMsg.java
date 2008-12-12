0f7868949000558a2de7094e75d6c2626cfb8675
Douglas Lau <doug.lau@state.mn.us>
2008-12-12 14:32:41 -0600

    Use String.split instead of StringTokenizer in D10CmsMsg (CAWS).

--- src/us/mn/state/dot/tms/comm/caws/D10CmsMsg.java
+++ src/us/mn/state/dot/tms/comm/caws/D10CmsMsg.java
@@ -18,7 +18,6 @@ import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.StringTokenizer;
 import us.mn.state.dot.tms.DMS;
 import us.mn.state.dot.tms.DMSImpl;
 import us.mn.state.dot.tms.DMSMessagePriority;
@@ -33,6 +32,7 @@ import us.mn.state.dot.tms.utils.SString;
  * CAWS D10CmsMsg. This is a single CMS message.
  *
  * @author Michael Darter
+ * @author Douglas Lau
  */
 public class D10CmsMsg implements Serializable
 {
@@ -68,68 +68,51 @@ public class D10CmsMsg implements Serializable
 	 * @param argline a single CMS message, fields delimited with ';'.
 	 */
 	public void parse(String argline) throws IllegalArgumentException {
-
-		// add a space between successive delimiters. This is done so the
-		// tokenizer doesn't skip over delimeters with nothing between them.
-		// System.err.println("D10CmsMsg.D10CmsMsg() called, argline="+argline);
-		String line = argline.replace(";;", "; ;");
-
-		// System.err.println("D10CmsMsg.D10CmsMsg() called, line1="+line);
-		line = line.replace(";;", "; ;");
-
-		// System.err.println("D10CmsMsg.D10CmsMsg() called, line2="+line);
-		// verify syntax
-		StringTokenizer tok = new StringTokenizer(line, ";");
-
-		// validity check
-		int numtoks = tok.countTokens();
-		if(numtoks != 13) {
+		String[] f = (argline + ' ').split(";");
+		if(f.length != 13) {
 			throw new IllegalArgumentException(
 			    "Bogus CMS message format (" + argline + ").");
 		}
 
 		// date: 20080403085910
-		m_date = convertDate(tok.nextToken());
+		m_date = convertDate(f[0]);
 
 		// id: 39
-		m_cmsid = SString.stringToInt(tok.nextToken());
+		m_cmsid = SString.stringToInt(f[1]);
 
 		// message description
-		String f02 = tok.nextToken();
-		if(!f02.equals(DESC_BLANK) &&!f02.equals(
-			DESC_ONEPAGENORM) &&!f02.equals(
-			DESC_TWOPAGENORM)) {    // FIXME: verify possibilities
-			System.err.println(
-			    "WARNING: unknown message description received in D10CmsMsg.parse(): "
-			    + f02);
+		if(!f[2].equals(DESC_BLANK) &&
+		   !f[2].equals(DESC_ONEPAGENORM) &&
+		   !f[2].equals(DESC_TWOPAGENORM))
+		{
+			// FIXME: verify possibilities
+			System.err.println("WARNING: unknown message " +
+				"description received in D10CmsMsg.parse(): " +
+				f[2]);
 		}
 
-		m_desc = f02;
+		m_desc = f[2];
 
 		// pg 1 font
-		String f03 = tok.nextToken();
-		if(!f03.equals(SINGLESTROKE) &&!f03.equals(DOUBLESTROKE)) {
-			System.err.println(
-			    "WARNING: unknown pg 1 font received in D10CmsMsg.parse(): "
-			    + f03);
+		if(!f[3].equals(SINGLESTROKE) && !f[3].equals(DOUBLESTROKE)) {
+			System.err.println("WARNING: unknown pg 1 font " +
+				"received in D10CmsMsg.parse(): " + f[3]);
 		}
 
 		// pg 2 font
-		String f04 = tok.nextToken();
-		if(!f04.equals(SINGLESTROKE) &&!f04.equals(DOUBLESTROKE)) {
-			System.err.println(
-			    "WARNING: unknown pg 2 font received in D10CmsMsg.parse(): "
-			    + f04);
+		if(!f[4].equals(SINGLESTROKE) &&! f[4].equals(DOUBLESTROKE)) {
+			System.err.println("WARNING: unknown pg 2 font " +
+				"received in D10CmsMsg.parse(): " + f[4]);
 		}
 
 		// rows of text
 		{
-			String row1 = tok.nextToken().trim().toUpperCase();
-			String row2 = tok.nextToken().trim().toUpperCase();
-			String row3 = tok.nextToken().trim().toUpperCase();
-			String row4 = tok.nextToken().trim().toUpperCase();
-			String row5 = tok.nextToken().trim().toUpperCase();
-			String row6 = tok.nextToken().trim().toUpperCase();
+			String row1 = f[5].trim().toUpperCase();
+			String row2 = f[6].trim().toUpperCase();
+			String row3 = f[7].trim().toUpperCase();
+			String row4 = f[8].trim().toUpperCase();
+			String row5 = f[9].trim().toUpperCase();
+			String row6 = f[10].trim().toUpperCase();
 
 			// create message-pg1
 			MultiString m = new MultiString();
@@ -152,10 +135,8 @@ public class D10CmsMsg implements Serializable
 		}
 
 		// on time: 0.0
-		m_ontime = SString.stringToDouble(tok.nextToken());
+		m_ontime = SString.stringToDouble(f[11]);
 
-		// ignore this field, nothing there
-		String f12 = tok.nextToken();
 		if(!m_desc.equals(DESC_BLANK)) {
 			System.err.println("D10CmsMsg.D10CmsMsg():" + m_date
 				+ "," + m_cmsid + "," + m_multistring + ","
