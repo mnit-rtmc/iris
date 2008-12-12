839e01b1549b46dbecf263b91e44361dad758ede
Douglas Lau <doug.lau@state.mn.us>
2008-12-12 15:03:30 -0600

    Whitespace cleanups.

--- src/us/mn/state/dot/tms/comm/caws/D10CmsMsg.java
+++ src/us/mn/state/dot/tms/comm/caws/D10CmsMsg.java
@@ -14,7 +14,6 @@
  */
 package us.mn.state.dot.tms.comm.caws;
 
-import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -33,7 +32,7 @@ import us.mn.state.dot.tms.utils.SString;
  * @author Michael Darter
  * @author Douglas Lau
  */
-public class D10CmsMsg implements Serializable {
+public class D10CmsMsg {
 
 	// consts
 	private static final String DESC_BLANK = "Blank";
@@ -55,7 +54,7 @@ public class D10CmsMsg implements Serializable {
 	/**
 	 * Create a new object using text line.
 	 * @param line A single line from the CAWS message file. e.g.
-	 *             "20080403085910;25;Blank;Single Stroke;Single Stroke;;;;;;;0.0;"
+	 *     "20080403085910;25;Blank;Single Stroke;Single Stroke;;;;;;;0.0;"
 	 */
 	public D10CmsMsg(String line) throws IllegalArgumentException {
 		String[] f = (argline + ' ').split(";");
@@ -200,7 +199,6 @@ public class D10CmsMsg implements Serializable {
 
 	/** get message type */
 	public CawsMsgType getCawsMsgType() {
-
 		CawsMsgType ret = CawsMsgType.BLANK;
 
 		if(m_desc.equals(DESC_BLANK)) {
@@ -212,57 +210,64 @@ public class D10CmsMsg implements Serializable {
 		} else if(false) {	//FIXME: add in the future
 			ret = CawsMsgType.TRAVELTIME;
 		} else {
-			String msg="D10CmsMsg.getCawsMsgType: Warning: unknown D10 message description encountered ("+m_desc+").";
-			assert false : msg;
+			String msg = "D10CmsMsg.getCawsMsgType: Warning: " +
+				"unknown D10 message description (" + m_desc +
+				").";
+			assert false: msg;
 			System.err.println(msg);
-			ret = CawsMsgType.BLANK;
 		}
 
 		return ret;
 	}
 
 	/**
-	 *  Activate the message. CAWS messages are activated only if the DMS is
-	 *  currently blank or contains a message owned by CAWS.
-	 *  @param dms Activate the message on this DMS.
+	 * Activate the message. CAWS messages are activated only if the DMS is
+	 * currently blank or contains a message owned by CAWS.
+	 * @param dms Activate the message on this DMS.
 	 */
 	public void activate(DMSImpl dms) {
-		System.err.println("-----D10CmsMsg.activate("+dms+") called, msg=" + this);
-		boolean activate=shouldSendMessage(dms);
-		if(activate)
+		System.err.println("-----D10CmsMsg.activate(" + dms +
+			") called, msg=" + this);
+		if(shouldSendMessage(dms))
 			sendMessage(dms);
 	}
 
 	/**
-	 * decide if a caws message should be sent to a DMS.
+	 * Decide if a caws message should be sent to a DMS.
 	 * @params dms The associated DMS.
 	 * @return true to send the message.
 	 */
 	protected boolean shouldSendMessage(DMSImpl dms) {
-		if(dms == null || m_multistring.length()<=0)
+		if(dms == null)
 			return false;
 
 		// is caws activated for the sign?
 		if(!TrafficDeviceAttributeHelper.awsControlled(
 			getIrisCmsId())) 
 		{
-			System.err.println("D10CmsMsg.shouldSendMessage(): DMS " + getIrisCmsId() + " is NOT activated for CAWS control.");
+			System.err.println("D10CmsMsg.shouldSendMessage(): DMS "
+				+ getIrisCmsId() +
+				" is NOT activated for CAWS control.");
 			return false;
 		}
-		System.err.println("D10CmsMsg.shouldSendMessage(): DMS "+getIrisCmsId()+" is activated for CAWS control.");
+		System.err.println("D10CmsMsg.shouldSendMessage(): DMS " +
+			getIrisCmsId() + " is activated for CAWS control.");
 
 		// be safe and send the caws message by default
-		boolean send=true;
+		boolean send = true;
 
 		// message already deployed?
 		if(dms.getStatusCode() == DMS.STATUS_DEPLOYED) {
 			SignMessage cur = dms.getMessage();
-			if(cur!=null)
+			if(cur != null)
 				send = !cur.equals(m_multistring);
-			System.err.println("D10CmsMsg.shouldSendMessage(): DMS is deployed, m_multistring="+m_multistring+", cur msg="+cur.toString());
+			System.err.println("D10CmsMsg.shouldSendMessage(): " +
+				"DMS is deployed, m_multistring=" +
+				m_multistring + ", cur msg=" + cur.toString());
 		}
 
-		System.err.println("D10CmsMsg.shouldSendMessage(): should send="+send);
+		System.err.println("D10CmsMsg.shouldSendMessage(): should send="
+			+ send);
 		return send;
 	}
 
@@ -276,12 +281,14 @@ public class D10CmsMsg implements Serializable {
 		case ONEPAGEMSG:
 		case TWOPAGEMSG:
 			// blank, 1 or 2 pg msg
-			System.err.println("D10CmsMsg.sendMessage(): will activate DMS " + getIrisCmsId() + ":" + this);
+			System.err.println("D10CmsMsg.sendMessage(): will " +
+				"activate DMS " + getIrisCmsId() + ":" + this);
 			try {
 				dms.sendMessage(toSignMessage(dms));
 			}
 			catch(TMSException e) {
-				System.err.println("D10CmsMsg.sendMessage(): exception:" + e);
+				System.err.println("D10CmsMsg.sendMessage(): " +
+					"exception:" + e);
 			}
 			break;
 		case TRAVELTIME:
@@ -289,7 +296,8 @@ public class D10CmsMsg implements Serializable {
 			//FIXME: add in future
 			break;
 		default:
-			assert false: "D10CmsMsg.sendMessage(): unknown CawsMsgType";
+			assert false:
+				"D10CmsMsg.sendMessage(): unknown CawsMsgType";
 		}
 	}
 
@@ -318,7 +326,6 @@ public class D10CmsMsg implements Serializable {
 
 	/** get the CMS id in IRIS form, e.g. "V39" */
 	public String getIrisCmsId() {
-		Integer id = this.getCmsId();
-		return "V" + id.toString();
+		return "V" + getCmsId();
 	}
 }
