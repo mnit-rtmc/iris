83ead276e7f8a550b96021c809bba07bf4c6b95b
Douglas Lau <doug.lau@state.mn.us>
2009-01-26 09:31:10 -0600

    Fixed a fromInt -> fromOrdinal conversion.

--- src/us/mn/state/dot/tms/comm/ntcip/DefaultJustificationLine.java
+++ src/us/mn/state/dot/tms/comm/ntcip/DefaultJustificationLine.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2006  Minnesota Department of Transportation
+ * Copyright (C) 2000-2009  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -39,7 +39,7 @@ public class DefaultJustificationLine extends MultiCfg implements ASN1Integer {
 
 	/** Set the integer value */
 	public void setInteger(int value) {
-		justification = MultiString.JustificationLine.fromInt(value);
+		justification =MultiString.JustificationLine.fromOrdinal(value);
 	}
 
 	/** Get the integer value */
