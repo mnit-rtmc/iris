da952add8b38bac130efcf3a87e3a1b2a4b2f0d0
Douglas Lau <doug.lau@state.mn.us>
2009-02-02 10:33:21 -0600

    Minor cleanup.

--- src/us/mn/state/dot/tms/client/dms/SignTextComboBoxModel.java
+++ src/us/mn/state/dot/tms/client/dms/SignTextComboBoxModel.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2008  Minnesota Department of Transportation
+ * Copyright (C) 2008-2009  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -88,10 +88,12 @@ public class SignTextComboBoxModel extends AbstractListModel
 
 	/** Get the selected item */
 	public Object getSelectedItem() {
+		SignText st = m_selected;
 		// this is a hack, see the note in ignoreLineHack()
-		if(m_selected != null && SDMS.ignoreLineHack(m_selected.toString()))
+		if(st != null && SDMS.ignoreLineHack(st.toString()))
 			return BLANK_SIGN_TEXT;
-		return m_selected;
+		else
+			return st;
 	}
 
 	/** 
