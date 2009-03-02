a5b73ea4cbbb4e108226d47c49a5f247d2ef8ab8
Douglas Lau <doug.lau@state.mn.us>
2009-03-02 11:02:59 -0600

    Allow Device2 to be referred in other sonar interfaces (TimingPlan).

--- src/us/mn/state/dot/tms/Device2.java
+++ src/us/mn/state/dot/tms/Device2.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2008  Minnesota Department of Transportation
+ * Copyright (C) 2000-2009  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,6 +21,15 @@ package us.mn.state.dot.tms;
  * @author Douglas Lau
  */
 public interface Device2 extends ControllerIO_SONAR {
+
+	/** SONAR type names */
+	String[] SONAR_TYPES = {
+		Camera.SONAR_TYPE,
+		Detector.SONAR_TYPE,
+		DMS.SONAR_TYPE,
+		RampMeter.SONAR_TYPE,
+		WarningSign.SONAR_TYPE
+	};
 
 	/** Set the administrator notes */
 	void setNotes(String n);
