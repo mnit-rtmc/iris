8a2b11ebb5796ee96f2301dfa681a5adb51bac3d
Douglas Lau <doug.lau@state.mn.us>
2009-04-03 10:09:36 -0500

    Only check that major and minor database versions match before starting server.

--- src/us/mn/state/dot/tms/SystemAttributeHelper.java
+++ src/us/mn/state/dot/tms/SystemAttributeHelper.java
@@ -393,8 +393,35 @@ public class SystemAttributeHelper {
 
 	/** Validate the database version */
 	static protected void validateDatabaseVersion() {
-		shutdownValidate(SystemAttribute.DATABASE_VERSION,
-			new String[] {"@@VERSION@@"});
+		String c_version = "@@VERSION@@";
+		String db_version = getValue(SystemAttribute.DATABASE_VERSION);
+		if(!validateVersions(c_version, db_version)) {
+			StringBuilder b = new StringBuilder();
+			b.append("Failure: database_version (");
+			b.append(db_version);
+			b.append(") does not match the required value (");
+			b.append(c_version);
+			b.append(").  Shutting down.");
+			System.err.println(b.toString());
+			System.exit(1);
+		}
+	}
+
+	/** Validate the database version */
+	static protected boolean validateVersions(String v0, String v1) {
+		String[] va0 = v0.split("\\.");
+		String[] va1 = v1.split("\\.");
+		// Versions must be "major.minor.micro"
+		if(va0.length != 3 || va1.length != 3)
+			return false;
+		// Check that major versions match
+		if(!va0[0].equals(va1[0]))
+			return false;
+		// Check that minor versions match
+		if(!va0[1].equals(va1[1]))
+			return false;
+		// It's OK if micro versions don't match
+		return true;
 	}
 
 	/** Validate the agency id */
