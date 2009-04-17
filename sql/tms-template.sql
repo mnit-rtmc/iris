198f8b36cc515da48c6271e7cb08d615d668b0da
Douglas Lau <doug.lau@state.mn.us>
2009-04-17 15:16:48 -0500

    Added SQL migrate script for 3.83.

--- sql/tms-template.sql
+++ sql/tms-template.sql
@@ -979,12 +979,38 @@ CREATE VIEW controller_view AS
 	JOIN cabinet cab ON c.cabinet = cab.name;
 GRANT SELECT ON controller_view TO PUBLIC;
 
+CREATE VIEW iris.controller_dms AS
+	SELECT dio.name, dio.controller, dio.pin, d.geo_loc
+	FROM iris._device_io dio
+	JOIN iris.dms d ON dio.name = d.name;
+
+CREATE VIEW iris.controller_meter AS
+	SELECT dio.name, dio.controller, dio.pin, m.geo_loc
+	FROM iris._device_io dio
+	JOIN iris.ramp_meter m ON dio.name = m.name;
+
+CREATE VIEW iris.controller_warning_sign AS
+	SELECT dio.name, dio.controller, dio.pin, s.geo_loc
+	FROM iris._device_io dio
+	JOIN iris.warning_sign s ON dio.name = s.name;
+
+CREATE VIEW iris.controller_camera AS
+	SELECT dio.name, dio.controller, dio.pin, c.geo_loc
+	FROM iris._device_io dio
+	JOIN iris.camera c ON dio.name = c.name;
+
+CREATE VIEW iris.controller_device AS
+	SELECT * FROM iris.controller_dms UNION ALL
+	SELECT * FROM iris.controller_meter UNION ALL
+	SELECT * FROM iris.controller_warning_sign UNION ALL
+	SELECT * FROM iris.controller_camera;
+
 CREATE VIEW controller_device_view AS
-	SELECT d.id, d.controller, d.pin, d.geo_loc,
+	SELECT d.name, d.controller, d.pin, d.geo_loc,
 	trim(l.freeway || ' ' || l.free_dir) AS freeway,
 	trim(trim(' @' FROM l.cross_mod || ' ' || l.cross_street)
 		|| ' ' || l.cross_dir) AS cross_street
-	FROM traffic_device d
+	FROM iris.controller_device d
 	JOIN geo_loc_view l ON d.geo_loc = l.name;
 GRANT SELECT ON controller_device_view TO PUBLIC;
 
@@ -992,19 +1018,13 @@ CREATE VIEW controller_report AS
 	SELECT c.name, c.comm_link, c.drop_id, cab.mile, cab.geo_loc,
 	trim(l.freeway || ' ' || l.free_dir) || ' ' || l.cross_mod || ' ' ||
 		trim(l.cross_street || ' ' || l.cross_dir) AS "location",
-	cab.style AS "type", d1.id AS "id (meter1)",
-	d1.cross_street AS "from (meter1)", d1.freeway AS "to (meter1)",
-	d2.id AS meter2, d2.cross_street AS "from (meter2)",
-	d2.freeway AS "to (meter2)", c.notes
+	cab.style AS "type", d.name AS device, d.pin,
+	d.cross_street AS cross_street, d.freeway AS freeway, c.notes
 	FROM controller c
 	LEFT JOIN cabinet cab ON c.cabinet = cab.name
 	LEFT JOIN geo_loc_view l ON cab.geo_loc = l.name
-	LEFT JOIN controller_device_view d1 ON
-		d1.pin = 2 AND d1.controller = c.name
-	LEFT JOIN controller_device_view d2 ON
-		d2.pin = 3 AND d2.controller = c.name;
+	LEFT JOIN controller_device_view d ON d.controller = c.name;
 GRANT SELECT ON controller_report TO PUBLIC;
-
 
 -- FIXME: this will be removed soon
 COPY vault_types (vault_oid, vault_type, vault_refs, "table", "className") FROM stdin;
@@ -1125,7 +1145,7 @@ COPY iris.timing_plan_type (id, description) FROM stdin;
 \.
 
 COPY system_attribute (name, value) FROM stdin;
-database_version	3.81.0
+database_version	3.83.0
 dms_page_on_secs	2.0
 dms_page_off_secs	0.0
 dms_default_justification_line	3
