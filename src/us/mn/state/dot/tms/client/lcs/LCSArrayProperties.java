898c05d38d0523e37bc5e5dd5b4f1496c5c1438e
Douglas Lau <doug.lau@state.mn.us>
2009-05-14 14:40:58 -0500

    Added LCS table model.

--- src/us/mn/state/dot/tms/client/lcs/LCSArrayProperties.java
+++ src/us/mn/state/dot/tms/client/lcs/LCSArrayProperties.java
@@ -15,17 +15,24 @@
 package us.mn.state.dot.tms.client.lcs;
 
 import java.awt.Color;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.ListModel;
+import javax.swing.ListSelectionModel;
+import us.mn.state.dot.sched.ActionJob;
+import us.mn.state.dot.sched.ListSelectionJob;
+import us.mn.state.dot.sonar.User;
 import us.mn.state.dot.sonar.client.TypeCache;
 import us.mn.state.dot.tms.LCSArray;
 import us.mn.state.dot.tms.client.SonarState;
 import us.mn.state.dot.tms.client.TmsConnection;
+import us.mn.state.dot.tms.client.toast.FormPanel;
 import us.mn.state.dot.tms.client.toast.SonarObjectForm;
+import us.mn.state.dot.tms.client.toast.ZTable;
 
 /**
  * LCSArrayProperties is a dialog for editing the properties of an LCS array.
@@ -40,10 +47,22 @@ public class LCSArrayProperties extends SonarObjectForm<LCSArray> {
 	/** SONAR state */
 	protected final SonarState state;
 
+	/** LCS table model */
+	protected final LCSTableModel table_model;
+
+	/** LCS table */
+	protected final ZTable lcs_table = new ZTable();
+
+	/** Button to delete the selected LCS */
+	protected final JButton delete_btn = new JButton("Delete");
+
 	/** Create a new lane control signal properties form */
 	public LCSArrayProperties(TmsConnection tc, LCSArray proxy) {
 		super(TITLE, tc, proxy);
 		state = tc.getSonarState();
+		User user = state.lookupUser(tc.getUser().getName());
+		table_model = new LCSTableModel(proxy, state.getLCSs(),
+			user);
 	}
 
 	/** Get the SONAR type cache */
@@ -63,9 +82,57 @@ public class LCSArrayProperties extends SonarObjectForm<LCSArray> {
 		setBackground(Color.LIGHT_GRAY);
 	}
 
+	/** Dispose of the form */
+	protected void dispose() {
+		table_model.dispose();
+		super.dispose();
+	}
+
 	/** Create setup panel */
 	protected JPanel createSetupPanel() {
-		JPanel panel = new JPanel();
+		FormPanel panel = new FormPanel(true);
+		initTable();
+		panel.addRow(lcs_table);
+		panel.addRow(delete_btn);
 		return panel;
+	}
+
+	/** Initialize the table */
+	protected void initTable() {
+		ListSelectionModel s = lcs_table.getSelectionModel();
+		s.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		new ListSelectionJob(this, s) {
+			public void perform() {
+				selectLCS();
+			}
+		};
+		lcs_table.setAutoCreateColumnsFromModel(false);
+		lcs_table.setColumnModel(LCSTableModel.createColumnModel());
+		lcs_table.setModel(table_model);
+		lcs_table.setVisibleRowCount(12);
+		new ActionJob(this, delete_btn) {
+			public void perform() throws Exception {
+				final ListSelectionModel s = 
+					lcs_table.getSelectionModel();
+				int row = s.getMinSelectionIndex();
+				if(row >= 0)
+					table_model.deleteRow(row);
+			}
+		};
+	}
+
+	/** Select an LCS in the table */
+	protected void selectLCS() {
+		final ListSelectionModel s = 
+			lcs_table.getSelectionModel();
+		int row = s.getMinSelectionIndex();
+		if(row >= 0) {
+			// FIXME: update indications
+		}
+	}
+
+	/** Update one attribute on the form */
+	protected void updateAttribute(String a) {
+		// FIXME
 	}
 }
---
+++ src/us/mn/state/dot/tms/client/lcs/LCSTableModel.java
@@ -0,0 +1,158 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2009  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.client.lcs;
+
+import java.util.HashMap;
+import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+import us.mn.state.dot.sonar.User;
+import us.mn.state.dot.sonar.client.TypeCache;
+import us.mn.state.dot.tms.LCSArray;
+import us.mn.state.dot.tms.LCS;
+import us.mn.state.dot.tms.client.proxy.ProxyTableModel;
+
+/**
+ * Table model for LCS within an array.
+ *
+ * @author Douglas Lau
+ */
+public class LCSTableModel extends ProxyTableModel<LCS> {
+
+	/** Create a SONAR name to check for allowed updates */
+	static protected String createNamespaceString(String name) {
+		return LCS.SONAR_TYPE + "/" + name;
+	}
+
+	/** Count of columns in table model */
+	static protected final int COLUMN_COUNT = 2;
+
+	/** Message line column number */
+	static protected final int COL_LANE = 0;
+
+	/** Message text column number */
+	static protected final int COL_NAME = 1;
+
+	/** Create a new table column */
+	static protected TableColumn createColumn(int column, int width,
+		String header)
+	{
+		TableColumn c = new TableColumn(column, width);
+		c.setHeaderValue(header);
+		return c;
+	}
+
+	/** Add a new proxy to the table model */
+	protected int doProxyAdded(LCS proxy) {
+		if(proxy.getArray() == lcs_array)
+			return super.doProxyAdded(proxy);
+		else
+			return -1;
+	}
+
+	/** LCS array */
+	protected final LCSArray lcs_array;
+
+	/** SONAR User for permission checks */
+	protected final User user;
+
+	/** Create a new LCS table model */
+	public LCSTableModel(LCSArray la, TypeCache<LCS> c, User u) {
+		super(c, true);
+		lcs_array = la;
+		user = u;
+		initialize();
+	}
+
+	/** Get the count of columns in the table */
+	public int getColumnCount() {
+		return COLUMN_COUNT;
+	}
+
+	/** Get the value at the specified cell */
+	public Object getValueAt(int row, int column) {
+		LCS lcs = getProxy(row);
+		if(lcs != null)
+			return getValue(lcs, column);
+		else
+			return null;
+	}
+
+	/** Get the value of an LCS column */
+	protected Object getValue(LCS lcs, int column) {
+		switch(column) {
+		case COL_LANE:
+			return lcs.getLane();
+		case COL_NAME:
+			return lcs.getName();
+		default:
+			return null;
+		}
+	}
+
+	/** Get the column class */
+	public Class getColumnClass(int column) {
+		if(column == COL_NAME)
+			return String.class;
+		else
+			return Integer.class;
+	}
+
+	/** Check if the specified cell is editable */
+	public boolean isCellEditable(int row, int column) {
+		synchronized(proxies) {
+			if(row == proxies.size() && column == COL_NAME)
+				return canAddLCS("arbitrary_name");
+		}
+		return false;
+	}
+
+	/** Check if the user can add the named LCS */
+	public boolean canAddLCS(String name) {
+		return user.canAdd(createNamespaceString(name));
+	}
+
+	/** Set the value at the specified cell */
+	public void setValueAt(Object value, int row, int column) {
+		assert column == COL_NAME;
+		addRow(value, row);
+	}
+
+	/** Add a row to the sign text table */
+	protected void addRow(Object value, int row) {
+		String name = value.toString();
+		if(name.length() > 0)
+			createLCS(name, row + 1);
+	}
+
+	/** Create a new LCS */
+	protected void createLCS(String name, int lane) {
+		if(canAddLCS(name)) {
+			HashMap<String, Object> attrs =
+				new HashMap<String, Object>();
+			attrs.put("array", lcs_array);
+			attrs.put("lane", new Integer(lane));
+			cache.createObject(name, attrs);
+		}
+	}
+
+	/** Create the table column model */
+	static public TableColumnModel createColumnModel() {
+		TableColumnModel m = new DefaultTableColumnModel();
+		m.addColumn(createColumn(COL_LANE, 36, "Lane"));
+		m.addColumn(createColumn(COL_NAME, 140, "LCS"));
+		return m;
+	}
+}
