6c8e81fec467bb462518699f8b32d5636889044b
Douglas Lau <doug.lau@state.mn.us>
2009-05-22 16:15:09 -0500

    Added release notes for v3.87.0

--- sql/tms-template.sql
+++ sql/tms-template.sql
@@ -1079,7 +1079,7 @@ COPY iris.timing_plan_type (id, description) FROM stdin;
 \.
 
 COPY system_attribute (name, value) FROM stdin;
-database_version	3.85.0
+database_version	3.87.0
 dms_default_justification_line	3
 dms_default_justification_page	2
 dms_max_lines	3
