9de55bd693cf1818f47326ca200da2b70fdfeb9b
Michael Darter <mdarter@iteris.com>
2009-05-28 12:38:08 -0700

    Reverted 2 lines in ClearDmsAction from prior change set re converting clear button to an IButton.

--- src/us/mn/state/dot/tms/client/dms/ClearDmsAction.java
+++ src/us/mn/state/dot/tms/client/dms/ClearDmsAction.java
@@ -48,6 +48,9 @@ public class ClearDmsAction extends AbstractAction {
 		selectionModel = s;
 		dispatcher = d;
 		owner = o;
+		putValue(Action.NAME, I18N.get("dms.clear"));
+		putValue(Action.SHORT_DESCRIPTION, 
+			I18N.get("dms.clear.tooltip")); 
 	}
 
 	/** Schedule the action to be performed */
