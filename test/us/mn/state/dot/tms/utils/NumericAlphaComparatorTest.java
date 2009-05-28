7a46c9ec3e0d040f264c2d38770e4e4652081274
Douglas Lau <doug.lau@state.mn.us>
2009-05-28 15:02:14 -0500

    Moved NumericAlphaComparatorTest to utils package.

--- test/us/mn/state/dot/tms/utils/NumericAlphaComparatorTest.java
+++ test/us/mn/state/dot/tms/utils/NumericAlphaComparatorTest.java
@@ -12,7 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-package us.mn.state.dot.tms.client;
+package us.mn.state.dot.tms.utils;
 
 import junit.framework.TestCase;
 
