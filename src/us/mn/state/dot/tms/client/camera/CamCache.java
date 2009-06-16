2b33fed77bfae127c2e71bb220ec2c1ead313530
Douglas Lau <doug.lau@state.mn.us>
2009-06-16 16:27:08 -0500

    Added missing class.

---
+++ src/us/mn/state/dot/tms/client/camera/CamCache.java
@@ -0,0 +1,80 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2009  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.client.camera;
+
+import us.mn.state.dot.sonar.client.Client;
+import us.mn.state.dot.sonar.client.TypeCache;
+import us.mn.state.dot.tms.Camera;
+import us.mn.state.dot.tms.VideoMonitor;
+import us.mn.state.dot.tms.client.proxy.ProxyListModel;
+
+/**
+ * Cache for camera-related proxy objects.
+ *
+ * @author Douglas Lau
+ */
+public class CamCache {
+
+	/** Cache of cameras */
+	protected final TypeCache<Camera> cameras;
+
+	/** Get the camera cache */
+	public TypeCache<Camera> getCameras() {
+		return cameras;
+	}
+
+	/** Camera proxy list model */
+	protected final ProxyListModel<Camera> camera_model;
+
+	/** Get the camera list model */
+	public ProxyListModel<Camera> getCameraModel() {
+		return camera_model;
+	}
+
+	/** Cache of video monitor proxies */
+	protected final TypeCache<VideoMonitor> monitors;
+
+	/** Get the video monitor type cache */
+	public TypeCache<VideoMonitor> getVideoMonitors() {
+		return monitors;
+	}
+
+	/** VideoMonitor proxy list model */
+	protected final ProxyListModel<VideoMonitor> monitor_model;
+
+	/** Get the VideoMonitor list model */
+	public ProxyListModel<VideoMonitor> getMonitorModel() {
+		return monitor_model;
+	}
+
+	/** Create a new camera cache */
+	public CamCache(Client client) throws IllegalAccessException,
+		NoSuchFieldException
+	{
+		cameras = new TypeCache<Camera>(Camera.class, client);
+		camera_model = new ProxyListModel<Camera>(cameras);
+		camera_model.initialize();
+		monitors = new TypeCache<VideoMonitor>(VideoMonitor.class,
+			client);
+		monitor_model = new ProxyListModel<VideoMonitor>(monitors);
+		monitor_model.initialize();
+	}
+
+	/** Populate the type caches */
+	public void populate(Client client) {
+		client.populate(cameras);
+		client.populate(monitors);
+	}
+}
