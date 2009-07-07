f3bebfecf07fbe979dade5678c8a90194fabcffe
Douglas Lau <doug.lau@state.mn.us>
2009-07-07 13:28:50 -0500

    Increased the name length for action plans.

--- sql/migrate-3.92.sql
+++ sql/migrate-3.92.sql
@@ -40,22 +40,22 @@ DROP TABLE dms_sign_group;
 DROP TABLE sign_text;
 
 CREATE TABLE iris.action_plan (
-	name VARCHAR(8) PRIMARY KEY,
-	description VARCHAR(32) NOT NULL,
+	name VARCHAR(16) PRIMARY KEY,
+	description VARCHAR(64) NOT NULL,
 	active BOOLEAN NOT NULL,
 	deployed BOOLEAN NOT NULL
 );
 
 CREATE TABLE iris.time_action (
-	name VARCHAR(12) PRIMARY KEY,
-	action_plan VARCHAR(8) NOT NULL REFERENCES iris.action_plan,
+	name VARCHAR(20) PRIMARY KEY,
+	action_plan VARCHAR(16) NOT NULL REFERENCES iris.action_plan,
 	minute SMALLINT NOT NULL,
 	deploy BOOLEAN NOT NULL
 );
 
 CREATE TABLE iris.dms_action (
-	name VARCHAR(12) PRIMARY KEY,
-	action_plan VARCHAR(8) NOT NULL REFERENCES iris.action_plan,
+	name VARCHAR(20) PRIMARY KEY,
+	action_plan VARCHAR(16) NOT NULL REFERENCES iris.action_plan,
 	sign_group VARCHAR(16) NOT NULL REFERENCES iris.sign_group,
 	on_deploy BOOLEAN NOT NULL,
 	quick_message VARCHAR(20) REFERENCES iris.quick_message,
