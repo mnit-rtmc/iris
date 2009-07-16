ace0478b33f29c3298abc28f7d78ea8bea565c33
Douglas Lau <doug.lau@state.mn.us>
2009-07-16 16:18:28 -0500

    NTCIP: Reworked OpSendLCSIndications to use DMSImpl.doSetMessageNext.

--- src/us/mn/state/dot/tms/server/comm/ntcip/OpSendLCSIndications.java
+++ src/us/mn/state/dot/tms/server/comm/ntcip/OpSendLCSIndications.java
@@ -14,7 +14,6 @@
  */
 package us.mn.state.dot.tms.server.comm.ntcip;
 
-import java.util.LinkedList;
 import us.mn.state.dot.sonar.User;
 import us.mn.state.dot.tms.DMS;
 import us.mn.state.dot.tms.DMSHelper;
@@ -25,9 +24,9 @@ import us.mn.state.dot.tms.LaneUseMultiHelper;
 import us.mn.state.dot.tms.LCS;
 import us.mn.state.dot.tms.LCSArrayHelper;
 import us.mn.state.dot.tms.SignMessage;
+import us.mn.state.dot.tms.TMSException;
 import us.mn.state.dot.tms.server.DMSImpl;
 import us.mn.state.dot.tms.server.LCSArrayImpl;
-import us.mn.state.dot.tms.server.comm.AddressedMessage;
 
 /**
  * Operation to send new indicaitons to a Lane Control Signal array.
@@ -61,113 +60,48 @@ public class OpSendLCSIndications extends OpLCS {
 	/** User who sent the indications */
 	protected final User user;
 
-	/** List of operations to send indications to DMS in LCS array */
-	protected final LinkedList<OpDMS> ops = new LinkedList<OpDMS>();
-
 	/** Create a new operation to send LCS indications */
 	public OpSendLCSIndications(LCSArrayImpl l, Integer[] ind, User u) {
 		super(DEVICE_DATA, l);
 		indications = ind;
 		user = u;
-		createOperations();
+		sendIndications();
 	}
 
 	/** Create the first real phase of the operation */
 	protected Phase phaseOne() {
-		if(ops.isEmpty())
-			return null;
-		else
-			return new StartOperations();
+		return null;
 	}
 
-	/** Phase to start DMS operations */
-	protected class StartOperations extends Phase {
-
-		/** Start all DMS operations */
-		protected Phase poll(AddressedMessage mess) {
-			for(OpDMS op: ops)
-				op.start();
-			return new WaitForCompletion();
-		}
-	}
-
-	/** Phase to wait for operations to complete */
-	protected class WaitForCompletion extends Phase {
-
-		/** Time to stop waiting for completion (20 seconds) */
-		protected final long expire = System.currentTimeMillis() + 
-			20 * 1000;
-
-		/** Wait for operations to complete */
-		protected Phase poll(AddressedMessage mess) {
-			try {
-				Thread.sleep(200);
-			}
-			catch(InterruptedException e) {
-				// Ignore
-			}
-			OpDMS op = ops.peekFirst();
-			if(op == null)
-				return null;
-			if(op.isDone())
-				ops.removeFirst();
-			else {
-				if(System.currentTimeMillis() > expire) {
-					System.err.println(lcs_array.getName() +
-						": LCS timeout expired -- " +
-						"giving up");
-					success = false;
-					return null;
-				}
-			}
-			return this;
-		}
-	}
-
-	/** Cleanup the operation */
-	public void cleanup() {
-		if(success)
-			lcs_array.setIndicationsCurrent(indications, user);
-		super.cleanup();
-	}
-
-	/** Create operations to send new indications to an LCS array */
-	protected void createOperations() {
+	/** Send new indications to an LCS array */
+	protected void sendIndications() {
 		LCS[] lcss = LCSArrayHelper.lookupLCSs(lcs_array);
 		if(lcss.length != indications.length) {
-			System.err.println("createOperations: array invalid");
+			System.err.println("sendIndications: array invalid");
 			return;
 		}
 		for(int i = 0; i < lcss.length; i++) {
 			DMS dms = DMSHelper.lookup(lcss[i].getName());
-			if(dms instanceof DMSImpl) {
-				OpDMS op = createGraphicOperation((DMSImpl)dms,
-					indications[i]);
-				if(op != null) {
-					ops.add(op);
-					continue;
-				}
-			}
-			System.err.println("createOperations: aborted");
-			return;
+			if(dms instanceof DMSImpl)
+				sendIndication((DMSImpl)dms, indications[i]);
 		}
 	}
 
-	/** Create an operation to set an indication on a DMS */
-	protected OpDMS createGraphicOperation(DMSImpl dms, int ind) {
+	/** Send an indication to a DMS */
+	protected void sendIndication(DMSImpl dms, int ind) {
 		String ms = createIndicationMulti(dms, ind);
 		if(ms != null) {
 			SignMessage sm = dms.createMessage(ms,
 				getActivationPriority(ind));
-			// We need to call shouldActivate here, because we're
-			// bypassing the standard doSetMessageNext call.
-			// The reason we bypass doSetMessageNext is we need
-			// to keep track of the operations, so we can tell
-			// when they have completed.  Ugly, but it works.
-			if(dms.shouldActivate(sm))
-				return NtcipPoller.createOperation(dms,sm,user);
+			if(sm != null) {
+				try {
+					dms.doSetMessageNext(sm, user);
+				}
+				catch(TMSException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-		return null;
 	}
 
 	/** Create a MULTI string for a lane use indication */
