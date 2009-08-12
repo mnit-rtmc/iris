8e6041ac9ac29b0b31e3ce64b2980d05311dc5f5
Douglas Lau <doug.lau@state.mn.us>
2009-08-12 17:10:41 -0500

    Added freeway theme to segment layer.

---
+++ src/us/mn/state/dot/tms/client/roads/FreewayTheme.java
@@ -0,0 +1,36 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2009  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.client.roads;
+
+import us.mn.state.dot.map.MapObject;
+import us.mn.state.dot.map.Style;
+
+/**
+ * A theme for drawing segment objects in a dull gray color.
+ *
+ * @author Douglas Lau
+ */
+public class FreewayTheme extends SegmentTheme {
+
+	/** Create a new freeway theme */
+	public FreewayTheme() {
+		super("Freeway");
+	}
+
+	/** Get the style to draw a given segment */
+	protected Style getStyle(Segment s) {
+		return DEFAULT_STYLE;
+	}
+}
--- src/us/mn/state/dot/tms/client/roads/SegmentLayer.java
+++ src/us/mn/state/dot/tms/client/roads/SegmentLayer.java
@@ -164,8 +164,10 @@ public class SegmentLayer extends Layer implements DynamicLayer {
 	public LayerState createState() {
 		DensityTheme den = new DensityTheme();
 		LayerState ls = new LayerState(this, den);
+		ls.addTheme(new DensityTheme());
 		ls.addTheme(new SpeedTheme());
 		ls.addTheme(new FlowTheme());
+		ls.addTheme(new FreewayTheme());
 		return ls;
 	}
 }
