07ff49bceca221fa196f51cd832ace7ee996fb73
Douglas Lau <doug.lau@state.mn.us>
2009-09-18 14:17:14 -0500

    Minor cleanup of Profile class.

--- src/us/mn/state/dot/tms/server/Profile.java
+++ src/us/mn/state/dot/tms/server/Profile.java
@@ -37,38 +37,38 @@ public class Profile {
 		ps.println("Total memory: " + total / MEGABYTE + "M");
 	}
 
-	/** Print thread profiling information for all threads */
-	static public void printThreads(PrintStream ps) {
-		ThreadGroup group = Thread.currentThread().getThreadGroup();
-		while(group.getParent() != null)
-			group = group.getParent();
-		printThreads(ps, group, 0);
-	}
-
 	/** Print thread profiling information for the specified group */
 	static protected void printThreads(PrintStream ps, ThreadGroup group,
 		int deep)
 	{
 		Thread[] thread = new Thread[group.activeCount() + 1];
 		int count = group.enumerate(thread, false);
-		StringBuffer g = new StringBuffer();
-		while(g.length() < deep) g.append(" ");
+		StringBuilder g = new StringBuilder();
+		while(g.length() < deep)
+			g.append(" ");
 		g.append(group.getName());
 		g.append(" thread group: ");
 		g.append(count);
-		while(g.length() < 66) g.append(" ");
+		while(g.length() < 66)
+			g.append(" ");
 		g.append(group.getMaxPriority());
-		while(g.length() < 68) g.insert(66, " ");
+		while(g.length() < 68)
+			g.insert(66, " ");
 		ps.println(g);
 		for(int i = 0; i < count; i++) {
-			StringBuffer t = new StringBuffer();
-			while(t.length() < 4 + deep) t.insert(0, " ");
+			StringBuilder t = new StringBuilder();
+			while(t.length() < 4 + deep)
+				t.insert(0, " ");
 			t.append(thread[i].getName());
-			if(!thread[i].isAlive()) t.append(" (dead)");
-			if(thread[i].isDaemon()) t.append(" (daemon)");
-			while(t.length() < 66) t.append(" ");
+			if(!thread[i].isAlive())
+				t.append(" (dead)");
+			if(thread[i].isDaemon())
+				t.append(" (daemon)");
+			while(t.length() < 66)
+				t.append(" ");
 			t.append(thread[i].getPriority());
-			while(t.length() < 68) t.insert(66, " ");
+			while(t.length() < 68)
+				t.insert(66, " ");
 			ps.println(t);
 		}
 		ThreadGroup[] groups =
@@ -76,5 +76,13 @@ public class Profile {
 		count = group.enumerate(groups, false);
 		for(int i = 0; i < count; i++)
 			printThreads(ps, groups[i], deep + 2);
+	}
+
+	/** Print thread profiling information for all threads */
+	static public void printThreads(PrintStream ps) {
+		ThreadGroup group = Thread.currentThread().getThreadGroup();
+		while(group.getParent() != null)
+			group = group.getParent();
+		printThreads(ps, group, 0);
 	}
 }
