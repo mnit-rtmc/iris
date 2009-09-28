8dfa421dbef1063353f6468129b4516b79b41fa1
Michael Darter <mdarter@iteris.com>
2009-09-28 13:31:01 -0700

    dmslite: Created junit test cases for Pair.

--- src/us/mn/state/dot/tms/server/comm/dmslite/Pair.java
+++ src/us/mn/state/dot/tms/server/comm/dmslite/Pair.java
@@ -18,42 +18,38 @@ package us.mn.state.dot.tms.server.comm.dmslite;
 import java.io.IOException;
 
 /**
- * Pair class. A Pair Comparable is a LISP like pair, with a car and cdr.
+ * Pair class. A Pair is a LISP like pair, with a car and cdr.
  * @author      Michael Darter
- * @company     AHMCT
+ * @company     AHMCT, University of California, Davis
  * @created     03/12/08
  */
 public class Pair
 {
-	// fields
+	/** Fields */
 	private final Comparable m_car;
 	private final Comparable m_cdr;
 
-	/**
-	 * Constructor.
-	 */
+	/** Constructor */
 	Pair(Comparable car, Comparable cdr) {
 		m_car = car;
 		m_cdr = cdr;
 	}
 
-	/** return car */
+	/** Return car */
 	public Comparable car() {
 		return m_car;
 	}
 
-	/** return cdr */
+	/** Return cdr */
 	public Comparable cdr() {
 		return m_cdr;
 	}
 
-	/**
-	 *  find a Pair in a list by comparing the car of each list 
+	/** find a Pair in a list by comparing the car of each list 
 	 *  element with the argument.
-	 *  @returns the first item in the list that matches else null 
-	 *	     if not found.
-	 *  @throws IOException if the specified car is not found.
-	 */
+	 * @returns the first item in the list that matches else null 
+	 *          if not found.
+	 * @throws IOException if the specified car is not found. */
 	public static Pair findCar(Pair[] list, Comparable argcar)
 		throws IOException {
 		for(Pair i : list)
@@ -62,50 +58,5 @@ public class Pair
 			}
 		throw new IOException("Pair.findCar(" + argcar.toString()
 			+ ") not found.");
-	}
-
-	/** test this class */
-	public static boolean test() {
-		boolean ok = true;
-
-		ok = ok && ((Integer) (new Pair(
-			new Integer(1), new Double(3.25)).car())).compareTo(
-				new Integer(1)) == 0;
-		ok = ok && ((Integer) (new Pair(new Integer(1), new Double(
-			3.25)).car())).compareTo(new Integer(2)) < 0;
-		ok = ok && ((Integer) (new Pair(
-			new Integer(1), new Double(3.25)).car())).compareTo(
-				new Integer(-3)) > 0;
-		ok = ok && ((Double) (new Pair(
-			new Integer(1), new Double(3.25)).cdr())).compareTo(
-				new Double(3.25)) == 0;
-		ok = ok && ((Double) (new Pair(new Integer(1),
-			new Double(3.25)).cdr())).compareTo(new Double(4)) < 0;
-		ok = ok && ((Double) (new Pair(
-			new Integer(1), new Double(3.25)).cdr())).compareTo(
-				new Double(.33)) > 0;
-
-		// test findCar()
-		{
-			Pair[] p = new Pair[2];
-
-			p[0] = new Pair("a", "aa");
-			p[1] = new Pair("b", "bb");
-
-			try {
-				ok = ok && (Pair.findCar(p,
-					"b").cdr().compareTo("bb") == 0);
-			} catch (IOException ex) {
-				ok = false;
-			}
-
-			try {
-				ok = ok && (Pair.findCar(p, "bx") == null);
-				ok = false;
-			} catch (IOException ex) {
-				ok = ok && true;
-			}
-		}
-		return ok;
 	}
 }
---
+++ test/us/mn/state/dot/tms/server/comm/dmslite/PairTest.java
@@ -0,0 +1,78 @@
+/*
+ * SONAR -- Simple Object Notification And Replication
+ * Copyright (C) 2009  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.server.comm.dmslite;
+
+import junit.framework.TestCase;
+import java.io.IOException;
+import java.util.Arrays;
+
+/** 
+ * Pair test cases
+ * @author Michael Darter
+ */
+public class PairTest extends TestCase {
+
+	/** constructor */
+	public PairTest(String name) {
+		super(name);
+	}
+
+	/** test cases */
+	public void test() {
+
+		boolean ok = true;
+
+		assertTrue(((Integer)(new Pair(new Integer(1), 
+			new Double(3.25)).car())).compareTo(
+			new Integer(1)) == 0);
+		ok = ok && ((Integer) (new Pair(new Integer(1), new Double(
+			3.25)).car())).compareTo(new Integer(2)) < 0;
+		ok = ok && ((Integer) (new Pair(
+			new Integer(1), new Double(3.25)).car())).compareTo(
+				new Integer(-3)) > 0;
+		ok = ok && ((Double) (new Pair(
+			new Integer(1), new Double(3.25)).cdr())).compareTo(
+				new Double(3.25)) == 0;
+		ok = ok && ((Double) (new Pair(new Integer(1),
+			new Double(3.25)).cdr())).compareTo(new Double(4)) < 0;
+		ok = ok && ((Double) (new Pair(
+			new Integer(1), new Double(3.25)).cdr())).compareTo(
+				new Double(.33)) > 0;
+		assertTrue(ok);
+
+		// test findCar()
+		{
+			Pair[] p = new Pair[2];
+
+			p[0] = new Pair("a", "aa");
+			p[1] = new Pair("b", "bb");
+
+			try {
+				ok = ok && (Pair.findCar(p,
+					"b").cdr().compareTo("bb") == 0);
+			} catch (IOException ex) {
+				ok = false;
+			}
+
+			try {
+				ok = ok && (Pair.findCar(p, "bx") == null);
+				ok = false;
+			} catch (IOException ex) {
+				ok = ok && true;
+			}
+		}
+		assertTrue(ok);
+	}
+}
