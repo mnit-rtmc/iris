df107ac1cb7396ae872aca6fe438474fe8662b82
Douglas Lau <doug.lau@state.mn.us>
2009-10-23 12:01:55 -0500

    Add ability to change impact for incident impact panel.

--- src/us/mn/state/dot/tms/client/incident/ImpactPanel.java
+++ src/us/mn/state/dot/tms/client/incident/ImpactPanel.java
@@ -21,8 +21,13 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Insets;
 import java.awt.Shape;
+import java.awt.TexturePaint;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
 /**
@@ -31,6 +36,32 @@ import javax.swing.JPanel;
  * @author Douglas Lau
  */
 public class ImpactPanel extends JPanel {
+
+	/** Impact for one lane */
+	static enum LaneImpact {
+		open('.'), blocked('!'), caution('?');
+		protected final char _char;
+		LaneImpact(char c) {
+			_char = c;
+		}
+		protected LaneImpact next() {
+			switch(this) {
+			case open:
+				return blocked;
+			case blocked:
+				return caution;
+			default:
+				return open;
+			}
+		}
+		static LaneImpact fromChar(char i) {
+			for(LaneImpact li: LaneImpact.values()) {
+				if(li._char == i)
+					return li;
+			}
+			return null;
+		}
+	}
 
 	/** Width of one lane */
 	static protected final int LANE_WIDTH = 32;
@@ -42,6 +73,10 @@ public class ImpactPanel extends JPanel {
 	static protected final BasicStroke LINE_SOLID = new BasicStroke(4,
 		BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
 
+	/** Impact stroke line */
+	static protected final BasicStroke LINE_IMPACT = new BasicStroke(1,
+		BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
+
 	/** Dashed stroke line */
 	static protected final BasicStroke LINE_DASHED = new BasicStroke(4,
 		BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 1,
@@ -49,18 +84,62 @@ public class ImpactPanel extends JPanel {
 		2 * LANE_HEIGHT / 3
 	);
 
+	/** Color for blocked impact */
+	static protected final Color COLOR_BLOCKED = new Color(208, 208, 208);
+
+	/** Image for caution impact */
+	static protected final BufferedImage IMAGE_CAUTION =
+		new BufferedImage(4, 4, BufferedImage.TYPE_BYTE_GRAY);
+	static {
+		IMAGE_CAUTION.setRGB(0, 0, 0xffff);
+		IMAGE_CAUTION.setRGB(1, 1, 0xffff);
+		IMAGE_CAUTION.setRGB(2, 2, 0xffff);
+		IMAGE_CAUTION.setRGB(3, 3, 0xffff);
+	}
+
+	/** Paint for caution impact */
+	static protected final TexturePaint PAINT_CAUTION = new TexturePaint(
+		IMAGE_CAUTION, new Rectangle2D.Float(0, 0, 4, 4));
+
 	/** Renderer component width */
 	protected int width = 0;
 
 	/** Renderer component height */
 	protected int height = 0;
 
-	/** Number of lanes */
-	protected int n_lanes;
+	/** Lane impact array */
+	protected LaneImpact[] impact;
+
+	/** Set the impact */
+	public void setImpact(String im) {
+		LaneImpact[] imp = new LaneImpact[im.length()];
+		for(int i = 0; i < imp.length; i++)
+			imp[i] = LaneImpact.fromChar(im.charAt(i));
+		impact = imp;
+	}
+
+	/** Get the impact */
+	public String getImpact() {
+		LaneImpact[] imp = impact;
+		char[] im = new char[imp.length];
+		for(int i = 0; i < im.length; i++)
+			im[i] = imp[i]._char;
+		return new String(im);
+	}
 
 	/** Create a new roadway node renderer */
 	public ImpactPanel() {
-		n_lanes = 4;
+		impact = new LaneImpact[6];
+		for(int i = 0; i < impact.length; i++)
+			impact[i] = LaneImpact.open;
+		impact[1] = LaneImpact.blocked;
+		impact[2] = LaneImpact.caution;
+		addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				if(e.getClickCount() == 1)
+					doClick(e.getX(), e.getY());
+			}
+		});
 	}
 
 	/** Get the lane line (left) for the given lane */
@@ -82,13 +161,14 @@ public class ImpactPanel extends JPanel {
 		drawYellowLine(g2);
 		drawSkipStripes(g2);
 		drawWhiteLine(g2);
+		drawImpact(g2);
 	}
 
 	/** Fill the roadway area */
 	protected void fillRoadway(Graphics2D g) {
 		g.setColor(Color.BLACK);
 		int x = getLaneLine(0);
-		int w = getLaneLine(2 + n_lanes) - x;
+		int w = getLaneLine(impact.length) - x;
 		g.fillRect(x, 0, w, height);
 	}
 
@@ -104,7 +184,7 @@ public class ImpactPanel extends JPanel {
 	protected void drawWhiteLine(Graphics2D g) {
 		g.setStroke(LINE_SOLID);
 		g.setColor(Color.WHITE);
-		int x = getLaneLine(n_lanes + 1);
+		int x = getLaneLine(impact.length - 1);
 		g.draw(new Line2D.Double(x, height, x, 0));
 	}
 
@@ -112,15 +192,80 @@ public class ImpactPanel extends JPanel {
 	protected void drawSkipStripes(Graphics2D g) {
 		g.setStroke(LINE_DASHED);
 		g.setColor(Color.WHITE);
-		for(int i = 0; i < n_lanes - 1; i++) {
+		for(int i = 0; i < impact.length - 3; i++) {
 			int x = getLaneLine(i + 2);
 			g.draw(new Line2D.Double(x, 0, x, height));
 		}
 	}
 
+	/** Draw the impact */
+	protected void drawImpact(Graphics2D g) {
+		g.setStroke(LINE_IMPACT);
+		for(int i = 0; i < impact.length; i++) {
+			int x = getLaneLine(i);
+			drawImpact(g, x, impact[i]);
+		}
+	}
+
+	/** Draw the impact for one lane */
+	protected void drawImpact(Graphics2D g, int x, LaneImpact li) {
+		int s = LANE_WIDTH / 2;
+		x += (LANE_WIDTH - s) / 2;
+		int h = (height - s) / 2;
+		switch(li) {
+		case open:
+			g.setColor(Color.WHITE);
+			g.drawRect(x, h, s, s);
+			break;
+		case blocked:
+			g.setColor(COLOR_BLOCKED);
+			g.fillRect(x, h, s, s);
+			g.setColor(Color.WHITE);
+			g.drawRect(x, h, s, s);
+			break;
+		case caution:
+			g.setColor(Color.GRAY);
+			g.setPaint(PAINT_CAUTION);
+			g.fillRect(x, h, s, s);
+			g.setColor(Color.WHITE);
+			g.setPaint(null);
+			g.drawRect(x, h, s, s);
+			break;
+		}
+	}
+
+	/** Perform a mouse click event */
+	protected void doClick(final int x, final int y) {
+		Dimension d = (Dimension)getSize();
+		Insets insets = getInsets();
+		int height = (int)d.getHeight() - insets.top - insets.bottom;
+		int s = LANE_WIDTH / 2;
+		int xs = insets.left + (LANE_WIDTH - s) / 2;
+		int h = (height - s) / 2;
+		int top = insets.top + h;
+		if(y < top || y > top + s)
+			return;
+		for(int i = 0; i < impact.length; i++) {
+			int x0 = getLaneLine(i) + xs;
+			if(x >= x0 && x <= x0 + s) {
+				incrementImpact(i);
+				break;
+			}
+		}
+	}
+
+	/** Increment lane impact */
+	protected void incrementImpact(int lane) {
+		LaneImpact[] imp = impact;	// Avoid race
+		if(lane < 0 || lane >= imp.length)
+			return;
+		imp[lane] = imp[lane].next();
+		repaint();
+	}
+
 	/** Get the preferred size */
 	public Dimension getPreferredSize() {
-		return new Dimension(LANE_WIDTH * (n_lanes + 3),
+		return new Dimension(LANE_WIDTH * (impact.length + 1),
 			LANE_HEIGHT * 4);
 	}
 
