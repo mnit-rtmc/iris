19c820c4d73b4aa62adcac0543424e4c59745b66
Douglas Lau <doug.lau@state.mn.us>
2009-10-29 11:20:59 -0500

    Added incident_update table and trigger to log updates to impact/cleared cols.

--- sql/migrate-3.106.sql
+++ sql/migrate-3.106.sql
@@ -11,3 +11,24 @@ UPDATE event.event_description SET description = 'Incident ROADWORK'
 	WHERE event_desc_id = 24;
 
 ALTER TABLE event.incident ADD COLUMN camera VARCHAR(10);
+
+CREATE TABLE event.incident_update (
+	event_id INTEGER PRIMARY KEY DEFAULT nextval('event.event_id_seq'),
+	incident VARCHAR(16) NOT NULL REFERENCES event.incident(name),
+	event_date timestamp WITH time zone NOT NULL,
+	impact VARCHAR(20) NOT NULL,
+	cleared BOOLEAN NOT NULL
+);
+
+CREATE FUNCTION event.incident_update_trig() RETURNS "trigger" AS
+'
+BEGIN
+	INSERT INTO event.incident_update
+		(incident, event_date, impact, cleared)
+	VALUES (NEW.name, now(), NEW.impact, NEW.cleared);
+	RETURN NEW;
+END;' LANGUAGE plpgsql;
+
+CREATE TRIGGER incident_update_trigger
+	AFTER UPDATE ON event.incident
+	FOR EACH ROW EXECUTE PROCEDURE event.incident_update_trig();
--- sql/tms-template.sql
+++ sql/tms-template.sql
@@ -1448,6 +1448,27 @@ CREATE TABLE event.incident (
 	cleared BOOLEAN NOT NULL
 );
 
+CREATE TABLE event.incident_update (
+	event_id INTEGER PRIMARY KEY DEFAULT nextval('event.event_id_seq'),
+	incident VARCHAR(16) NOT NULL REFERENCES event.incident(name),
+	event_date timestamp WITH time zone NOT NULL,
+	impact VARCHAR(20) NOT NULL,
+	cleared BOOLEAN NOT NULL
+);
+
+CREATE FUNCTION event.incident_update_trig() RETURNS "trigger" AS
+'
+BEGIN
+	INSERT INTO event.incident_update
+		(incident, event_date, impact, cleared)
+	VALUES (NEW.name, now(), NEW.impact, NEW.cleared);
+	RETURN NEW;
+END;' LANGUAGE plpgsql;
+
+CREATE TRIGGER incident_update_trigger
+	AFTER UPDATE ON event.incident
+	FOR EACH ROW EXECUTE PROCEDURE event.incident_update_trig();
+
 SET search_path = public, event, pg_catalog;
 
 CREATE VIEW alarm_event_view AS
