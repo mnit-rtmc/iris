3ba4c9217e95494e7072dfe11515ea4838bfc709
Douglas Lau <doug.lau@state.mn.us>
2009-12-21 16:10:53 -0600

    Converted CabinetStyleForm to subclass ProxyTableForm.

--- src/us/mn/state/dot/tms/client/toast/CabinetStyleForm.java
+++ src/us/mn/state/dot/tms/client/toast/CabinetStyleForm.java
@@ -14,82 +14,24 @@
  */
 package us.mn.state.dot.tms.client.toast;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.ListSelectionModel;
-import us.mn.state.dot.sched.ActionJob;
-import us.mn.state.dot.sched.ListSelectionJob;
 import us.mn.state.dot.tms.CabinetStyle;
 import us.mn.state.dot.tms.client.Session;
-import us.mn.state.dot.tms.client.widget.ZTable;
+import us.mn.state.dot.tms.client.proxy.ProxyTableForm;
 
 /**
  * A form for displaying and editing cabinet styles
  *
  * @author Douglas Lau
  */
-public class CabinetStyleForm extends AbstractForm {
-
-	/** Frame title */
-	static protected final String TITLE = "Cabinet Styles";
-
-	/** Table model for cabinet styles */
-	protected final CabinetStyleModel model;
-
-	/** Table to hold the cabinet style list */
-	protected final ZTable table = new ZTable();
-
-	/** Button to delete the selected cabinet style */
-	protected final JButton del_button = new JButton("Delete");
+public class CabinetStyleForm extends ProxyTableForm<CabinetStyle> {
 
 	/** Create a new cabinet style form */
 	public CabinetStyleForm(Session s) {
-		super(TITLE);
-		model = new CabinetStyleModel(s);
+		super("Cabinet Styles", new CabinetStyleModel(s));
 	}
 
-	/** Initializze the widgets in the form */
-	protected void initialize() {
-		model.initialize();
-		add(createCabinetStylePanel());
-	}
-
-	/** Dispose of the form */
-	protected void dispose() {
-		model.dispose();
-	}
-
-	/** Create cabinet style panel */
-	protected JPanel createCabinetStylePanel() {
-		table.setModel(model);
-		table.setAutoCreateColumnsFromModel(false);
-		table.setColumnModel(model.createColumnModel());
-		table.setVisibleRowCount(12);
-		final ListSelectionModel s = table.getSelectionModel();
-		s.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		new ListSelectionJob(this, s) {
-			public void perform() {
-				if(!event.getValueIsAdjusting())
-					selectCabinetStyle();
-			}
-		};
-		new ActionJob(this, del_button) {
-			public void perform() throws Exception {
-				int row = s.getMinSelectionIndex();
-				if(row >= 0)
-					model.deleteRow(row);
-			}
-		};
-		FormPanel panel = new FormPanel(true);
-		panel.addRow(table);
-		panel.addRow(del_button);
-		del_button.setEnabled(false);
-		return panel;
-	}
-
-	/** Change the selected cabinet style */
-	protected void selectCabinetStyle() {
-		CabinetStyle cs = model.getProxy(table.getSelectedRow());
-		del_button.setEnabled(model.canRemove(cs));
+	/** Get the visible row count */
+	protected int getVisibleRowCount() {
+		return 12;
 	}
 }
