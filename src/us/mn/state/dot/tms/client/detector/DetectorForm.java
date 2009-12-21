7d89d144a72b62d361db4f11ba309759029db971
Douglas Lau <doug.lau@state.mn.us>
2009-12-21 16:14:30 -0600

    Converted DetectorForm to subclass ProxyTableForm.

--- src/us/mn/state/dot/tms/client/detector/DetectorForm.java
+++ src/us/mn/state/dot/tms/client/detector/DetectorForm.java
@@ -14,85 +14,24 @@
  */
 package us.mn.state.dot.tms.client.detector;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.ListSelectionModel;
-import us.mn.state.dot.sched.ActionJob;
-import us.mn.state.dot.sched.ListSelectionJob;
 import us.mn.state.dot.tms.Detector;
 import us.mn.state.dot.tms.client.Session;
-import us.mn.state.dot.tms.client.toast.AbstractForm;
-import us.mn.state.dot.tms.client.toast.FormPanel;
-import us.mn.state.dot.tms.client.widget.ZTable;
+import us.mn.state.dot.tms.client.proxy.ProxyTableForm;
 
 /**
  * A form for displaying and editing detectors
  *
  * @author Douglas Lau
  */
-public class DetectorForm extends AbstractForm {
-
-	/** Frame title */
-	static protected final String TITLE = "Detectors";
-
-	/** Table model for detectors */
-	protected final DetectorModel model;
-
-	/** Table to hold the detector list */
-	protected final ZTable table = new ZTable();
-
-	/** Button to delete the selected detector */
-	protected final JButton del_button = new JButton("Delete");
+public class DetectorForm extends ProxyTableForm<Detector> {
 
 	/** Create a new detector form */
 	public DetectorForm(Session s) {
-		super(TITLE);
-		model = new DetectorModel(s);
+		super("Detectors", new DetectorModel(s));
 	}
 
-	/** Initializze the widgets in the form */
-	protected void initialize() {
-		model.initialize();
-		add(createDetectorPanel());
-	}
-
-	/** Dispose of the form */
-	protected void dispose() {
-		model.dispose();
-	}
-
-	/** Create detector panel */
-	protected JPanel createDetectorPanel() {
-		final ListSelectionModel s = table.getSelectionModel();
-		s.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		new ListSelectionJob(this, s) {
-			public void perform() {
-				if(!event.getValueIsAdjusting())
-					selectDetector();
-			}
-		};
-		new ActionJob(this, del_button) {
-			public void perform() throws Exception {
-				int row = s.getMinSelectionIndex();
-				if(row >= 0)
-					model.deleteRow(row);
-			}
-		};
-		table.setModel(model);
-		table.setAutoCreateColumnsFromModel(false);
-		table.setColumnModel(model.createColumnModel());
-		table.setRowHeight(20);
-		table.setVisibleRowCount(16);
-		FormPanel panel = new FormPanel(true);
-		panel.addRow(table);
-		panel.addRow(del_button);
-		del_button.setEnabled(false);
-		return panel;
-	}
-
-	/** Change the selected detector */
-	protected void selectDetector() {
-		Detector d = model.getProxy(table.getSelectedRow());
-		del_button.setEnabled(model.canRemove(d));
+	/** Get the row height */
+	protected int getRowHeight() {
+		return 20;
 	}
 }
