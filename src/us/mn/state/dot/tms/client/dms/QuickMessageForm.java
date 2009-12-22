572eeb40f1dfb2af5c8aef480a030aa88b191efd
Douglas Lau <doug.lau@state.mn.us>
2009-12-22 08:37:34 -0600

    Converted QuickMessageForm to subclass ProxyTableForm.

--- src/us/mn/state/dot/tms/client/dms/QuickMessageForm.java
+++ src/us/mn/state/dot/tms/client/dms/QuickMessageForm.java
@@ -14,15 +14,9 @@
  */
 package us.mn.state.dot.tms.client.dms;
 
-import javax.swing.JButton;
-import javax.swing.ListSelectionModel;
-import us.mn.state.dot.sched.ActionJob;
-import us.mn.state.dot.sched.ListSelectionJob;
 import us.mn.state.dot.tms.QuickMessage;
 import us.mn.state.dot.tms.client.Session;
-import us.mn.state.dot.tms.client.toast.AbstractForm;
-import us.mn.state.dot.tms.client.toast.FormPanel;
-import us.mn.state.dot.tms.client.widget.ZTable;
+import us.mn.state.dot.tms.client.proxy.ProxyTableForm;
 
 /**
  * A form for displaying and editing quick messages.
@@ -31,69 +25,22 @@ import us.mn.state.dot.tms.client.widget.ZTable;
  * @author Michael Darter
  * @author Doug Lau
  */
-public class QuickMessageForm extends AbstractForm {
-
-	/** Table row height */
-	static protected final int ROW_HEIGHT = 20;
-
-	/** Table model */
-	protected final QuickMessageTableModel model;
-
-	/** Table */
-	protected final ZTable table = new ZTable();
-
-	/** Button to delete the selected row */
-	protected final JButton del_btn = new JButton("Delete");
+public class QuickMessageForm extends ProxyTableForm<QuickMessage> {
 
 	/** Create a new quick message form.
-	 * @param session Session. */
-	public QuickMessageForm(Session session) {
-		super("Quick Message Form");
-		model = new QuickMessageTableModel(session);
+	 * @param s Session. */
+	public QuickMessageForm(Session s) {
+		super("Quick Messages", new QuickMessageTableModel(s));
 	}
 
-	/** Initialize the form */
-	protected void initialize() {
-		add(createMessagePanel());
-		del_btn.setEnabled(false);
+	/** Get the row height */
+	protected int getRowHeight() {
+		return 20;
 	}
 
-	/** Create the message panel */
-	protected FormPanel createMessagePanel() {
-		final ListSelectionModel s = table.getSelectionModel();
-		s.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		new ListSelectionJob(this, s) {
-			public void perform() {
-				selectRow();
-			}
-		};
-		table.setAutoCreateColumnsFromModel(false);
-		table.setColumnModel(model.createColumnModel());
-		table.setModel(model);
-		table.setRowHeight(ROW_HEIGHT);
-		table.setVisibleRowCount(12);
-		new ActionJob(this, del_btn) {
-			public void perform() throws Exception {
-				int row = s.getMinSelectionIndex();
-				if(row >= 0)
-					model.deleteRow(row);
-			}
-		};
-		FormPanel panel = new FormPanel(true);
-		panel.addRow(table);
-		panel.addRow(del_btn);
-		return panel;
-	}
-
-	/** Dispose of the quick message form */
-	public void dispose() {
-		model.dispose();
-	}
-
-	/** Handle row selection change */
-	protected void selectRow() {
-		QuickMessage qm = model.getProxy(table.getSelectedRow());
-		del_btn.setEnabled(model.canRemove(qm));
+	/** Get the visible row count */
+	protected int getVisibleRowCount() {
+		return 12;
 	}
 }
 
