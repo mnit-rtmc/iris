1308177ea42de87509753836e7f6c3563a9a3bc3
Douglas Lau <doug.lau@state.mn.us>
2009-12-22 08:48:16 -0600

    Converted MapExtentForm to subclass ProxyTableForm.

--- src/us/mn/state/dot/tms/client/roads/MapExtentForm.java
+++ src/us/mn/state/dot/tms/client/roads/MapExtentForm.java
@@ -14,84 +14,24 @@
  */
 package us.mn.state.dot.tms.client.roads;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.ListSelectionModel;
-import us.mn.state.dot.sched.ActionJob;
-import us.mn.state.dot.sched.ListSelectionJob;
 import us.mn.state.dot.tms.MapExtent;
 import us.mn.state.dot.tms.client.Session;
-import us.mn.state.dot.tms.client.toast.AbstractForm;
-import us.mn.state.dot.tms.client.toast.FormPanel;
-import us.mn.state.dot.tms.client.widget.ZTable;
+import us.mn.state.dot.tms.client.proxy.ProxyTableForm;
 
 /**
  * A form for editing map extents
  *
  * @author Douglas Lau
  */
-public class MapExtentForm extends AbstractForm {
-
-	/** Frame title */
-	static protected final String TITLE = "Map Extents";
-
-	/** Table model */
-	protected final MapExtentModel model;
-
-	/** Table to hold the map extents */
-	protected final ZTable table = new ZTable();
-
-	/** Button to delete the selected map extent */
-	protected final JButton del_btn = new JButton("Delete");
+public class MapExtentForm extends ProxyTableForm<MapExtent> {
 
 	/** Create a new map extent form */
 	public MapExtentForm(Session s) {
-		super(TITLE);
-		model = new MapExtentModel(s);
+		super("Map Extents", new MapExtentModel(s));
 	}
 
-	/** Initializze the widgets in the form */
-	protected void initialize() {
-		model.initialize();
-		add(createMapExtentPanel());
-	}
-
-	/** Dispose of the form */
-	protected void dispose() {
-		model.dispose();
-	}
-
-	/** Create map extent panel */
-	protected JPanel createMapExtentPanel() {
-		final ListSelectionModel s = table.getSelectionModel();
-		s.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		new ListSelectionJob(this, s) {
-			public void perform() {
-				if(!event.getValueIsAdjusting())
-					selectMapExtent();
-			}
-		};
-		new ActionJob(this, del_btn) {
-			public void perform() throws Exception {
-				int row = s.getMinSelectionIndex();
-				if(row >= 0)
-					model.deleteRow(row);
-			}
-		};
-		table.setModel(model);
-		table.setAutoCreateColumnsFromModel(false);
-		table.setColumnModel(model.createColumnModel());
-		table.setVisibleRowCount(20);
-		FormPanel panel = new FormPanel(true);
-		panel.addRow(table);
-		panel.addRow(del_btn);
-		del_btn.setEnabled(false);
-		return panel;
-	}
-
-	/** Change the selected map extent */
-	protected void selectMapExtent() {
-		MapExtent me = model.getProxy(table.getSelectedRow());
-		del_btn.setEnabled(model.canRemove(me));
+	/** Get the visible row count */
+	protected int getVisibleRowCount() {
+		return 20;
 	}
 }
