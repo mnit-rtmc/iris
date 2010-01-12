5f19ff1a9c8661443ba5648dd0334e7b9a1d39ea
Douglas Lau <doug.lau@state.mn.us>
2010-01-12 09:59:43 -0600

    NTCIP: try to send graphics to all DMS when sending settings.

--- src/us/mn/state/dot/tms/server/comm/ntcip/NtcipPoller.java
+++ src/us/mn/state/dot/tms/server/comm/ntcip/NtcipPoller.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2009  Minnesota Department of Transportation
+ * Copyright (C) 2000-2010  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -93,6 +93,7 @@ public class NtcipPoller extends MessagePoller implements DMSPoller, LCSPoller {
 		case SEND_SETTINGS:
 			new OpSendDMSFonts(dms).start();
 			new OpSendDMSDefaults(dms).start();
+			new OpSendDMSGraphics(dms).start();
 			break;
 		case QUERY_CONFIGURATION:
 			new OpQueryDMSConfiguration(dms).start();
