9e38ea181a27fd82142975857c01137a5061ded9
Douglas Lau <doug.lau@state.mn.us>
2010-02-04 11:31:58 -0600

    Removed an unused import.

--- src/us/mn/state/dot/tms/client/dms/DMSTab.java
+++ src/us/mn/state/dot/tms/client/dms/DMSTab.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2009  Minnesota Department of Transportation
+ * Copyright (C) 2000-2010  Minnesota Department of Transportation
  * Copyright (C) 2010 AHMCT, University of California, Davis
  *
  * This program is free software; you can redistribute it and/or modify
@@ -15,7 +15,6 @@
  */
 package us.mn.state.dot.tms.client.dms;
 
-import java.awt.BorderLayout;
 import javax.swing.BoxLayout;
 import java.util.List;
 import us.mn.state.dot.map.LayerState;
