07e27eee27cd17f7fa1fecf1fe6367da6f5af073
Douglas Lau <doug.lau@state.mn.us>
2010-02-11 10:18:12 -0600

    Added missing marker scaling code to IrisMarker.

--- src/us/mn/state/dot/tms/client/IrisMarker.java
+++ src/us/mn/state/dot/tms/client/IrisMarker.java
@@ -1,7 +1,7 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2009  Minnesota Department of Transportation
  * Copyright (C) 2010  AHMCT, University of California
+ * Copyright (C) 2010  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,35 +19,51 @@ import us.mn.state.dot.map.marker.AbstractMarker;
 import us.mn.state.dot.tms.SystemAttrEnum;
 
 /**
- * Map marker.
+ * An iris marker uses system attributes to determine the default size and
+ * maximum scale to draw on the map.
  *
  * @author Michael Darter
+ * @author Douglas Lau
  */
 public class IrisMarker extends AbstractMarker {
 
 	/** Initial scale used in subclass constructors, pixels/map unit */
-	protected static final float INIT_SCALE = 1000;
+	static protected final float INIT_SCALE = 1000;
 
-	/** Icon size scale */
-	private static float m_iconScale = getIconSizeScale();
-
-	/** Maximum icon size scale */
-	private static float m_iconScaleMax = getIconSizeScaleMax();
-
-	/** Create a new ramp meter marker.
-	 * @param FIXME */
-	public IrisMarker(int c, int sizePixels, float iconMaxSize) {
-		super(c, sizePixels, iconMaxSize * m_iconScaleMax, 
-			m_iconScale);
-	}
-
-	/** Get map icon size scale. */
+	/** Get map icon size scale */
 	static private float getIconSizeScale() {
 		return SystemAttrEnum.MAP_ICON_SIZE_SCALE.getFloat();
 	}
 
-	/** Get the map icon maximum size scale. */
+	/** Get the map icon maximum size scale */
 	static private float getIconSizeScaleMax() {
 		return SystemAttrEnum.MAP_ICON_SIZE_SCALE_MAX.getFloat();
+	}
+
+	/** Marker size in pixels */
+	protected final float size_pixels;
+
+	/** Maximum size in user coordinates (zero for no maximum) */
+	protected final float max_size;
+
+	/** Create a new iris marker.
+	 * @param c Count of nodes on marker path.
+	 * @param sp Default size of marker in pixels.
+	 * @param ms Maximum size of marker in user coordinates. */
+	public IrisMarker(int c, int sp, int ms) {
+		super(c);
+		size_pixels = sp * getIconSizeScale();
+		max_size = ms * getIconSizeScaleMax();
+	}
+
+	/** Get the scaled marker size.
+	 * @param scale Map scale in pixels per user coordinate.
+	 * @return Marker size in user coordinates. */
+	protected float getMarkerSize(float scale) {
+		float size = size_pixels * scale;
+		if(max_size > 0)
+			return Math.min(size, max_size);
+		else
+			return size;
 	}
 }
