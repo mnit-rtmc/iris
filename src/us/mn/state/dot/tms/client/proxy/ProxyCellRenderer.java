f1d8b6bc8102898007d8861b8e4f9efb3fc10c29
Douglas Lau <doug.lau@state.mn.us>
2010-02-17 14:56:50 -0600

    Removed an unused import.

--- src/us/mn/state/dot/tms/client/proxy/ProxyCellRenderer.java
+++ src/us/mn/state/dot/tms/client/proxy/ProxyCellRenderer.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2008-2009  Minnesota Department of Transportation
+ * Copyright (C) 2008-2010  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,7 +18,6 @@ import java.awt.Component;
 import javax.swing.JList;
 import javax.swing.DefaultListCellRenderer;
 import us.mn.state.dot.sonar.SonarObject;
-import us.mn.state.dot.tms.GeoLocHelper;
 
 /**
  * ListCellRenderer for ProxyJList cells.
