e1a040a0d1cc8531dad2e2961e9d1e5b35ab2642
Douglas Lau <doug.lau@state.mn.us>
2010-02-24 11:33:41 -0600

    Split IncidentPolicy out of IncidentDeployForm.

--- src/us/mn/state/dot/tms/client/incident/IncidentDeployForm.java
+++ src/us/mn/state/dot/tms/client/incident/IncidentDeployForm.java
@@ -14,7 +14,6 @@
  */
 package us.mn.state.dot.tms.client.incident;
 
-import java.awt.Color;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -50,30 +49,6 @@ import us.mn.state.dot.tms.client.toast.SonarObjectForm;
  */
 public class IncidentDeployForm extends SonarObjectForm<Incident> {
 
-	/** Impact codes */
-	static private enum ImpactCode {
-		FREE_FLOWING, PARTIALLY_BLOCKED, BLOCKED;
-		static protected ImpactCode fromChar(char im) {
-			switch(im) {
-			case '?':
-				return PARTIALLY_BLOCKED;
-			case '!':
-				return BLOCKED;
-			default:
-				return FREE_FLOWING;
-			}
-		}
-	};
-
-	/** Distance 1 upstream of incident to deploy devices */
-	static protected final float DIST_UPSTREAM_1_MILES = 0.5f;
-
-	/** Distance 2 upstream of incident to deploy devices */
-	static protected final float DIST_UPSTREAM_2_MILES = 1.0f;
-
-	/** Distance 3 upstream of incident to deploy devices */
-	static protected final float DIST_UPSTREAM_3_MILES = 1.5f;
-
 	/** Frame title */
 	static private final String TITLE = "Incident: ";
 
@@ -82,6 +57,9 @@ public class IncidentDeployForm extends SonarObjectForm<Incident> {
 
 	/** Incident manager */
 	protected final IncidentManager manager;
+
+	/** Incident deployment policy */
+	protected final IncidentPolicy policy;
 
 	/** Mapping of LCS array names to proposed indications */
 	protected final HashMap<String, Integer []> indications =
@@ -101,6 +79,7 @@ public class IncidentDeployForm extends SonarObjectForm<Incident> {
 		super(TITLE, s, inc);
 		user = session.getUser();
 		manager = man;
+		policy = new IncidentPolicy(inc);
 	}
 
 	/** Get the SONAR type cache */
@@ -145,185 +124,20 @@ public class IncidentDeployForm extends SonarObjectForm<Incident> {
 			Float lp = cb.calculateMilePoint(loc);
 			if(lp != null) {
 				float up = mp - lp;
-				if(up > 0 && up < DIST_UPSTREAM_3_MILES)
+				if(up > 0)
 					upstream.put(up, lcs_array);
 			}
 		}
 		int shift = cb.getShift(proxy.getEasting(),proxy.getNorthing());
 		for(Float up: upstream.keySet()) {
 			LCSArray lcs_array = upstream.get(up);
-			Integer[] ind = createIndications(lcs_array, up, shift);
+			Integer[] ind = policy.createIndications(lcs_array, up,
+				shift);
 			if(shouldDeploy(ind)) {
 				model.addElement(lcs_array);
 				indications.put(lcs_array.getName(), ind);
 			}
 		}
-	}
-
-	/** Create indications for an LCS array.
-	 * @param lcs_array LCS array to deploy.
-	 * @param up Distance upstream from incident (miles).
-	 * @param shift Left lane shift at incident.
-	 * @return Array of LaneUseIndication ordinal values. */
-	protected Integer[] createIndications(LCSArray lcs_array, float up,
-		int shift)
-	{
-		int n_lanes = lcs_array.getIndicationsCurrent().length;
-		int l_shift = lcs_array.getShift() - shift;
-		if(up < DIST_UPSTREAM_1_MILES)
-			return createIndications1(n_lanes, l_shift);
-		if(up < DIST_UPSTREAM_2_MILES)
-			return createIndications2(n_lanes, l_shift);
-		else
-			return createIndications3(n_lanes, l_shift);
-	}
-
-	/** Create first indications for an LCS array.
-	 * @param n_lanes Number of lanes on LCS array.
-	 * @param l_shift Lane shift relative to incident.
-	 * @return Array of LaneUseIndication ordinal values. */
-	protected Integer[] createIndications1(int n_lanes, int l_shift) {
-		Integer[] ind = new Integer[n_lanes];
-		for(int i = 0; i < ind.length; i++)
-			ind[i] = getIndication1(l_shift + n_lanes -i).ordinal();
-		return ind;
-	}
-
-	/** Get the first indication for one lane.
-	 * @param ln Lane shift relative to incident.
-	 * @return LaneUseIndication value. */
-	protected LaneUseIndication getIndication1(int ln) {
-		ImpactCode ic = getImpact(ln);
-		if(ic == ImpactCode.BLOCKED)
-			return LaneUseIndication.LANE_CLOSED;
-		if(ic == ImpactCode.PARTIALLY_BLOCKED)
-			return LaneUseIndication.USE_CAUTION;
-		ImpactCode right = getImpact(ln + 1);
-		if(right == ImpactCode.BLOCKED)
-			return LaneUseIndication.USE_CAUTION;
-		ImpactCode left = getImpact(ln - 1);
-		if(left == ImpactCode.BLOCKED)
-			return LaneUseIndication.USE_CAUTION;
-		return LaneUseIndication.LANE_OPEN;
-	}
-
-	/** Get the impact at the specified lane */
-	protected ImpactCode getImpact(int ln) {
-		String impact = proxy.getImpact();
-		if(ln < 0 || ln >= impact.length())
-			return ImpactCode.FREE_FLOWING;
-		else
-			return ImpactCode.fromChar(impact.charAt(ln));
-	}
-
-	/** Create second indications for an LCS array.
-	 * @param n_lanes Number of lanes on LCS array.
-	 * @param l_shift Lane shift relative to incident.
-	 * @return Array of LaneUseIndication ordinal values. */
-	protected Integer[] createIndications2(int n_lanes, int l_shift) {
-		Integer[] ind = new Integer[n_lanes];
-		for(int i = 0; i < ind.length; i++)
-			ind[i] = getIndication2(n_lanes, l_shift, i).ordinal();
-		return ind;
-	}
-
-	/** Get the second indication for one lane.
-	 * @param n_lanes Number of lanes in array.
-	 * @param l_shift Lane shift relative to incident.
-	 * @return LaneUseIndication value. */
-	protected LaneUseIndication getIndication2(int n_lanes, int l_shift,
-		int i)
-	{
-		int ln = l_shift + n_lanes - i;
-		ImpactCode ic = getImpact2(ln);
-		if(ic != ImpactCode.BLOCKED)
-			return LaneUseIndication.LANE_OPEN;
-		ImpactCode right = getImpact2(ln + 1);
-		if(i - 1 < 0)
-			right = ImpactCode.BLOCKED;
-		ImpactCode left = getImpact2(ln - 1);
-		if(i + 1 >= n_lanes)
-			left = ImpactCode.BLOCKED;
-		if(left == ImpactCode.BLOCKED && right == ImpactCode.BLOCKED)
-			return LaneUseIndication.LANE_CLOSED;
-		if(left != ImpactCode.BLOCKED && right != ImpactCode.BLOCKED)
-			return LaneUseIndication.MERGE_BOTH;
-		if(left == ImpactCode.BLOCKED)
-			return LaneUseIndication.MERGE_RIGHT;
-		else
-			return LaneUseIndication.MERGE_LEFT;
-	}
-
-	/** Get the impact at the specified lane */
-	protected ImpactCode getImpact2(int ln) {
-		String impact = proxy.getImpact();
-		if(ln < 0 || ln >= impact.length())
-			return ImpactCode.BLOCKED;
-		else
-			return ImpactCode.fromChar(impact.charAt(ln));
-	}
-
-	/** Create third indications for an LCS array.
-	 * @param n_lanes Number of lanes on LCS array.
-	 * @param l_shift Lane shift relative to incident.
-	 * @return Array of LaneUseIndication ordinal values. */
-	protected Integer[] createIndications3(int n_lanes, int l_shift) {
-		Integer[] ind = new Integer[n_lanes];
-		for(int i = 0; i < ind.length; i++)
-			ind[i] = getIndication3(n_lanes, l_shift, i).ordinal();
-		return ind;
-	}
-
-	/** Get the third indication for one lane.
-	 * @param n_lanes Number of lanes in array.
-	 * @param l_shift Lane shift relative to incident.
-	 * @return LaneUseIndication value. */
-	protected LaneUseIndication getIndication3(int n_lanes, int l_shift,
-		int i)
-	{
-		int ln = l_shift + n_lanes - i;
-		ImpactCode ic = getImpact2(ln);
-		if(ic != ImpactCode.BLOCKED)
-			return LaneUseIndication.LANE_OPEN;
-		ImpactCode right = getImpact2(ln + 1);
-		if(i - 1 < 0)
-			right = ImpactCode.BLOCKED;
-		ImpactCode left = getImpact2(ln - 1);
-		if(i + 1 >= n_lanes)
-			left = ImpactCode.BLOCKED;
-		if(left == ImpactCode.BLOCKED && right == ImpactCode.BLOCKED) {
-			// NOTE: adjacent lanes are also blocked, so find the
-			//       impact 2 lanes to the left and right
-			right = getImpact2(ln + 2);
-			if(i - 2 < 0)
-				right = ImpactCode.BLOCKED;
-			left = getImpact2(ln - 2);
-			if(i + 2 >= n_lanes)
-				left = ImpactCode.BLOCKED;
-			if(left != ImpactCode.BLOCKED &&
-			   right != ImpactCode.BLOCKED)
-				return LaneUseIndication.MERGE_BOTH;
-			if(left == ImpactCode.BLOCKED)
-				return LaneUseIndication.MERGE_RIGHT;
-			else
-				return LaneUseIndication.MERGE_LEFT;
-		} else
-			return LaneUseIndication.LANE_CLOSED_AHEAD;
-	}
-
-	/** Check if a set of indications should be deployed */
-	static protected boolean shouldDeploy(Integer[] ind) {
-		for(int i: ind) {
-			LaneUseIndication li = LaneUseIndication.fromOrdinal(i);
-			switch(LaneUseIndication.fromOrdinal(i)) {
-			case DARK:
-			case LANE_OPEN:
-				continue;
-			default:
-				return true;
-			}
-		}
-		return false;
 	}
 
 	/** Find all LCS arrays on the given corridor */
@@ -345,6 +159,21 @@ public class IncidentDeployForm extends SonarObjectForm<Incident> {
 				it.remove();
 		}
 		return lcss;
+	}
+
+	/** Check if a set of indications should be deployed */
+	static protected boolean shouldDeploy(Integer[] ind) {
+		for(int i: ind) {
+			LaneUseIndication li = LaneUseIndication.fromOrdinal(i);
+			switch(LaneUseIndication.fromOrdinal(i)) {
+			case DARK:
+			case LANE_OPEN:
+				continue;
+			default:
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/** Create the panel for the form */
---
+++ src/us/mn/state/dot/tms/client/incident/IncidentPolicy.java
@@ -0,0 +1,212 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2010  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.client.incident;
+
+import us.mn.state.dot.tms.Incident;
+import us.mn.state.dot.tms.LaneUseIndication;
+import us.mn.state.dot.tms.LCSArray;
+
+/**
+ * IncidentPolicy determines which LCS indications to propose for an incident.
+ *
+ * @author Douglas Lau
+ */
+public class IncidentPolicy {
+
+	/** Impact codes */
+	static private enum ImpactCode {
+		FREE_FLOWING, PARTIALLY_BLOCKED, BLOCKED;
+		static protected ImpactCode fromChar(char im) {
+			switch(im) {
+			case '?':
+				return PARTIALLY_BLOCKED;
+			case '!':
+				return BLOCKED;
+			default:
+				return FREE_FLOWING;
+			}
+		}
+	};
+
+	/** Distance 1 upstream of incident to deploy devices */
+	static protected final float DIST_UPSTREAM_1_MILES = 0.5f;
+
+	/** Distance 2 upstream of incident to deploy devices */
+	static protected final float DIST_UPSTREAM_2_MILES = 1.0f;
+
+	/** Distance 3 upstream of incident to deploy devices */
+	static protected final float DIST_UPSTREAM_3_MILES = 1.5f;
+
+	/** Incident in question */
+	protected final Incident incident;
+
+	/** Create a new incident policy */
+	public IncidentPolicy(Incident inc) {
+		incident = inc;
+	}
+
+	/** Create proposed indications for an LCS array.
+	 * @param lcs_array LCS array to deploy.
+	 * @param up Distance upstream from incident (miles).
+	 * @param shift Left lane shift at incident.
+	 * @return Array of LaneUseIndication ordinal values. */
+	public Integer[] createIndications(LCSArray lcs_array, float up,
+		int shift)
+	{
+		if(up < 0 || up > DIST_UPSTREAM_3_MILES)
+			return new Integer[0];
+		int n_lanes = lcs_array.getIndicationsCurrent().length;
+		int l_shift = lcs_array.getShift() - shift;
+		if(up < DIST_UPSTREAM_1_MILES)
+			return createIndications1(n_lanes, l_shift);
+		if(up < DIST_UPSTREAM_2_MILES)
+			return createIndications2(n_lanes, l_shift);
+		else
+			return createIndications3(n_lanes, l_shift);
+	}
+
+	/** Create first indications for an LCS array.
+	 * @param n_lanes Number of lanes on LCS array.
+	 * @param l_shift Lane shift relative to incident.
+	 * @return Array of LaneUseIndication ordinal values. */
+	protected Integer[] createIndications1(int n_lanes, int l_shift) {
+		Integer[] ind = new Integer[n_lanes];
+		for(int i = 0; i < ind.length; i++)
+			ind[i] = getIndication1(l_shift + n_lanes -i).ordinal();
+		return ind;
+	}
+
+	/** Get the first indication for one lane.
+	 * @param ln Lane shift relative to incident.
+	 * @return LaneUseIndication value. */
+	protected LaneUseIndication getIndication1(int ln) {
+		ImpactCode ic = getImpact(ln);
+		if(ic == ImpactCode.BLOCKED)
+			return LaneUseIndication.LANE_CLOSED;
+		if(ic == ImpactCode.PARTIALLY_BLOCKED)
+			return LaneUseIndication.USE_CAUTION;
+		ImpactCode right = getImpact(ln + 1);
+		if(right == ImpactCode.BLOCKED)
+			return LaneUseIndication.USE_CAUTION;
+		ImpactCode left = getImpact(ln - 1);
+		if(left == ImpactCode.BLOCKED)
+			return LaneUseIndication.USE_CAUTION;
+		return LaneUseIndication.LANE_OPEN;
+	}
+
+	/** Get the impact at the specified lane */
+	protected ImpactCode getImpact(int ln) {
+		String impact = incident.getImpact();
+		if(ln < 0 || ln >= impact.length())
+			return ImpactCode.FREE_FLOWING;
+		else
+			return ImpactCode.fromChar(impact.charAt(ln));
+	}
+
+	/** Create second indications for an LCS array.
+	 * @param n_lanes Number of lanes on LCS array.
+	 * @param l_shift Lane shift relative to incident.
+	 * @return Array of LaneUseIndication ordinal values. */
+	protected Integer[] createIndications2(int n_lanes, int l_shift) {
+		Integer[] ind = new Integer[n_lanes];
+		for(int i = 0; i < ind.length; i++)
+			ind[i] = getIndication2(n_lanes, l_shift, i).ordinal();
+		return ind;
+	}
+
+	/** Get the second indication for one lane.
+	 * @param n_lanes Number of lanes in array.
+	 * @param l_shift Lane shift relative to incident.
+	 * @return LaneUseIndication value. */
+	protected LaneUseIndication getIndication2(int n_lanes, int l_shift,
+		int i)
+	{
+		int ln = l_shift + n_lanes - i;
+		ImpactCode ic = getImpact2(ln);
+		if(ic != ImpactCode.BLOCKED)
+			return LaneUseIndication.LANE_OPEN;
+		ImpactCode right = getImpact2(ln + 1);
+		if(i - 1 < 0)
+			right = ImpactCode.BLOCKED;
+		ImpactCode left = getImpact2(ln - 1);
+		if(i + 1 >= n_lanes)
+			left = ImpactCode.BLOCKED;
+		if(left == ImpactCode.BLOCKED && right == ImpactCode.BLOCKED)
+			return LaneUseIndication.LANE_CLOSED;
+		if(left != ImpactCode.BLOCKED && right != ImpactCode.BLOCKED)
+			return LaneUseIndication.MERGE_BOTH;
+		if(left == ImpactCode.BLOCKED)
+			return LaneUseIndication.MERGE_RIGHT;
+		else
+			return LaneUseIndication.MERGE_LEFT;
+	}
+
+	/** Get the impact at the specified lane */
+	protected ImpactCode getImpact2(int ln) {
+		String impact = incident.getImpact();
+		if(ln < 0 || ln >= impact.length())
+			return ImpactCode.BLOCKED;
+		else
+			return ImpactCode.fromChar(impact.charAt(ln));
+	}
+
+	/** Create third indications for an LCS array.
+	 * @param n_lanes Number of lanes on LCS array.
+	 * @param l_shift Lane shift relative to incident.
+	 * @return Array of LaneUseIndication ordinal values. */
+	protected Integer[] createIndications3(int n_lanes, int l_shift) {
+		Integer[] ind = new Integer[n_lanes];
+		for(int i = 0; i < ind.length; i++)
+			ind[i] = getIndication3(n_lanes, l_shift, i).ordinal();
+		return ind;
+	}
+
+	/** Get the third indication for one lane.
+	 * @param n_lanes Number of lanes in array.
+	 * @param l_shift Lane shift relative to incident.
+	 * @return LaneUseIndication value. */
+	protected LaneUseIndication getIndication3(int n_lanes, int l_shift,
+		int i)
+	{
+		int ln = l_shift + n_lanes - i;
+		ImpactCode ic = getImpact2(ln);
+		if(ic != ImpactCode.BLOCKED)
+			return LaneUseIndication.LANE_OPEN;
+		ImpactCode right = getImpact2(ln + 1);
+		if(i - 1 < 0)
+			right = ImpactCode.BLOCKED;
+		ImpactCode left = getImpact2(ln - 1);
+		if(i + 1 >= n_lanes)
+			left = ImpactCode.BLOCKED;
+		if(left == ImpactCode.BLOCKED && right == ImpactCode.BLOCKED) {
+			// NOTE: adjacent lanes are also blocked, so find the
+			//       impact 2 lanes to the left and right
+			right = getImpact2(ln + 2);
+			if(i - 2 < 0)
+				right = ImpactCode.BLOCKED;
+			left = getImpact2(ln - 2);
+			if(i + 2 >= n_lanes)
+				left = ImpactCode.BLOCKED;
+			if(left != ImpactCode.BLOCKED &&
+			   right != ImpactCode.BLOCKED)
+				return LaneUseIndication.MERGE_BOTH;
+			if(left == ImpactCode.BLOCKED)
+				return LaneUseIndication.MERGE_RIGHT;
+			else
+				return LaneUseIndication.MERGE_LEFT;
+		} else
+			return LaneUseIndication.LANE_CLOSED_AHEAD;
+	}
+}
