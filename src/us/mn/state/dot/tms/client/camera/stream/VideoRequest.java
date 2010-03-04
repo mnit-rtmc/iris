0e975e7ad728c1109dc2e22860e77963d09e1eb5
Douglas Lau <doug.lau@state.mn.us>
2010-03-04 09:14:14 -0600

    Added project name to file headers.

--- src/us/mn/state/dot/tms/client/camera/stream/VideoRequest.java
+++ src/us/mn/state/dot/tms/client/camera/stream/VideoRequest.java
@@ -1,4 +1,5 @@
 /*
+ * IRIS -- Intelligent Roadway Information System
  * Copyright (C) 2003-2010  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
--- src/us/mn/state/dot/tms/client/camera/stream/VideoStream.java
+++ src/us/mn/state/dot/tms/client/camera/stream/VideoStream.java
@@ -1,4 +1,5 @@
 /*
+ * IRIS -- Intelligent Roadway Information System
  * Copyright (C) 2007-2010  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
