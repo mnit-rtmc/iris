7fa281e85457360ebab45a94f42e47723d08b3de
Douglas Lau <doug.lau@state.mn.us>
2010-04-01 14:43:14 -0500

    Fixed an ArrayIndexOutOfBoundsException when rendering MULTI text.

--- src/us/mn/state/dot/tms/MultiRenderer.java
+++ src/us/mn/state/dot/tms/MultiRenderer.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2009  Minnesota Department of Transportation
+ * Copyright (C) 2009-2010  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -167,6 +167,9 @@ public class MultiRenderer extends MultiStringStateAdapter {
 			}
 			catch(InvalidMessageException e) {
 				syntax_err = SyntaxError.characterNotDefined;
+			}
+			catch(ArrayIndexOutOfBoundsException e) {
+				syntax_err = SyntaxError.textTooBig;
 			}
 		}
 		blocks.clear();
