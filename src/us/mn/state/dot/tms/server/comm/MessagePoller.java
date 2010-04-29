a624200101b983f14aca185deccc5b5719f56a7c
Douglas Lau <doug.lau@state.mn.us>
2010-04-29 13:46:03 -0500

    Removed FIXME with unneeded catch for NumberFormatException.

--- src/us/mn/state/dot/tms/server/comm/MessagePoller.java
+++ src/us/mn/state/dot/tms/server/comm/MessagePoller.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2009  Minnesota Department of Transportation
+ * Copyright (C) 2000-2010  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -199,11 +199,6 @@ abstract public class MessagePoller extends Thread {
 		catch(SocketTimeoutException e) {
 			o.handleCommError(EventType.POLL_TIMEOUT_ERROR,
 				exceptionMessage(e));
-		}
-		catch(NumberFormatException e) {
-			// FIXME: this should be caught at lower levels ...
-			o.handleCommError(EventType.PARSING_ERROR,
-				"NUMBER FORMAT ERROR");
 		}
 		finally {
 			if(o.isDone() || !queueOperation(o))
