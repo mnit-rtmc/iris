304f757aa3f2c5877ef01d5430d5f415fd103a51
Douglas Lau <doug.lau@state.mn.us>
2010-04-29 11:44:37 -0500

    Minor whitespace cleanup.

--- src/us/mn/state/dot/tms/server/comm/mndot/BCD.java
+++ src/us/mn/state/dot/tms/server/comm/mndot/BCD.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2009  Minnesota Department of Transportation
+ * Copyright (C) 2000-2010  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -33,56 +33,56 @@ public interface BCD {
 		protected final java.io.OutputStream out;
 
 		/** Create a new BCD output stream */
-		public OutputStream( java.io.OutputStream os ) {
+		public OutputStream(java.io.OutputStream os) {
 			out = os;
 		}
 
 		/** Write one byte to the output stream */
-		public void write( int b ) throws IOException {
-			out.write( b );
+		public void write(int b) throws IOException {
+			out.write(b);
 		}
 
 		/** Get the first digit (on the right) */
-		static protected int digit1( int i ) {
+		static protected int digit1(int i) {
 			return i % 10;
 		}
 
 		/** Get the second digit (from the right) */
-		static protected int digit2( int i ) {
-			return ( i / 10 ) % 10;
+		static protected int digit2(int i) {
+			return (i / 10) % 10;
 		}
 
 		/** Get the third digit (from the right) */
-		static protected int digit3( int i ) {
-			return ( i / 100 ) % 10;
+		static protected int digit3(int i) {
+			return (i / 100) % 10;
 		}
 
 		/** Get the fourth digit (from the right) */
-		static protected int digit4( int i ) {
-			return ( i / 1000 ) % 10;
+		static protected int digit4(int i) {
+			return (i / 1000) % 10;
 		}
 
 		/** Write the specified integer to the output stream as an
 		 * 8-bit BCD.  Note: the valid range for an 8-bit BCD
 		 * value is 0 to 99. */
-		public void write8Bit( int i ) throws IOException {
-			if( i < 0 || i > 99 ) {
+		public void write8Bit(int i) throws IOException {
+			if(i < 0 || i > 99) {
 				throw new NumberFormatException(
-					"Invalid 8-bit BCD: " + i );
+					"Invalid 8-bit BCD: " + i);
 			}
-			write( digit2( i ) << 4 | digit1( i ) );
+			write(digit2(i) << 4 | digit1(i));
 		}
 
 		/** Write the specified integer to the output stream as a
 		 * 16-bit BCD.  Note: the valid range for a 16-bit BCD
 		 * value is 0 to 9999. */
-		public void write16Bit( int i ) throws IOException {
-			if( i < 0 || i > 9999 ) {
+		public void write16Bit(int i) throws IOException {
+			if(i < 0 || i > 9999) {
 				throw new NumberFormatException(
-					"Invalid 16-bit BCD: " + i );
+					"Invalid 16-bit BCD: " + i);
 			}
-			write( digit4( i ) << 4 | digit3( i ) );
-			write( digit2( i ) << 4 | digit1( i ) );
+			write(digit4(i) << 4 | digit3(i));
+			write(digit2(i) << 4 | digit1(i));
 		}
 	}
 
@@ -96,7 +96,7 @@ public interface BCD {
 		protected final java.io.InputStream in;
 
 		/** Create a new BCD input stream */
-		public InputStream( java.io.InputStream is ) {
+		public InputStream(java.io.InputStream is) {
 			in = is;
 		}
 
@@ -106,23 +106,25 @@ public interface BCD {
 		}
 
 		/** Get the first digit (on the right) */
-		static protected int digit1( int i ) {
+		static protected int digit1(int i) {
 			i &= DIGIT_MASK;
-			if( i < 0 || i > 9 ) throw new
-				NumberFormatException( "Invalid BCD: " + i );
+			if(i < 0 || i > 9) {
+				throw new NumberFormatException(
+					"Invalid BCD: " + i);
+			}
 			else return i;
 		}
 
 		/** Get the second digit (on the right) */
-		static protected int digit2( int i ) {
-			return digit1( i >> 4 );
+		static protected int digit2(int i) {
+			return digit1(i >> 4);
 		}
 
 		/** Read an 8-bit BCD value from the stream.  Note: the valid
 		 * range for an 8-bit BCD value is 0 to 99. */
 		public int read8Bit() throws IOException {
 			int i = read();
-			return 10 * digit2( i ) + digit1( i );
+			return 10 * digit2(i) + digit1(i);
 		}
 
 		/** Read a 16-bit BCD value from the stream.  Note: the valid
@@ -130,8 +132,8 @@ public interface BCD {
 		public int read16Bit() throws IOException {
 			int hi = read();
 			int lo = read();
-			return 1000 * digit2( hi ) + 100 * digit1( hi ) +
-				10 * digit2( lo ) + digit1( lo );
+			return 1000 * digit2(hi) + 100 * digit1(hi) +
+				10 * digit2(lo) + digit1(lo);
 		}
 	}
 }
