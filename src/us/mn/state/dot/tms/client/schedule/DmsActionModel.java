68e1db14a97173031daa996c8fb62063da5adb0b
Douglas Lau <doug.lau@state.mn.us>
2010-06-08 11:29:59 -0500

    Allow SPEED_LIMIT priority for DMS action plans.

--- src/us/mn/state/dot/tms/client/schedule/DmsActionModel.java
+++ src/us/mn/state/dot/tms/client/schedule/DmsActionModel.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2009  Minnesota Department of Transportation
+ * Copyright (C) 2009-2010  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -48,6 +48,7 @@ public class DmsActionModel extends ProxyTableModel<DmsAction> {
 	static protected final DMSMessagePriority[] PRIORITIES = {
 		DMSMessagePriority.PSA,
 		DMSMessagePriority.TRAVEL_TIME,
+		DMSMessagePriority.SPEED_LIMIT,
 		DMSMessagePriority.SCHEDULED,
 		DMSMessagePriority.INCIDENT_LOW,
 		DMSMessagePriority.INCIDENT_MED,
