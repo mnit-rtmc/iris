fedab4ebcf8671ab95b0e34068811c39e102edd1
Douglas Lau <doug.lau@state.mn.us>
2010-06-14 16:30:36 -0500

    Removed an unused import.

--- src/us/mn/state/dot/tms/SignMessage.java
+++ src/us/mn/state/dot/tms/SignMessage.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2009  Minnesota Department of Transportation
+ * Copyright (C) 2000-2010  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
 package us.mn.state.dot.tms;
 
 import us.mn.state.dot.sonar.SonarObject;
-import us.mn.state.dot.sonar.User;
 
 /**
  * A sign message represents a message which can be displayed on a dynamic
