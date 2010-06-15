0d75f2b42772613fc5a6c5df2f59765e02c1f110
Douglas Lau <doug.lau@state.mn.us>
2010-06-15 10:11:04 -0500

    Split ConnectionPanel out from UserRoleForm.

---
+++ src/us/mn/state/dot/tms/client/system/ConnectionPanel.java
@@ -0,0 +1,89 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2007-2010  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.client.system;
+
+import javax.swing.JButton;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import us.mn.state.dot.sched.ActionJob;
+import us.mn.state.dot.sonar.Connection;
+import us.mn.state.dot.tms.client.Session;
+import us.mn.state.dot.tms.client.toast.FormPanel;
+import us.mn.state.dot.tms.client.widget.ZTable;
+
+/**
+ * A panel for viewing connections.
+ *
+ * @author Douglas Lau
+ */
+public class ConnectionPanel extends FormPanel {
+
+	/** Table model for connections */
+	protected final ConnectionModel c_model;
+
+	/** Table to hold the connection list */
+	protected final ZTable c_table = new ZTable();
+
+	/** Button to delete the selected connection */
+	protected final JButton del_conn = new JButton("Disconnect");
+
+	/** Create a new connection panel */
+	public ConnectionPanel(Session s) {
+		super(true);
+		c_model = new ConnectionModel(s);
+		c_table.setModel(c_model);
+		c_table.setAutoCreateColumnsFromModel(false);
+		c_table.setColumnModel(c_model.createColumnModel());
+		c_table.setVisibleRowCount(16);
+		addRow(c_table);
+		if(false) {
+			del_conn.setEnabled(false);
+			addRow(del_conn);
+		}
+	}
+
+	/** Initializze the panel */
+	protected void initialize() {
+		c_model.initialize();
+		final ListSelectionModel s = c_table.getSelectionModel();
+		s.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		s.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent e) {
+				if(!e.getValueIsAdjusting())
+					selectConnection();
+			}
+		});
+		new ActionJob(this, del_conn) {
+			public void perform() throws Exception {
+				int row = s.getMinSelectionIndex();
+				if(row >= 0)
+					c_model.deleteRow(row);
+			}
+		};
+	}
+
+	/** Dispose of the panel */
+	protected void dispose() {
+		c_model.dispose();
+	}
+
+	/** Change the selected connection */
+	protected void selectConnection() {
+		ListSelectionModel s = c_table.getSelectionModel();
+		Connection c = c_model.getProxy(s.getMinSelectionIndex());
+		del_conn.setEnabled(c != null);
+	}
+}
--- src/us/mn/state/dot/tms/client/system/UserRoleForm.java
+++ src/us/mn/state/dot/tms/client/system/UserRoleForm.java
@@ -14,24 +14,13 @@
  */
 package us.mn.state.dot.tms.client.system;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
-import javax.swing.ListSelectionModel;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-import us.mn.state.dot.sched.ActionJob;
 import us.mn.state.dot.sonar.Capability;
-import us.mn.state.dot.sonar.Connection;
 import us.mn.state.dot.sonar.Privilege;
 import us.mn.state.dot.sonar.Role;
 import us.mn.state.dot.sonar.User;
 import us.mn.state.dot.tms.client.Session;
 import us.mn.state.dot.tms.client.toast.AbstractForm;
-import us.mn.state.dot.tms.client.widget.ZTable;
 
 /**
  * A form for displaying and editing the users and roles
@@ -63,27 +52,17 @@ public class UserRoleForm extends AbstractForm {
 	/** Capability panel */
 	protected final CapabilityPanel cap_panel;
 
-	/** Table model for connections */
-	protected final ConnectionModel c_model;
-
-	/** Table to hold the connection list */
-	protected final ZTable c_table = new ZTable();
-
-	/** Button to delete the selected connection */
-	protected final JButton del_conn = new JButton("Disconnect");
-
-	/** User session */
-	protected final Session session;
+	/** Connection panel */
+	protected final ConnectionPanel c_panel;
 
 	/** Create a new user role form */
 	public UserRoleForm(Session s) {
 		super(TITLE);
-		session = s;
 		setHelpPageName("Help.UserRoleForm");
 		u_panel = new UserPanel(s);
 		r_panel = new RolePanel(s);
 		cap_panel = new CapabilityPanel(s);
-		c_model = new ConnectionModel(session);
+		c_panel = new ConnectionPanel(s);
 	}
 
 	/** Initializze the widgets in the form */
@@ -91,11 +70,11 @@ public class UserRoleForm extends AbstractForm {
 		u_panel.initialize();
 		r_panel.initialize();
 		cap_panel.initialize();
-		c_model.initialize();
+		c_panel.initialize();
 		tab.add("Users", u_panel);
 		tab.add("Roles", r_panel);
 		tab.add("Capabilities", cap_panel);
-		tab.add("Connections", createConnectionPanel());
+		tab.add("Connections", c_panel);
 		add(tab);
 	}
 
@@ -105,48 +84,6 @@ public class UserRoleForm extends AbstractForm {
 		u_panel.dispose();
 		r_panel.dispose();
 		cap_panel.dispose();
-		c_model.dispose();
-	}
-
-	/** Create connection panel */
-	protected JPanel createConnectionPanel() {
-		JPanel panel = new JPanel(new GridBagLayout());
-		panel.setBorder(BORDER);
-		GridBagConstraints bag = new GridBagConstraints();
-		final ListSelectionModel s = c_table.getSelectionModel();
-		s.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		s.addListSelectionListener(new ListSelectionListener() {
-			public void valueChanged(ListSelectionEvent e) {
-				if(e.getValueIsAdjusting())
-					return;
-				selectConnection();
-			}
-		});
-		c_table.setModel(c_model);
-		c_table.setAutoCreateColumnsFromModel(false);
-		c_table.setColumnModel(c_model.createColumnModel());
-		c_table.setVisibleRowCount(16);
-		JScrollPane pane = new JScrollPane(c_table);
-		panel.add(pane, bag);
-		if(false) {
-			del_conn.setEnabled(false);
-			bag.insets.left = 6;
-			panel.add(del_conn, bag);
-			new ActionJob(this, del_conn) {
-				public void perform() throws Exception {
-					int row = s.getMinSelectionIndex();
-					if(row >= 0)
-						c_model.deleteRow(row);
-				}
-			};
-		}
-		return panel;
-	}
-
-	/** Change the selected connection */
-	protected void selectConnection() {
-		ListSelectionModel s = c_table.getSelectionModel();
-		Connection c = c_model.getProxy(s.getMinSelectionIndex());
-		del_conn.setEnabled(c != null);
+		c_panel.dispose();
 	}
 }
