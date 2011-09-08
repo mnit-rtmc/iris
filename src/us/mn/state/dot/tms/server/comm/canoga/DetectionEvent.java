cfd37f1c3967728a46e186268c8961eec2dcf5f2
Douglas Lau <doug.lau@state.mn.us>
2011-09-08 11:25:23 -0500

    canoga: Minor whitespace cleanups.

--- src/us/mn/state/dot/tms/server/comm/canoga/DetectionEvent.java
+++ src/us/mn/state/dot/tms/server/comm/canoga/DetectionEvent.java
@@ -135,7 +135,7 @@ public class DetectionEvent {
 		if(isHeadwayValid(prev)) {
 			int missed = calculateMissed(prev);
 			for(int i = 0; i < missed; i++)
-				controller.logEvent(stamp, inp+1,0,0,0);
+				controller.logEvent(stamp, inp + 1, 0, 0, 0);
 			// If no vehicles were missed, log headway
 			if(missed == 0)
 				headway = calculateElapsed(prev);
@@ -143,8 +143,7 @@ public class DetectionEvent {
 			// There is a gap in vehicle event log
 			controller.logEvent(null, inp + 1, 0, 0, 0);
 		}
-		controller.logEvent(stamp, inp + 1, duration, headway,
-			speed);
+		controller.logEvent(stamp, inp + 1, duration, headway, speed);
 	}
 
 	/** Test if headway from previous event is valid */
