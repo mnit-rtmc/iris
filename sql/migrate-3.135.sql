885f3b63489a1d70164d8bb24af8eea17de2c07c
Douglas Lau <doug.lau@state.mn.us>
2011-10-14 15:23:12 -0500

    Change trigger to create an incident_update record on INSERT to incident table.

---
+++ sql/migrate-3.135.sql
@@ -0,0 +1,12 @@
+\set ON_ERROR_STOP
+
+SET SESSION AUTHORIZATION 'tms';
+
+UPDATE iris.system_attribute SET value = '3.135.0'
+	WHERE name = 'database_version';
+
+DROP TRIGGER IF EXISTS incident_update_trigger ON event.incident;
+
+CREATE TRIGGER incident_update_trigger
+	AFTER INSERT OR UPDATE ON event.incident
+	FOR EACH ROW EXECUTE PROCEDURE event.incident_update_trig();
--- sql/tms-template.sql
+++ sql/tms-template.sql
@@ -1366,7 +1366,7 @@ camera_id_blank
 camera_num_preset_btns	3
 camera_ptz_panel_enable	false
 camera_stream_duration_secs	60
-database_version	3.134.0
+database_version	3.135.0
 detector_auto_fail_enable	true
 dialup_poll_period_mins	120
 dms_aws_enable	false
@@ -1741,7 +1741,7 @@ BEGIN
 END;' LANGUAGE plpgsql;
 
 CREATE TRIGGER incident_update_trigger
-	AFTER UPDATE ON event.incident
+	AFTER INSERT OR UPDATE ON event.incident
 	FOR EACH ROW EXECUTE PROCEDURE event.incident_update_trig();
 
 SET search_path = public, event, pg_catalog;
