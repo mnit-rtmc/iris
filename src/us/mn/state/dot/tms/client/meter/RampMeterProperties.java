9affcfcb25663ceb740ced0faeaf58586ee99ae9
Douglas Lau <doug.lau@state.mn.us>
2011-11-03 10:14:59 -0500

    Removed old TimingPlan GUI stuff.

--- src/us/mn/state/dot/tms/client/meter/RampMeterProperties.java
+++ src/us/mn/state/dot/tms/client/meter/RampMeterProperties.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2010  Minnesota Department of Transportation
+ * Copyright (C) 2000-2011  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,10 +20,8 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
 import us.mn.state.dot.sched.ActionJob;
 import us.mn.state.dot.sched.FocusJob;
 import us.mn.state.dot.sonar.client.TypeCache;
@@ -36,7 +34,6 @@ import us.mn.state.dot.tms.RampMeterQueue;
 import us.mn.state.dot.tms.RampMeterType;
 import us.mn.state.dot.tms.client.Session;
 import us.mn.state.dot.tms.client.SonarState;
-import us.mn.state.dot.tms.client.schedule.TimingPlanModel;
 import us.mn.state.dot.tms.client.toast.ControllerForm;
 import us.mn.state.dot.tms.client.toast.FormPanel;
 import us.mn.state.dot.tms.client.toast.LocationPanel;
@@ -84,15 +81,6 @@ public class RampMeterProperties extends SonarObjectForm<RampMeter> {
 	/** Field for Maximum wait time (seconds) */
 	protected final JTextField wait = new JTextField();
 
-	/** Timing plan table component */
-	protected final JTable plan_table = new JTable();
-
-	/** Timing plan model */
-	protected final TimingPlanModel plan_model;
-
-	/** Button to delete a timing plan */
-	protected final JButton deleteBtn = new JButton("Delete");
-
 	/** Release rate component */
 	protected final JLabel release = new JLabel();
 
@@ -122,7 +110,6 @@ public class RampMeterProperties extends SonarObjectForm<RampMeter> {
 	public RampMeterProperties(Session s, RampMeter meter) {
 		super(TITLE, s, meter);
 		state = s.getSonarState();
-		plan_model = new TimingPlanModel(s, meter);
 	}
 
 	/** Get the SONAR type cache */
@@ -133,11 +120,9 @@ public class RampMeterProperties extends SonarObjectForm<RampMeter> {
 	/** Initialize the widgets on the form */
 	protected void initialize() {
 		super.initialize();
-		plan_model.initialize();
 		JTabbedPane tab = new JTabbedPane();
 		tab.add("Location", createLocationPanel());
 		tab.add("Setup", createSetupPanel());
-		tab.add("Timing Plans", createTimingPlanPanel());
 		tab.add("Status", createStatusPanel());
 		add(tab);
 		updateAttribute(null);
@@ -240,26 +225,6 @@ public class RampMeterProperties extends SonarObjectForm<RampMeter> {
 		panel.addRow("Meter Type", meterType);
 		panel.addRow("Storage (feet)", storage);
 		panel.addRow("Max Wait (seconds)", wait);
-		return panel;
-	}
-
-	/** Create timing plan panel */
-	protected JPanel createTimingPlanPanel() {
-		final ListSelectionModel s = plan_table.getSelectionModel();
-		s.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		new ActionJob(deleteBtn) {
-			public void perform() {
-				int row = s.getMinSelectionIndex();
-				if(row >= 0)
-					plan_model.deleteRow(row);
-			}
-		};
-		plan_table.setAutoCreateColumnsFromModel(false);
-		plan_table.setModel(plan_model);
-		plan_table.setColumnModel(plan_model.createColumnModel());
-		FormPanel panel = new FormPanel(true);
-		panel.addRow(plan_table);
-		panel.addRow(deleteBtn);
 		return panel;
 	}
 
