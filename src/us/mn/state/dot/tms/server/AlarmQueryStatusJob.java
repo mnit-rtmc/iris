bf4f814b759131608849db6e443265bb5b1cc8ef
Douglas Lau <doug.lau@state.mn.us>
2012-01-04 16:35:50 -0600

    Removed an unused import.

--- src/us/mn/state/dot/tms/server/AlarmQueryStatusJob.java
+++ src/us/mn/state/dot/tms/server/AlarmQueryStatusJob.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2009-2011  Minnesota Department of Transportation
+ * Copyright (C) 2009-2012  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,7 +16,6 @@ package us.mn.state.dot.tms.server;
 
 import java.util.Calendar;
 import us.mn.state.dot.sched.Job;
-import us.mn.state.dot.sched.Scheduler;
 import us.mn.state.dot.sonar.Checker;
 import us.mn.state.dot.tms.Controller;
 import us.mn.state.dot.tms.ControllerHelper;
