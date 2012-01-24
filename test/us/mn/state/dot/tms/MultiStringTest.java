9ccdd801311fb2d03d5fca6b8769e02788c40251
Douglas Lau <doug.lau@state.mn.us>
2012-01-24 09:41:06 -0600

    Added a unit test for the pb (page background) MULTI tag.

--- test/us/mn/state/dot/tms/MultiStringTest.java
+++ test/us/mn/state/dot/tms/MultiStringTest.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2009-2011  Minnesota Department of Transportation
+ * Copyright (C) 2009-2012  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -308,6 +308,8 @@ public class MultiStringTest extends TestCase {
 			equals("[tr1,1,40,20]"));
 		assertTrue(MultiString.normalize("[tr1,1,0,0]").
 			equals("[tr1,1,0,0]"));
+		assertTrue(MultiString.normalize("[pb0,128,255]").
+			equals("[pb0,128,255]"));
 		assertTrue(MultiString.normalize("[ttS100]").
 			equals("[ttS100]"));
 	}
