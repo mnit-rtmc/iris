8aa2a51b98bf3f7ce2d5bf57f5adad73913784a9
Douglas Lau <doug.lau@state.mn.us>
2012-04-04 14:41:14 -0500

    Added cleanup to MapTab.dispose()

--- src/us/mn/state/dot/tms/client/MapTab.java
+++ src/us/mn/state/dot/tms/client/MapTab.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2010  Minnesota Department of Transportation
+ * Copyright (C) 2000-2012  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -76,6 +76,6 @@ abstract public class MapTab extends JPanel {
 
 	/** Perform any clean up necessary */
 	public void dispose() {
-		// nothing to do
+		removeAll();
 	}
 }
