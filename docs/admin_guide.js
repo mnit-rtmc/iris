b2641415dae72f25c22c74cb7854d72158c403a4
Douglas Lau <doug.lau@state.mn.us>
2012-05-18 09:49:50 -0500

    Improved admin guide contents sidebar.

--- docs/admin_guide.js
+++ docs/admin_guide.js
@@ -9,26 +9,10 @@ function showOnly(sections, sec) {
 		for(var i = 0; i < sections.length; i++) {
 			if(sections[i] == sec) {
 				show(sections[i] + '_div');
-				hide(sections[i] + '_show');
-				show(sections[i] + '_hide');
 			} else {
 				hide(sections[i] + '_div');
-				show(sections[i] + '_show');
-				hide(sections[i] + '_hide');
 			}
 		}
-	}
-}
-function bindShowHide(sec) {
-	document.getElementById(sec + '_show').onclick = function() {
-		show(sec + '_div');
-		hide(sec + '_show');
-		show(sec + '_hide');
-	}
-	document.getElementById(sec + '_hide').onclick = function() {
-		hide(sec + '_div');
-		hide(sec + '_hide');
-		show(sec + '_show');
 	}
 }
 function bindClicks() {
@@ -36,9 +20,7 @@ function bindClicks() {
 		'features', 'troubleshooting', 'maintenance', 'development'];
 	for(var i = 0; i < sections.length; i++) {
 		hide(sections[i] + '_div');
-		hide(sections[i] + '_hide');
 		showOnly(sections, sections[i]);
-		bindShowHide(sections[i]);
 	}
 }
 
