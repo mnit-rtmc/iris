daad23f76c218b7e0727c330156483a83256b9b2
Douglas Lau <doug.lau@state.mn.us>
2012-07-02 09:01:46 -0500

    Allow controller drop addresses to be up to 32767.

--- src/us/mn/state/dot/tms/client/toast/DropNumberModel.java
+++ src/us/mn/state/dot/tms/client/toast/DropNumberModel.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2008  Minnesota Department of Transportation
+ * Copyright (C) 2008-2012  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,7 +29,7 @@ import us.mn.state.dot.tms.Controller;
 public class DropNumberModel extends SpinnerNumberModel {
 
 	/** Maximum allowed drop address */
-	static protected final short MAX_DROP_ID = 9999;
+	static protected final short MAX_DROP_ID = 32767;
 
 	/** Comm link */
 	protected final CommLink comm_link;
