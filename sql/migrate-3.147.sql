006a42f430ebd4afd77db3bb51fbef3efb5a300b
Douglas Lau <doug.lau@state.mn.us>
2012-07-17 14:20:50 -0500

    Delete 'temp_fahrenheit_enable' system attribute in migrate script.

--- sql/migrate-3.147.sql
+++ sql/migrate-3.147.sql
@@ -6,3 +6,4 @@ UPDATE iris.system_attribute SET value = '3.147.0'
 	WHERE name = 'database_version';
 
 INSERT INTO iris.system_attribute (name, value) VALUES ('client_units_si', 'true');
+DELETE FROM iris.system_attribute WHERE name = 'temp_fahrenheit_enable';
