dc81ffc0bbc064cd1f22daa85629eebc6b5eec71
Douglas Lau <doug.lau@state.mn.us>
2012-08-28 11:30:51 -0500

    Removed some dead code.

--- src/us/mn/state/dot/tms/utils/SFile.java
+++ src/us/mn/state/dot/tms/utils/SFile.java
@@ -15,112 +15,12 @@
 package us.mn.state.dot.tms.utils;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.SecurityException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
 
 /**
  * File convienence methods.
  * @author Michael Darter
  */
-public class SFile 
-{
-	/** Write a string to a file, terminated w/ \r\n. */
-	public static boolean writeLineToFile(String fname, String s,
-		boolean append)
-	{
-		return(SFile.writeStringToFile(fname, s + "\r\n", append));
-	}
-
-	/** Write a string to a file.
-	 * @param fname File name, may be null.
-	 * @param s String to write, may be null.
-	 * @param append False to overwrite existing file else true. */
-	public static boolean writeStringToFile(String fname, 
-		String s, boolean append)
-	{
-		s = (s == null ? "" : s);
-		FileWriter f = null;
-		boolean ok = true;
-		try {
-			f = new FileWriter(fname, append);
-			f.write(s);
-		} catch(Exception ex) {
-			Log.warning(
-				"Warning: writeStringToFile(): fname=" + 
-					fname + ", ex=" + ex);
-			ok = false;
-		} finally {
-			try {
-				f.close();
-			} catch(Exception ex) {}
-		}
-		return ok;
-	}
-
-	/** Read the specified URL and return a byte array or null on error.
-	 * @param surl URL which may be null */
-	public static byte[] readUrl(String surl) {
-		if(surl == null)
-			return null;
-		URL url = null;
-		try {
-			url = new URL(surl);
-		} catch(MalformedURLException ex) {
-			Log.warning("SFile.readUrl(), malformed URL: "+ex);
-			return null;
-		}
-		InputStream in = null;
-		byte[] ret = new byte[0];
-		try {
-			// open
-			URLConnection c = url.openConnection();
-			int pl = c.getContentLength();
-
-			// Log.finest("SFile.readUrl(), content len="+pl);
-			long fdate = c.getDate();
-			// Log.finest("SFile.readUrl(), date="+d);
-
-			// read until eof
-			in = c.getInputStream();
-			ArrayList<Byte> al = new ArrayList(pl);
-			while(true) {
-				int b = in.read(); // throws IOException on error
-				if(b < 0)
-					break; // eof
-				al.add(new Byte((byte)b));
-			}
-
-			// create byte[]
-			ret = new byte[al.size()];
-			for(int i = 0; i < ret.length; ++i)
-				ret[i] = (byte)(al.get(i));
-
-			// for(int i = 0; i < len; ++i ) 
-			//	System.err.print(ret[i]+" "+(char)ret[i]+","); 
-			//	Log.finest(" ");
-			//Log.finest("SFile.readUrl(), read "
-			//	+ al.size() + " bytes, file date=" + fdate + ".");
-		} catch(UnknownHostException e) {
-			//Log.finest(
-			//	"SFile.readUrl(): ignoring bogus url: "+e);
-		} catch(IOException e) {
-			Log.warning(
-				"SFile.readUrl(), caught exception: " + e);
-		} finally {
-			try {
-				if(in != null)
-					in.close();
-			} catch(IOException ex) {}
-		}
-		return ret;
-	}
+public class SFile {
 
 	/** Return an absolute file path.
 	 * @param fn File name. Can be null. If "", then "" is returned.
@@ -131,34 +31,5 @@ public class SFile
 			return "";
 		File fh = new File(fn);
 		return fh.getAbsolutePath();
-	}
-
-	/** Return true if specified file exists else false.
-	 * @param fn File name, may be null. */
-	public static boolean fileExists(String fn) {
-		if(fn == null)
-			return false;
-		boolean exists = true;
-		try {
-			exists = new File(fn).exists();
-		} catch(Exception ex) {
-			exists = false;
-		}
-		return exists;
-	}
-
-	/** Delete file */
-	public static boolean delete(String fn) {
-		if(!fileExists(fn))
-			return true;
-		boolean ok = false;
-		try {
-			File fd = new File(fn);
-			ok = fd.delete();
-		} catch(Exception ex) {
-			ok = false;
-			Log.fine("SFile.delete() ex=" + ex);
-		}
-		return ok;
 	}
 }
