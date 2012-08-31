625d71b12ce72dda7617405e73e403bd808cf1c3
Douglas Lau <doug.lau@state.mn.us>
2012-08-31 10:51:08 -0500

    Added updates for dms_action message priorities to migrate script for 3.149

--- sql/migrate-3.149.sql
+++ sql/migrate-3.149.sql
@@ -2,5 +2,10 @@
 
 SET SESSION AUTHORIZATION 'tms';
 
-UPDATE iris.system_attribute SET value = '3.149.0'
+UPDATE iris.system_attribute SET value = '3.149.1'
 	WHERE name = 'database_version';
+
+UPDATE iris.dms_action SET a_priority = a_priority + 1 WHERE a_priority > 1;
+UPDATE iris.dms_action SET r_priority = r_priority + 1 WHERE r_priority > 1;
+UPDATE iris.sign_message SET a_priority = a_priority + 1 WHERE a_priority > 1;
+UPDATE iris.sign_message SET r_priority = r_priority + 1 WHERE r_priority > 1;
