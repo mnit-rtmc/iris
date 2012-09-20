73a06924db18cb8769cbd074ea508117acdbc624
Douglas Lau <doug.lau@state.mn.us>
2012-09-20 11:45:31 -0500

    Fixed IrisUserImpl.getColumns to return "role" (was incorrectly spelled "rolw").

--- src/us/mn/state/dot/tms/server/IrisUserImpl.java
+++ src/us/mn/state/dot/tms/server/IrisUserImpl.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2007-2010  Minnesota Department of Transportation
+ * Copyright (C) 2007-2012  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -59,7 +59,7 @@ public class IrisUserImpl extends UserImpl implements Storable {
 		map.put("name", name);
 		map.put("full_name", fullName);
 		map.put("dn", dn);
-		map.put("rolw", role);
+		map.put("role", role);
 		map.put("enabled", enabled);
 		return map;
 	}
