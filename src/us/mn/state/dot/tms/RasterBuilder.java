b8abee84fef1317bcb873a5c98b7a84e742d25f4
Douglas Lau <doug.lau@state.mn.us>
2012-10-01 08:59:36 -0500

    Minor comment fix.

--- src/us/mn/state/dot/tms/RasterBuilder.java
+++ src/us/mn/state/dot/tms/RasterBuilder.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2008-2011  Minnesota Department of Transportation
+ * Copyright (C) 2008-2012  Minnesota Department of Transportation
  * Copyright (C) 2009-2010  AHMCT, University of California
  *
  * This program is free software; you can redistribute it and/or modify
