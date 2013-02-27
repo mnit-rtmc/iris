2cfc1974b926c5807ca2c03b3af3340e6634951e
Douglas Lau <doug.lau@state.mn.us>
2013-02-27 10:31:27 -0600

    Don't notify "operation" attribute when acquire/release fails.

--- src/us/mn/state/dot/tms/server/DeviceImpl.java
+++ src/us/mn/state/dot/tms/server/DeviceImpl.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2011  Minnesota Department of Transportation
+ * Copyright (C) 2000-2013  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -185,7 +185,8 @@ abstract public class DeviceImpl extends BaseObjectImpl implements Device,
 			}
 		}
 		finally {
-			notifyAttribute("operation");
+			if(owner == o)
+				notifyAttribute("operation");
 		}
 	}
 
@@ -201,7 +202,8 @@ abstract public class DeviceImpl extends BaseObjectImpl implements Device,
 			}
 		}
 		finally {
-			notifyAttribute("operation");
+			if(owner == null)
+				notifyAttribute("operation");
 		}
 	}
 
