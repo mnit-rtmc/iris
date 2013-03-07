a28770e221b12bfb9c17e6f98e3ba04ee1bfe9b2
Douglas Lau <doug.lau@state.mn.us>
2013-03-07 11:02:08 -0600

    Added Speed units class.

---
+++ src/us/mn/state/dot/tms/units/Speed.java
@@ -0,0 +1,203 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2013  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.units;
+
+import java.text.NumberFormat;
+import static us.mn.state.dot.tms.units.Distance.Units.FEET;
+import static us.mn.state.dot.tms.units.Distance.Units.MILES;
+import static us.mn.state.dot.tms.units.Distance.Units.KILOMETERS;
+import static us.mn.state.dot.tms.units.Interval.Units.SECONDS;
+import static us.mn.state.dot.tms.units.Interval.Units.HOURS;
+
+/**
+ * Speed of travel units.
+ *
+ * @author Douglas Lau
+ */
+public final class Speed {
+
+	/** Create a value based on a given distance / interval pair.
+	 * @param d Distance part of pair.
+	 * @param i Interval part of pair.
+	 * @return Value to use for speed. */
+	static private double createValue(Distance d, Interval i) {
+		if(Units.lookup(d.units, i.units) != null)
+			return d.value / i.value;
+		else {
+			d = d.convert(KILOMETERS);
+			i = i.convert(HOURS);
+			return d.value / i.value;
+		}
+	}
+
+	/** Create units based on a given distance / interval pair.
+	 * @param d Distance part of pair.
+	 * @param i Interval part of pair.
+	 * @return Units to use for speed. */
+	static private Units createUnits(Distance d, Interval i) {
+		Units u = Units.lookup(d.units, i.units);
+		return u != null ? u : Units.KPH;
+	}
+
+	/** Enumeration of speed units */
+	public enum Units {
+		FPS(1.097280, "fps", FEET, SECONDS),
+		MPH(1.609344, "mph", MILES, HOURS),
+		KPH(1.000000, "kph", KILOMETERS, HOURS);
+
+		/** Conversion rate to kilometers per hour*/
+		public final double kph;
+
+		/** Unit label */
+		public final String label;
+
+		/** Distance units */
+		public final Distance.Units d_units;
+
+		/** Interval units */
+		public final Interval.Units i_units;
+
+		/** Create units */
+		private Units(double k, String l, Distance.Units d,
+			Interval.Units i)
+		{
+			kph = k;
+			label = l;
+			d_units = d;
+			i_units = i;
+		}
+
+		/** Lookup declared units.
+		 * @param d Distance units.
+		 * @param i Interval units.
+		 * @return Speed units. */
+		static private Units lookup(Distance.Units d, Interval.Units i){
+			for(Units u: Units.values()) {
+				if(u.d_units == d && u.i_units == i)
+					return u;
+			}
+			return null;
+		}
+	}
+
+	/** Speed value */
+	public final double value;
+
+	/** Speed units */
+	public final Units units;
+
+	/** Create a new speed.
+	 * @param v Value of speed.
+	 * @param u Units of speed. */
+	public Speed(double v, Units u) {
+		value = v;
+		units = u;
+	}
+
+	/** Create a new speed.
+	 * @param v Value in kph. */
+	public Speed(double v) {
+		this(v, Units.KPH);
+	}
+
+	/** Create a new speed.
+	 * @param d Distance for speed.
+	 * @param i Interval for speed. */
+	public Speed(Distance d, Interval i) {
+		this(createValue(d, i), createUnits(d, i));
+	}
+
+	/** Get the speed in kph */
+	public double kph() {
+		return value * units.kph;
+	}
+
+	/** Convert a speed to specified units.
+	 * @param u Units to convert to.
+	 * @return Speed in specified units. */
+	public Speed convert(Units u) {
+		if(u == units)
+			return this;
+		else {
+			double v = kph();
+			return new Speed(v / u.kph, u);
+		}
+	}
+
+	/** Get a speed as a float in specified units.
+	 * @param u Units to return.
+	 * @return Speed as a float value. */
+	public float asFloat(Units u) {
+		if(u == units)
+			return (float)value;
+		else
+			return (float)(kph() / u.kph);
+	}
+
+	/** Round a speed to nearest whole unit.
+	 * @param u Units to return.
+	 * @return Speed rounded to nearest whole unit. */
+	public int round(Units u) {
+		if(u == units)
+			return (int)Math.round(value);
+		else
+			return (int)Math.round(kph() / u.kph);
+	}
+
+	/** Add another speed.
+	 * @param d Other speed.
+	 * @return Sum of speeds. */
+	public Speed add(Speed d) {
+		if(d.units == units)
+			return new Speed(value + d.value, units);
+		else
+			return new Speed(kph() + d.kph());
+	}
+
+	/** Compare for equality */
+	@Override public boolean equals(Object other) {
+		if(other instanceof Speed) {
+			Speed o = (Speed)other;
+			if(units == o.units)
+				return value == o.value;
+			else
+				return kph() == o.kph();
+		} else
+			return false;
+	}
+
+	/** Get a speed hash code */
+	@Override public int hashCode() {
+		return new Double(kph()).hashCode();
+	}
+
+	/** Get a string representation of a speed */
+	@Override public String toString() {
+		return value + " " + units.label;
+	}
+
+	/** Speed formatter */
+	static public class Formatter {
+		private final NumberFormat format;
+		public Formatter(int d) {
+			format = NumberFormat.getInstance();
+			format.setMaximumFractionDigits(d);
+			format.setMinimumFractionDigits(d);
+		}
+		public String format(Speed d) {
+			return format.format(d.value) + " " + d.units.label;
+		}
+	}
+}
---
+++ test/us/mn/state/dot/tms/units/SpeedTest.java
@@ -0,0 +1,71 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2013  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.units;
+
+import junit.framework.TestCase;
+
+/**
+ * Speed tests
+ *
+ * @author Douglas Lau
+ */
+public class SpeedTest extends TestCase {
+
+	public SpeedTest(String name) {
+		super(name);
+	}
+
+	public void test() {
+		Speed.Formatter df = new Speed.Formatter(2);
+		assertTrue(df.format(new Speed(1)).equals("1.00 kph"));
+		assertTrue(df.format(new Speed(5)).equals("5.00 kph"));
+		assertTrue(df.format(new Speed(1).convert(
+			Speed.Units.MPH)).equals("0.62 mph"));
+		assertTrue(df.format(new Speed(100, Speed.Units.KPH).convert(
+			Speed.Units.MPH)).equals("62.14 mph"));
+		assertTrue(df.format(new Speed(60, Speed.Units.MPH).convert(
+			Speed.Units.KPH)).equals("96.56 kph"));
+		assertTrue(df.format(new Speed(60, Speed.Units.MPH).convert(
+			Speed.Units.FPS)).equals("88.00 fps"));
+		assertTrue(df.format(new Speed(88, Speed.Units.FPS).convert(
+			Speed.Units.MPH)).equals("60.00 mph"));
+	}
+
+	public void test2() {
+		Distance d;
+		Interval i;
+		Speed.Formatter df = new Speed.Formatter(2);
+		d = new Distance(60, Distance.Units.MILES);
+		i = new Interval(1, Interval.Units.HOURS);
+		assertTrue(df.format(new Speed(d, i).convert(
+			Speed.Units.MPH)).equals("60.00 mph"));
+		d = new Distance(60, Distance.Units.MILES);
+		i = new Interval(2, Interval.Units.HOURS);
+		assertTrue(df.format(new Speed(d, i).convert(
+			Speed.Units.MPH)).equals("30.00 mph"));
+		d = new Distance(88, Distance.Units.FEET);
+		i = new Interval(1, Interval.Units.SECONDS);
+		assertTrue(df.format(new Speed(d, i).convert(
+			Speed.Units.FPS)).equals("88.00 fps"));
+		d = new Distance(316800, Distance.Units.FEET);
+		i = new Interval(1, Interval.Units.HOURS);
+		assertTrue(df.format(new Speed(d, i).convert(
+			Speed.Units.MPH)).equals("60.00 mph"));
+		d = new Distance(0.01, Distance.Units.KILOMETERS);
+		i = new Interval(1, Interval.Units.SECONDS);
+		assertTrue(df.format(new Speed(d, i).convert(
+			Speed.Units.KPH)).equals("36.00 kph"));
+	}
+}
