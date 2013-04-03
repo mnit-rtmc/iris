43d52aa5bb9961c6d234fc1390fac9645e4d14bf
Douglas Lau <doug.lau@state.mn.us>
2013-04-03 09:06:13 -0500

    Cleaned up DMSPanelPager.

--- src/us/mn/state/dot/tms/client/dms/DMSPanelPager.java
+++ src/us/mn/state/dot/tms/client/dms/DMSPanelPager.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2012  Minnesota Department of Transportation
+ * Copyright (C) 2000-2013  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -33,41 +33,41 @@ import us.mn.state.dot.tms.RasterGraphic;
 public class DMSPanelPager {
 
 	/** Time counter for amount of time message has been displayed */
-	static protected final int TIMER_TICK_MS = 100;
+	static private final int TIMER_TICK_MS = 100;
 
 	/** Sign pixel panel being controlled */
-	protected final SignPixelPanel panel;
+	private final SignPixelPanel panel;
 
 	/** Selected DMS */
-	protected final DMS dms;
+	private final DMS dms;
 
 	/** Rasters for each page */
-	protected final RasterGraphic[] rasters;
+	private final RasterGraphic[] rasters;
 
 	/** Current page being displayed */
-	protected int page = 0;
+	private int page = 0;
 
 	/** Multipage message on time */
-	protected final DmsPgTime pgOnTime;
+	private final DmsPgTime pgOnTime;
 
 	/** Multipage message off time */
-	protected final DmsPgTime pgOffTime;
+	private final DmsPgTime pgOffTime;
 
 	/** Swing timer */
-	protected final Timer timer;
+	private final Timer timer;
 
 	/** Time counter for amount of time message has been displayed */
-	protected int phaseTimeMS = 0;
+	private int phase_ms = 0;
 
 	/** Blanking state -- true during blank time between pages */
-	protected boolean isBlanking = false;
+	private boolean isBlanking = false;
 
 	/** Create a new DMS panel pager.
-	 *  @param p SignPixelPanel.
-	 *  @param proxy DMS proxy.
-	 *  @param rg Array of raster graphics.
-	 *  @param ot Page on-time, which is validated, so if zero, is 
-	 *	   assigned the system default. */
+	 * @param p SignPixelPanel.
+	 * @param proxy DMS proxy.
+	 * @param rg Array of raster graphics.
+	 * @param ot Page on-time, which is validated, so if zero, is
+	 *           assigned the system default. */
 	public DMSPanelPager(SignPixelPanel p, DMS proxy, RasterGraphic[] rg,
 		DmsPgTime ot)
 	{
@@ -79,10 +79,10 @@ public class DMSPanelPager {
 		pgOffTime = DmsPgTime.getDefaultOff();
 		setDimensions();
 		panel.setGraphic(rasters[page]);
-		timer = new Timer(TIMER_TICK_MS, new ActionListener() { 
+		timer = new Timer(TIMER_TICK_MS, new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				pageTimerTick();
-			} 
+			}
 		});
 		if(isMultipage())
 			timer.start();
@@ -94,7 +94,7 @@ public class DMSPanelPager {
 	}
 
 	/** Get rasters to display on the panel */
-	protected RasterGraphic[] getRasters(RasterGraphic[] rg) {
+	private RasterGraphic[] getRasters(RasterGraphic[] rg) {
 		if(rg != null && rg.length > 0)
 			return rg;
 		else {
@@ -105,14 +105,14 @@ public class DMSPanelPager {
 	}
 
 	/** Set the dimensions of the pixel panel */
-	protected void setDimensions() {
+	private void setDimensions() {
 		setPhysicalDimensions();
 		setLogicalDimensions();
 		panel.repaint();
 	}
 
 	/** Set the physical dimensions of the pixel panel */
-	protected void setPhysicalDimensions() {
+	private void setPhysicalDimensions() {
 		Integer w = dms.getFaceWidth();
 		Integer h = dms.getFaceHeight();
 		Integer hp = dms.getHorizontalPitch();
@@ -127,7 +127,7 @@ public class DMSPanelPager {
 	}
 
 	/** Set the logical dimensions of the pixel panel */
-	protected void setLogicalDimensions() {
+	private void setLogicalDimensions() {
 		Integer wp = dms.getWidthPixels();
 		Integer hp = dms.getHeightPixels();
 		Integer cw = dms.getCharWidthPixels();
@@ -137,7 +137,7 @@ public class DMSPanelPager {
 	}
 
 	/** Create a blank raster graphic */
-	protected RasterGraphic createBlankPage() {
+	private RasterGraphic createBlankPage() {
 		Integer wp = dms.getWidthPixels();
 		Integer hp = dms.getHeightPixels();
 		if(wp != null && hp != null)
@@ -146,11 +146,9 @@ public class DMSPanelPager {
 			return new BitmapGraphic(0, 0);
 	}
 
-	/** 
-	 * Page timer tick. Called periodically to change the sign contents
-	 * for multipage signs.
-	 */
-	protected void pageTimerTick() {
+	/** Page timer tick. Called periodically to change the sign contents
+	 * for multipage signs. */
+	private void pageTimerTick() {
 		if(doTick()) {
 			if(isBlanking)
 				makeBlank();
@@ -161,19 +159,19 @@ public class DMSPanelPager {
 
 	/** Update the timer for one tick.
 	 * @return True if panel needs repainting */
-	protected boolean doTick() {
-		phaseTimeMS += TIMER_TICK_MS;
+	private boolean doTick() {
+		phase_ms += TIMER_TICK_MS;
 		if(isBlanking) {
-			if(phaseTimeMS >= pgOffTime.toMs()) {
+			if(phase_ms >= pgOffTime.toMs()) {
 				isBlanking = false;
-				phaseTimeMS = 0;
+				phase_ms = 0;
 				return true;
 			}
 		} else {
-			if(phaseTimeMS >= pgOnTime.toMs()) {
+			if(phase_ms >= pgOnTime.toMs()) {
 				if(pgOffTime.toMs() > 0)
 					isBlanking = true;
-				phaseTimeMS = 0;
+				phase_ms = 0;
 				return true;
 			}
 		}
@@ -181,13 +179,13 @@ public class DMSPanelPager {
 	}
 
 	/** Make the display blank (without advancing the page number) */
-	protected void makeBlank() {
+	private void makeBlank() {
 		if(isMultipage())
 			panel.setGraphic(createBlankPage());
 	}
 
 	/** Display the next page of the message */
-	protected void nextPage() {
+	private void nextPage() {
 		if(isMultipage()) {
 			page++;
 			if(page >= rasters.length)
@@ -197,7 +195,7 @@ public class DMSPanelPager {
 	}
 
 	/** Check if the current message has multiple pages */
-	protected boolean isMultipage() {
+	private boolean isMultipage() {
 		return rasters.length > 1;
 	}
 }
