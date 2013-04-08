30d5ede0ebfc4cd30783cce7de4eed004557d958
Douglas Lau <doug.lau@state.mn.us>
2013-04-08 11:09:49 -0500

    Fixed a NullPointerException when a fake detector refers to an non-existant detector.

--- src/us/mn/state/dot/tms/server/FakeDetector.java
+++ src/us/mn/state/dot/tms/server/FakeDetector.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2002-2012  Minnesota Department of Transportation
+ * Copyright (C) 2002-2013  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -120,10 +120,12 @@ public class FakeDetector {
 			}
 			DetectorImpl det = (DetectorImpl)ns.lookupObject(
 				Detector.SONAR_TYPE, t);
-			if(type == PLUS)
-				p.add(det);
-			if(type == MINUS)
-				m.add(det);
+			if(det != null) {
+				if(type == PLUS)
+					p.add(det);
+				if(type == MINUS)
+					m.add(det);
+			}
 		}
 		plus = (DetectorImpl [])p.toArray(new DetectorImpl[0]);
 		minus = (DetectorImpl [])m.toArray(new DetectorImpl[0]);
