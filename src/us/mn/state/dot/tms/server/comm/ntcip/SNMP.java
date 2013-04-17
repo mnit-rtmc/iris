0dd4ee620fb51377208aedaab22e40075c2ae4de
Douglas Lau <doug.lau@state.mn.us>
2013-04-17 14:59:36 -0500

    ntcip: Added a FIXME

--- src/us/mn/state/dot/tms/server/comm/ntcip/SNMP.java
+++ src/us/mn/state/dot/tms/server/comm/ntcip/SNMP.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2012  Minnesota Department of Transportation
+ * Copyright (C) 2000-2013  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -301,6 +301,7 @@ public class SNMP extends BER {
 			throws IOException
 		{
 			decodeSequence(is);
+			// FIXME: compare with OID from mo
 			decodeObjectIdentifier(is);
 			decodeValue(is, mo);
 		}
