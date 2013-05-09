a9e695552097cf19e7099cbe4931aec654f1c9a7
Douglas Lau <doug.lau@state.mn.us>
2013-05-09 12:00:49 -0500

    Widened characters in 20_full font.

--- sql/fonts/20_full.sql
+++ sql/fonts/20_full.sql
@@ -15,23 +15,23 @@ INSERT INTO iris.graphic (name, bpp, height, width, pixels)
 INSERT INTO iris.glyph (name, font, code_point, graphic)
     VALUES ('20_full_49', '20_full', 49, '20_full_49');
 INSERT INTO iris.graphic (name, bpp, height, width, pixels)
-    VALUES ('20_full_50', 1, 20, 9, 'Pj+4+DAYDAYDA4ODg4ODg4GAwGA///A=');
+    VALUES ('20_full_50', 1, 20, 10, 'Px/uHwMAwDAMBwOBwOBwOBwOAwDAMA///w==');
 INSERT INTO iris.glyph (name, font, code_point, graphic)
     VALUES ('20_full_50', '20_full', 50, '20_full_50');
 INSERT INTO iris.graphic (name, bpp, height, width, pixels)
-    VALUES ('20_full_51', 1, 20, 9, 'Pj+4+DAYDAYDA4eDwHAYDAYDwfHfx8A=');
+    VALUES ('20_full_51', 1, 20, 10, 'Px/uHwMAwDAMAwHB4HgHAMAwDAPA+Hf4/A==');
 INSERT INTO iris.glyph (name, font, code_point, graphic)
     VALUES ('20_full_51', '20_full', 51, '20_full_51');
 INSERT INTO iris.graphic (name, bpp, height, width, pixels)
-    VALUES ('20_full_52', 1, 20, 9, 'BgcHh8dnMxmMxmM///BgMBgMBgMBgMA=');
+    VALUES ('20_full_52', 1, 20, 10, 'AwHA8Hw7HM4zDMMwz///AwDAMAwDAMAwDA==');
 INSERT INTO iris.glyph (name, font, code_point, graphic)
     VALUES ('20_full_52', '20_full', 52, '20_full_52');
 INSERT INTO iris.graphic (name, bpp, height, width, pixels)
-    VALUES ('20_full_53', 1, 20, 9, '///wGAwGAwGA/j+A4DAYDAYDAeH/z8A=');
+    VALUES ('20_full_53', 1, 20, 10, '///8AwDAMAwDAP8f4BwDAMAwDAMA8H/5/A==');
 INSERT INTO iris.glyph (name, font, code_point, graphic)
     VALUES ('20_full_53', '20_full', 53, '20_full_53');
 INSERT INTO iris.graphic (name, bpp, height, width, pixels)
-    VALUES ('20_full_54', 1, 20, 9, 'Pz/4eAwGAwGA/n+w+DweDweDwfHfx8A=');
+    VALUES ('20_full_54', 1, 20, 10, 'P5/+DwDAMAwDAP8/7B8DwPA8DwPA+Hf4/A==');
 INSERT INTO iris.glyph (name, font, code_point, graphic)
     VALUES ('20_full_54', '20_full', 54, '20_full_54');
 INSERT INTO iris.graphic (name, bpp, height, width, pixels)
@@ -39,10 +39,10 @@ INSERT INTO iris.graphic (name, bpp, height, width, pixels)
 INSERT INTO iris.glyph (name, font, code_point, graphic)
     VALUES ('20_full_55', '20_full', 55, '20_full_55');
 INSERT INTO iris.graphic (name, bpp, height, width, pixels)
-    VALUES ('20_full_56', 1, 20, 9, 'Pj+4+DweDwfHfx8f3HweDweDwfHfx8A=');
+    VALUES ('20_full_56', 1, 20, 10, 'Px/uHwPA8DwPh3+Px/uHwPA8DwPA+Hf4/A==');
 INSERT INTO iris.glyph (name, font, code_point, graphic)
     VALUES ('20_full_56', '20_full', 56, '20_full_56');
 INSERT INTO iris.graphic (name, bpp, height, width, pixels)
-    VALUES ('20_full_57', 1, 20, 9, 'Pj+4+DweDweD4b/P4DAYDAYDAeH/z8A=');
+    VALUES ('20_full_57', 1, 20, 10, 'Px/uHwPA8DwPA+Df8/wDAMAwDAMA8H/5/A==');
 INSERT INTO iris.glyph (name, font, code_point, graphic)
     VALUES ('20_full_57', '20_full', 57, '20_full_57');
