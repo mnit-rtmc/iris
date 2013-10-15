e64c25621e3072bee84c7c1b0a51efb07880faf9
Douglas Lau <doug.lau@state.mn.us>
2013-10-15 15:33:33 -0500

    Fixed problem with PeriodicSampleWriterTest.

--- test/us/mn/state/dot/tms/server/PeriodicSampleWriterTest.java
+++ test/us/mn/state/dot/tms/server/PeriodicSampleWriterTest.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2012  Minnesota Department of Transportation
+ * Copyright (C) 2012-2013  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -99,6 +99,9 @@ public class PeriodicSampleWriterTest extends TestCase {
 		{
 			return new File("/tmp", sensor_id + "." +
 				s_type.extension + ps.period);
+		}
+		public boolean hasKnownExtension(String name) {
+			return true;
 		}
 	}
 }
