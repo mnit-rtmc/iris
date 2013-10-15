891a9e5d30ac7d3434996fe91aeb19a256cca517
Douglas Lau <doug.lau@state.mn.us>
2013-10-15 15:34:34 -0500

    Added simple CIDR address util class.

---
+++ src/us/mn/state/dot/tms/utils/CIDRAddress.java
@@ -0,0 +1,81 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2013  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.utils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * CIDR (Classless Inter-Domain Routing) Address.
+ *
+ * @author Douglas Lau
+ */
+public class CIDRAddress {
+
+	/** Inet address */
+	private final byte[] address;
+
+	/** Prefix bits */
+	private final int prefix;
+
+	/** Number of full bytes to match in addresses */
+	private final int n_bytes;
+
+	/** Mask of bits in final byte */
+	private final int mask;
+
+	/** Create a new CIDR address */
+	public CIDRAddress(String a) throws UnknownHostException,
+		NumberFormatException
+	{
+		String[] p = a.split("/");
+		if(p.length > 2)
+			throw new UnknownHostException("Invalid CIDR");
+		address = InetAddress.getByName(p[0]).getAddress();
+		prefix = p.length > 1 ? Integer.parseInt(p[1]) : bits();
+		mask = makeMask();
+		n_bytes = prefix / 8;
+	}
+
+	/** Get the number of bits in the address */
+	private int bits() {
+		return address.length * 8;
+	}
+
+	/** Make bit mask for final byte */
+	private int makeMask() {
+		int m = 0;
+		for(int b = 0; b < prefix % 8; b++) {
+			m >>= 1;
+			m |= 0x80;
+		}
+		return m;
+	}
+
+	/** Test if an inet address matches */
+	public boolean matches(InetAddress a) {
+		byte[] ad = a.getAddress();
+		if(ad.length != address.length)
+			return false;
+		// Test full bytes in address
+		for(int b = 0; b < n_bytes; b++) {
+			if(ad[b] != address[b])
+				return false;
+		}
+		// Test trailing bits in address
+		return n_bytes >= address.length ||
+		       ((ad[n_bytes] & mask) == (address[n_bytes] & mask));
+	}
+}
---
+++ test/us/mn/state/dot/tms/utils/CIDRAddressTest.java
@@ -0,0 +1,63 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2013  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.utils;
+
+import java.net.InetAddress;
+import junit.framework.TestCase;
+
+/** CIDRAddress tests
+ *
+ * @author Douglas Lau
+ */
+public class CIDRAddressTest extends TestCase {
+
+	static private InetAddress addr(String a) throws Exception {
+		return InetAddress.getByName(a);
+	}
+
+	public CIDRAddressTest(String name) {
+		super(name);
+	}
+
+	public void test1() {
+		try {
+			CIDRAddress a = new CIDRAddress("192.168.1.0/24");
+			assertTrue(a.matches(addr("192.168.1.1")));
+			assertTrue(a.matches(addr("192.168.1.254")));
+			assertFalse(a.matches(addr("192.168.2.1")));
+			assertFalse(a.matches(addr("10.1.1.1")));
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
+	}
+
+	public void test2() {
+		try {
+			CIDRAddress a = new CIDRAddress("192.168.0.0/20");
+			assertTrue(a.matches(addr("192.168.1.1")));
+			assertTrue(a.matches(addr("192.168.1.254")));
+			assertTrue(a.matches(addr("192.168.2.1")));
+			assertTrue(a.matches(addr("192.168.15.254")));
+			assertFalse(a.matches(addr("192.168.16.1")));
+			assertFalse(a.matches(addr("10.1.1.1")));
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
+	}
+}
