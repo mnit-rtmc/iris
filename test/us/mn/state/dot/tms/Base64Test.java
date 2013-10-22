5b11fa0e126cdd59327f6ee6224615edd7fadb9c
Douglas Lau <doug.lau@state.mn.us>
2013-10-22 09:32:55 -0500

    Added another unit test to Base64

--- test/us/mn/state/dot/tms/Base64Test.java
+++ test/us/mn/state/dot/tms/Base64Test.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2009  Minnesota Department of Transportation
+ * Copyright (C) 2009-2013  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,27 +12,33 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-
 package us.mn.state.dot.tms;
 
 import java.io.IOException;
 import junit.framework.TestCase;
-import us.mn.state.dot.tms.Base64;
 
 /** 
  * Base64 test cases
  * @author Doug Lau
  * @author Michael Darter
- * @created 05/05/09
  */
 public class Base64Test extends TestCase {
 
-	/** constructor */
 	public Base64Test(String name) {
 		super(name);
 	}
 
-	/** test cases */
+	public void testEmpty() {
+		try {
+			byte[] m = Base64.decode("");
+			assertTrue(m.length == 0);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
+	}
+
 	public void test() {
 		// encode, decode
 		for(int l = 1; l < 80; l++) {
@@ -46,8 +52,9 @@ public class Base64Test extends TestCase {
 					byte[] b = Base64.decode(v);
 					assertTrue("l=" + l + ", i=" + i, 
 						java.util.Arrays.equals(b, m));
-				} catch(IOException ex) {
-					ex.printStackTrace();
+				}
+				catch(IOException e) {
+					e.printStackTrace();
 					assertTrue(false);
 				}
 			}
