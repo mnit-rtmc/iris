74cc2e85d1914c2d3f9165813749ba46f7a06709
Douglas Lau <doug.lau@state.mn.us>
2013-10-23 10:44:41 -0500

    Fixed DMSHelper.isActive.

--- src/us/mn/state/dot/tms/DMSHelper.java
+++ src/us/mn/state/dot/tms/DMSHelper.java
@@ -44,7 +44,7 @@ public class DMSHelper extends BaseHelper {
 
 	/** Test if a DMS is active */
 	static public boolean isActive(DMS proxy) {
-		return ItemStyle.FAILED.checkBit(proxy.getStyles());
+		return ItemStyle.ACTIVE.checkBit(proxy.getStyles());
 	}
 
 	/** Get the maintenance status of a DMS */
