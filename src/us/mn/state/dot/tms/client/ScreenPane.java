421ef94bdd5124c4f030d99909f6793f3e1a229c
Douglas Lau <doug.lau@state.mn.us>
2013-11-08 09:06:59 -0600

    Decomposed SidePanel out of ScreenPane.

--- src/us/mn/state/dot/tms/client/ScreenPane.java
+++ src/us/mn/state/dot/tms/client/ScreenPane.java
@@ -17,36 +17,22 @@ package us.mn.state.dot.tms.client;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.Point2D;
-import java.util.LinkedList;
 import java.util.TreeMap;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
 import javax.swing.border.BevelBorder;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import us.mn.state.dot.geokit.Position;
 import us.mn.state.dot.geokit.SphericalMercatorPosition;
 import us.mn.state.dot.geokit.ZoomLevel;
-import us.mn.state.dot.map.LayerState;
 import us.mn.state.dot.map.MapBean;
 import us.mn.state.dot.map.MapToolBar;
-import us.mn.state.dot.sonar.SonarObject;
 import us.mn.state.dot.sonar.client.TypeCache;
 import us.mn.state.dot.tms.MapExtent;
-import us.mn.state.dot.tms.SystemAttrEnum;
-import us.mn.state.dot.tms.client.proxy.ProxyLayerState;
-import us.mn.state.dot.tms.client.proxy.ProxySelectionListener;
-import us.mn.state.dot.tms.client.proxy.ProxySelectionModel;
-import us.mn.state.dot.tms.client.roads.SegmentLayerState;
 import us.mn.state.dot.tms.client.toolbar.IrisToolBar;
-import static us.mn.state.dot.tms.client.widget.Widgets.UI;
 
 /**
  * A screen pane is a pane which contains all components for one screen on
@@ -67,164 +53,68 @@ public class ScreenPane extends JPanel {
 		return new Point2D.Double(c.getX(), c.getY());
 	}
 
-	/** Side panel for tabs and menu */
-	private final JPanel side_panel;
-
-	/** Tabbed side pane */
-	protected final JTabbedPane tab_pane;
-
 	/** Map to be displayed on the screen pane */
-	protected final MapBean map;
+	private final MapBean map;
 
 	/** Get the map */
 	public MapBean getMap() {
 		return map;
 	}
 
+	/** Side panel for tabs and menu */
+	private final SidePanel side_panel;
+
 	/** Map tool bar */
-	protected final MapToolBar map_bar;
+	private final MapToolBar map_bar;
 
 	/** Map panel */
-	protected final JPanel map_panel;
+	private final JPanel map_panel;
 
 	/** IRIS tool bar */
-	protected final IrisToolBar tool_bar;
-
-	/** List of tab switchers */
-	protected final LinkedList<TabSwitcher> switchers =
-		new LinkedList<TabSwitcher>();
+	private final IrisToolBar tool_bar;
 
 	/** Create a new screen pane */
 	public ScreenPane() {
 		setLayout(new BorderLayout());
-		side_panel = new JPanel(new BorderLayout());
-		side_panel.setMinimumSize(UI.dimension(500, 200));
-		side_panel.setPreferredSize(UI.dimension(500, 200));
-		tab_pane = new JTabbedPane(JTabbedPane.TOP);
-		side_panel.add(tab_pane, BorderLayout.CENTER);
-		add(side_panel, BorderLayout.WEST);
 		map = new MapBean(true);
 		map.setBackground(new Color(208, 216, 208));
+		side_panel = new SidePanel(map);
+		add(side_panel, BorderLayout.WEST);
 		map_bar = new MapToolBar(map);
 		map_bar.setFloatable(false);
 		tool_bar = new IrisToolBar(map);
 		tool_bar.setFloatable(false);
 		map_panel = createMapPanel();
 		add(map_panel, BorderLayout.CENTER);
-		tab_pane.addChangeListener(new ChangeListener() {
-			public void stateChanged(ChangeEvent e) {
-				map.setPointSelector(null);
-				setSelectedLayer(getSelectedHomeLayer());
-				storeSelectedTabIndex();
-			}
-		});
-	}
-
-	/** Get the currently selected tab's home layer */
-	private ProxyLayerState getSelectedHomeLayer() {
-		Component tab = tab_pane.getSelectedComponent();
-		if(tab instanceof MapTab)
-			return getHomeProxyLayerState((MapTab)tab);
-		else
-			return null;
-	}
-
-	/** Get the home proxy layer state for a map tab */
-	private ProxyLayerState getHomeProxyLayerState(MapTab mt) {
-		LayerState ls = mt.getHomeLayer();
-		if(ls instanceof ProxyLayerState)
-			return (ProxyLayerState)ls;
-		else
-			return null;
-	}
-
-	/** Most recently selected layer */
-	protected ProxyLayerState sel_layer;
-
-	/** Set the selected layer for the screen pane */
-	private void setSelectedLayer(ProxyLayerState sel) {
-		if(sel_layer != null && sel != sel_layer)
-			sel_layer.setTabSelected(false);
-		if(sel != null)
-			sel.setTabSelected(true);
-		sel_layer = sel;
-	}
-
-	/** Last selected tab, which stores the index of last user selected
-	 * tab. A field is used to track this, rather than dynamically
-	 * calling tab_pane.getSelectedIndex() because if the method is 
-	 * called during app shutdown, it can erroneously return a -1. */
-	private int sel_tab;
-
-	/** Store the currently selected tab index. If no tab is 
-	 *  selected (e.g. during tear down), the change is ignored. */
-	private void storeSelectedTabIndex() {
-		int sel = tab_pane.getSelectedIndex();
-		if(sel >= 0)
-			sel_tab = sel;
 	}
 
 	/** Get the index of the currently selected tab */
 	public int getSelectedTabIndex() {
-		return sel_tab;
+		return side_panel.getSelectedTabIndex();
 	}
 
 	/** Set the currently selected tab */
 	public void setSelectedTabIndex(int i) {
-		if(i >= 0 && i < tab_pane.getTabCount())
-			tab_pane.setSelectedIndex(i);
+		side_panel.setSelectedTabIndex(i);
 	}
 
 	/** Add a tab to the screen pane */
 	public void addTab(MapTab mt) {
-		tab_pane.addTab(mt.getName(), null, mt, mt.getTip());
-		mt.setMap(map);
-		ProxyLayerState pls = getHomeProxyLayerState(mt);
-		if(pls != null) {
-			if(tab_pane.getTabCount() == 1)
-				setSelectedLayer(pls);
-			TabSwitcher ts = new TabSwitcher(mt,
-				pls.getSelectionModel());
-			switchers.add(ts);
-		}
-	}
-
-	/** Class to listen for proxy selection events and select tabs */
-	protected class TabSwitcher implements ProxySelectionListener {
-		protected final MapTab tab;
-		protected final ProxySelectionModel model;
-		protected TabSwitcher(MapTab mt, ProxySelectionModel psm) {
-			tab = mt;
-			model = psm;
-			model.addProxySelectionListener(this);
-		}
-		protected void dispose() {
-			model.removeProxySelectionListener(this);
-		}
-		public void selectionAdded(SonarObject proxy) {
-			tab_pane.setSelectedComponent(tab);
-		}
-		public void selectionRemoved(SonarObject proxy) { }
+		side_panel.addTab(mt);
 	}
 
 	/** Remove all the tabs */
 	public void removeTabs() {
-		for(TabSwitcher ts: switchers)
-			ts.dispose();
-		switchers.clear();
-		tab_pane.removeAll();
+		side_panel.removeTabs();
 	}
 
 	/** Set the menu bar */
 	public void setMenuBar(IMenuBar bar) {
-		side_panel.removeAll();
-		if(bar != null)
-			side_panel.add(bar, BorderLayout.NORTH);
-		side_panel.add(tab_pane, BorderLayout.CENTER);
+		side_panel.setMenuBar(bar);
 	}
 
 	/** Create the map panel */
-	protected JPanel createMapPanel() {
+	private JPanel createMapPanel() {
 		JPanel p = new JPanel(new BorderLayout());
 		p.setBorder(BorderFactory.createBevelBorder(
 			BevelBorder.LOWERED));
---
+++ src/us/mn/state/dot/tms/client/SidePanel.java
@@ -0,0 +1,170 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2007-2013  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.client;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.util.LinkedList;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import us.mn.state.dot.map.LayerState;
+import us.mn.state.dot.map.MapBean;
+import us.mn.state.dot.sonar.SonarObject;
+import us.mn.state.dot.tms.client.proxy.ProxyLayerState;
+import us.mn.state.dot.tms.client.proxy.ProxySelectionListener;
+import us.mn.state.dot.tms.client.proxy.ProxySelectionModel;
+import static us.mn.state.dot.tms.client.widget.Widgets.UI;
+
+/**
+ * The side panel contains the tab panel on the left side of the IRIS client.
+ *
+ * @author Douglas Lau
+ */
+public class SidePanel extends JPanel {
+
+	/** Tabbed side pane */
+	private final JTabbedPane tab_pane;
+
+	/** Map associated with the pane */
+	private final MapBean map;
+
+	/** List of tab switchers */
+	private final LinkedList<TabSwitcher> switchers =
+		new LinkedList<TabSwitcher>();
+
+	/** Create a new side panel */
+	public SidePanel(MapBean m) {
+		super(new BorderLayout());
+		map = m;
+		setMinimumSize(UI.dimension(500, 200));
+		setPreferredSize(UI.dimension(500, 200));
+		tab_pane = new JTabbedPane(JTabbedPane.TOP);
+		add(tab_pane, BorderLayout.CENTER);
+		tab_pane.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				map.setPointSelector(null);
+				setSelectedLayer(getSelectedHomeLayer());
+				storeSelectedTabIndex();
+			}
+		});
+	}
+
+	/** Get the currently selected tab's home layer */
+	private ProxyLayerState getSelectedHomeLayer() {
+		Component tab = tab_pane.getSelectedComponent();
+		if(tab instanceof MapTab)
+			return getHomeProxyLayerState((MapTab)tab);
+		else
+			return null;
+	}
+
+	/** Get the home proxy layer state for a map tab */
+	private ProxyLayerState getHomeProxyLayerState(MapTab mt) {
+		LayerState ls = mt.getHomeLayer();
+		if(ls instanceof ProxyLayerState)
+			return (ProxyLayerState)ls;
+		else
+			return null;
+	}
+
+	/** Most recently selected layer */
+	private ProxyLayerState sel_layer;
+
+	/** Set the selected layer */
+	private void setSelectedLayer(ProxyLayerState sel) {
+		if(sel_layer != null && sel != sel_layer)
+			sel_layer.setTabSelected(false);
+		if(sel != null)
+			sel.setTabSelected(true);
+		sel_layer = sel;
+	}
+
+	/** Last selected tab, which stores the index of last user selected
+	 * tab. A field is used to track this, rather than dynamically
+	 * calling tab_pane.getSelectedIndex() because if the method is
+	 * called during app shutdown, it can erroneously return a -1. */
+	private int sel_tab;
+
+	/** Store the currently selected tab index. If no tab is
+	 *  selected (e.g. during tear down), the change is ignored. */
+	private void storeSelectedTabIndex() {
+		int sel = tab_pane.getSelectedIndex();
+		if(sel >= 0)
+			sel_tab = sel;
+	}
+
+	/** Get the index of the currently selected tab */
+	public int getSelectedTabIndex() {
+		return sel_tab;
+	}
+
+	/** Set the currently selected tab */
+	public void setSelectedTabIndex(int i) {
+		if(i >= 0 && i < tab_pane.getTabCount())
+			tab_pane.setSelectedIndex(i);
+	}
+
+	/** Add a tab to the screen pane */
+	public void addTab(MapTab mt) {
+		tab_pane.addTab(mt.getName(), null, mt, mt.getTip());
+		mt.setMap(map);
+		ProxyLayerState pls = getHomeProxyLayerState(mt);
+		if(pls != null) {
+			if(tab_pane.getTabCount() == 1)
+				setSelectedLayer(pls);
+			TabSwitcher ts = new TabSwitcher(mt,
+				pls.getSelectionModel());
+			switchers.add(ts);
+		}
+	}
+
+	/** Class to listen for proxy selection events and select tabs */
+	private class TabSwitcher implements ProxySelectionListener {
+		private final MapTab tab;
+		private final ProxySelectionModel model;
+		protected TabSwitcher(MapTab mt, ProxySelectionModel psm) {
+			tab = mt;
+			model = psm;
+			model.addProxySelectionListener(this);
+		}
+		protected void dispose() {
+			model.removeProxySelectionListener(this);
+		}
+		@Override
+		public void selectionAdded(SonarObject proxy) {
+			tab_pane.setSelectedComponent(tab);
+		}
+		@Override
+		public void selectionRemoved(SonarObject proxy) { }
+	}
+
+	/** Remove all the tabs */
+	public void removeTabs() {
+		for(TabSwitcher ts: switchers)
+			ts.dispose();
+		switchers.clear();
+		tab_pane.removeAll();
+	}
+
+	/** Set the menu bar */
+	public void setMenuBar(IMenuBar bar) {
+		removeAll();
+		if(bar != null)
+			add(bar, BorderLayout.NORTH);
+		add(tab_pane, BorderLayout.CENTER);
+	}
+}
