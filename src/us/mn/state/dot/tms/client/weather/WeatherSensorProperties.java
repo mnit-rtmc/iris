ca2f2b6fcd1ee5a96030812e7b5ad3f0e359702a
Douglas Lau <doug.lau@state.mn.us>
2013-11-14 08:43:55 -0600

    Cleaned up threading in WeatherSensorProperties form.

--- src/us/mn/state/dot/tms/client/weather/WeatherSensorProperties.java
+++ src/us/mn/state/dot/tms/client/weather/WeatherSensorProperties.java
@@ -15,15 +15,15 @@
 package us.mn.state.dot.tms.client.weather;
 
 import java.awt.Color;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
-import us.mn.state.dot.sched.FocusLostJob;
 import us.mn.state.dot.sonar.client.TypeCache;
 import us.mn.state.dot.tms.Controller;
 import us.mn.state.dot.tms.WeatherSensor;
-import static us.mn.state.dot.tms.client.IrisClient.WORKER;
 import us.mn.state.dot.tms.client.Session;
 import us.mn.state.dot.tms.client.comm.ControllerForm;
 import us.mn.state.dot.tms.client.proxy.SonarObjectForm;
@@ -93,8 +93,9 @@ public class WeatherSensorProperties extends SonarObjectForm<WeatherSensor> {
 
 	/** Create the widget jobs */
 	private void createUpdateJobs() {
-		notes_txt.addFocusListener(new FocusLostJob(WORKER) {
-			@Override public void perform() {
+		notes_txt.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {
 				proxy.setNotes(notes_txt.getText());
 			}
 		});
