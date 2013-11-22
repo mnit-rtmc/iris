d852cc47a8dc338f3a350a5fe7e15ece008ee79b
Douglas Lau <doug.lau@state.mn.us>
2013-11-22 09:24:28 -0600

    Minor cleanups.

--- src/us/mn/state/dot/tms/client/dms/SignTextComboBoxModel.java
+++ src/us/mn/state/dot/tms/client/dms/SignTextComboBoxModel.java
@@ -25,7 +25,7 @@ import us.mn.state.dot.tms.SignText;
 import us.mn.state.dot.tms.SignGroup;
 
 /**
- * Model for a sign text line combo box
+ * Model for a sign text line combo box.
  *
  * @author Douglas Lau
  * @author Michael Darter
@@ -37,35 +37,33 @@ public class SignTextComboBoxModel extends AbstractListModel
 	static private final short ON_THE_FLY_RANK = 99;
 
 	/** Blank client-side sign text object */
-	static protected final SignText BLANK_SIGN_TEXT =
+	static private final SignText BLANK_SIGN_TEXT =
 		new ClientSignText("");
 
-	/** Set of sorted SignText items in the line model */
-	protected final TreeSet<SignText> m_items =
+	/** Set of sorted SignText items */
+	private final TreeSet<SignText> items =
 		new TreeSet<SignText>(new SignTextComparator());
 
-	/** combobox line number */
-	protected final short m_cbline;
+	/** Sign text line number */
+	private final short line;
 
-	/** Shortcut to container */
-	protected final SignTextModel m_signTextModel;
+	/** Owning sign text model */
+	private final SignTextModel model;
 
-	/** 
-	 * Create a new line model.
-	 * @param cbline Combobox line number associated with this
-	 * SignTextComboBoxModel.
-	 * @param smm The container.
-	 */
-	protected SignTextComboBoxModel(short cbline, SignTextModel stm) {
-		m_cbline = cbline;
-		m_signTextModel = stm;
-		m_items.add(BLANK_SIGN_TEXT);
+	/** Create a new sign text combo box model.
+	 * @param ln Sign text line number.
+	 * @param stm Sign text model. */
+	protected SignTextComboBoxModel(short ln, SignTextModel stm) {
+		line = ln;
+		model = stm;
+		items.add(BLANK_SIGN_TEXT);
 	}
 
 	/** Get the element at the specified index */
+	@Override
 	public Object getElementAt(int index) {
 		int i = 0;
-		for(SignText t: m_items) {
+		for(SignText t: items) {
 			if(i == index)
 				return t;
 			i++;
@@ -74,16 +72,18 @@ public class SignTextComboBoxModel extends AbstractListModel
 	}
 
 	/** Get the number of elements in the model */
+	@Override
 	public int getSize() {
-		return m_items.size();
+		return items.size();
 	}
 
-	/** Selected item, either a String or SignText */
-	protected SignText m_selected;
+	/** Selected SignText item */
+	private SignText selected;
 
 	/** Get the selected item */
+	@Override
 	public Object getSelectedItem() {
-		SignText st = m_selected;
+		SignText st = selected;
 		// filter lines that should be ignored
 		if(st != null && st instanceof ClientSignText) {
 			if(DMSHelper.ignoreLineFilter(st.getMulti()))
@@ -92,26 +92,26 @@ public class SignTextComboBoxModel extends AbstractListModel
 		return st;
 	}
 
-	/** 
+	/**
 	 * Set the selected item. This method is called by the combobox when:
 	 * 	-the focus leaves the combobox with a String arg when editable.
 	 *      -a combobox item is clicked on via the mouse.
 	 *      -a combobox item is moved to via the cursor keys.
 	 */
+	@Override
 	public void setSelectedItem(Object s) {
-		if(s instanceof String)
-			m_selected = getSignText((String)s);
-		else if(s instanceof SignText)
-			m_selected = (SignText)s;
-		else {
-			m_selected = null;
-		}
+		if(s instanceof SignText)
+			selected = (SignText)s;
+		else if(s instanceof String)
+			selected = getSignText((String)s);
+		else
+			selected = null;
 		// this results in a call to the editor's setSelectedItem method
 		fireContentsChanged(this, -1, -1);
 	}
 
 	/** Get or create a sign text for the given string */
-	protected SignText getSignText(String s) {
+	private SignText getSignText(String s) {
 		if(s.length() == 0)
 			return BLANK_SIGN_TEXT;
 		SignText st = lookupMessage(s);
@@ -123,13 +123,10 @@ public class SignTextComboBoxModel extends AbstractListModel
 		}
 	}
 
-	/** 
-	 * Lookup a sign text message 
-	 * @return SignText of existing message else null if message doesn't
-	 * exist.
-	 */
-	protected SignText lookupMessage(String t) {
-		for(SignText st: m_items) {
+	/** Lookup a sign text.
+	 * @return Existing SignText, or null if not found. */
+	private SignText lookupMessage(String t) {
+		for(SignText st: items) {
 			if(t.equals(st.getMulti()))
 				return st;
 		}
@@ -137,9 +134,9 @@ public class SignTextComboBoxModel extends AbstractListModel
 	}
 
 	/** Find the index of an item */
-	protected int find(SignText t) {
+	private int find(SignText t) {
 		int i = 0;
-		for(SignText st: m_items) {
+		for(SignText st: items) {
 			if(st.equals(t))
 				return i;
 			i++;
@@ -148,26 +145,26 @@ public class SignTextComboBoxModel extends AbstractListModel
 	}
 
 	/** Add a SignText to the model */
-	protected void add(SignText t) {
-		// fails if item already exists in list
-		if(!m_items.add(t))
-			return;
-		final int i = find(t);
-		assert i >= 0 : "Failed to find SignText after just adding in SignTextComboBoxModel.add()";
-		runSwing(new Runnable() {
-			public void run() {
-				fireIntervalAdded(this, i, i);
+	public void add(SignText t) {
+		if(items.add(t)) {
+			final int i = find(t);
+			if(i >= 0) {
+				runSwing(new Runnable() {
+					public void run() {
+						fireIntervalAdded(this, i, i);
+					}
+				});
 			}
-		});
+		}
 	}
 
 	/** Remove a sign text from the model */
-	protected void remove(SignText t) {
+	public void remove(SignText t) {
 		final int i = find(t);
 		if(i >= 0) {
-			m_items.remove(t);
-			if(t.equals(m_selected))
-				m_selected = null;
+			items.remove(t);
+			if(t.equals(selected))
+				selected = null;
 			runSwing(new Runnable() {
 				public void run() {
 					fireIntervalRemoved(this, i, i);
@@ -178,13 +175,13 @@ public class SignTextComboBoxModel extends AbstractListModel
 
 	/** Update the message library with the currently selected messages */
 	public void updateMessageLibrary() {
-		SignText st = m_selected;
+		SignText st = selected;
 		if(st instanceof ClientSignText && st != BLANK_SIGN_TEXT)
 			addMsgToLib(st.getMulti());
 	}
 
 	/** Add a message to the local sign group library */
-	protected void addMsgToLib(String multi) {
-		m_signTextModel.createSignText(m_cbline, multi,ON_THE_FLY_RANK);
+	private void addMsgToLib(String multi) {
+		model.createSignText(line, multi, ON_THE_FLY_RANK);
 	}
 }
