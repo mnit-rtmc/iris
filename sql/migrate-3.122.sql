e4ad3f3390e7fb58390e703551991e73bb1f782d
Douglas Lau <doug.lau@state.mn.us>
2014-02-11 14:47:37 -0600

    Removed old SQL migrate scripts.

--- sql/migrate-3.122.sql
+++
@@ -1,9 +0,0 @@
-\set ON_ERROR_STOP
-
-SET SESSION AUTHORIZATION 'tms';
-
-UPDATE iris.system_attribute SET value = '3.122.0'
-	WHERE name = 'database_version';
-
-UPDATE iris.system_attribute SET name = 'dms_aws_read_time'
-	WHERE name = 'dms_aws_read_offset';
