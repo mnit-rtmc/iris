bed73bbdce34ab377453d8d37120d2c97fa3c2f8
Douglas Lau <doug.lau@state.mn.us>
2014-04-30 08:56:34 -0500

    Increase max heap size for both client and server.

--- bin/iris.sh
+++ bin/iris.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xms256m -Xmx512m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n -jar lib/iris-client-@@VERSION@@.jar iris-client.properties
+java -Xms256m -Xmx768m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n -jar lib/iris-client-@@VERSION@@.jar iris-client.properties
