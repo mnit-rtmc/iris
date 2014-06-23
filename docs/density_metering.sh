136b46edf6026e38595259576d2f8d7af9968905
Douglas Lau <doug.lau@state.mn.us>
2014-06-23 10:53:33 -0500

    Renamed and simplified script to make pdf.

---
+++ docs/density_metering.sh
@@ -0,0 +1,1 @@
+wkhtmltopdf.static --print-media-type -s Letter density_metering.html density_metering.pdf
--- docs/meter_limits.sh
+++
@@ -1,8 +0,0 @@
-wkhtmltopdf.static --print-media-type -s Letter density_metering.html meter_text.pdf
-inkscape --export-pdf=meter_undercount.pdf meter_undercount.svg
-inkscape --export-pdf=meter_storage.pdf meter_storage.svg
-inkscape --export-pdf=meter_wait_limit.pdf meter_wait_limit.svg
-inkscape --export-pdf=meter_target_range.pdf meter_target_range.svg
-inkscape --export-pdf=meter_storage_fixed.pdf meter_storage_fixed.svg
-inkscape --export-pdf=meter_wait_fixed.pdf meter_wait_fixed.svg
-pdftk meter_text.pdf meter_undercount.pdf meter_storage.pdf meter_wait_limit.pdf meter_target_range.pdf meter_storage_fixed.pdf meter_wait_fixed.pdf cat output density_metering.pdf
