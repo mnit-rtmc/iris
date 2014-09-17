10bc3ade7f399f9ed492554a8bbd27050beea412
Douglas Lau <doug.lau@state.mn.us>
2014-09-17 13:29:28 -0500

    Replaced SQL rewrite rules on iris.detector view with triggers.

--- sql/migrate-4.16.sql
+++ sql/migrate-4.16.sql
@@ -5,6 +5,7 @@ SET SESSION AUTHORIZATION 'tms';
 UPDATE iris.system_attribute SET value = '4.16.0'
 	WHERE name = 'database_version';
 
+-- Replace iris.alarm rewrite rules with triggers
 DROP RULE alarm_insert ON iris.alarm;
 DROP RULE alarm_update ON iris.alarm;
 DROP RULE alarm_delete ON iris.alarm;
@@ -59,3 +60,68 @@ $alarm_delete$ LANGUAGE plpgsql;
 CREATE TRIGGER alarm_delete_trig
     INSTEAD OF DELETE ON iris.alarm
     FOR EACH ROW EXECUTE PROCEDURE iris.alarm_delete();
+
+-- Replace iris.detector rewrite rules with triggers
+DROP RULE detector_insert ON iris.detector;
+DROP RULE detector_update ON iris.detector;
+DROP RULE detector_delete ON iris.detector;
+
+CREATE FUNCTION iris.detector_insert() RETURNS TRIGGER AS
+	$detector_insert$
+BEGIN
+	INSERT INTO iris._device_io (name, controller, pin)
+	     VALUES (NEW.name, NEW.controller, NEW.pin);
+	INSERT INTO iris._detector
+	            (name, r_node, lane_type, lane_number, abandoned,
+	             force_fail, field_length, fake, notes)
+	     VALUES (NEW.name, NEW.r_node, NEW.lane_type, NEW.lane_number,
+	             NEW.abandoned, NEW.force_fail, NEW.field_length, NEW.fake,
+	             NEW.notes);
+	RETURN NEW;
+END;
+$detector_insert$ LANGUAGE plpgsql;
+
+CREATE TRIGGER detector_insert_trig
+    INSTEAD OF INSERT ON iris.detector
+    FOR EACH ROW EXECUTE PROCEDURE iris.detector_insert();
+
+CREATE FUNCTION iris.detector_update() RETURNS TRIGGER AS
+	$detector_update$
+BEGIN
+	UPDATE iris._device_io
+	   SET controller = NEW.controller,
+	       pin = NEW.pin
+	 WHERE name = OLD.name;
+	UPDATE iris._detector
+	   SET r_node = NEW.r_node,
+	       lane_type = NEW.lane_type,
+	       lane_number = NEW.lane_number,
+	       abandoned = NEW.abandoned,
+	       force_fail = NEW.force_fail,
+	       field_length = NEW.field_length,
+	       fake = NEW.fake,
+	       notes = NEW.notes
+	 WHERE name = OLD.name;
+	RETURN NEW;
+END;
+$detector_update$ LANGUAGE plpgsql;
+
+CREATE TRIGGER detector_update_trig
+    INSTEAD OF UPDATE ON iris.detector
+    FOR EACH ROW EXECUTE PROCEDURE iris.detector_update();
+
+CREATE FUNCTION iris.detector_delete() RETURNS TRIGGER AS
+	$detector_delete$
+BEGIN
+	DELETE FROM iris._device_io WHERE name = OLD.name;
+	IF FOUND THEN
+		RETURN OLD;
+	ELSE
+		RETURN NULL;
+	END IF;
+END;
+$detector_delete$ LANGUAGE plpgsql;
+
+CREATE TRIGGER detector_delete_trig
+    INSTEAD OF DELETE ON iris.detector
+    FOR EACH ROW EXECUTE PROCEDURE iris.detector_delete();
--- sql/tms-template.sql
+++ sql/tms-template.sql
@@ -495,34 +495,65 @@ CREATE VIEW iris.detector AS SELECT
 	force_fail, field_length, fake, notes
 	FROM iris._detector det JOIN iris._device_io d ON det.name = d.name;
 
-CREATE RULE detector_insert AS ON INSERT TO iris.detector DO INSTEAD
-(
-	INSERT INTO iris._device_io VALUES (NEW.name, NEW.controller, NEW.pin);
-	INSERT INTO iris._detector VALUES (NEW.name, NEW.r_node, NEW.lane_type,
-		NEW.lane_number, NEW.abandoned, NEW.force_fail,
-		NEW.field_length, NEW.fake, NEW.notes);
-);
+CREATE FUNCTION iris.detector_insert() RETURNS TRIGGER AS
+	$detector_insert$
+BEGIN
+	INSERT INTO iris._device_io (name, controller, pin)
+	     VALUES (NEW.name, NEW.controller, NEW.pin);
+	INSERT INTO iris._detector
+	            (name, r_node, lane_type, lane_number, abandoned,
+	             force_fail, field_length, fake, notes)
+	     VALUES (NEW.name, NEW.r_node, NEW.lane_type, NEW.lane_number,
+	             NEW.abandoned, NEW.force_fail, NEW.field_length, NEW.fake,
+	             NEW.notes);
+	RETURN NEW;
+END;
+$detector_insert$ LANGUAGE plpgsql;
 
-CREATE RULE detector_update AS ON UPDATE TO iris.detector DO INSTEAD
-(
-	UPDATE iris._device_io SET
-		controller = NEW.controller,
-		pin = NEW.pin
-	WHERE name = OLD.name;
-	UPDATE iris._detector SET
-		r_node = NEW.r_node,
-		lane_type = NEW.lane_type,
-		lane_number = NEW.lane_number,
-		abandoned = NEW.abandoned,
-		force_fail = NEW.force_fail,
-		field_length = NEW.field_length,
-		fake = NEW.fake,
-		notes = NEW.notes
-	WHERE name = OLD.name;
-);
+CREATE TRIGGER detector_insert_trig
+    INSTEAD OF INSERT ON iris.detector
+    FOR EACH ROW EXECUTE PROCEDURE iris.detector_insert();
 
-CREATE RULE detector_delete AS ON DELETE TO iris.detector DO INSTEAD
+CREATE FUNCTION iris.detector_update() RETURNS TRIGGER AS
+	$detector_update$
+BEGIN
+	UPDATE iris._device_io
+	   SET controller = NEW.controller,
+	       pin = NEW.pin
+	 WHERE name = OLD.name;
+	UPDATE iris._detector
+	   SET r_node = NEW.r_node,
+	       lane_type = NEW.lane_type,
+	       lane_number = NEW.lane_number,
+	       abandoned = NEW.abandoned,
+	       force_fail = NEW.force_fail,
+	       field_length = NEW.field_length,
+	       fake = NEW.fake,
+	       notes = NEW.notes
+	 WHERE name = OLD.name;
+	RETURN NEW;
+END;
+$detector_update$ LANGUAGE plpgsql;
+
+CREATE TRIGGER detector_update_trig
+    INSTEAD OF UPDATE ON iris.detector
+    FOR EACH ROW EXECUTE PROCEDURE iris.detector_update();
+
+CREATE FUNCTION iris.detector_delete() RETURNS TRIGGER AS
+	$detector_delete$
+BEGIN
 	DELETE FROM iris._device_io WHERE name = OLD.name;
+	IF FOUND THEN
+		RETURN OLD;
+	ELSE
+		RETURN NULL;
+	END IF;
+END;
+$detector_delete$ LANGUAGE plpgsql;
+
+CREATE TRIGGER detector_delete_trig
+    INSTEAD OF DELETE ON iris.detector
+    FOR EACH ROW EXECUTE PROCEDURE iris.detector_delete();
 
 CREATE TABLE iris.encoder_type (
 	id integer PRIMARY KEY,
