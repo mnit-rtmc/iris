5bef457abadc2239b32ae129289aa115cc6e2a8c
Douglas Lau <doug.lau@state.mn.us>
2014-09-17 11:33:19 -0500

    Added test for iris.detector view to SQL test script.

--- sql/test-tms.sql
+++ sql/test-tms.sql
@@ -103,6 +103,87 @@ SELECT assert (true = (SELECT state FROM iris.alarm WHERE name = 'A_TEST_2'),
 DELETE FROM iris.alarm WHERE name = 'A_TEST_2';
 DELETE FROM iris.alarm WHERE name = 'A_TEST_1';
 
+-- Test detector view
+INSERT INTO iris.r_node (name, geo_loc, node_type, pickable, above, transition,
+                         lanes, attach_side, shift, active, abandoned,
+                         station_id, speed_limit, notes)
+	VALUES ('NOD_TEST_1', 'LOC_TEST_1', 0, true, false, 6, 4, false, 5,
+                true, false, 'S_TEST_1', 55, 'notes');
+INSERT INTO iris.r_node (name, geo_loc, node_type, pickable, above, transition,
+                         lanes, attach_side, shift, active, abandoned,
+                         station_id, speed_limit, notes)
+	VALUES ('NOD_TEST_2', 'LOC_TEST_1', 0, true, false, 6, 4, false, 5,
+                true, false, 'S_TEST_2', 55, 'notes');
+INSERT INTO iris.detector (name, pin, r_node, lane_type, lane_number, abandoned,
+                           force_fail, field_length)
+	VALUES ('DET_TEST_1', 19, 'NOD_TEST_1', 1, 2, false, true, 37.0);
+
+\o /dev/null
+SELECT assert ('DET_TEST_1' = (SELECT name FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det insert name');
+SELECT assert ((SELECT controller FROM iris.detector
+               WHERE name = 'DET_TEST_1') IS NULL, 'det insert controller');
+SELECT assert (19 = (SELECT pin FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det insert pin');
+SELECT assert ('NOD_TEST_1' = (SELECT r_node FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det insert r_node');
+SELECT assert (1 = (SELECT lane_type FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det insert lane_type');
+SELECT assert (2 = (SELECT lane_number FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det insert lane_number');
+SELECT assert (false = (SELECT abandoned FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det insert abandoned');
+SELECT assert (true = (SELECT force_fail FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det insert force_fail');
+SELECT assert (37.0 = (SELECT field_length FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det insert field_length');
+SELECT assert ((SELECT fake FROM iris.detector
+               WHERE name = 'DET_TEST_1') IS NULL, 'det insert fake');
+SELECT assert ((SELECT notes FROM iris.detector
+               WHERE name = 'DET_TEST_1') IS NULL, 'det insert notes');
+\o
+
+UPDATE iris.detector SET controller = 'CTL_TEST_1' WHERE name = 'DET_TEST_1';
+UPDATE iris.detector SET pin = 20 WHERE name = 'DET_TEST_1';
+UPDATE iris.detector SET r_node = 'NOD_TEST_2' WHERE name = 'DET_TEST_1';
+UPDATE iris.detector SET lane_type = 10 WHERE name = 'DET_TEST_1';
+UPDATE iris.detector SET lane_number = 3 WHERE name = 'DET_TEST_1';
+UPDATE iris.detector SET abandoned = true WHERE name = 'DET_TEST_1';
+UPDATE iris.detector SET force_fail = false WHERE name = 'DET_TEST_1';
+UPDATE iris.detector SET field_length = 15.0 WHERE name = 'DET_TEST_1';
+UPDATE iris.detector SET fake = 'fake exp' WHERE name = 'DET_TEST_1';
+UPDATE iris.detector SET notes = 'notes text' WHERE name = 'DET_TEST_1';
+
+\o /dev/null
+SELECT assert ('DET_TEST_1' = (SELECT name FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det update name');
+SELECT assert ('CTL_TEST_1' = (SELECT controller FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det update controller');
+SELECT assert (20 = (SELECT pin FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det update pin');
+SELECT assert ('NOD_TEST_2' = (SELECT r_node FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det update r_node');
+SELECT assert (10 = (SELECT lane_type FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det update lane_type');
+SELECT assert (3 = (SELECT lane_number FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det update lane_number');
+SELECT assert (true = (SELECT abandoned FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det update abandoned');
+SELECT assert (false = (SELECT force_fail FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det update force_fail');
+SELECT assert (15.0 = (SELECT field_length FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det update field_length');
+SELECT assert ('fake exp' = (SELECT fake FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det update fake');
+SELECT assert ('notes text' = (SELECT notes FROM iris.detector
+               WHERE name = 'DET_TEST_1'), 'det update notes');
+\o
+
+
+DELETE FROM iris.detector WHERE name = 'DET_TEST_1';
+DELETE FROM iris.r_node WHERE name = 'NOD_TEST_2';
+DELETE FROM iris.r_node WHERE name = 'NOD_TEST_1';
+
 -- Delete controller stuff
 DELETE FROM iris.controller WHERE name = 'CTL_TEST_1';
 DELETE FROM iris.cabinet WHERE name = 'CAB_TEST_1';
