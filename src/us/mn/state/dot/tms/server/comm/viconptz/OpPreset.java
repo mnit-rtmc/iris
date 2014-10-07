9cea069f86fd36c5e770ec8b6dd27b07491b1795
Douglas Lau <doug.lau@state.mn.us>
2014-10-07 10:13:55 -0500

    viconptz: Workaround problem with presets 8-12 for TXB-V translator card.

--- src/us/mn/state/dot/tms/server/comm/viconptz/OpPreset.java
+++ src/us/mn/state/dot/tms/server/comm/viconptz/OpPreset.java
@@ -26,6 +26,25 @@ import us.mn.state.dot.tms.server.comm.CommMessage;
  */
 public class OpPreset extends OpViconPTZ {
 
+	/** Special preset for on-screen menu (store) */
+	static private final int PRESET_MENU = 8;
+
+	/** Beginning of extended preset block */
+	static private final int PRESET_EXT = 21;
+
+	/** Adjust a preset number.  This is needed for Pelco cameras with a
+	 * TXB-V translator board installed.  In this case, presets 8 through
+	 * 20 are special functions, and cannot be used as intended.  For
+	 * example, storing preset 8 brings up the on-screen menu.  So, presets
+	 * 8-12 (in IRIS) use 21-25 within the camera.
+	 * @param p Preset number.
+	 * @return Actual preset number to send to camera. */
+	static private int adjustPreset(int p) {
+		return (p < PRESET_MENU)
+		      ? p
+		      : p - PRESET_MENU + PRESET_EXT;
+	}
+
 	/** Store (or recall) */
 	private final boolean store;
 
@@ -36,7 +55,7 @@ public class OpPreset extends OpViconPTZ {
 	public OpPreset(CameraImpl c, boolean s, int p) {
 		super(c);
 		store = s;
-		preset = p;
+		preset = adjustPreset(p);
 	}
 
 	/** Create the second phase of the operation */
