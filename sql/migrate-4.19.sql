6de6087519f0ada1ba3b80aab601070730e5f523
Douglas Lau <doug.lau@state.mn.us>
2014-11-06 10:18:27 -0600

    Add multi column to sign_event_view and recent_sign_event_view.

--- sql/migrate-4.19.sql
+++ sql/migrate-4.19.sql
@@ -8,3 +8,23 @@ UPDATE iris.system_attribute SET value = '4.19.0'
 -- Replace obsolete Vicon protocol (id 5) with Pelco P (formerly id 30)
 UPDATE iris.comm_protocol SET description = 'Pelco P' WHERE id = 5;
 DELETE FROM iris.comm_protocol WHERE id = 30;
+
+-- Add multi column to sign_event_view and recent_sign_event_view
+DROP VIEW recent_sign_event_view;
+DROP VIEW sign_event_view;
+
+CREATE VIEW sign_event_view AS
+	SELECT event_id, event_date, description, device_id,
+	       regexp_replace(replace(replace(message, '[nl]', E'\n'), '[np]',
+	                      E'\n'), '\[.+?\]', ' ', 'g') AS message,
+	       message AS multi, iris_user
+	FROM event.sign_event JOIN event.event_description
+	ON sign_event.event_desc_id = event_description.event_desc_id;
+GRANT SELECT ON sign_event_view TO PUBLIC;
+
+CREATE VIEW recent_sign_event_view AS
+	SELECT event_id, event_date, description, device_id, message, multi,
+	       iris_user
+	FROM sign_event_view
+	WHERE event_date > (CURRENT_TIMESTAMP - interval '90 days');
+GRANT SELECT ON recent_sign_event_view TO PUBLIC;
--- sql/tms-template.sql
+++ sql/tms-template.sql
@@ -1998,13 +1998,15 @@ GRANT SELECT ON detector_event_view TO PUBLIC;
 CREATE VIEW sign_event_view AS
 	SELECT event_id, event_date, description, device_id,
 	       regexp_replace(replace(replace(message, '[nl]', E'\n'), '[np]',
-	                      E'\n'), '\[.+?\]', ' ', 'g') AS message, iris_user
+	                      E'\n'), '\[.+?\]', ' ', 'g') AS message,
+	       message AS multi, iris_user
 	FROM event.sign_event JOIN event.event_description
 	ON sign_event.event_desc_id = event_description.event_desc_id;
 GRANT SELECT ON sign_event_view TO PUBLIC;
 
 CREATE VIEW recent_sign_event_view AS
-	SELECT event_id, event_date, description, device_id, message, iris_user
+	SELECT event_id, event_date, description, device_id, message, multi,
+	       iris_user
 	FROM sign_event_view
 	WHERE event_date > (CURRENT_TIMESTAMP - interval '90 days');
 GRANT SELECT ON recent_sign_event_view TO PUBLIC;
