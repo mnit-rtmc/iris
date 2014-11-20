81ae4b4afbce0dbed4f726b4eb5fb258efc40b0e
Douglas Lau <doug.lau@state.mn.us>
2014-11-20 09:41:47 -0600

    Added ProxyListModel2, which is simpler than ProxyListModel.

---
+++ src/us/mn/state/dot/tms/client/proxy/ProxyListModel2.java
@@ -0,0 +1,156 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2007-2014  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.client.proxy;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import javax.swing.AbstractListModel;
+import us.mn.state.dot.sonar.SonarObject;
+import us.mn.state.dot.sonar.client.TypeCache;
+import us.mn.state.dot.tms.utils.NumericAlphaComparator;
+
+/**
+ * A swing ListModel kept in sync with a SONAR TypeCache.
+ *
+ * @author Douglas Lau
+ */
+public class ProxyListModel2<T extends SonarObject>
+	extends AbstractListModel
+{
+	/** Proxy type cache */
+	private final TypeCache<T> cache;
+
+	/** Proxy list */
+	private final ArrayList<T> list;
+
+	/** Proxy comparator */
+	private final Comparator<T> comp = comparator();
+
+	/** Get a proxy comparator */
+	protected Comparator<T> comparator() {
+		return new NumericAlphaComparator<T>();
+	}
+
+	/** Proxy listener for SONAR updates */
+	private final SwingProxyAdapter<T> listener = new SwingProxyAdapter<T>() {
+		protected Comparator<T> comparator() {
+			return ProxyListModel2.this.comp;
+		}
+		protected void proxyAddedSwing(T proxy) {
+			int i = doProxyAdded(proxy);
+			if (i >= 0)
+				fireIntervalAdded(this, i, i);
+		}
+		protected void enumerationCompleteSwing(Collection<T> proxies) {
+			for (T proxy: proxies) {
+				if (isIncluded(proxy))
+					list.add(proxy);
+			}
+			int sz = list.size() - 1;
+			if (sz >= 0)
+				fireIntervalAdded(this, 0, sz);
+		}
+		protected void proxyRemovedSwing(T proxy) {
+			int i = doProxyRemoved(proxy);
+			if (i >= 0)
+				fireIntervalRemoved(this, i, i);
+		}
+		protected void proxyChangedSwing(T proxy, String attr) {
+			proxyChangedSwing(proxy);
+		}
+	};
+
+	/** Create a new proxy list model */
+	public ProxyListModel2(TypeCache<T> c) {
+		cache = c;
+		list = new ArrayList<T>();
+	}
+
+	/** Initialize the proxy list model. This cannot be done in the
+	 * constructor because subclasses may not be fully constructed. */
+	public void initialize() {
+		cache.addProxyListener(listener);
+	}
+
+	/** Dispose of the proxy model */
+	public void dispose() {
+		cache.removeProxyListener(listener);
+		listener.dispose();
+	}
+
+	/** Check if a proxy is included in the list */
+	protected boolean isIncluded(T proxy) {
+		return true;
+	}
+
+	/** Add a new proxy to the model */
+	private int doProxyAdded(T proxy) {
+		if (isIncluded(proxy)) {
+			int n_size = list.size();
+			for (int i = 0; i < n_size; ++i) {
+				if (comp.compare(proxy, list.get(i)) < 0) {
+					list.add(i, proxy);
+					return i;
+				}
+			}
+			list.add(proxy);
+			return n_size;
+		} else
+			return -1;
+	}
+
+	/** Remove a proxy from the model */
+	private int doProxyRemoved(T proxy) {
+		for (int i = 0; i < list.size(); ++i) {
+			if (proxy == list.get(i)) {
+				list.remove(i);
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	/** Change a proxy in the list model */
+	private void proxyChangedSwing(T proxy) {
+		int pre = doProxyRemoved(proxy);
+		int post = doProxyAdded(proxy);
+		if (pre >= 0 && post >= 0) {
+			int r0 = Math.min(pre, post);
+			int r1 = Math.max(pre, post);
+			fireContentsChanged(this, r0, r1);
+		} else if (pre >= 0 && post < 0)
+			fireIntervalRemoved(this, pre, pre);
+		else if (pre < 0 && post >= 0)
+			fireIntervalAdded(this, post, post);
+	}
+
+	/** Get the size (for ListModel) */
+	@Override
+	public int getSize() {
+		return list.size();
+	}
+
+	/** Get the element at the specified index (for ListModel) */
+	@Override
+	public Object getElementAt(int index) {
+		return list.get(index);
+	}
+
+	/** Get the proxy at the specified index */
+	public T getProxy(int i) {
+		return list.get(i);
+	}
+}
---
+++ src/us/mn/state/dot/tms/client/proxy/SwingProxyAdapter.java
@@ -0,0 +1,110 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2014  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.client.proxy;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.TreeSet;
+import us.mn.state.dot.sonar.SonarObject;
+import us.mn.state.dot.sonar.client.ProxyListener;
+import static us.mn.state.dot.tms.client.widget.SwingRunner.runSwing;
+
+/**
+ * An adapter to use ProxyListener for Swing models.
+ *
+ * @author Douglas Lau
+ */
+abstract public class SwingProxyAdapter<T extends SonarObject>
+	implements ProxyListener<T>
+{
+	/** Set of proxies used until the enumeration is complete */
+	private final TreeSet<T> proxies = new TreeSet<T>(comparator());
+
+	/** Flag to indicate enumeration complete has been received */
+	private boolean enumerated = false;
+
+	/** Add a proxy.
+	 * @see ProxyListener. */
+	@Override
+	public final void proxyAdded(final T proxy) {
+		if (enumerated) {
+			runSwing(new Runnable() {
+				public void run() {
+					proxyAddedSwing(proxy);
+				}
+			});
+		} else
+			proxies.add(proxy);
+	}
+
+	/** Enumeration of proxies is complete.
+	 * @see ProxyListener. */
+	@Override
+	public final void enumerationComplete() {
+		enumerated = true;
+		runSwing(new Runnable() {
+			public void run() {
+				enumerationCompleteSwing(proxies);
+				proxies.clear();
+			}
+		});
+	}
+
+	/** Remove a proxy.
+	 * @see ProxyListener. */
+	@Override
+	public final void proxyRemoved(final T proxy) {
+		if (enumerated) {
+			runSwing(new Runnable() {
+				public void run() {
+					proxyRemovedSwing(proxy);
+				}
+			});
+		}
+	}
+
+	/** A proxy has been changed.
+	 * @see ProxyListener. */
+	@Override
+	public final void proxyChanged(final T proxy, final String attr) {
+		if (enumerated) {
+			runSwing(new Runnable() {
+				public void run() {
+					proxyChangedSwing(proxy, attr);
+				}
+			});
+		}
+	}
+
+	/** Dispose of the adapter */
+	public final void dispose() {
+		proxies.clear();
+	}
+
+	/** Get a proxy comparator */
+	abstract protected Comparator<T> comparator();
+
+	/** Add a proxy */
+	abstract protected void proxyAddedSwing(T proxy);
+
+	/** Enumeration of proxies is complete */
+	abstract protected void enumerationCompleteSwing(Collection<T> proxies);
+
+	/** Remove a proxy */
+	abstract protected void proxyRemovedSwing(T proxy);
+
+	/** A proxy has been changed */
+	abstract protected void proxyChangedSwing(T proxy, String attr);
+}
