276150c61ab2d1fb6577401029f0f5b1899dc0b5
Douglas Lau <doug.lau@state.mn.us>
2014-11-21 13:30:24 -0600

    Improved sizing of ExceptionDialog.

--- src/us/mn/state/dot/tms/client/help/ExceptionDialog.java
+++ src/us/mn/state/dot/tms/client/help/ExceptionDialog.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2013  Minnesota Department of Transportation
+ * Copyright (C) 2000-2014  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@
  */
 package us.mn.state.dot.tms.client.help;
 
-import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -71,76 +70,7 @@ public class ExceptionDialog extends JDialog {
 	public void show(final Exception e) {
 		e.printStackTrace();
 		setFatal(false);
-		TextPanel tpanel = new TextPanel();
-		tpanel.addGlue();
-		try {
-			throw e;
-		}
-		catch(AuthenticationException ee) {
-			tpanel.addText("Authentication failed:");
-			tpanel.addText(ee.getMessage());
-			tpanel.addSpacing();
-			tpanel.addText("Please make sure your user");
-			tpanel.addText("name is correct, then");
-			tpanel.addText("type your password again.");
-		}
-		catch(ChangeVetoException ee) {
-			tpanel.addText("The change has been prevented");
-			tpanel.addText("for the following reason:");
-			tpanel.addSpacing();
-			tpanel.addText(ee.getMessage());
-		}
-		catch(PermissionException ee) {
-			tpanel.addText("Permission denied:");
-			tpanel.addSpacing();
-			tpanel.addText(ee.getMessage());
-		}
-		catch(SonarShowException ee) {
-			tpanel.addText("The following message was");
-			tpanel.addText("received from the IRIS server:");
-			tpanel.addSpacing();
-			tpanel.addText(ee.getMessage());
-		}
-		catch(NumberFormatException ee) {
-			tpanel.addText("Number formatting error");
-			tpanel.addSpacing();
-			tpanel.addText("Please check all numeric");
-			tpanel.addText("fields and try again.");
-		}
-		catch(InvalidMessageException ee) {
-			tpanel.addText("Invalid message");
-			tpanel.addSpacing();
-			tpanel.addText("The sign is unable to display");
-			tpanel.addText("the following message:");
-			tpanel.addText(ee.getMessage());
-			tpanel.addText("Please select a different message");
-		}
-		catch(ParseException ee) {
-			tpanel.addText("Parsing error");
-			tpanel.addText(ee.getMessage());
-			tpanel.addText("Please try again.");
-		}
-		catch(SonarException ee) {
-			setFatal(true);
-			tpanel.addText("This program has encountered");
-			tpanel.addText("a problem while communicating");
-			tpanel.addText("with the IRIS server.");
-			tpanel.addSpacing();
-			tpanel.addText(ee.getMessage());
-		}
-		catch(Exception ee) {
-			sendEmailAlert(e, tpanel);
-			setFatal(true);
-			tpanel.addText("This program has encountered");
-			tpanel.addText("a serious problem.");
-			addAssistanceMessage(tpanel);
-		}
-		tpanel.addSpacing();
-		String lastLine = I18N.get("help.exception.lastline");
-		if(lastLine != null)
-			tpanel.addText(lastLine);
-		tpanel.addGlue();
-		tpanel.addSpacing();
+		TextPanel tpanel = createMessagePanel(e);
 		Box hbox = Box.createHorizontalBox();
 		hbox.add(Box.createHorizontalGlue());
 		JButton button = new JButton("OK");
@@ -167,14 +97,84 @@ public class ExceptionDialog extends JDialog {
 		hbox.add(Box.createHorizontalGlue());
 		tpanel.add(hbox);
 		tpanel.addSpacing();
-		Dimension size = tpanel.getPreferredSize();
-		size.height += 32;
-		size.width += 16;
 		getContentPane().removeAll();
 		getContentPane().add(tpanel);
-		setSize(size);
+		pack();
 		Screen.centerOnCurrent(this);
 		setVisible(true);
+	}
+
+	/** Create a text panel for an exception.
+	 * FIXME: add I18n strings. */
+	private TextPanel createMessagePanel(final Exception e) {
+		TextPanel tpanel = new TextPanel();
+		tpanel.addGlue();
+		if (e instanceof AuthenticationException) {
+			tpanel.addText("Authentication failed:");
+			tpanel.addText(e.getMessage());
+			tpanel.addSpacing();
+			tpanel.addText("Please make sure your user");
+			tpanel.addText("name is correct, then");
+			tpanel.addText("type your password again.");
+		}
+		else if (e instanceof ChangeVetoException) {
+			tpanel.addText("The change has been prevented");
+			tpanel.addText("for the following reason:");
+			tpanel.addSpacing();
+			tpanel.addText(e.getMessage());
+		}
+		else if (e instanceof PermissionException) {
+			tpanel.addText("Permission denied:");
+			tpanel.addSpacing();
+			tpanel.addText(e.getMessage());
+		}
+		else if (e instanceof SonarShowException) {
+			tpanel.addText("The following message was");
+			tpanel.addText("received from the IRIS server:");
+			tpanel.addSpacing();
+			tpanel.addText(e.getMessage());
+		}
+		else if (e instanceof NumberFormatException) {
+			tpanel.addText("Number formatting error");
+			tpanel.addSpacing();
+			tpanel.addText("Please check all numeric");
+			tpanel.addText("fields and try again.");
+		}
+		else if (e instanceof InvalidMessageException) {
+			tpanel.addText("Invalid message");
+			tpanel.addSpacing();
+			tpanel.addText("The sign is unable to display");
+			tpanel.addText("the following message:");
+			tpanel.addText(e.getMessage());
+			tpanel.addText("Please select a different message");
+		}
+		else if (e instanceof ParseException) {
+			tpanel.addText("Parsing error");
+			tpanel.addText(e.getMessage());
+			tpanel.addText("Please try again.");
+		}
+		else if (e instanceof SonarException) {
+			setFatal(true);
+			tpanel.addText("This program has encountered");
+			tpanel.addText("a problem while communicating");
+			tpanel.addText("with the IRIS server.");
+			tpanel.addSpacing();
+			tpanel.addText(e.getMessage());
+		}
+		else if (e instanceof Exception) {
+			sendEmailAlert(e, tpanel);
+			setFatal(true);
+			tpanel.addText("This program has encountered");
+			tpanel.addText("a serious problem.");
+			addAssistanceMessage(tpanel);
+		}
+		tpanel.addSpacing();
+		String lastLine = I18N.get("help.exception.lastline");
+		if (lastLine != null)
+			tpanel.addText(lastLine);
+		tpanel.addGlue();
+		tpanel.addSpacing();
+		return tpanel;
 	}
 
 	/** Add a message about what to do for assistance */
