d67357dcf19072a209d3b90ccda04f45be3cbce4
Douglas Lau <doug.lau@state.mn.us>
2014-11-24 14:04:18 -0600

    Converted AlarmForm to use ProxyTablePanel.

--- src/us/mn/state/dot/tms/client/comm/AlarmForm.java
+++ src/us/mn/state/dot/tms/client/comm/AlarmForm.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2008-2012  Minnesota Department of Transportation
+ * Copyright (C) 2008-2014  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@ package us.mn.state.dot.tms.client.comm;
 
 import us.mn.state.dot.tms.Alarm;
 import us.mn.state.dot.tms.client.Session;
-import us.mn.state.dot.tms.client.proxy.ProxyTableForm;
+import us.mn.state.dot.tms.client.proxy.ProxyTableForm2;
 import us.mn.state.dot.tms.utils.I18N;
 
 /**
@@ -24,7 +24,7 @@ import us.mn.state.dot.tms.utils.I18N;
  *
  * @author Douglas Lau
  */
-public class AlarmForm extends ProxyTableForm<Alarm> {
+public class AlarmForm extends ProxyTableForm2<Alarm> {
 
 	/** Check if the user is permitted to use the form */
 	static public boolean isPermitted(Session s) {
--- src/us/mn/state/dot/tms/client/comm/AlarmModel.java
+++ src/us/mn/state/dot/tms/client/comm/AlarmModel.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2008-2013  Minnesota Department of Transportation
+ * Copyright (C) 2008-2014  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@ import javax.swing.table.TableCellRenderer;
 import us.mn.state.dot.tms.Alarm;
 import us.mn.state.dot.tms.client.Session;
 import us.mn.state.dot.tms.client.proxy.ProxyColumn;
-import us.mn.state.dot.tms.client.proxy.ProxyTableModel;
+import us.mn.state.dot.tms.client.proxy.ProxyTableModel2;
 import us.mn.state.dot.tms.utils.I18N;
 
 /**
@@ -33,23 +33,16 @@ import us.mn.state.dot.tms.utils.I18N;
  *
  * @author Douglas Lau
  */
-public class AlarmModel extends ProxyTableModel<Alarm> {
+public class AlarmModel extends ProxyTableModel2<Alarm> {
 
 	/** Create the columns in the model */
+	@Override
 	protected ArrayList<ProxyColumn<Alarm>> createColumns() {
 		ArrayList<ProxyColumn<Alarm>> cols =
 			new ArrayList<ProxyColumn<Alarm>>(6);
 		cols.add(new ProxyColumn<Alarm>("alarm", 80) {
 			public Object getValueAt(Alarm a) {
 				return a.getName();
-			}
-			public boolean isEditable(Alarm a) {
-				return (a == null) && canAdd();
-			}
-			public void setValueAt(Alarm a, Object value) {
-				String v = value.toString().trim();
-				if(v.length() > 0)
-					cache.createObject(v);
 			}
 		});
 		cols.add(new ProxyColumn<Alarm>("device.description", 200) {
@@ -96,7 +89,10 @@ public class AlarmModel extends ProxyTableModel<Alarm> {
 
 	/** Create a new alarm table model */
 	public AlarmModel(Session s) {
-		super(s, s.getSonarState().getAlarms());
+		super(s, s.getSonarState().getAlarms(),
+		      false,	/* has_properties */
+		      true,	/* has_create */
+		      true);	/* has_delete */
 	}
 
 	/** Renderer for state in a table cell */
@@ -109,14 +105,14 @@ public class AlarmModel extends ProxyTableModel<Alarm> {
 				(JLabel)super.getTableCellRendererComponent(
 				table, "", isSelected, hasFocus, row,
 				column);
-			if(!isSelected) {
+			if (!isSelected) {
 				label.setBackground(null);
 				label.setForeground(null);
 			}
-			if(value instanceof Boolean) {
+			if (value instanceof Boolean) {
 				Boolean t = (Boolean)value;
-				if(t) {
-					if(!isSelected) {
+				if (t) {
+					if (!isSelected) {
 						label.setBackground(Color.RED);
 						label.setForeground(
 							Color.WHITE);
@@ -131,6 +127,7 @@ public class AlarmModel extends ProxyTableModel<Alarm> {
 	}
 
 	/** Get the SONAR type name */
+	@Override
 	protected String getSonarType() {
 		return Alarm.SONAR_TYPE;
 	}
