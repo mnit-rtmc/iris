64bc0d81afb2cc26b548a04620b9e7459c611798
Douglas Lau <doug.lau@state.mn.us>
2014-11-24 10:33:23 -0600

    Converted LcsForm to use ProxyTablePanel.

--- src/us/mn/state/dot/tms/client/lcs/LCSArrayModel.java
+++ src/us/mn/state/dot/tms/client/lcs/LCSArrayModel.java
@@ -19,15 +19,14 @@ import us.mn.state.dot.tms.LCSArray;
 import us.mn.state.dot.tms.LCSArrayHelper;
 import us.mn.state.dot.tms.client.Session;
 import us.mn.state.dot.tms.client.proxy.ProxyColumn;
-import us.mn.state.dot.tms.client.proxy.ProxyTableModel;
-import us.mn.state.dot.tms.client.proxy.SonarObjectForm;
+import us.mn.state.dot.tms.client.proxy.ProxyTableModel2;
 
 /**
  * Table model for LCS arrays.
  *
  * @author Douglas Lau
  */
-public class LCSArrayModel extends ProxyTableModel<LCSArray> {
+public class LCSArrayModel extends ProxyTableModel2<LCSArray> {
 
 	/** Create the columns in the model */
 	@Override
@@ -37,14 +36,6 @@ public class LCSArrayModel extends ProxyTableModel<LCSArray> {
 		cols.add(new ProxyColumn<LCSArray>("lcs.array", 200) {
 			public Object getValueAt(LCSArray a) {
 				return a.getName();
-			}
-			public boolean isEditable(LCSArray a) {
-				return (a == null) && canAdd();
-			}
-			public void setValueAt(LCSArray a, Object value) {
-				String v = value.toString().trim();
-				if(v.length() > 0)
-					cache.createObject(v);
 			}
 		});
 		cols.add(new ProxyColumn<LCSArray>("location", 300) {
@@ -57,26 +48,21 @@ public class LCSArrayModel extends ProxyTableModel<LCSArray> {
 
 	/** Create a new LCS array table model */
 	public LCSArrayModel(Session s) {
-		super(s, s.getSonarState().getLcsCache().getLCSArrays());
-	}
-
-	/** Determine if a properties form is available */
-	@Override
-	public boolean hasProperties() {
-		return true;
-	}
-
-	/** Create a properties form for one proxy */
-	@Override
-	protected SonarObjectForm<LCSArray> createPropertiesForm(
-		LCSArray proxy)
-	{
-		return new LCSArrayProperties(session, proxy);
+		super(s, s.getSonarState().getLcsCache().getLCSArrays(),
+		      true,	/* has_properties */
+		      true,	/* has_create */
+		      true);	/* has_delete */
 	}
 
 	/** Get the SONAR type name */
 	@Override
 	protected String getSonarType() {
 		return LCSArray.SONAR_TYPE;
+	}
+
+	/** Create a properties form for one proxy */
+	@Override
+	protected LCSArrayProperties createPropertiesForm(LCSArray proxy) {
+		return new LCSArrayProperties(session, proxy);
 	}
 }
