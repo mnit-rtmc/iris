343ab1248771bb6bd716a2637e861fb37b467499
Douglas Lau <doug.lau@state.mn.us>
2015-01-23 11:25:37 -0600

    Cleaned up I18N code.

--- src/us/mn/state/dot/tms/utils/I18N.java
+++ src/us/mn/state/dot/tms/utils/I18N.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2007-2012  Minnesota Department of Transportation
+ * Copyright (C) 2007-2015  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -39,10 +39,10 @@ public class I18N {
 	static private final String NOT_READ = "Message bundle not read";
 
 	/** Key code to use for lookup failure */
-	static protected final int FAILURE_CODE = 0;
+	static private final int FAILURE_CODE = 0;
 
 	/** Char to use for lookup failure */
-	static protected final char FAILURE_CHAR = '\0';
+	static private final char FAILURE_CHAR = '\0';
 
 	/** Resource bundle with language, country and variant */
 	static private ResourceBundle lcv_bundle = null;
@@ -62,7 +62,7 @@ public class I18N {
 	 * Read resource bundle and assign to public static field.
 	 * This method should be called once at program startup.
 	 *
-	 * @param props Opened property file which contains the 
+	 * @param props Opened property file which contains the
 	 * 		language, country, and variant.
 	 */
 	static public void initialize(Properties props) {
@@ -76,7 +76,7 @@ public class I18N {
 
 	/** Get a resource bundle */
 	static private ResourceBundle getBundle(String l, String c, String v) {
-		if(l != null && c != null && v != null)
+		if (l != null && c != null && v != null)
 			return getBundle(new Locale(l, c, v));
 		else
 			return null;
@@ -84,7 +84,7 @@ public class I18N {
 
 	/** Get a resource bundle */
 	static private ResourceBundle getBundle(String l, String c) {
-		if(l != null && c != null)
+		if (l != null && c != null)
 			return getBundle(new Locale(l, c));
 		else
 			return null;
@@ -92,7 +92,7 @@ public class I18N {
 
 	/** Get a resource bundle */
 	static private ResourceBundle getBundle(String l) {
-		if(l != null)
+		if (l != null)
 			return getBundle(new Locale(l));
 		else
 			return null;
@@ -103,9 +103,8 @@ public class I18N {
 		try {
 			return ResourceBundle.getBundle(BASENAME, loc);
 		}
-		catch(MissingResourceException e) {
-			System.err.println("I18N could not load bundle: " +
-				loc);
+		catch (MissingResourceException e) {
+			System.err.println("I18N could not load bundle: " +loc);
 			return null;
 		}
 	}
@@ -115,7 +114,7 @@ public class I18N {
 	 * @param id Name of I18N string in the bundle.
 	 * @return The I18N string cooresponding to id, else null on error */
 	static public String getSilent(String id) {
-		if(id == null || id.isEmpty())
+		if (id == null || id.isEmpty())
 			return null;
 		else
 			return getString(id);
@@ -125,11 +124,11 @@ public class I18N {
 	 * @param id Name of I18N string in the bundle.
 	 * @return The I18N string cooresponding to id, else error message */
 	static public String get(String id) {
-		if(id == null || id.isEmpty())
+		if (id == null || id.isEmpty())
 			return UNDEFINED;
 		else {
 			String val = getString(id);
-			if(val != null)
+			if (val != null)
 				return val;
 			else {
 				System.err.println("I18N: failed to read (" +
@@ -141,71 +140,77 @@ public class I18N {
 
 	/** Get a string value from a resource bundle */
 	static private String getString(String key) {
-		if(lcv_bundle != null) {
+		if (lcv_bundle != null) {
 			try {
 				return lcv_bundle.getString(key);
 			}
-			catch(Exception e) { }	// fall through
+			catch (Exception e) {
+				// fall through
+			}
 		}
-		if(lc_bundle != null) {
+		if (lc_bundle != null) {
 			try {
 				return lc_bundle.getString(key);
 			}
-			catch(Exception e) { }	// fall through
+			catch (Exception e) {
+				// fall through
+			}
 		}
-		if(l_bundle != null) {
+		if (l_bundle != null) {
 			try {
 				return l_bundle.getString(key);
 			}
-			catch(Exception e) { }	// fall through
+			catch (Exception e) {
+				// fall through
+			}
 		}
 		return null;
 	}
 
 	/** Return the implied key mnemonic in the specified I18N string.
-	 *  For example, if the I18N string is "<html><u>B</u>lank</html>"
-	 *  then VK_B is returned. 
-	 *  @param id Name of I18N string in the bundle.
-	 *  @return The KeyEvent code else 0 on failure.
-	 *  @see java.awt.event.KeyEvent */
+	 * For example, if the I18N string is "<html><u>B</u>lank</html>"
+	 * then VK_B is returned.
+	 * @param id Name of I18N string in the bundle.
+	 * @return The KeyEvent code else 0 on failure.
+	 * @see java.awt.event.KeyEvent */
 	static public int getKeyEvent(String id) {
 		char c = getKeyEventChar(id);
-		if(c == '\0')
+		if (c == FAILURE_CHAR)
 			return FAILURE_CODE;
 		else
 			return charToKeyCode(c);
 	}
 
 	/** Return the implied key mnemonic in the specified I18N
-	 *  string as a character. For example, if the I18N string
-	 *  is "<html><u>B</u>lank</html>" then 'B' is returned.
-	 *  @param id Name of I18N string in the bundle.
-	 *  @return The 1st underlined char else '\0' on failure. */
+	 * string as a character. For example, if the I18N string
+	 * is "<html><u>B</u>lank</html>" then 'B' is returned.
+	 * @param id Name of I18N string in the bundle.
+	 * @return The 1st underlined char else '\0' on failure. */
 	static public char getKeyEventChar(String id) {
 		String s = getSilent(id);
-		if(s == null)
+		if (s == null)
 			return FAILURE_CHAR;
 		String utext = extractUnderline(s);
-		if(utext == null || utext.isEmpty())
+		if (utext == null || utext.isEmpty())
 			return FAILURE_CHAR;
 		return Character.toUpperCase(utext.charAt(0));
 	}
 
 	/** Extract underlined text from the argument.
-	 *  @return Null on failure or if no underline text exists,
-	 *	    else the underlined text, which might have length 0. */
+	 * @return Null on failure or if no underline text exists,
+	 *         else the underlined text, which might have length 0. */
 	static public String extractUnderline(String xml) {
 		final String TAG_OPEN = "<u>";
 		final String TAG_CLOSE = "</u>";
-		if(xml == null || xml.isEmpty())
+		if (xml == null || xml.isEmpty())
 			return null;
 		int s = xml.indexOf(TAG_OPEN);
-		if(s < 0)
+		if (s < 0)
 			return null;
 		int e = xml.indexOf(TAG_CLOSE, s);
-		if(e < 0)
+		if (e < 0)
 			return null;
-		if(s >= e)
+		if (s >= e)
 			return "";
 		return xml.substring(s + TAG_OPEN.length(), e);
 	}
@@ -213,44 +218,44 @@ public class I18N {
 	/** Return KeyEvent representation of specified character */
 	static private int charToKeyCode(char c) {
 		c = Character.toUpperCase(c);
-		switch(c) {
-			case 'A': return KeyEvent.VK_A;
-			case 'B': return KeyEvent.VK_B;
-			case 'C': return KeyEvent.VK_C;
-			case 'D': return KeyEvent.VK_D;
-			case 'E': return KeyEvent.VK_E;
-			case 'F': return KeyEvent.VK_F;
-			case 'G': return KeyEvent.VK_G;
-			case 'H': return KeyEvent.VK_H;
-			case 'I': return KeyEvent.VK_I;
-			case 'J': return KeyEvent.VK_J;
-			case 'K': return KeyEvent.VK_K;
-			case 'L': return KeyEvent.VK_L;
-			case 'M': return KeyEvent.VK_M;
-			case 'N': return KeyEvent.VK_N;
-			case 'O': return KeyEvent.VK_O;
-			case 'P': return KeyEvent.VK_P;
-			case 'Q': return KeyEvent.VK_Q;
-			case 'R': return KeyEvent.VK_R;
-			case 'S': return KeyEvent.VK_S;
-			case 'T': return KeyEvent.VK_T;
-			case 'U': return KeyEvent.VK_U;
-			case 'V': return KeyEvent.VK_V;
-			case 'W': return KeyEvent.VK_W;
-			case 'X': return KeyEvent.VK_X;
-			case 'Y': return KeyEvent.VK_Y;
-			case 'Z': return KeyEvent.VK_Z;
-			case '0': return KeyEvent.VK_0;
-			case '1': return KeyEvent.VK_1;
-			case '2': return KeyEvent.VK_2;
-			case '3': return KeyEvent.VK_3;
-			case '4': return KeyEvent.VK_4;
-			case '5': return KeyEvent.VK_5;
-			case '6': return KeyEvent.VK_6;
-			case '7': return KeyEvent.VK_7;
-			case '8': return KeyEvent.VK_8;
-			case '9': return KeyEvent.VK_9;
-			default: return FAILURE_CODE;
+		switch (c) {
+		case 'A': return KeyEvent.VK_A;
+		case 'B': return KeyEvent.VK_B;
+		case 'C': return KeyEvent.VK_C;
+		case 'D': return KeyEvent.VK_D;
+		case 'E': return KeyEvent.VK_E;
+		case 'F': return KeyEvent.VK_F;
+		case 'G': return KeyEvent.VK_G;
+		case 'H': return KeyEvent.VK_H;
+		case 'I': return KeyEvent.VK_I;
+		case 'J': return KeyEvent.VK_J;
+		case 'K': return KeyEvent.VK_K;
+		case 'L': return KeyEvent.VK_L;
+		case 'M': return KeyEvent.VK_M;
+		case 'N': return KeyEvent.VK_N;
+		case 'O': return KeyEvent.VK_O;
+		case 'P': return KeyEvent.VK_P;
+		case 'Q': return KeyEvent.VK_Q;
+		case 'R': return KeyEvent.VK_R;
+		case 'S': return KeyEvent.VK_S;
+		case 'T': return KeyEvent.VK_T;
+		case 'U': return KeyEvent.VK_U;
+		case 'V': return KeyEvent.VK_V;
+		case 'W': return KeyEvent.VK_W;
+		case 'X': return KeyEvent.VK_X;
+		case 'Y': return KeyEvent.VK_Y;
+		case 'Z': return KeyEvent.VK_Z;
+		case '0': return KeyEvent.VK_0;
+		case '1': return KeyEvent.VK_1;
+		case '2': return KeyEvent.VK_2;
+		case '3': return KeyEvent.VK_3;
+		case '4': return KeyEvent.VK_4;
+		case '5': return KeyEvent.VK_5;
+		case '6': return KeyEvent.VK_6;
+		case '7': return KeyEvent.VK_7;
+		case '8': return KeyEvent.VK_8;
+		case '9': return KeyEvent.VK_9;
+		default: return FAILURE_CODE;
 		}
 	}
 }
