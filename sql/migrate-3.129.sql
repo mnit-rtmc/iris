47554fa333322cbaa7b71a13e94b3aea9ebab50a
Douglas Lau <doug.lau@state.mn.us>
2015-09-01 08:50:35 -0500

    Removed all pre-4.0 migrate scripts.

--- sql/migrate-3.129.sql
+++
@@ -1,6 +0,0 @@
-\set ON_ERROR_STOP
-
-SET SESSION AUTHORIZATION 'tms';
-
-UPDATE iris.system_attribute SET value = '3.129.0'
-	WHERE name = 'database_version';
--- sql/migrate-3.145.sql
+++
@@ -1,8 +0,0 @@
-\set ON_ERROR_STOP
-
-SET SESSION AUTHORIZATION 'tms';
-
-UPDATE iris.system_attribute SET value = '3.145.0'
-	WHERE name = 'database_version';
-
-INSERT INTO iris.system_attribute (name, value) values ('msg_feed_verify', 'true');
