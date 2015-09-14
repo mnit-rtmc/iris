ce8655c9bed72f4c58e4e31e32aa08801ca6fca2
Douglas Lau <doug.lau@state.mn.us>
2015-09-14 14:22:32 -0500

    e6: Added CommandGroup and Command classes.

---
+++ src/us/mn/state/dot/tms/server/comm/e6/Command.java
@@ -0,0 +1,63 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2015  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.server.comm.e6;
+
+/**
+ * E6 commands.
+ *
+ * @author Douglas Lau
+ */
+public class Command {
+
+	/** Unsolicited command bit */
+	static private final int UNSOLICITED_BIT = 0x0010;
+
+	/** Acknowledge command bit */
+	static private final int ACKNOWLEDGE_BIT = 0x0001;
+
+	/** Create a command from bits.
+	 * @param b Bits of command from packet.
+	 * @return Valid command, or null on error. */
+	static public Command create(int b) {
+		CommandGroup cg = CommandGroup.lookup(b);
+		if (cg != null) {
+			int bits = cg.bits |
+			          (b & UNSOLICITED_BIT) |
+			          (b & ACKNOWLEDGE_BIT);
+			if (bits == b) {
+				boolean uns = (b & UNSOLICITED_BIT) != 0;
+				boolean ack = (b & ACKNOWLEDGE_BIT) != 0;
+				return new Command(cg, uns, ack);
+			}
+		}
+		return null;
+	}
+
+	/** Command group */
+	public final CommandGroup group;
+
+	/** Flag for unsolicited */
+	public final boolean unsolicited;
+
+	/** Flag for acknowledge */
+	public final boolean acknowledge;
+
+	/** Create a new command */
+	public Command(CommandGroup cg, boolean uns, boolean ack) {
+		group = cg;
+		unsolicited = uns;
+		acknowledge = ack;
+	}
+}
---
+++ src/us/mn/state/dot/tms/server/comm/e6/CommandGroup.java
@@ -0,0 +1,56 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2015  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.server.comm.e6;
+
+/**
+ * E6 command groups.
+ *
+ * @author Douglas Lau
+ */
+public enum CommandGroup {
+	SYSTEM_INFO		(0x8000),
+	DIGITAL_IO		(0x4000),
+	RF_TRANCEIVER		(0x2000),
+	TAG_TRANSACTION_CONFIG	(0x1000),
+	TAG_TRANSACTION		(0x0800),
+	MODE			(0x0400),
+	DIAGNOSTIC		(0x0200);
+
+	/** Create a new command group */
+	private CommandGroup(int b) {
+		bits = b;
+	}
+
+	/** Bits for command group */
+	public int bits;
+
+	/** Get the bits for all command groups */
+	static private int group_bits() {
+		int b = 0;
+		for (CommandGroup cg: values())
+			b |= cg.bits;
+		return b;
+	}
+
+	/** Lookup the command group for a command */
+	static public CommandGroup lookup(int b) {
+		int g_bits = group_bits();
+		for (CommandGroup cg: values()) {
+			if ((cg.bits & g_bits) == cg.bits)
+				return cg;
+		}
+		return null;
+	}
+}
