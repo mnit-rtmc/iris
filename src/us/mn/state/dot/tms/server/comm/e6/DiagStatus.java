c2d4b7b8fd1e1a42fb4d2ee1a5616f7ffb566220
Douglas Lau <doug.lau@state.mn.us>
2015-09-18 08:48:46 -0500

    e6: Used an enum for diagnostic error bits.

--- src/us/mn/state/dot/tms/server/comm/e6/DiagStatus.java
+++ src/us/mn/state/dot/tms/server/comm/e6/DiagStatus.java
@@ -58,6 +58,54 @@ public class DiagStatus extends E6Property {
 		System.arraycopy(d, 4, stat, 0, stat.length);
 	}
 
+	/** Diag error bits */
+	public enum ErrorBit {
+		FRAM_test		(58),
+		FRAM_data		(57),
+		FPGA1			(50),
+		FPGA2			(49),
+		Power_supply		(48),
+		DIO_overvoltage		(47),
+		DIO_undervoltage	(46),
+		RF_ADC_gt_max		(43),
+		RF_ADC_lt_min		(42),
+		RF_ATTN_DAC1_gt_max	(41),
+		RF_ATTN_DAC1_lt_min	(40),
+		RF_ATTN_DAC2_gt_max	(39),
+		RF_ATTN_DAC2_lt_min	(38),
+		RF_DOM_DAC_gt_max	(37),
+		RF_DOM_DAC_lt_min	(36),
+		RF_XCVR_src1_PLL_unlocked(35),
+		RF_XCVR_src2_PLL_unlocked(34),
+		RF_XCVR_uncalibrated	(33),
+		RF_5VDC_overvoltage	(32),
+		RF_5VDC_undervoltage	(31),
+		GPS_T_RAIM_alarm	(22),
+		GPS_self_test_fault	(21),
+		GPS_power_on_fault	(20),
+		TDM_two_masters		(19),
+		TDM_master_slave	(18),
+		TDM_clock		(17),
+		GPS_window		(16),
+		GPS_one_PPS		(15),
+		GPS_comm_link		(14),
+		serial_comm_link	(13),
+		UDP_IP_comm_link	(12),
+		serial_debug_comm_link	(11),
+		RF_XCVR_comm_link	(10),
+		CPU_firmware		(3),
+		Buffered_transactions	(1),
+		Error_log_entries	(0);
+
+		/** Create a new diag error bit */
+		private ErrorBit(int b) {
+			bit = b;
+		}
+
+		/** Bit number */
+		public final int bit;
+	};
+
 	/** Test status bit for an error */
 	private boolean testBit(int b) {
 		int i = (63 - b) / 8;
@@ -69,80 +117,14 @@ public class DiagStatus extends E6Property {
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		if (testBit(58))
-			sb.append("FRAM test, ");
-		if (testBit(57))
-			sb.append("FRAM data, ");
-		if (testBit(50))
-			sb.append("FPGA1, ");
-		if (testBit(49))
-			sb.append("FPGA2, ");
-		if (testBit(48))
-			sb.append("Power supply, ");
-		if (testBit(47))
-			sb.append("Digital overvoltage, ");
-		if (testBit(46))
-			sb.append("Digital undervoltage, ");
-		if (testBit(43))
-			sb.append("RF ADC > max, ");
-		if (testBit(42))
-			sb.append("RF ADC < min, ");
-		if (testBit(41))
-			sb.append("RF ATTN DAC1 > max, ");
-		if (testBit(40))
-			sb.append("RF ATTN DAC1 < min, ");
-		if (testBit(39))
-			sb.append("RF ATTN DAC2 > max, ");
-		if (testBit(38))
-			sb.append("RF ATTN DAC2 < min, ");
-		if (testBit(37))
-			sb.append("RF DOM DAC > max, ");
-		if (testBit(36))
-			sb.append("RF DOM DAC < min, ");
-		if (testBit(35))
-			sb.append("RF XCVR source1 PLL unlocked, ");
-		if (testBit(34))
-			sb.append("RF XCVR source2 PLL unlocked, ");
-		if (testBit(33))
-			sb.append("RF XCVR uncalibrated, ");
-		if (testBit(32))
-			sb.append("RF 5VDC overvoltage, ");
-		if (testBit(31))
-			sb.append("RF 5VDC undervoltage, ");
-		if (testBit(22))
-			sb.append("GPS T-RAIM alarm, ");
-		if (testBit(21))
-			sb.append("GPS self-test fault, ");
-		if (testBit(20))
-			sb.append("GPS power-on fault, ");
-		if (testBit(19))
-			sb.append("TDM two-masters, ");
-		if (testBit(18))
-			sb.append("TDM master-slave, ");
-		if (testBit(17))
-			sb.append("TDM clock, ");
-		if (testBit(16))
-			sb.append("GPS window, ");
-		if (testBit(15))
-			sb.append("GPS one-PPS, ");
-		if (testBit(14))
-			sb.append("GPS comm-link, ");
-		if (testBit(13))
-			sb.append("serial comm-link, ");
-		if (testBit(12))
-			sb.append("UDP/IP comm-link, ");
-		if (testBit(11))
-			sb.append("serial debug comm-link, ");
-		if (testBit(10))
-			sb.append("RF XCVR comm-link, ");
-		if (testBit(3))
-			sb.append("CPU firmware, ");
-		if (testBit(1))
-			sb.append("Buffered transactions, ");
-		if (testBit(0))
-			sb.append("Error log entries, ");
+		for (ErrorBit eb: ErrorBit.values()) {
+			if (testBit(eb.bit)) {
+				sb.append(eb);
+				sb.append(',');
+			}
+		}
 		if (sb.length() > 0)
-			sb.setLength(sb.length() - 2);
+			sb.setLength(sb.length() - 1);
 		return sb.toString();
 	}
 }
