e3740c75d5948fe40c2742edea063e4aa96a056e
Douglas Lau <doug.lau@state.mn.us>
2016-01-04 09:33:57 -0600

    Tweaked the ALPHA and BETA values for calculating tolls.

--- src/us/mn/state/dot/tms/server/TollZoneImpl.java
+++ src/us/mn/state/dot/tms/server/TollZoneImpl.java
@@ -39,10 +39,10 @@ public class TollZoneImpl extends BaseObjectImpl implements TollZone {
 	static private final int MAX_STEPS = 12;
 
 	/** Magic constant to convert density to price dollars */
-	static private final double ALPHA = 0.0575;
+	static private final double ALPHA = 0.045;
 
 	/** Magic exponent to convert density to price dollars */
-	static private final double BETA = 1.156;
+	static private final double BETA = 1.10;
 
 	/** Load all the toll zones */
 	static protected void loadAll() throws TMSException {
