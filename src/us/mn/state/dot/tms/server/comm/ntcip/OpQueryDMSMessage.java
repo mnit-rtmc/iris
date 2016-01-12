d22f1464ab75d1eee34e92d2ffb5ee6ee3001fc5
Douglas Lau <doug.lau@state.mn.us>
2016-01-12 09:25:34 -0600

    ntcip: Don't set runtime priority to null if not in DMSMessagePriority enum.

--- src/us/mn/state/dot/tms/server/comm/ntcip/OpQueryDMSMessage.java
+++ src/us/mn/state/dot/tms/server/comm/ntcip/OpQueryDMSMessage.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2015  Minnesota Department of Transportation
+ * Copyright (C) 2000-2016  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -147,8 +147,12 @@ public class OpQueryDMSMessage extends OpDMS {
 			logQuery(time);
 			if (status.getEnum() == DmsMessageStatus.valid) {
 				Integer d = parseDuration(time.getInteger());
+				DMSMessagePriority rp = prior.getEnum();
+				/* If it's null, IRIS didn't send it ... */
+				if (rp == null)
+					rp = DMSMessagePriority.OTHER_SYSTEM;
 				setCurrentMessage(ms.getValue(),
-					beacon.getInteger(), prior.getEnum(),d);
+					beacon.getInteger(), rp, d);
 			} else {
 				logError("INVALID STATUS");
 				setErrorStatus(status.toString());
