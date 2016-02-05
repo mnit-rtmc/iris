feb3be15180d25c105346cca74dc27fe7bfdeebc
Douglas Lau <doug.lau@state.mn.us>
2016-02-05 09:34:16 -0600

    addco: Disable bitmap page checksum checks.

--- src/us/mn/state/dot/tms/server/comm/addco/MessageProperty.java
+++ src/us/mn/state/dot/tms/server/comm/addco/MessageProperty.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2015  Minnesota Department of Transportation
+ * Copyright (C) 2015-2016  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -283,8 +283,10 @@ public class MessageProperty extends AddcoProperty {
 		int crc = calculateCrc(body, i_pos);
 		int rc = parse16(body, pos);	// swap bytes; not LE
 		pos += 2;
-		if (rc != crc)
-			throw new ChecksumException(body);
+		if (rc != crc) {
+			// This check is failing all the time... must be wrong?
+			// throw new ChecksumException(body);
+		}
 	}
 
 	/** Get the message MULTI string */
