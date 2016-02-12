0d99a0e72d1842c7c2a5b8f9803205c213f2868d
Douglas Lau <doug.lau@state.mn.us>
2016-02-12 11:31:05 -0600

    Fixed unchecked warnings in PlanDispatcher.

--- src/us/mn/state/dot/tms/client/schedule/PlanDispatcher.java
+++ src/us/mn/state/dot/tms/client/schedule/PlanDispatcher.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2011-2015  Minnesota Department of Transportation
+ * Copyright (C) 2011-2016  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -74,7 +74,8 @@ public class PlanDispatcher extends IPanel implements ProxyView<ActionPlan> {
 	private final JLabel meter_lbl = createValueLabel();
 
 	/** Plan phase combo box */
-	private final JComboBox phase_cbx = new JComboBox();
+	private final JComboBox<PlanPhase> phase_cbx =
+		new JComboBox<PlanPhase>();
 
 	/** Current session */
 	private final Session session;
@@ -147,31 +148,31 @@ public class PlanDispatcher extends IPanel implements ProxyView<ActionPlan> {
 	/** Update one attribute on the form */
 	@Override
 	public void update(ActionPlan ap, String a) {
-		if(a == null) {
+		if (a == null) {
 			phase_cbx.setAction(null);
 			phase_cbx.setModel(createPhaseModel(ap));
 			phase_cbx.setEnabled(isUpdatePermitted(ap));
 		}
-		if(a == null || a.equals("name"))
+		if (a == null || a.equals("name"))
 			name_lbl.setText(ap.getName());
-		if(a == null || a.equals("description"))
+		if (a == null || a.equals("description"))
 			description_lbl.setText(ap.getDescription());
-		if(a == null || a.equals("active")) {
+		if (a == null || a.equals("active")) {
 			dms_lbl.setText(Integer.toString(countDMS(ap)));
 			beacon_lbl.setText(Integer.toString(countBeacons(ap)));
 			lane_lbl.setText(Integer.toString(countLanes(ap)));
 			meter_lbl.setText(Integer.toString(countMeters(ap)));
 		}
-		if(a == null || a.equals("phase")) {
+		if (a == null || a.equals("phase")) {
 			phase_cbx.setAction(null);
 			ComboBoxModel mdl = phase_cbx.getModel();
 			// We must call setSelectedItem on the model, because
 			// it might not contain the phase.  In that case,
 			// calling JComboBox.setSelectedItem will fail.
-			if(mdl instanceof DefaultComboBoxModel) {
-				DefaultComboBoxModel model =
-					(DefaultComboBoxModel)mdl;
-				model.setSelectedItem(ap.getPhase());
+			if (mdl instanceof DefaultComboBoxModel) {
+				DefaultComboBoxModel dcm =
+					(DefaultComboBoxModel) mdl;
+				dcm.setSelectedItem(ap.getPhase());
 			}
 			phase_cbx.setAction(new ChangePhaseAction(ap,
 				phase_cbx));
@@ -179,16 +180,17 @@ public class PlanDispatcher extends IPanel implements ProxyView<ActionPlan> {
 	}
 
 	/** Create a combo box model for plan phases */
-	private DefaultComboBoxModel createPhaseModel(final ActionPlan ap) {
+	private ComboBoxModel<PlanPhase> createPhaseModel(final ActionPlan ap) {
 		TreeSet<PlanPhase> phases = createPhaseSet(ap);
 		removeNextPhases(phases);
-		DefaultComboBoxModel model = new DefaultComboBoxModel();
-		model.addElement(ap.getDefaultPhase());
+		DefaultComboBoxModel<PlanPhase> mdl = new DefaultComboBoxModel
+			<PlanPhase>();
+		mdl.addElement(ap.getDefaultPhase());
 		phases.remove(ap.getDefaultPhase());
-		for(PlanPhase p: phases)
-			model.addElement(p);
-		model.setSelectedItem(ap.getPhase());
-		return model;
+		for (PlanPhase p: phases)
+			mdl.addElement(p);
+		mdl.setSelectedItem(ap.getPhase());
+		return mdl;
 	}
 
 	/** Create a set of phases for an action plan */
@@ -196,27 +198,27 @@ public class PlanDispatcher extends IPanel implements ProxyView<ActionPlan> {
 		final TreeSet<PlanPhase> phases =
 			new TreeSet<PlanPhase>(comparator);
 		Iterator<DmsAction> dit = DmsActionHelper.iterator();
-		while(dit.hasNext()) {
+		while (dit.hasNext()) {
 			DmsAction da = dit.next();
-			if(da.getActionPlan() == ap)
+			if (da.getActionPlan() == ap)
 				phases.add(da.getPhase());
 		}
 		Iterator<BeaconAction> bit = BeaconActionHelper.iterator();
-		while(bit.hasNext()) {
+		while (bit.hasNext()) {
 			BeaconAction ba = bit.next();
-			if(ba.getActionPlan() == ap)
+			if (ba.getActionPlan() == ap)
 				phases.add(ba.getPhase());
 		}
 		Iterator<LaneAction> lit = LaneActionHelper.iterator();
-		while(lit.hasNext()) {
+		while (lit.hasNext()) {
 			LaneAction la = lit.next();
-			if(la.getActionPlan() == ap)
+			if (la.getActionPlan() == ap)
 				phases.add(la.getPhase());
 		}
 		Iterator<MeterAction> mit = MeterActionHelper.iterator();
-		while(mit.hasNext()) {
+		while (mit.hasNext()) {
 			MeterAction ma = mit.next();
-			if(ma.getActionPlan() == ap)
+			if (ma.getActionPlan() == ap)
 				phases.add(ma.getPhase());
 		}
 		return phases;
@@ -237,9 +239,9 @@ public class PlanDispatcher extends IPanel implements ProxyView<ActionPlan> {
 	private void removeNextPhases(TreeSet<PlanPhase> phases) {
 		TreeSet<PlanPhase> n_phases =
 			new TreeSet<PlanPhase>(comparator);
-		for(PlanPhase p: phases) {
+		for (PlanPhase p: phases) {
 			PlanPhase np = p.getNextPhase();
-			if(np != null)
+			if (np != null)
 				n_phases.add(np);
 		}
 		phases.removeAll(n_phases);
@@ -249,16 +251,16 @@ public class PlanDispatcher extends IPanel implements ProxyView<ActionPlan> {
 	private int countDMS(ActionPlan p) {
 		HashSet<SignGroup> plan_groups = new HashSet<SignGroup>();
 		Iterator<DmsAction> dit = DmsActionHelper.iterator();
-		while(dit.hasNext()) {
+		while (dit.hasNext()) {
 			DmsAction da = dit.next();
-			if(da.getActionPlan() == p)
+			if (da.getActionPlan() == p)
 				plan_groups.add(da.getSignGroup());
 		}
 		HashSet<DMS> plan_signs = new HashSet<DMS>();
 		Iterator<DmsSignGroup> git = DmsSignGroupHelper.iterator();
-		while(git.hasNext()) {
+		while (git.hasNext()) {
 			DmsSignGroup dsg = git.next();
-			if(plan_groups.contains(dsg.getSignGroup()))
+			if (plan_groups.contains(dsg.getSignGroup()))
 				plan_signs.add(dsg.getDms());
 		}
 		return plan_signs.size();
@@ -268,9 +270,9 @@ public class PlanDispatcher extends IPanel implements ProxyView<ActionPlan> {
 	private int countBeacons(ActionPlan p) {
 		HashSet<Beacon> plan_beacons = new HashSet<Beacon>();
 		Iterator<BeaconAction> bit = BeaconActionHelper.iterator();
-		while(bit.hasNext()) {
+		while (bit.hasNext()) {
 			BeaconAction ba = bit.next();
-			if(ba.getActionPlan() == p)
+			if (ba.getActionPlan() == p)
 				plan_beacons.add(ba.getBeacon());
 		}
 		return plan_beacons.size();
@@ -280,9 +282,9 @@ public class PlanDispatcher extends IPanel implements ProxyView<ActionPlan> {
 	private int countLanes(ActionPlan p) {
 		HashSet<LaneMarking> plan_lanes = new HashSet<LaneMarking>();
 		Iterator<LaneAction> lit = LaneActionHelper.iterator();
-		while(lit.hasNext()) {
+		while (lit.hasNext()) {
 			LaneAction la = lit.next();
-			if(la.getActionPlan() == p)
+			if (la.getActionPlan() == p)
 				plan_lanes.add(la.getLaneMarking());
 		}
 		return plan_lanes.size();
@@ -292,9 +294,9 @@ public class PlanDispatcher extends IPanel implements ProxyView<ActionPlan> {
 	private int countMeters(ActionPlan p) {
 		HashSet<RampMeter> plan_meters = new HashSet<RampMeter>();
 		Iterator<MeterAction> mit = MeterActionHelper.iterator();
-		while(mit.hasNext()) {
+		while (mit.hasNext()) {
 			MeterAction ma = mit.next();
-			if(ma.getActionPlan() == p)
+			if (ma.getActionPlan() == p)
 				plan_meters.add(ma.getRampMeter());
 		}
 		return plan_meters.size();
@@ -315,7 +317,7 @@ public class PlanDispatcher extends IPanel implements ProxyView<ActionPlan> {
 		lane_lbl.setText("");
 		meter_lbl.setText("");
 		phase_cbx.setAction(null);
-		phase_cbx.setModel(new DefaultComboBoxModel());
+		phase_cbx.setModel(new DefaultComboBoxModel<PlanPhase>());
 		phase_cbx.setSelectedItem(null);
 		phase_cbx.setEnabled(false);
 	}
