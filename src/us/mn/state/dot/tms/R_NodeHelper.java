238e909ba33dbc89f682d30de3d89412e95b567d
Douglas Lau <doug.lau@state.mn.us>
2016-02-26 13:10:32 -0600

    Cleaned up R_NodeHelper.

--- src/us/mn/state/dot/tms/R_NodeHelper.java
+++ src/us/mn/state/dot/tms/R_NodeHelper.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2009-2012  Minnesota Department of Transportation
+ * Copyright (C) 2009-2016  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -35,84 +35,103 @@ public class R_NodeHelper extends BaseHelper {
 	}
 
 	/** Check if an r_node is a station */
-	static public boolean isStation(R_Node r_node) {
-		return r_node.getNodeType() == R_NodeType.STATION.ordinal();
+	static public boolean isStation(R_Node n) {
+		return n.getNodeType() == R_NodeType.STATION.ordinal();
 	}
 
 	/** Check if an r_node is an entrance */
-	static public boolean isEntrance(R_Node r_node) {
-		return r_node.getNodeType() == R_NodeType.ENTRANCE.ordinal();
+	static public boolean isEntrance(R_Node n) {
+		return n.getNodeType() == R_NodeType.ENTRANCE.ordinal();
 	}
 
 	/** Check if an r_node is an exit */
-	static public boolean isExit(R_Node r_node) {
-		return r_node.getNodeType() == R_NodeType.EXIT.ordinal();
-	}
-
-	/** Check if an r_node links to a CD road */
-	static public boolean isCD(R_Node r_node) {
-		return r_node.getTransition() == R_NodeTransition.CD.ordinal();
+	static public boolean isExit(R_Node n) {
+		return n.getNodeType() == R_NodeType.EXIT.ordinal();
 	}
 
 	/** Check if an r_node an an intersection */
-	static public boolean isIntersection(R_Node r_node) {
-		return r_node.getNodeType() ==
-		       R_NodeType.INTERSECTION.ordinal();
-	}
-
-	/** Test if an r_node (acc) is linked by another r_node (ot) */
-	static public boolean isAccessLink(R_Node acc, R_Node ot) {
-		return isEntrance(ot) && GeoLocHelper.accessMatches(
-		       acc.getGeoLoc(), ot.getGeoLoc());
+	static public boolean isIntersection(R_Node n) {
+		return n.getNodeType() == R_NodeType.INTERSECTION.ordinal();
 	}
 
 	/** Check if an r_node is an access node */
-	static public boolean isAccess(R_Node r_node) {
-		return r_node.getNodeType() == R_NodeType.ACCESS.ordinal();
+	static public boolean isAccess(R_Node n) {
+		return n.getNodeType() == R_NodeType.ACCESS.ordinal();
 	}
 
-	/** Test if an r_node (en) links with another r_node (ex) */
-	static public boolean isExitLink(R_Node ex, R_Node en) {
-		return isMatchingEntrance(ex, en) || isMatchingAccess(ex, en);
+	/** Test if an access node links with an entrance node.
+	 * @param ac Access node.
+	 * @param n Node to check.
+	 * @return true If nodes should link. */
+	static public boolean isAccessLink(R_Node ac, R_Node n) {
+		return isEntrance(n) &&
+		       GeoLocHelper.accessMatches(ac.getGeoLoc(),n.getGeoLoc());
 	}
 
-	/** Test if an r_node (en) is a matching entrance for (ex) */
-	static protected boolean isMatchingEntrance(R_Node ex, R_Node en) {
-		return isEntrance(en) &&
-		       GeoLocHelper.rampMatches(ex.getGeoLoc(), en.getGeoLoc());
+	/** Test if an exit node links with another node.
+	 * @param ex Exit node.
+	 * @param n Node to check.
+	 * @return true If nodes should link. */
+	static public boolean isExitLink(R_Node ex, R_Node n) {
+		return isMatchingEntrance(ex, n)
+		    || isMatchingAccess(ex, n);
 	}
 
-	/** Test if an r_node (ac) is a matching access for (ex) */
-	static protected boolean isMatchingAccess(R_Node ex, R_Node ac) {
-		return isAccess(ac) && GeoLocHelper.accessMatches(
-		       ex.getGeoLoc(), ac.getGeoLoc());
+	/** Test if an exit node links with a matching entrance node.
+	 * @param ex Exit node.
+	 * @param n Possible entrance node.
+	 * @return true If nodes should link. */
+	static private boolean isMatchingEntrance(R_Node ex, R_Node n) {
+		return isEntrance(n) &&
+		       GeoLocHelper.rampMatches(ex.getGeoLoc(), n.getGeoLoc());
+	}
+
+	/** Test if an exit node links with a matching access node.
+	 * @param ex Exit node.
+	 * @param n Possible access node.
+	 * @return true If nodes should link. */
+	static private boolean isMatchingAccess(R_Node ex, R_Node n) {
+		return isAccess(n) &&
+		       GeoLocHelper.accessMatches(ex.getGeoLoc(),n.getGeoLoc());
+	}
+
+	/** Check if an r_node links to a CD road */
+	static public boolean isCD(R_Node n) {
+		return n.getTransition() == R_NodeTransition.CD.ordinal();
+	}
+
+	/** Check if an r_node has a COMMON transition */
+	static public boolean isCommon(R_Node n) {
+		return n.getTransition() == R_NodeTransition.COMMON.ordinal();
+	}
+
+	/** Check if a node is at a station break */
+	static public boolean isStationBreak(R_Node n) {
+		return n.getActive() &&
+		       isStation(n) &&
+		       hasDetection(n);
 	}
 
 	/** Check if an R_Node has detection (not abandoned) */
-	static public boolean hasDetection(R_Node r_node) {
+	static private boolean hasDetection(R_Node n) {
 		Iterator<Detector> it = DetectorHelper.iterator();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			Detector d = it.next();
-			if(d.getR_Node() == r_node &&
-			  (!d.getAbandoned()) &&
-			   DetectorHelper.isActive(d))
+			if (d.getR_Node() == n && DetectorHelper.isActive(d))
 				return true;
 		}
 		return false;
 	}
 
-	/** Check if a node is at a station break */
-	static public boolean isStationBreak(R_Node r_node) {
-		return r_node.getActive() &&
-		       isStation(r_node) &&
-		       hasDetection(r_node);
+	/** Check if a given node is a continuity break */
+	static public boolean isContinuityBreak(R_Node n) {
+		return isAccess(n) || isCommon(n);
 	}
 
 	/** Check if a node should be joined with a segment */
 	static public boolean isJoined(R_Node r_node) {
-		return r_node.getTransition() !=
-		       R_NodeTransition.COMMON.ordinal() ||
-		       r_node.getNodeType() != R_NodeType.ENTRANCE.ordinal();
+		return (!isCommon(r_node)) ||
+		       (!isEntrance(r_node));
 	}
 
 	/** Get the roadway corridor name */
