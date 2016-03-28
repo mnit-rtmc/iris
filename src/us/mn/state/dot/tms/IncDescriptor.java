d13d7bfe484f94345617dcd9fbb14cc9ae623d0a
Douglas Lau <doug.lau@state.mn.us>
2016-03-28 12:53:52 -0500

    Added interfaces for new incident DMS deployment stuff.

---
+++ src/us/mn/state/dot/tms/IncDescriptor.java
@@ -0,0 +1,65 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2016  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms;
+
+import us.mn.state.dot.sonar.SonarObject;
+
+/**
+ * An incident descriptor is part of a message to deploy on a DMS, matching
+ * incident attributes.
+ *
+ * @author Douglas Lau
+ */
+public interface IncDescriptor extends SonarObject {
+
+	/** SONAR type name */
+	String SONAR_TYPE = "inc_descriptor";
+
+	/** Set the sign group */
+	void setSignGroup(SignGroup sg);
+
+	/** Get the sign group */
+	SignGroup getSignGroup();
+
+	/** Set the event type */
+	void setEventType(int et);
+
+	/** Get the event type */
+	int getEventType();
+
+	/** Set the lane type ordinal */
+	void setLaneType(short lt);
+
+	/** Get the lane type ordinal */
+	short getLaneType();
+
+	/** Set the incident detail */
+	void setDetail(IncidentDetail dtl);
+
+	/** Get the incident detail */
+	IncidentDetail getDetail();
+
+	/** Set the cleared status */
+	void setCleared(boolean c);
+
+	/** Get the cleared status */
+	boolean getCleared();
+
+	/** Set the MULTI string */
+	void setMulti(String m);
+
+	/** Get the MULTI string */
+	String getMulti();
+}
---
+++ src/us/mn/state/dot/tms/IncLocator.java
@@ -0,0 +1,59 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2016  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms;
+
+import us.mn.state.dot.sonar.SonarObject;
+
+/**
+ * An incident locator is part of a message to deploy on a DMS, matching
+ * incident attributes.
+ *
+ * @author Douglas Lau
+ */
+public interface IncLocator extends SonarObject {
+
+	/** SONAR type name */
+	String SONAR_TYPE = "inc_locator";
+
+	/** Set the sign group */
+	void setSignGroup(SignGroup sg);
+
+	/** Get the sign group */
+	SignGroup getSignGroup();
+
+	/** Set the range */
+	void setRange(int r);
+
+	/** Get the range */
+	int getRange();
+
+	/** Set the branched flag */
+	void setBranched(boolean b);
+
+	/** Get the branched flag */
+	boolean getBranched();
+
+	/** Set the pickable flag */
+	void setPickable(boolean p);
+
+	/** Get the pickable flag */
+	boolean getPickable();
+
+	/** Set the MULTI string */
+	void setMulti(String m);
+
+	/** Get the MULTI string */
+	String getMulti();
+}
