f107709260684d02c34d8747432c66bc4235a2b9
Douglas Lau <doug.lau@state.mn.us>
2016-03-29 10:01:51 -0500

    Minor cleanups to IncidentPolicy.

--- src/us/mn/state/dot/tms/client/incident/IncidentPolicy.java
+++ src/us/mn/state/dot/tms/client/incident/IncidentPolicy.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2010-2014  Minnesota Department of Transportation
+ * Copyright (C) 2010-2016  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -44,6 +44,42 @@ public class IncidentPolicy {
 	/** Long distance upstream of incident to deploy devices */
 	static private final Distance DIST_LONG = new Distance(1.5f, MILES);
 
+	/** Assign a requested indication to an available indication.
+	 * @param lui Requested lane use indication.
+	 * @param available Array of available lane use indications.
+	 * @return "Best" lane use indication in available array. */
+	static private LaneUseIndication assignIndication(LaneUseIndication lui,
+		LaneUseIndication[] available)
+	{
+		for (LaneUseIndication a: available) {
+			if (lui == a)
+				return lui;
+		}
+		LaneUseIndication alt = altIndication(lui);
+		for (LaneUseIndication a: available) {
+			if (alt == a)
+				return alt;
+		}
+		return LaneUseIndication.DARK;
+	}
+
+	/** Get alternate indication for "dumb" LCS devices. */
+	static private LaneUseIndication altIndication(LaneUseIndication lui) {
+		switch(lui) {
+		case LOW_VISIBILITY:
+		case LANE_CLOSED_AHEAD:
+		case MERGE_RIGHT:
+		case MERGE_LEFT:
+		case MERGE_BOTH:
+			return LaneUseIndication.USE_CAUTION;
+		case MUST_EXIT_RIGHT:
+		case MUST_EXIT_LEFT:
+			return LaneUseIndication.LANE_CLOSED;
+		default:
+			return LaneUseIndication.LANE_OPEN;
+		}
+	}
+
 	/** Incident in question */
 	private final Incident incident;
 
@@ -63,11 +99,11 @@ public class IncidentPolicy {
 	{
 		int n_lcs = lcs_array.getIndicationsCurrent().length;
 		LCS[] lcss = LCSArrayHelper.lookupLCSs(lcs_array);
-		if(n_lcs != lcss.length)
+		if (n_lcs != lcss.length)
 			return new Integer[0];
 		LaneUseIndication[] ind = createIndications(up, n_lcs, shift);
 		Integer[] oin = new Integer[ind.length];
-		for(int i = 0; i < ind.length; i++) {
+		for (int i = 0; i < ind.length; i++) {
 			LaneUseIndication[] available =
 				LCSHelper.lookupIndications(lcss[i]);
 			oin[i] = assignIndication(ind[i], available).ordinal();
@@ -84,7 +120,7 @@ public class IncidentPolicy {
 		int shift)
 	{
 		LaneUseIndication[] ind = new LaneUseIndication[n_lcs];
-		for(int i = 0; i < ind.length; i++) {
+		for (int i = 0; i < ind.length; i++) {
 			int ln = shift + n_lcs - i;
 			ind[i] = createIndication(up, ln);
 		}
@@ -96,16 +132,16 @@ public class IncidentPolicy {
 	 * @param ln Lane number (0 for left shoulder, increasing to right).
 	 * @return LaneUseIndication value. */
 	private LaneUseIndication createIndication(Distance up, int ln) {
-		if(isShoulder(ln))
+		if (isShoulder(ln))
 			return LaneUseIndication.DARK;
 		double m = up.m();
-		if(m < 0)
+		if (m < 0)
 			return LaneUseIndication.DARK;
-		if(m < DIST_SHORT.m())
+		if (m < DIST_SHORT.m())
 			return createIndicationShort(ln);
-		if(m < DIST_MEDIUM.m())
+		if (m < DIST_MEDIUM.m())
 			return createIndicationMedium(ln);
-		if(m < DIST_LONG.m())
+		if (m < DIST_LONG.m())
 			return createIndicationLong(ln);
 		else
 			return LaneUseIndication.DARK;
@@ -117,9 +153,9 @@ public class IncidentPolicy {
 	 * @return LaneUseIndication value. */
 	private LaneUseIndication createIndicationShort(int ln) {
 		IncidentImpact ii = getImpact(ln);
-		if(ii == BLOCKED)
+		if (ii == BLOCKED)
 			return LaneUseIndication.LANE_CLOSED;
-		else if(ii == PARTIALLY_BLOCKED || isAdjacentLaneBlocked(ln))
+		else if (ii == PARTIALLY_BLOCKED || isAdjacentLaneBlocked(ln))
 			return LaneUseIndication.USE_CAUTION;
 		else
 			return LaneUseIndication.LANE_OPEN;
@@ -131,7 +167,7 @@ public class IncidentPolicy {
 	private IncidentImpact getImpact(int ln) {
 		String impact = incident.getImpact();
 		// Don't look at the shoulder lanes
-		if(ln <= 0 || ln >= impact.length() - 1)
+		if (ln <= 0 || ln >= impact.length() - 1)
 			return FREE_FLOWING;
 		else
 			return IncidentImpact.fromChar(impact.charAt(ln));
@@ -152,7 +188,7 @@ public class IncidentPolicy {
 	 * @return IncidentImpact for given adjacent lane. */
 	private IncidentImpact getAdjacentImpact(int ln, IncidentImpact def) {
 		String impact = incident.getImpact();
-		if(ln < 0 || ln >= impact.length())
+		if (ln < 0 || ln >= impact.length())
 			return def;
 		else
 			return IncidentImpact.fromChar(impact.charAt(ln));
@@ -163,15 +199,15 @@ public class IncidentPolicy {
 	 * @param ln Lane number (0 for left shoulder, increasing to right).
 	 * @return LaneUseIndication value. */
 	private LaneUseIndication createIndicationMedium(int ln) {
-		if(!isLaneBlocked(ln))
+		if (!isLaneBlocked(ln))
 			return LaneUseIndication.LANE_OPEN;
 		int n_left = unblockedLeftMainline(ln);
 		int n_right = unblockedRightMainline(ln);
-		if(n_left < n_right)
+		if (n_left < n_right)
 			return LaneUseIndication.MERGE_LEFT;
-		else if(n_right < n_left)
+		else if (n_right < n_left)
 			return LaneUseIndication.MERGE_RIGHT;
-		else if(n_left < MAX_SHIFT)
+		else if (n_left < MAX_SHIFT)
 			return LaneUseIndication.MERGE_BOTH;
 		else
 			return indication2MainlineBlocked(ln);
@@ -182,7 +218,7 @@ public class IncidentPolicy {
 	 * @return true if lane is blocked, false otherwise. */
 	private boolean isLaneBlocked(int ln) {
 		String impact = incident.getImpact();
-		if(ln > 0 && ln < impact.length()) {
+		if (ln > 0 && ln < impact.length()) {
 			char c = impact.charAt(ln);
 			IncidentImpact ii = IncidentImpact.fromChar(c);
 			return ii == BLOCKED;
@@ -209,8 +245,8 @@ public class IncidentPolicy {
 	 * @param ln Lane number (0 for left shoulder, increasing to right).
 	 * @return Number of lanes to left until a lane is not blocked. */
 	private int unblockedLeftMainline(int ln) {
-		for(int i = 0; i < MAX_SHIFT; i++) {
-			if(!isLaneBlockedOrShoulder(ln - i))
+		for (int i = 0; i < MAX_SHIFT; i++) {
+			if (!isLaneBlockedOrShoulder(ln - i))
 				return i;
 		}
 		return MAX_SHIFT;
@@ -220,8 +256,8 @@ public class IncidentPolicy {
 	 * @param ln Lane number (0 for left shoulder, increasing to right).
 	 * @return Number of lanes to right until a lane is not blocked. */
 	private int unblockedRightMainline(int ln) {
-		for(int i = 0; i < MAX_SHIFT; i++) {
-			if(!isLaneBlockedOrShoulder(ln + i))
+		for (int i = 0; i < MAX_SHIFT; i++) {
+			if (!isLaneBlockedOrShoulder(ln + i))
 				return i;
 		}
 		return MAX_SHIFT;
@@ -233,11 +269,11 @@ public class IncidentPolicy {
 	private LaneUseIndication indication2MainlineBlocked(int ln) {
 		int n_left = unblockedLeft(ln);
 		int n_right = unblockedRight(ln);
-		if(n_left < n_right)
+		if (n_left < n_right)
 			return LaneUseIndication.MERGE_LEFT;
-		else if(n_right < n_left)
+		else if (n_right < n_left)
 			return LaneUseIndication.MERGE_RIGHT;
-		else if(n_left < MAX_SHIFT)
+		else if (n_left < MAX_SHIFT)
 			return LaneUseIndication.MERGE_BOTH;
 		else
 			return LaneUseIndication.LANE_CLOSED;
@@ -247,8 +283,8 @@ public class IncidentPolicy {
 	 * @param ln Lane number (0 for left shoulder, increasing to right).
 	 * @return Number of lanes to left until a lane is not blocked. */
 	private int unblockedLeft(int ln) {
-		for(int i = 0; i < MAX_SHIFT; i++) {
-			if(!isLaneBlocked(ln - i))
+		for (int i = 0; i < MAX_SHIFT; i++) {
+			if (!isLaneBlocked(ln - i))
 				return i;
 		}
 		return MAX_SHIFT;
@@ -258,8 +294,8 @@ public class IncidentPolicy {
 	 * @param ln Lane number (0 for left shoulder, increasing to right).
 	 * @return Number of lanes to right until a lane is not blocked. */
 	private int unblockedRight(int ln) {
-		for(int i = 0; i < MAX_SHIFT; i++) {
-			if(!isLaneBlocked(ln + i))
+		for (int i = 0; i < MAX_SHIFT; i++) {
+			if (!isLaneBlocked(ln + i))
 				return i;
 		}
 		return MAX_SHIFT;
@@ -270,45 +306,9 @@ public class IncidentPolicy {
 	 * @param ln Lane number (0 for left shoulder, increasing to right).
 	 * @return LaneUseIndication value. */
 	private LaneUseIndication createIndicationLong(int ln) {
-		if(isLaneBlocked(ln))
+		if (isLaneBlocked(ln))
 			return LaneUseIndication.LANE_CLOSED_AHEAD;
 		else
 			return LaneUseIndication.LANE_OPEN;
-	}
-
-	/** Assign a requested indication to an available indication.
-	 * @param lui Requested lane use indication.
-	 * @param available Array of available lane use indications.
-	 * @return "Best" lane use indication in available array. */
-	static private LaneUseIndication assignIndication(LaneUseIndication lui,
-		LaneUseIndication[] available)
-	{
-		for(LaneUseIndication a: available) {
-			if(lui == a)
-				return lui;
-		}
-		LaneUseIndication alt = altIndication(lui);
-		for(LaneUseIndication a: available) {
-			if(alt == a)
-				return alt;
-		}
-		return LaneUseIndication.DARK;
-	}
-
-	/** Get alternate indication for "dumb" LCS devices. */
-	static private LaneUseIndication altIndication(LaneUseIndication lui) {
-		switch(lui) {
-		case LOW_VISIBILITY:
-		case LANE_CLOSED_AHEAD:
-		case MERGE_RIGHT:
-		case MERGE_LEFT:
-		case MERGE_BOTH:
-			return LaneUseIndication.USE_CAUTION;
-		case MUST_EXIT_RIGHT:
-		case MUST_EXIT_LEFT:
-			return LaneUseIndication.LANE_CLOSED;
-		default:
-			return LaneUseIndication.LANE_OPEN;
-		}
 	}
 }
