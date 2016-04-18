e7a3ccfdad8f35600904c40ecaa02e32c947a13c
Douglas Lau <doug.lau@state.mn.us>
2016-04-18 10:03:17 -0500

    Use toggle buttons with a group layout in style summary.

--- src/us/mn/state/dot/tms/client/proxy/StyleSummary.java
+++ src/us/mn/state/dot/tms/client/proxy/StyleSummary.java
@@ -15,38 +15,27 @@
  */
 package us.mn.state.dot.tms.client.proxy;
 
-import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.HashMap;
 import java.util.List;
-import javax.swing.AbstractButton;
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.DefaultListSelectionModel;
+import javax.swing.GroupLayout;
 import javax.swing.Icon;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JToggleButton;
-import javax.swing.border.TitledBorder;
 import us.mn.state.dot.sonar.SonarObject;
 import us.mn.state.dot.sonar.client.ProxyListener;
 import us.mn.state.dot.tms.client.map.Style;
 import us.mn.state.dot.tms.client.widget.IWorker;
+import static us.mn.state.dot.tms.client.widget.Widgets.UI;
 import us.mn.state.dot.tms.ItemStyle;
 import us.mn.state.dot.tms.utils.I18N;
 
 /**
- * Panel to display a summary of styled objects, which contains radio
+ * Panel to display a summary of styled objects, which contains toggle
  * buttons which selected the current view, and a listbox below to show
  * the objects associated with the selected style.
  *
@@ -55,24 +44,30 @@ import us.mn.state.dot.tms.utils.I18N;
  */
 public class StyleSummary<T extends SonarObject> extends JPanel {
 
-	/** Number of style columns in summary */
+	/** Style button columns */
 	static private final int STYLE_COLS = 3;
 
-	/** Number of grid columns for each style column */
-	static private final int GRID_COLS = 4;
+	/** Style button */
+	static private class StyleButton extends JToggleButton {
+		private final ItemStyle i_style;
+		private int n_count;
+		private StyleButton(ItemStyle is, Icon legend) {
+			super(is.toString(), legend);
+			setBorder(UI.buttonBorder());
+			setMargin(UI.buttonInsets());
+			i_style = is;
+			n_count = 0;
+		}
+	}
 
 	/** Proxy manager */
 	private final ProxyManager<T> manager;
 
-	/** Radio button group */
-	private final ButtonGroup r_buttons = new ButtonGroup();
+	/** Default style */
+	private final ItemStyle def_style;
 
-	/** Mapping of style names to widgets */
-	private final HashMap<String, StyleWidgets> widgets =
-		new HashMap<String, StyleWidgets>();
-
-	/** Titled border */
-	private final TitledBorder border;
+	/** Style buttons */
+	private final StyleButton[] buttons;
 
 	/** Dummy list selection model */
 	private final DefaultListSelectionModel dummy_model =
@@ -81,139 +76,186 @@ public class StyleSummary<T extends SonarObject> extends JPanel {
 	/** Selected style list model */
 	private StyleListModel<T> model;
 
+	/** Cell size buttons */
+	private final JToggleButton[] sz_btns;
+
 	/** Proxy list */
 	private final ProxyJList<T> p_list;
+
+	/** List scrollpane */
+	private final JScrollPane s_pane;
 
 	/** Style status counter */
 	private final ProxyListener<T> counter = new ProxyListener<T>() {
 		private boolean complete = false;
-		@Override
-		public void proxyAdded(T proxy) {
-			if(complete)
+		@Override public void proxyAdded(T proxy) {
+			if (complete)
 				updateCounts();
 		}
-		@Override
-		public void enumerationComplete() {
+		@Override public void enumerationComplete() {
 			complete = true;
 			updateCounts();
 		}
-		@Override
-		public void proxyRemoved(T proxy) {
+		@Override public void proxyRemoved(T proxy) {
 			updateCounts();
 		}
-		@Override
-		public void proxyChanged(T proxy, String attrib) {
-			if(manager.isStyleAttrib(attrib))
+		@Override public void proxyChanged(T proxy, String attrib) {
+			if (manager.isStyleAttrib(attrib))
 				updateCounts();
 		}
 	};
 
-	/** Widgets for one style */
-	private class StyleWidgets {
-		private final ItemStyle i_style;
-		private final JRadioButton btn;
-		private final JLabel legend_lbl;
-		private final JLabel count_lbl;
-		private int n_count;
-		private StyleWidgets(Style sty, Icon legend) {
-			i_style = ItemStyle.lookupStyle(sty.toString());
-			btn = createRadioButton(i_style);
-			legend_lbl = new JLabel(legend);
-			count_lbl = new JLabel();
-			n_count = 0;
-		}
-		private void countProxy(T proxy) {
-			if (manager.checkStyle(i_style, proxy))
-				n_count++;
-		}
-		private void updateCountLabel() {
-			count_lbl.setText(Integer.toString(n_count));
-		}
-	}
-
-	/** Create a new style summary panel, with optional cell size buttons.
-	 * @param man ProxyManager */
-	public StyleSummary(final ProxyManager<T> man, ItemStyle def_style,
+	/** Create a new style summary panel.
+	 * @param m ProxyManager.
+	 * @param ds Default style.
+	 * @param enableCellSizeBtns Flag to enable cell size buttons. */
+	public StyleSummary(ProxyManager<T> m, ItemStyle ds,
 		boolean enableCellSizeBtns)
 	{
-		super(new GridBagLayout());
-		manager = man;
-		border = BorderFactory.createTitledBorder("");
-		setBorder(border);
-		GridBagConstraints bag = new GridBagConstraints();
+		manager = m;
+		def_style = ds;
+		sz_btns = enableCellSizeBtns ? createSizeButtons() : null;
+		buttons = createStyleButtons();
+		p_list = manager.createList();
+ 		s_pane = new JScrollPane(p_list);
+	}
+
+	/** Create cell size buttons */
+	private JToggleButton[] createSizeButtons() {
+		JToggleButton[] btns = new JToggleButton[3];
+		btns[0] = createSizeButton(CellRendererSize.SMALL);
+		btns[1] = createSizeButton(CellRendererSize.MEDIUM);
+		btns[2] = createSizeButton(CellRendererSize.LARGE);
+		ButtonGroup bg = new ButtonGroup();
+		for (JToggleButton b : btns)
+			bg.add(b);
+		btns[2].setSelected(true);
+		return btns;
+	}
+
+	/** Create a toggle button for changing cell renderer size */
+	private JToggleButton createSizeButton(final CellRendererSize size) {
+		JToggleButton b = new JToggleButton(I18N.get(size.text_id));
+		b.setBorder(UI.buttonBorder());
+		b.setMargin(UI.buttonInsets());
+		Font f = b.getFont();
+		b.setFont(f.deriveFont(0.8f * f.getSize2D()));
+		b.setToolTipText(I18N.get(size.text_id + ".tooltip"));
+		b.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				updateRenderer(size);
+			}
+		});
+		return b;
+	}
+
+	/** Update cell renderer size */
+	private void updateRenderer(CellRendererSize size) {
+		manager.setCellSize(size);
+		p_list.setCellRenderer(manager.createCellRenderer());
+	}
+
+	/** Create style buttons */
+	private StyleButton[] createStyleButtons() {
 		ProxyTheme<T> theme = manager.getTheme();
 		List<Style> styles = theme.getStyles();
-		p_list = manager.createList();
-		p_list.setCellRenderer(manager.createCellRenderer());
-		JScrollPane sp = new JScrollPane(p_list);
-		final int n_rows = (styles.size() - 1) / STYLE_COLS + 1;
-		// grid is filled top to bottom, left to right
-		for (int i = 0; i < styles.size() ; i++) {
-			int col = i / n_rows;
-			int row = i % n_rows;
+		ButtonGroup bg = new ButtonGroup();
+		int n_styles = styles.size();
+		StyleButton[] btns = new StyleButton[n_styles];
+		for (int i = 0; i < btns.length; i++) {
 			Style sty = styles.get(i);
-			StyleWidgets sw = new StyleWidgets(sty,
+			final ItemStyle i_style = ItemStyle.lookupStyle(
+				sty.toString());
+			StyleButton btn = new StyleButton(i_style,
 				theme.getLegend(sty));
-			widgets.put(sty.toString(), sw);
-			bag.gridx = col * GRID_COLS;
-			bag.gridy = row;
-			bag.insets = new Insets(0, 0, 0, 2);
-			bag.anchor = GridBagConstraints.EAST;
-			add(sw.legend_lbl, bag);
-			bag.gridx = GridBagConstraints.RELATIVE;
-			add(sw.count_lbl, bag);
-			bag.anchor = GridBagConstraints.WEST;
-			add(sw.btn, bag);
+			btn.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent a) {
+					setStyleAction(i_style);
+				}
+			});
+			btns[i] = btn;
+			bg.add(btn);
 		}
-
-		// add space right of each column (except last)
-		for(int c = 1; c < STYLE_COLS; c++) {
-			bag.gridx = c * GRID_COLS - 1;
-			bag.gridy = 0;
-			bag.fill = GridBagConstraints.HORIZONTAL;
-			bag.weightx = 0.1f;
-			bag.insets = new Insets(2, 2, 2, 2);
-			add(new JLabel(), bag);
-		}
-
-		// add optional panel with cell size selection buttons
-		if (enableCellSizeBtns) {
-			bag.gridx = 0;
-			bag.gridwidth = 1;
-			bag.gridheight = 1;
-			bag.insets = new Insets(8, 2, 8, 2);
-			bag.gridy = n_rows + 1;
-			add(createCellSizePanel(), bag);
-		}
-
-		// add listbox
-		bag.gridx = (enableCellSizeBtns ? 1 : 0);
-		bag.gridwidth = GridBagConstraints.REMAINDER;
-		bag.insets = new Insets(8, 0, 0, 0);
-		bag.gridy = n_rows + 1;
-		bag.weightx = 1;
-		bag.weighty = 1;
-		bag.fill = GridBagConstraints.BOTH;
-		add(sp, bag);
-
-		setStyle(def_style);
+		return btns;
 	}
 
 	/** Initialize the style summary */
 	public void initialize() {
 		manager.getCache().addProxyListener(counter);
+		p_list.setCellRenderer(manager.createCellRenderer());
+		layoutPanel();
+		setStyle(def_style);
 	}
 
-	/** Create a radio button for the given style list model */
-	private JRadioButton createRadioButton(final ItemStyle i_style) {
-		final JRadioButton btn = new JRadioButton(i_style.toString());
-		r_buttons.add(btn);
-		btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setStyleAction(i_style);
+	/** Layout the panel */
+	private void layoutPanel() {
+		GroupLayout gl = new GroupLayout(this);
+		gl.setHonorsVisibility(false);
+		gl.setAutoCreateGaps(false);
+		gl.setAutoCreateContainerGaps(false);
+		gl.setHorizontalGroup(createHorizontalGroup(gl));
+		gl.setVerticalGroup(createVerticalGroup(gl));
+		setLayout(gl);
+	}
+
+	/** Create the horizontal group */
+	private GroupLayout.Group createHorizontalGroup(GroupLayout gl) {
+		GroupLayout.ParallelGroup hg = gl.createParallelGroup();
+		GroupLayout.SequentialGroup sg = gl.createSequentialGroup();
+		if (sz_btns != null) {
+			GroupLayout.ParallelGroup sz = gl.createParallelGroup();
+			for (JToggleButton b : sz_btns)
+				sz.addComponent(b);
+			sg.addGroup(sz);
+		}
+		int n_rows = (buttons.length - 1) / STYLE_COLS + 1;
+		for (int c = 0; c < STYLE_COLS; c++) {
+			sg.addGap(UI.hgap);
+			GroupLayout.ParallelGroup cg = gl.createParallelGroup();
+			for (int r = 0; r < n_rows; r++) {
+				int i = c * n_rows + r;
+				if (i < buttons.length)
+					cg.addComponent(buttons[i]);
 			}
-		});
-		return btn;
+			sg.addGroup(cg);
+		}
+		hg.addGroup(sg);
+		hg.addComponent(s_pane);
+		return hg;
+	}
+
+	/** Create the vertical group */
+	private GroupLayout.Group createVerticalGroup(GroupLayout gl) {
+		GroupLayout.SequentialGroup vg = gl.createSequentialGroup();
+		GroupLayout.ParallelGroup pg = gl.createParallelGroup();
+		if (sz_btns != null) {
+			GroupLayout.SequentialGroup sz =
+				gl.createSequentialGroup();
+			sz.addComponent(sz_btns[0]);
+			sz.addGap(UI.vgap);
+			sz.addComponent(sz_btns[1]);
+			sz.addGap(UI.vgap);
+			sz.addComponent(sz_btns[2]);
+			pg.addGroup(sz);
+		}
+		GroupLayout.SequentialGroup bg = gl.createSequentialGroup();
+		int n_rows = (buttons.length - 1) / STYLE_COLS + 1;
+		for (int r = 0; r < n_rows; r++) {
+			if (r > 0)
+				bg.addGap(UI.vgap);
+			GroupLayout.ParallelGroup rg = gl.createParallelGroup();
+			for (int c = 0; c < STYLE_COLS; c++) {
+				int i = c * n_rows + r;
+				if (i < buttons.length)
+					rg.addComponent(buttons[i]);
+			}
+			bg.addGroup(rg);
+		}
+		pg.addGroup(bg);
+		vg.addGroup(pg);
+		vg.addGap(UI.vgap);
+		vg.addComponent(s_pane);
+		return vg;
 	}
 
 	/** Update the count labels for each style status */
@@ -235,68 +277,32 @@ public class StyleSummary<T extends SonarObject> extends JPanel {
 	/** Update the counts for each style status.  Must be synchronized
 	 * in case multiple IWorkers are created. */
 	private synchronized void doUpdateCounts() {
-		for (StyleWidgets sw: widgets.values())
-			sw.n_count = 0;
-		for (T proxy: manager.getCache()) {
-			for (StyleWidgets sw: widgets.values())
-				sw.countProxy(proxy);
+		for (StyleButton btn : buttons)
+			btn.n_count = 0;
+		for (T proxy : manager.getCache()) {
+			for (StyleButton btn : buttons) {
+				if (manager.checkStyle(btn.i_style, proxy))
+					btn.n_count++;
+			}
 		}
 	}
 
 	/** Update the count labels.  Must be synchronized in case multiple
 	 * IWorkers are created. */
 	private synchronized void updateCountLabels() {
-		for (StyleWidgets sw: widgets.values())
-			sw.updateCountLabel();
+		for (StyleButton btn : buttons) {
+			btn.setText(Integer.toString(btn.n_count) + ' ' +
+				btn.i_style);
+		}
 	}
 
-	/** Create the optional panel that contains cell size buttons. */
-	private JPanel createCellSizePanel() {
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(3, 1, 0, 4));
-		AbstractButton bs = createSizeButton(CellRendererSize.SMALL);
-		AbstractButton bm = createSizeButton(CellRendererSize.MEDIUM);
-		AbstractButton bl = createSizeButton(CellRendererSize.LARGE);
-		panel.add(bs);
-		panel.add(bm);
-		panel.add(bl);
-		ButtonGroup bgroup = new ButtonGroup();
-		bgroup.add(bs);
-		bgroup.add(bm);
-		bgroup.add(bl);
-		bl.setSelected(true);
-		return panel;
-	}
-
-	/** Create a toggle button for changing cell renderer size */
-	private AbstractButton createSizeButton(final CellRendererSize size) {
-		String label = I18N.get(size.text_id);
-		JToggleButton b = new JToggleButton(label);
-		b.setMargin(new Insets(1, 1, 1, 1));
-		Font f = b.getFont();
-		b.setFont(f.deriveFont(0.8f * f.getSize2D()));
-		b.setToolTipText(I18N.get(size.text_id + ".tooltip"));
-		b.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				updateRenderer(size);
-			}
-		});
-		return b;
-	}
-
-	/** The cell renderer has changed sizes, update cell renderers. */
-	private void updateRenderer(CellRendererSize size) {
-		manager.setCellSize(size);
-		p_list.setCellRenderer(manager.createCellRenderer());
-	}
-
-	/** Set the selected style, results in action + button selection
-	 * changes. */
+	/** Set the selected style */
 	private void setStyle(ItemStyle i_style) {
-		StyleWidgets sw = widgets.get(i_style.toString());
-		if (sw != null) {
-			sw.btn.setSelected(true);
-			setStyleAction(i_style);
+		for (StyleButton btn : buttons) {
+			if (i_style == btn.i_style) {
+				btn.setSelected(true);
+				setStyleAction(i_style);
+			}
 		}
 	}
 
@@ -304,9 +310,6 @@ public class StyleSummary<T extends SonarObject> extends JPanel {
 	private void setStyleAction(ItemStyle i_style) {
 		String t = I18N.get(manager.getSonarType()) + " " +
 			I18N.get("device.status") + ": " + i_style;
-		border.setTitle(t);
-		// Force the border title to be repainted
-		repaint();
 		StyleListModel<T> mdl = model;
 		model = manager.getStyleModel(i_style.toString());
 		// JList.setModel clears the selection, so let's use
