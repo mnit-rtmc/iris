46de4c1c4d70bd7897d3dce3a01ea356e64a7c03
Douglas Lau <doug.lau@state.mn.us>
2016-05-13 10:18:31 -0500

    Cleaned up CameraImpl.

--- src/us/mn/state/dot/tms/server/CameraImpl.java
+++ src/us/mn/state/dot/tms/server/CameraImpl.java
@@ -54,7 +54,7 @@ public class CameraImpl extends DeviceImpl implements Camera {
 			"FROM iris." + SONAR_TYPE + ";", new ResultFactory()
 		{
 			public void create(ResultSet row) throws Exception {
-				namespace.addObject(new CameraImpl(namespace,
+				namespace.addObject(new CameraImpl(
 					row.getString(1),	// name
 					row.getString(2),	// geo_loc
 					row.getString(3),	// controller
@@ -70,6 +70,7 @@ public class CameraImpl extends DeviceImpl implements Camera {
 	}
 
 	/** Get a mapping of the columns */
+	@Override
 	public Map<String, Object> getColumns() {
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		map.put("name", name);
@@ -85,11 +86,13 @@ public class CameraImpl extends DeviceImpl implements Camera {
 	}
 
 	/** Get the database table name */
+	@Override
 	public String getTable() {
 		return "iris." + SONAR_TYPE;
 	}
 
 	/** Get the SONAR type name */
+	@Override
 	public String getTypeName() {
 		return SONAR_TYPE;
 	}
@@ -103,7 +106,7 @@ public class CameraImpl extends DeviceImpl implements Camera {
 	}
 
 	/** Create a camera */
-	protected CameraImpl(String n, GeoLocImpl l, ControllerImpl c, int p,
+	private CameraImpl(String n, GeoLocImpl l, ControllerImpl c, int p,
 		String nt, String e, int ec, int et, boolean pb)
 	{
 		super(n, c, p, nt);
@@ -116,74 +119,80 @@ public class CameraImpl extends DeviceImpl implements Camera {
 	}
 
 	/** Create a camera */
-	protected CameraImpl(Namespace ns, String n, String l, String c,
+	private CameraImpl(String n, String l, String c,
 		int p, String nt, String e, int ec, int et, boolean pb)
 	{
-		this(n, (GeoLocImpl)ns.lookupObject(GeoLoc.SONAR_TYPE, l),
-			(ControllerImpl)ns.lookupObject(Controller.SONAR_TYPE,
-			c), p, nt, e, ec, et, pb);
+		this(n, lookupGeoLoc(l), lookupController(c), p, nt, e, ec, et,
+		     pb);
 	}
 
 	/** Destroy an object */
+	@Override
 	public void doDestroy() throws TMSException {
 		super.doDestroy();
 		geo_loc.notifyRemove();
 	}
 
 	/** Device location */
-	protected GeoLocImpl geo_loc;
+	private GeoLocImpl geo_loc;
 
 	/** Get the device location */
+	@Override
 	public GeoLoc getGeoLoc() {
 		return geo_loc;
 	}
 
 	/** Host (and port) of encoder for digital video stream */
-	protected String encoder = "";
+	private String encoder = "";
 
 	/** Set the video encoder host name (and port) */
+	@Override
 	public void setEncoder(String enc) {
 		encoder = enc;
 	}
 
 	/** Set the video encoder host name (and port) */
 	public void doSetEncoder(String enc) throws TMSException {
-		if(enc.equals(encoder))
+		if (enc.equals(encoder))
 			return;
 		store.update(this, "encoder", enc);
 		setEncoder(enc);
 	}
 
 	/** Get the video encoder host name (and port) */
+	@Override
 	public String getEncoder() {
 		return encoder;
 	}
 
 	/** Input channel for video stream on encoder */
-	protected int encoder_channel;
+	private int encoder_channel;
 
 	/** Set the input channel on the encoder */
+	@Override
 	public void setEncoderChannel(int c) {
 		encoder_channel = c;
 	}
 
 	/** Set the input channel on the encoder */
 	public void doSetEncoderChannel(int c) throws TMSException {
-		if(c == encoder_channel)
+		if (c == encoder_channel)
 			return;
 		store.update(this, "encoder_channel", c);
 		setEncoderChannel(c);
 	}
 
 	/** Get the input channel on the encoder */
+	@Override
 	public int getEncoderChannel() {
 		return encoder_channel;
 	}
 
 	/** Encoder type */
-	protected EncoderType encoder_type = EncoderType.NONE;
+	private EncoderType encoder_type = EncoderType.NONE;
 
 	/** Set the encoder type */
+	@Override
 	public void setEncoderType(int et) {
 		encoder_type = EncoderType.fromOrdinal(et);
 	}
@@ -191,45 +200,47 @@ public class CameraImpl extends DeviceImpl implements Camera {
 	/** Set the encoder type */
 	public void doSetEncoderType(int t) throws TMSException {
 		EncoderType et = EncoderType.fromOrdinal(t);
-		if(et == encoder_type)
+		if (et == encoder_type)
 			return;
 		store.update(this, "encoder_type", t);
 		setEncoderType(t);
 	}
 
 	/** Get the encoder type */
+	@Override
 	public int getEncoderType() {
 		return encoder_type.ordinal();
 	}
 
 	/** Flag to allow publishing camera images */
-	protected boolean publish;
+	private boolean publish;
 
 	/** Set flag to allow publishing camera images */
+	@Override
 	public void setPublish(boolean p) {
 		publish = p;
 	}
 
 	/** Set flag to allow publishing camera images */
 	public void doSetPublish(boolean p) throws TMSException {
-		if(p == publish)
+		if (p == publish)
 			return;
 		store.update(this, "publish", p);
 		setPublish(p);
-		if(!p)
+		if (!p)
 			blankRestrictedMonitors();
 	}
 
 	/** Blank restricted video monitors viewing the camera */
 	private void blankRestrictedMonitors() throws TMSException {
 		Iterator<VideoMonitor> it = VideoMonitorHelper.iterator();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			VideoMonitor m = it.next();
-			if(m instanceof VideoMonitorImpl) {
-				VideoMonitorImpl vm = (VideoMonitorImpl)m;
-				if(vm.getRestricted()) {
+			if (m instanceof VideoMonitorImpl) {
+				VideoMonitorImpl vm = (VideoMonitorImpl) m;
+				if (vm.getRestricted()) {
 					Camera c = vm.getCamera();
-					if(c == this || c == null)
+					if (c == this || c == null)
 						vm.setCameraNotify(null);
 				}
 			}
@@ -237,6 +248,7 @@ public class CameraImpl extends DeviceImpl implements Camera {
 	}
 
 	/** Get flag to allow publishing camera images */
+	@Override
 	public boolean getPublish() {
 		return publish;
 	}
@@ -244,7 +256,7 @@ public class CameraImpl extends DeviceImpl implements Camera {
 	/** Clear the camera failed status */
 	public void clearFailed() {
 		ControllerImpl ctl = controller;
-		if(ctl != null)
+		if (ctl != null)
 			ctl.setFailed(false);
 	}
 
@@ -283,16 +295,18 @@ public class CameraImpl extends DeviceImpl implements Camera {
 	}
 
 	/** Command the camera to store a preset */
+	@Override
 	public void setStorePreset(int preset) {
 		CameraPoller cp = getCameraPoller();
-		if(cp != null)
+		if (cp != null)
 			cp.sendStorePreset(this, preset);
 	}
 
 	/** Command the camera to recall a preset */
+	@Override
 	public void setRecallPreset(int preset) {
 		CameraPoller cp = getCameraPoller();
-		if(cp != null)
+		if (cp != null)
 			cp.sendRecallPreset(this, preset);
 	}
 
