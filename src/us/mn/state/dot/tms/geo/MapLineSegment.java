dc02dd0b13d55f5b3b569712b0a2dde0f6b30921
Douglas Lau <doug.lau@state.mn.us>
2016-09-27 13:48:25 -0500

    Cleaned up MapLineSegment.

--- src/us/mn/state/dot/tms/geo/MapLineSegment.java
+++ src/us/mn/state/dot/tms/geo/MapLineSegment.java
@@ -22,16 +22,16 @@ package us.mn.state.dot.tms.geo;
 public class MapLineSegment {
 
 	/** X-coordinate of point A */
-	protected final double ax;
+	private final double ax;
 
 	/** Y-coordinate of point A */
-	protected final double ay;
+	private final double ay;
 
 	/** X-coordinate of point B */
-	protected final double bx;
+	private final double bx;
 
 	/** Y-coordinate of point B */
-	protected final double by;
+	private final double by;
 
 	/** Create a new map line segment */
 	public MapLineSegment(double x0, double y0, double x1, double y1) {
@@ -47,13 +47,13 @@ public class MapLineSegment {
 		// then the nearest point on the segment is b.
 		MapVector ab = new MapVector(bx - ax, by - ay);
 		MapVector bc = new MapVector(cx - bx, cy - by);
-		if(ab.dot(bc) > 0)
+		if (ab.dot(bc) > 0)
 			return bc.getMagnitude();
 		// If the dot product of ba and ac is greater than zero,
 		// then the nearest point on the segment is a.
 		MapVector ba = new MapVector(ax - bx, ay - by);
 		MapVector ac = new MapVector(cx - ax, cy - ay);
-		if(ba.dot(ac) > 0)
+		if (ba.dot(ac) > 0)
 			return ac.getMagnitude();
 		// Otherwise, the nearest point on the segment is between
 		// a and b, so calculate the point-line distance.
@@ -66,13 +66,13 @@ public class MapLineSegment {
 		// then the nearest point on the segment is b.
 		MapVector ab = new MapVector(bx - ax, by - ay);
 		MapVector bc = new MapVector(cx - bx, cy - by);
-		if(ab.dot(bc) > 0)
+		if (ab.dot(bc) > 0)
 			return new MapVector(bx, by);
 		// If the dot product of ba and ac is greater than zero,
 		// then the nearest point on the segment is a.
 		MapVector ba = new MapVector(ax - bx, ay - by);
 		MapVector ac = new MapVector(cx - ax, cy - ay);
-		if(ba.dot(ac) > 0)
+		if (ba.dot(ac) > 0)
 			return new MapVector(ax, ay);
 		// The nearest point is somewhere between a and b, so just
 		// project c onto the line fromed by a and b.
