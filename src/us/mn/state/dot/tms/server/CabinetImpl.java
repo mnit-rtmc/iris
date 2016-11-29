febe7a2e94ddbc51cf8b01513022b7ff1f600e03
Douglas Lau <doug.lau@state.mn.us>
2016-11-29 09:36:53 -0600

    Cleaned up CabinetImpl.

--- src/us/mn/state/dot/tms/server/CabinetImpl.java
+++ src/us/mn/state/dot/tms/server/CabinetImpl.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2008-2013  Minnesota Department of Transportation
+ * Copyright (C) 2008-2016  Minnesota Department of Transportation
  * Copyright (C) 2014  AHMCT, University of California
  *
  * This program is free software; you can redistribute it and/or modify
@@ -50,6 +50,7 @@ public class CabinetImpl extends BaseObjectImpl implements Cabinet {
 	}
 
 	/** Get a mapping of the columns */
+	@Override
 	public Map<String, Object> getColumns() {
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		map.put("name", name);
@@ -59,11 +60,13 @@ public class CabinetImpl extends BaseObjectImpl implements Cabinet {
 	}
 
 	/** Get the database table name */
+	@Override
 	public String getTable() {
 		return "iris." + SONAR_TYPE;
 	}
 
 	/** Get the SONAR type name */
+	@Override
 	public String getTypeName() {
 		return SONAR_TYPE;
 	}
@@ -77,49 +80,52 @@ public class CabinetImpl extends BaseObjectImpl implements Cabinet {
 	}
 
 	/** Create a new cabinet */
-	protected CabinetImpl(String n, CabinetStyle s, GeoLocImpl l) {
+	private CabinetImpl(String n, CabinetStyle s, GeoLocImpl l) {
 		super(n);
 		style = s;
 		geo_loc = l;
 	}
 
 	/** Create a new cabinet */
-	protected CabinetImpl(Namespace ns, String n, String s, String l)
-	{
-		this(n, (CabinetStyle)ns.lookupObject( CabinetStyle.SONAR_TYPE,
-		     s), (GeoLocImpl)ns.lookupObject(GeoLoc.SONAR_TYPE, l));
+	private CabinetImpl(Namespace ns, String n, String s, String l) {
+		this(n, (CabinetStyle) ns.lookupObject(CabinetStyle.SONAR_TYPE,
+		     s), lookupGeoLoc(l));
 	}
 
 	/** Cabinet style */
-	protected CabinetStyle style;
+	private CabinetStyle style;
 
 	/** Set the cabinet style */
+	@Override
 	public void setStyle(CabinetStyle s) {
 		style = s;
 	}
 
 	/** Set the cabinet style */
 	public void doSetStyle(CabinetStyle s) throws TMSException {
-		if(s == style)
-			return;
-		store.update(this, "style", s);
-		setStyle(s);
+		if (s != style) {
+			store.update(this, "style", s);
+			setStyle(s);
+		}
 	}
 
 	/** Get the cabinet style */
+	@Override
 	public CabinetStyle getStyle() {
 		return style;
 	}
 
 	/** Cabinet location */
-	protected GeoLocImpl geo_loc;
+	private GeoLocImpl geo_loc;
 
 	/** Get the cabinet location */
+	@Override
 	public GeoLoc getGeoLoc() {
 		return geo_loc;
 	}
 
 	/** Destroy an object */
+	@Override
 	public void doDestroy() throws TMSException {
 		super.doDestroy();
 		geo_loc.notifyRemove();
