87556003678078c8bb9b9ba1ad6cd4f4b7a55563
Douglas Lau <doug.lau@state.mn.us>
2018-01-03 13:00:02 -0600

    Removed an unused import.

--- src/us/mn/state/dot/tms/server/SampleArchiveFactoryImpl.java
+++ src/us/mn/state/dot/tms/server/SampleArchiveFactoryImpl.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2010-2014  Minnesota Department of Transportation
+ * Copyright (C) 2010-2018  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,7 +18,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
 import us.mn.state.dot.sched.TimeSteward;
-import us.mn.state.dot.tms.SystemAttrEnum;
 
 /**
  * Factory for creating sample archive files.
