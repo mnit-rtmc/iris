c3f27db3b20187e961c74e29306731cb43068dda
Douglas Lau <doug.lau@state.mn.us>
2018-03-29 09:54:59 -0500

    Do not remove unconfirmed incidents immediately when they are cleared.

--- src/us/mn/state/dot/tms/client/incident/IncidentDispatcher.java
+++ src/us/mn/state/dot/tms/client/incident/IncidentDispatcher.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2009-2017  Minnesota Department of Transportation
+ * Copyright (C) 2009-2018  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -160,8 +160,6 @@ public class IncidentDispatcher extends IPanel
 			if (inc != null) {
 				boolean c = clear_btn.isSelected();
 				inc.setCleared(c);
-				if (c && !inc.getConfirmed())
-					inc.destroy();
 			}
 		}
 	};
