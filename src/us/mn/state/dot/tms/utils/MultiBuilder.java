00731cf0fc76f922c03b537ad8c9583534748208
Douglas Lau <doug.lau@state.mn.us>
2018-04-03 15:10:56 -0500

    Updated copyright notice.

--- src/us/mn/state/dot/tms/utils/MultiBuilder.java
+++ src/us/mn/state/dot/tms/utils/MultiBuilder.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2006-2016  Minnesota Department of Transportation
+ * Copyright (C) 2006-2018  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
