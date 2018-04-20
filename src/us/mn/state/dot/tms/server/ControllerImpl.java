ca05c103db5ec82d49aff0d36b8d27f8315bef46
Douglas Lau <doug.lau@state.mn.us>
2018-04-20 11:08:01 -0500

    Changed devices/controllers to be "active" only if polling is enabled on comm_link.

--- src/us/mn/state/dot/tms/server/ControllerImpl.java
+++ src/us/mn/state/dot/tms/server/ControllerImpl.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2017  Minnesota Department of Transportation
+ * Copyright (C) 2000-2018  Minnesota Department of Transportation
  * Copyright (C) 2011  Berkeley Transportation Systems Inc.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -313,9 +313,15 @@ public class ControllerImpl extends BaseObjectImpl implements Controller {
 		return condition.ordinal();
 	}
 
-	/** Check if condition is active */
+	/** Check if condition is active (and polling enabled) */
 	public boolean isActive() {
-		return condition == CtrlCondition.ACTIVE;
+		return (CtrlCondition.ACTIVE == condition) && isPollEnabled();
+	}
+
+	/** Is comm link polling enabled? */
+	private boolean isPollEnabled() {
+		CommLinkImpl cl = comm_link;
+		return (cl != null) && cl.getPollEnabled();
 	}
 
 	/** Access password */
