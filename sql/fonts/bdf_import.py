067a379e5f3815eefe6ade2e1137d76ceb715be2
Douglas Lau <doug.lau@state.mn.us>
2018-09-06 16:46:38 -0500

    Added python script to allow importing BDF fonts.

---
+++ sql/fonts/bdf_import.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+
+'''This simple script imports a font in BDF format and creates a series
+of SQL statements to import the font into IRIS.
+
+BDF fonts can be created by converting from OTF with otf2bdf:
+
+otf2bdf -l "32_90" -p 27 /usr/share/fonts/overpass/overpass-regular.otf
+otf2bdf -l "32_90" -p 26 /usr/share/fonts/urw-base35/NimbusSansNarrow-Regular.otf
+'''
+
+from sys import argv, exit
+from base64 import b64encode
+from binascii import unhexlify
+
+HEADER = """\set ON_ERROR_STOP
+SET SESSION AUTHORIZATION 'tms';
+BEGIN;
+"""
+HFONT = """INSERT INTO iris.font (name, f_number, height, width, line_spacing,
+    char_spacing, version_id) VALUES ('%s', %s, %s, %s, %s, %s, %s);
+"""
+GRAPHIC = """INSERT INTO iris.graphic (name, color_scheme, height, width, pixels)
+    VALUES ('%s_%s', 1, %s, %s, '%s');"""
+GLYPH = """INSERT INTO iris.glyph (name, font, code_point, graphic)
+    VALUES ('%s_%s', '%s', %s, '%s_%s');"""
+FOOTER = """
+COMMIT;"""
+
+def set_pixel(bmap, width, row, col):
+    if col < width:
+        pos = row * width + col
+        b8 = pos // 8
+        bit = 7 - pos % 8
+        b = bmap[b8] | (1 << bit)
+        bmap[b8] = b
+
+def create_glyph_sql(lines, fname, height, char_spacing):
+    _, c_point = next(lines).split(' ')
+    _, swidth, _ = next(lines).split(' ')
+    _, dwidth, _ = next(lines).split(' ')
+    _, w, h, xd, yd = next(lines).split(' ')
+    if 'BITMAP' != next(lines):
+        return
+    width = max(int(dwidth), int(w))
+    n = (width * height + 7) // 8
+    bmap = bytearray(n)
+    for i in range(len(bmap)):
+        bmap[i] = 0
+    row = height - (int(h) + int(yd))
+    while True:
+        b = next(lines)
+        if 'ENDCHAR' == b:
+            break
+        if row < 0 or row >= height:
+            row += 1
+            continue
+        for x4 in range(len(b)):
+            hx = int(b[x4], 16)
+            if hx & 0b1000:
+                set_pixel(bmap, width, row, x4 * 4)
+            if hx & 0b0100:
+                set_pixel(bmap, width, row, x4 * 4 + 1)
+            if hx & 0b0010:
+                set_pixel(bmap, width, row, x4 * 4 + 2)
+            if hx & 0b0001:
+                set_pixel(bmap, width, row, x4 * 4 + 3)
+        row += 1
+    print (GRAPHIC % (fname, c_point, height, width,
+        b64encode(bmap).decode('ASCII')))
+    print (GLYPH % (fname, c_point, fname, c_point, fname, c_point))
+
+def create_font_sql(lines):
+    print (HEADER)
+    f_num = 21
+    name = '26_nimbus'
+    version_id = '0'
+    line_height = 26
+    char_spacing = 5
+    line_spacing = '9'
+    print (HFONT % (name, f_num, line_height, 0, line_spacing, char_spacing,
+        version_id))
+    while True:
+        v = next(lines)
+        if v.startswith('CHARS '):
+            break
+    while True:
+        v = next(lines)
+        if 'ENDFONT' == v:
+            break
+        if v.startswith('STARTCHAR'):
+            create_glyph_sql(lines, name, line_height, char_spacing)
+    print (FOOTER)
+
+if len(argv) != 2:
+    print ("Usage:\n./bdf_import.py [file.bdf]\n")
+    exit(1)
+create_font_sql(line.strip() for line in open(argv[1]))
