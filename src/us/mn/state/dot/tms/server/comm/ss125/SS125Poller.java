cc794a4555f5ddb5c8ad32ea2e829285245cb686
Douglas Lau <doug.lau@state.mn.us>
2018-11-29 09:16:13 -0600

    ss125: Only query samples when polling period matches binning interval.

--- src/us/mn/state/dot/tms/server/comm/ss125/SS125Poller.java
+++ src/us/mn/state/dot/tms/server/comm/ss125/SS125Poller.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2009-2016  Minnesota Department of Transportation
+ * Copyright (C) 2009-2018  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -61,6 +61,7 @@ public class SS125Poller extends ThreadedPoller<SS125Property>
  	 * @param p Sample period in seconds. */
 	@Override
 	public void querySamples(ControllerImpl c, int p) {
-		addOp(new OpQuerySamples(c, p));
+		if (c.getPollPeriod() == p)
+			addOp(new OpQuerySamples(c, p));
 	}
 }
