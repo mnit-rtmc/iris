51c67c87a3f00a07a48e9a052a366ab8fd5fb41f
Douglas Lau <doug.lau@state.mn.us>
2018-12-13 12:49:56 -0600

    ntcip: Added comments.

--- src/us/mn/state/dot/tms/server/comm/ntcip/mib1203/DmsMsgSourceMode.java
+++ src/us/mn/state/dot/tms/server/comm/ntcip/mib1203/DmsMsgSourceMode.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2000-2015  Minnesota Department of Transportation
+ * Copyright (C) 2000-2018  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,10 +24,10 @@ public enum DmsMsgSourceMode {
 	other,
 	local,
 	external,
-	otherCom1,
-	otherCom2,
-	otherCom3,
-	otherCom4,
+	otherCom1,	// retired in v2
+	otherCom2,	// retired in v2
+	otherCom3,	// retired in v2
+	otherCom4,	// retired in v2
 	central,
 	timebasedScheduler,
 	powerRecovery,
