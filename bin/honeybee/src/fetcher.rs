d405fbf593fdad6dd722c485d09258ab2269a9db
Douglas Lau <doug.lau@state.mn.us>
2019-03-22 07:50:40 -0500

    honeybee: Removed support for old NOTIFY scheme.

--- bin/honeybee/src/fetcher.rs
+++ bin/honeybee/src/fetcher.rs
@@ -66,8 +66,6 @@ fn db_thread(uds: String, tx: Sender<PathBuf>) -> Result<(), Error> {
     conn.execute("LISTEN sign_config", &[])?;
     conn.execute("LISTEN sign_detail", &[])?;
     conn.execute("LISTEN sign_message", &[])?;
-    // FIXME: remove this after DB has been updated
-    conn.execute("LISTEN tms", &[])?;
     // Initialize all the resources
     for r in resource::ALL {
         fetch_resource(&conn, &tx, r)?;
@@ -118,15 +116,10 @@ fn notify_loop(conn: &Connection, tx: Sender<PathBuf>) -> Result<(), Error> {
 
 /// Lookup resource from PostgreSQL notification channel / payload
 fn lookup_resource(chan: &str, payload: &str) -> Option<&'static Resource> {
-    // FIXME: remove this after DB has been updated
-    if chan == "tms" {
-        resource::lookup(payload)
-    } else {
-        match (chan, payload) {
-            ("dms", "msg_current") => Some(&resource::DMS_MSG_RES),
-            ("parking_area", "time_stamp") => Some(&resource::TPIMS_DYN_RES),
-            ("glyph", _) => Some(&resource::FONT_RES),
-            (_, _) => resource::lookup(chan),
-        }
+    match (chan, payload) {
+        ("dms", "msg_current") => Some(&resource::DMS_MSG_RES),
+        ("parking_area", "time_stamp") => Some(&resource::TPIMS_DYN_RES),
+        ("glyph", _) => Some(&resource::FONT_RES),
+        (_, _) => resource::lookup(chan),
     }
 }
