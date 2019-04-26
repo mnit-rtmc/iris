e9b6b20b05b39eca72514931327d35013815ca7a
Douglas Lau <doug.lau@state.mn.us>
2019-04-26 14:49:45 -0500

    honeybee: Forbid unsafe code.

--- bin/honeybee/src/bin/honeybee.rs
+++ bin/honeybee/src/bin/honeybee.rs
@@ -11,6 +11,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+#![forbid(unsafe_code)]
+
 #[macro_use]
 extern crate log;
 
