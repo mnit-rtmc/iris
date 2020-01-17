d36d8a9a7c3263fbf8afeb17e1ecd779059e85ca
Douglas Lau <doug.lau@state.mn.us>
2020-01-17 15:23:24 -0600

    sql: Renamed "flow" to "flow_stream"

--- sql/migrate-5.11.sql
+++ sql/migrate-5.11.sql
@@ -19,20 +19,20 @@ BEGIN
 END;
 $device_delete$ LANGUAGE plpgsql;
 
--- Add flow status look up table
-CREATE TABLE iris.flow_status (
+-- Add flow stream status look up table
+CREATE TABLE iris.flow_stream_status (
 	id INTEGER PRIMARY KEY,
 	description VARCHAR(8) NOT NULL
 );
 
-COPY iris.flow_status (id, description) FROM stdin;
+COPY iris.flow_stream_status (id, description) FROM stdin;
 0	FAILED
 1	STARTING
 2	PLAYING
 \.
 
--- Add video flow
-CREATE TABLE iris._flow (
+-- Add video flow stream
+CREATE TABLE iris._flow_stream (
 	name VARCHAR(12) PRIMARY KEY,
 	restricted BOOLEAN NOT NULL,
 	loc_overlay BOOLEAN NOT NULL,
@@ -41,43 +41,43 @@ CREATE TABLE iris._flow (
 	mon_num INTEGER,
 	address INET,
 	port INTEGER CHECK (port > 0 AND port <= 65535),
-	status INTEGER NOT NULL REFERENCES iris.flow_status,
+	status INTEGER NOT NULL REFERENCES iris.flow_stream_status,
 	CONSTRAINT camera_or_monitor CHECK (camera IS NULL OR mon_num IS NULL)
 );
 
-ALTER TABLE iris._flow ADD CONSTRAINT _flow_fkey
+ALTER TABLE iris._flow_stream ADD CONSTRAINT _flow_stream_fkey
 	FOREIGN KEY (name) REFERENCES iris._device_io(name) ON DELETE CASCADE;
 
-CREATE VIEW iris.flow AS
+CREATE VIEW iris.flow_stream AS
 	SELECT f.name, controller, pin, restricted, loc_overlay, quality,
 	       camera, mon_num, address, port, status
-	FROM iris._flow f JOIN iris._device_io d ON f.name = d.name;
+	FROM iris._flow_stream f JOIN iris._device_io d ON f.name = d.name;
 
-CREATE FUNCTION iris.flow_insert() RETURNS TRIGGER AS
-	$flow_insert$
+CREATE FUNCTION iris.flow_stream_insert() RETURNS TRIGGER AS
+	$flow_stream_insert$
 BEGIN
 	INSERT INTO iris._device_io (name, controller, pin)
 	     VALUES (NEW.name, NEW.controller, NEW.pin);
-	INSERT INTO iris._flow (name, restricted, loc_overlay, quality, camera,
-	                        mon_num, address, port, status)
+	INSERT INTO iris._flow_stream (name, restricted, loc_overlay, quality,
+	                               camera, mon_num, address, port, status)
 	     VALUES (NEW.name, NEW.restricted, NEW.loc_overlay, NEW.quality,
 	             NEW.camera, NEW.mon_num, NEW.address, NEW.port,NEW.status);
 	RETURN NEW;
 END;
-$flow_insert$ LANGUAGE plpgsql;
+$flow_stream_insert$ LANGUAGE plpgsql;
 
-CREATE TRIGGER flow_insert_trig
-    INSTEAD OF INSERT ON iris.flow
-    FOR EACH ROW EXECUTE PROCEDURE iris.flow_insert();
+CREATE TRIGGER flow_stream_insert_trig
+    INSTEAD OF INSERT ON iris.flow_stream
+    FOR EACH ROW EXECUTE PROCEDURE iris.flow_stream_insert();
 
-CREATE FUNCTION iris.flow_update() RETURNS TRIGGER AS
-	$flow_update$
+CREATE FUNCTION iris.flow_stream_update() RETURNS TRIGGER AS
+	$flow_stream_update$
 BEGIN
 	UPDATE iris._device_io
 	   SET controller = NEW.controller,
 	       pin = NEW.pin
 	 WHERE name = OLD.name;
-	UPDATE iris._flow
+	UPDATE iris._flow_stream
 	   SET restricted = NEW.restricted,
 	       loc_overlay = NEW.loc_overlay,
 	       quality = NEW.quality,
@@ -89,25 +89,25 @@ BEGIN
 	 WHERE name = OLD.name;
 	RETURN NEW;
 END;
-$flow_update$ LANGUAGE plpgsql;
+$flow_stream_update$ LANGUAGE plpgsql;
 
-CREATE TRIGGER flow_update_trig
-    INSTEAD OF UPDATE ON iris.flow
-    FOR EACH ROW EXECUTE PROCEDURE iris.flow_update();
+CREATE TRIGGER flow_stream_update_trig
+    INSTEAD OF UPDATE ON iris.flow_stream
+    FOR EACH ROW EXECUTE PROCEDURE iris.flow_stream_update();
 
-CREATE TRIGGER flow_delete_trig
-    INSTEAD OF DELETE ON iris.flow
+CREATE TRIGGER flow_stream_delete_trig
+    INSTEAD OF DELETE ON iris.flow_stream
     FOR EACH ROW EXECUTE PROCEDURE iris.device_delete();
 
-CREATE VIEW flow_view AS
+CREATE VIEW flow_stream_view AS
 	SELECT f.name, f.controller, pin, condition, comm_link, restricted,
 	       loc_overlay, eq.description AS quality, camera, mon_num, address,
 	       port, s.description AS status
-	FROM iris.flow f
-	JOIN iris.flow_status s ON f.status = s.id
+	FROM iris.flow_stream f
+	JOIN iris.flow_stream_status s ON f.status = s.id
 	LEFT JOIN controller_view ctr ON controller = ctr.name
 	LEFT JOIN iris.encoding_quality eq ON f.quality = eq.id;
-GRANT SELECT ON flow_view TO PUBLIC;
+GRANT SELECT ON flow_stream_view TO PUBLIC;
 
 -- Use device_delete instead of separate functions for delete triggers
 DROP TRIGGER camera_delete_trig ON iris.camera;
@@ -234,13 +234,13 @@ CREATE TRIGGER gate_arm_delete_trig
     INSTEAD OF DELETE ON iris.gate_arm
     FOR EACH ROW EXECUTE PROCEDURE iris.device_delete();
 
--- Add flow column to encoder stream
+-- Add flow_stream column to encoder stream
 DROP VIEW encoder_stream_view;
-ALTER TABLE iris.encoder_stream ADD COLUMN flow BOOLEAN;
-UPDATE iris.encoder_stream SET flow = false;
-ALTER TABLE iris.encoder_stream ALTER COLUMN flow SET NOT NULL;
+ALTER TABLE iris.encoder_stream ADD COLUMN flow_stream BOOLEAN;
+UPDATE iris.encoder_stream SET flow_stream = false;
+ALTER TABLE iris.encoder_stream ALTER COLUMN flow_stream SET NOT NULL;
 CREATE VIEW encoder_stream_view AS
-	SELECT es.name, encoder_type, make, model, config, view_num, flow,
+	SELECT es.name, encoder_type, make, model, config, view_num,flow_stream,
 	       enc.description AS encoding, eq.description AS quality,
 	       uri_scheme, uri_path, mcast_port, latency
 	FROM iris.encoder_stream es
--- sql/tms-template.sql
+++ sql/tms-template.sql
@@ -1388,7 +1388,7 @@ CREATE TABLE iris.encoder_stream (
 	name VARCHAR(8) PRIMARY KEY,
 	encoder_type VARCHAR(8) NOT NULL REFERENCES iris.encoder_type,
 	view_num INTEGER CHECK (view_num > 0 AND view_num <= 12),
-	flow BOOLEAN NOT NULL,
+	flow_stream BOOLEAN NOT NULL,
 	encoding INTEGER NOT NULL REFERENCES iris.encoding,
 	quality INTEGER NOT NULL REFERENCES iris.encoding_quality,
 	uri_scheme VARCHAR(8),
@@ -1403,7 +1403,7 @@ ALTER TABLE iris.encoder_stream
 	CHECK ((uri_scheme IS NULL AND uri_path IS NULL) OR mcast_port IS NULL);
 
 CREATE VIEW encoder_stream_view AS
-	SELECT es.name, encoder_type, make, model, config, view_num, flow,
+	SELECT es.name, encoder_type, make, model, config, view_num,flow_stream,
 	       enc.description AS encoding, eq.description AS quality,
 	       uri_scheme, uri_path, mcast_port, latency
 	FROM iris.encoder_stream es
@@ -4160,20 +4160,20 @@ CREATE VIEW video_monitor_view AS
 GRANT SELECT ON video_monitor_view TO PUBLIC;
 
 --
--- Video Flows
+-- Video Flow Streams
 --
-CREATE TABLE iris.flow_status (
+CREATE TABLE iris.flow_stream_status (
 	id INTEGER PRIMARY KEY,
 	description VARCHAR(8) NOT NULL
 );
 
-COPY iris.flow_status (id, description) FROM stdin;
+COPY iris.flow_stream_status (id, description) FROM stdin;
 0	FAILED
 1	STARTING
 2	PLAYING
 \.
 
-CREATE TABLE iris._flow (
+CREATE TABLE iris._flow_stream (
 	name VARCHAR(12) PRIMARY KEY,
 	restricted BOOLEAN NOT NULL,
 	loc_overlay BOOLEAN NOT NULL,
@@ -4182,43 +4182,43 @@ CREATE TABLE iris._flow (
 	mon_num INTEGER,
 	address INET,
 	port INTEGER CHECK (port > 0 AND port <= 65535),
-	status INTEGER NOT NULL REFERENCES iris.flow_status,
+	status INTEGER NOT NULL REFERENCES iris.flow_stream_status,
 	CONSTRAINT camera_or_monitor CHECK (camera IS NULL OR mon_num IS NULL)
 );
 
-ALTER TABLE iris._flow ADD CONSTRAINT _flow_fkey
+ALTER TABLE iris._flow_stream ADD CONSTRAINT _flow_stream_fkey
 	FOREIGN KEY (name) REFERENCES iris._device_io(name) ON DELETE CASCADE;
 
-CREATE VIEW iris.flow AS
+CREATE VIEW iris.flow_stream AS
 	SELECT f.name, controller, pin, restricted, loc_overlay, quality,
 	       camera, mon_num, address, port, status
-	FROM iris._flow f JOIN iris._device_io d ON f.name = d.name;
+	FROM iris._flow_stream f JOIN iris._device_io d ON f.name = d.name;
 
-CREATE FUNCTION iris.flow_insert() RETURNS TRIGGER AS
-	$flow_insert$
+CREATE FUNCTION iris.flow_stream_insert() RETURNS TRIGGER AS
+	$flow_stream_insert$
 BEGIN
 	INSERT INTO iris._device_io (name, controller, pin)
 	     VALUES (NEW.name, NEW.controller, NEW.pin);
-	INSERT INTO iris._flow (name, restricted, loc_overlay, quality, camera,
-	                        mon_num, address, port, status)
+	INSERT INTO iris._flow_stream (name, restricted, loc_overlay, quality,
+	                               camera, mon_num, address, port, status)
 	     VALUES (NEW.name, NEW.restricted, NEW.loc_overlay, NEW.quality,
 	             NEW.camera, NEW.mon_num, NEW.address, NEW.port,NEW.status);
 	RETURN NEW;
 END;
-$flow_insert$ LANGUAGE plpgsql;
+$flow_stream_insert$ LANGUAGE plpgsql;
 
-CREATE TRIGGER flow_insert_trig
-    INSTEAD OF INSERT ON iris.flow
-    FOR EACH ROW EXECUTE PROCEDURE iris.flow_insert();
+CREATE TRIGGER flow_stream_insert_trig
+    INSTEAD OF INSERT ON iris.flow_stream
+    FOR EACH ROW EXECUTE PROCEDURE iris.flow_stream_insert();
 
-CREATE FUNCTION iris.flow_update() RETURNS TRIGGER AS
-	$flow_update$
+CREATE FUNCTION iris.flow_stream_update() RETURNS TRIGGER AS
+	$flow_stream_update$
 BEGIN
 	UPDATE iris._device_io
 	   SET controller = NEW.controller,
 	       pin = NEW.pin
 	 WHERE name = OLD.name;
-	UPDATE iris._flow
+	UPDATE iris._flow_stream
 	   SET restricted = NEW.restricted,
 	       loc_overlay = NEW.loc_overlay,
 	       quality = NEW.quality,
@@ -4230,25 +4230,25 @@ BEGIN
 	 WHERE name = OLD.name;
 	RETURN NEW;
 END;
-$flow_update$ LANGUAGE plpgsql;
+$flow_stream_update$ LANGUAGE plpgsql;
 
-CREATE TRIGGER flow_update_trig
-    INSTEAD OF UPDATE ON iris.flow
-    FOR EACH ROW EXECUTE PROCEDURE iris.flow_update();
+CREATE TRIGGER flow_stream_update_trig
+    INSTEAD OF UPDATE ON iris.flow_stream
+    FOR EACH ROW EXECUTE PROCEDURE iris.flow_stream_update();
 
-CREATE TRIGGER flow_delete_trig
-    INSTEAD OF DELETE ON iris.flow
+CREATE TRIGGER flow_stream_delete_trig
+    INSTEAD OF DELETE ON iris.flow_stream
     FOR EACH ROW EXECUTE PROCEDURE iris.device_delete();
 
-CREATE VIEW flow_view AS
+CREATE VIEW flow_stream_view AS
 	SELECT f.name, f.controller, pin, condition, comm_link, restricted,
 	       loc_overlay, eq.description AS quality, camera, mon_num, address,
 	       port, s.description AS status
-	FROM iris.flow f
-	JOIN iris.flow_status s ON f.status = s.id
+	FROM iris.flow_stream f
+	JOIN iris.flow_stream_status s ON f.status = s.id
 	LEFT JOIN controller_view ctr ON controller = ctr.name
 	LEFT JOIN iris.encoding_quality eq ON f.quality = eq.id;
-GRANT SELECT ON flow_view TO PUBLIC;
+GRANT SELECT ON flow_stream_view TO PUBLIC;
 
 --
 -- Weather Sensors
