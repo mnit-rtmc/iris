9ff22d1763aab87d2f5f023022847493d6601532
Gordon Parikh <gparikh@srfconsulting.com>
2020-01-21 10:24:46 -0600

    Implemented page list (still a few minor cosmetic issues)

--- src/us/mn/state/dot/tms/client/wysiwyg/editor/WMsgEditorForm.java
+++ src/us/mn/state/dot/tms/client/wysiwyg/editor/WMsgEditorForm.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2017-2018  SRF Consulting Group
+ * Copyright (C) 2018-2019  SRF Consulting Group
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,9 +15,12 @@
 
 package us.mn.state.dot.tms.client.wysiwyg.editor;
 
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -26,7 +29,10 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -34,6 +40,10 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.ListCellRenderer;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import us.mn.state.dot.tms.DMS;
 import us.mn.state.dot.tms.DMSHelper;
@@ -50,6 +60,7 @@ import us.mn.state.dot.tms.client.dms.SignFacePanel;
 import us.mn.state.dot.tms.client.dms.SignPixelPanel;
 import us.mn.state.dot.tms.client.widget.AbstractForm;
 import us.mn.state.dot.tms.client.widget.IAction;
+import us.mn.state.dot.tms.client.widget.Icons;
 import us.mn.state.dot.tms.client.widget.Widgets;
 import us.mn.state.dot.tms.utils.I18N;
 import us.mn.state.dot.tms.utils.MultiString;
@@ -79,7 +90,15 @@ public class WMsgEditorForm extends AbstractForm {
 	private Map<String,DMS> dmsList = new HashMap<String,DMS>();
 	private String[] dmsNames;
 	private JComboBox<String> dms_list;
-	 
+	
+	/* Page List */
+	private JPanel page_btn_pnl;
+	private JButton page_add_btn;
+	private JButton page_del_btn;
+	private JButton page_mv_up_btn;
+	private JButton page_mv_down_btn;
+	private JList<WMsgSignPage> page_list;
+	
 	/** Buttons */
 	private JButton preview_btn;
 	private JButton cancel_btn;
@@ -88,27 +107,30 @@ public class WMsgEditorForm extends AbstractForm {
 	
 	public WMsgEditorForm(Session s) {
 		// TODO need to add the message name to the title (somehow...)
-		super(I18N.get("wysiwyg.editor.title"), true);
+		super(getWindowTitle(null), true);
 		session = s;
 		initForm();
 	}
 	
+	// TODO need to figure out how to deal with new QuickMessages (where/when/
+	// how is name created???)
+	
 	public WMsgEditorForm(Session s, DMS d) {
-		super(I18N.get("wysiwyg.editor.title"), true);
+		super(getWindowTitle(null), true);
 		session = s;
 		sign = d;
 		initForm();
 	}
 	
 	public WMsgEditorForm(Session s, SignGroup g) {
-		super(I18N.get("wysiwyg.editor.title"), true);
+		super(getWindowTitle(null), true);
 		session = s;
 		sg = g;
 		initForm();
 	}
 	
 	public WMsgEditorForm(Session s, QuickMessage q, DMS d) {
-		super(I18N.get("wysiwyg.editor.title"), true);
+		super(getWindowTitle(q), true);
 		session = s;
 		sign = d;
 		qm = q;
@@ -116,11 +138,17 @@ public class WMsgEditorForm extends AbstractForm {
 	}
 	
 	public WMsgEditorForm(Session s, QuickMessage q, SignGroup g) {
-		super(I18N.get("wysiwyg.editor.title"), true);
+		super(getWindowTitle(q), true);
 		session = s;
 		sg = g;
 		qm = q;
 		initForm();
+	}
+	
+	public static String getWindowTitle(QuickMessage q) {
+		String editorName = I18N.get("wysiwyg.editor.title");
+		String msgName = q != null ? q.getName() : "<Untitled>";
+		return String.format("%s - %s", editorName, msgName);
 	}
 	
 	protected void initForm() {
@@ -164,8 +192,26 @@ public class WMsgEditorForm extends AbstractForm {
 			dms_list.addActionListener(new SignSelectionListener());
 		}
 		
-		// TODO fill in placeholders (see placeholders in initialize() below)
+		/* Page buttons */
+		page_add_btn = new JButton(pageAdd);
+		ImageIcon pg_add_icon = Icons.getIconByPropName("wysiwyg.editor.page_add");
+		page_add_btn.setIcon(pg_add_icon);
+		page_add_btn.setHideActionText(true);
 		
+		page_del_btn = new JButton(pageDelete);
+		ImageIcon pg_del_icon = Icons.getIconByPropName("wysiwyg.editor.page_delete");
+		page_del_btn.setIcon(pg_del_icon);
+		page_del_btn.setHideActionText(true);
+		
+		page_mv_up_btn = new JButton(pageMoveUp);
+		ImageIcon pg_mv_up_icon = Icons.getIconByPropName("wysiwyg.editor.page_move_up");
+		page_mv_up_btn.setIcon(pg_mv_up_icon);
+		page_mv_up_btn.setHideActionText(true);
+		
+		page_mv_down_btn = new JButton(pageMoveDown);
+		ImageIcon pg_mv_down_icon = Icons.getIconByPropName("wysiwyg.editor.page_move_down");
+		page_mv_down_btn.setIcon(pg_mv_down_icon);
+		page_mv_down_btn.setHideActionText(true);
 		
 		/* Buttons - TODO finish implementing */
 		preview_btn = new JButton(preview);
@@ -190,8 +236,6 @@ public class WMsgEditorForm extends AbstractForm {
 		gbc.ipady = 0;
 		gbc.weightx = 0;
 		gbc.weighty = 0;
-		
-		/*** TODO need to add components below (and need to define above first) ***/
 		
 		/** TODO combine check box and label? (label would be on right) **/
 		/* Beacon Label */
@@ -242,28 +286,69 @@ public class WMsgEditorForm extends AbstractForm {
 		gbc.gridy = 2;
 		p.add(new JLabel(I18N.get("wysiwyg.editor.page_list")), gbc);
 		
-		/* Page List (may be a GBL itself) */
+		/* Page Buttons */
 		gbc.gridx = 0;
 		gbc.gridy = 3;
-		gbc.gridheight = 1;
 		gbc.gridwidth = 4;
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.anchor = GridBagConstraints.CENTER;
+		page_btn_pnl = new JPanel();
+		page_btn_pnl.setLayout(new FlowLayout(FlowLayout.CENTER));
+//		page_btn_pnl.add(Box.createHorizontalStrut(20));
+		page_btn_pnl.add(page_add_btn);
+		page_btn_pnl.add(page_del_btn);
+		page_btn_pnl.add(Box.createHorizontalStrut(20));
+		page_btn_pnl.add(page_mv_up_btn);
+		page_btn_pnl.add(page_mv_down_btn);
+//		page_btn_pnl.add(Box.createHorizontalStrut(20));
+		p.add(page_btn_pnl, gbc);
+		
+		/* Page List */
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		gbc.gridheight = 1;
 		gbc.weightx = 0.5;
 		gbc.weighty = 1;
 		gbc.fill = GridBagConstraints.BOTH;
+		gbc.anchor = GridBagConstraints.BASELINE_LEADING;
 		
 		// TODO PLACEHOLDER
 		
 		// TODO use MultiString object (see below) to get page information and
 		// pack into scroll pane - need to figure out how RasterGraphics work 
 		
-		p.add(new JScrollPane(new JList<String>(),
+		// TODO this needs to update when the sign changes (for sign groups)
+		
+//		updatePageList();
+		// reset the list
+//		page_btn_pnl = new JPanel();
+//		page_btn_pnl.setPreferredSize(new Dimension(100,200));
+		
+		// get the pages for the message
+		String ms = qm.getMulti();
+		MultiString mso = new MultiString(ms);
+		
+//		page_btn_pnl.setLayout(new GridLayout(mso.getNumPages(), 1));
+//		for (int i = 0; i < mso.getNumPages(); i++) {
+//			WMsgSignPage wm = new WMsgSignPage(sign, mso, i);
+//			SignFacePanel sf = new SignFacePanel();
+//			SignPixelPanel sp = sf.setSign(sign);
+//			sp.setGraphic(wm.getPageRaster());
+//			page_btn_pnl.add(sf);
+//		}
+		
+		updatePageList();
+		System.out.println(page_list.getModel().getSize());
+		p.add(new JScrollPane(page_list,
 				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER), gbc);
+		initPageSelector();
+//		p.add(new WMsgSignPagePanel(session, sign, mso, 0));
 		
 		/* Editor (will be a GBL itself) */
 		gbc.gridx = 4;
 		gbc.gridy = 2;
-		gbc.gridheight = 2;
+		gbc.gridheight = 3;
 		gbc.gridwidth = 6;
 		gbc.fill = GridBagConstraints.BOTH;
 
@@ -275,8 +360,8 @@ public class WMsgEditorForm extends AbstractForm {
 		SignFacePanel sfp = new SignFacePanel();
 		SignPixelPanel spp = sfp.setSign(sign);
 		RasterBuilder rb = DMSHelper.createRasterBuilder(sign);
-		String ms = qm.getMulti();
-		MultiString mso = new MultiString(ms);
+//		String ms = qm.getMulti();
+//		MultiString mso = new MultiString(ms);
 		
 		RasterGraphic[] rg = null;
 		try {
@@ -289,7 +374,7 @@ public class WMsgEditorForm extends AbstractForm {
 		
 		/* Preview Button */
 		gbc.gridx = 6;
-		gbc.gridy = 4;
+		gbc.gridy = 5;
 		gbc.gridheight = 1;
 		gbc.gridwidth = 1;
 		gbc.weightx = 0;
@@ -300,17 +385,17 @@ public class WMsgEditorForm extends AbstractForm {
 		
 		/* Cancel Button */
 		gbc.gridx = 7;
-		gbc.gridy = 4;
+		gbc.gridy = 5;
 		p.add(cancel_btn, gbc);
 		
 		/* Save As Button */
 		gbc.gridx = 8;
-		gbc.gridy = 4;
+		gbc.gridy = 5;
 		p.add(save_as_btn, gbc);
 		
 		/* Save Button */
 		gbc.gridx = 9;
-		gbc.gridy = 4;
+		gbc.gridy = 5;
 		p.add(save_btn, gbc);
 		
 		
@@ -322,6 +407,90 @@ public class WMsgEditorForm extends AbstractForm {
 
 		add(p);
 	}
+	
+	/** Initialize the page selection handler. */
+	private void initPageSelector() {
+		page_list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		class PageSelectionHandler implements ListSelectionListener {
+			public void valueChanged(ListSelectionEvent e) {
+				if (!e.getValueIsAdjusting()) {
+					ListSelectionModel lsm = (ListSelectionModel) e.getSource();
+					int indx = lsm.getMinSelectionIndex();
+					
+					if (indx != -1) {
+						WMsgSignPage sp = page_list.getModel().getElementAt(indx);
+						System.out.println(String.format("Selected page %s", sp.getPageNumberLabel()));
+					}
+				}
+			}
+		}
+		page_list.getSelectionModel().addListSelectionListener(new PageSelectionHandler());
+	}
+	
+	/** Update the page list from the selected/created message */
+	private void updatePageList() {
+		DefaultListModel model = new DefaultListModel();
+		
+		// get the pages for the message
+		String ms = qm.getMulti();
+		MultiString mso = new MultiString(ms);
+		for (int i = 0; i < mso.getNumPages(); i++) {
+			WMsgSignPage sp = new WMsgSignPage(sign, mso, i);
+			model.addElement(sp);
+		}
+
+		// reset the list
+		page_list = new JList<WMsgSignPage>(model);
+		
+		// set the renderer on the list
+		ListCellRenderer rndr = new WMsgSignPageListRenderer();
+		page_list.setCellRenderer(rndr);
+	}
+
+	/** Page Add action */
+	private final IAction pageAdd = new IAction("wysiwyg.editor.page_add") {
+		@SuppressWarnings("synthetic-access")
+		protected void doActionPerformed(ActionEvent e)
+			throws Exception
+		{
+			// TODO need to keep track of what page we're on somewhere
+			System.out.println("Adding page...");
+		}
+	};
+	
+	/** Page Delete action */
+	private final IAction pageDelete = new IAction("wysiwyg.editor.page_delete") {
+		@SuppressWarnings("synthetic-access")
+		protected void doActionPerformed(ActionEvent e)
+				throws Exception
+		{
+			// TODO need to keep track of what page we're on somewhere
+			System.out.println("Deleting page...");
+		}
+	};
+	
+	/** Page Move Up action */
+	private final IAction pageMoveUp = new IAction("wysiwyg.editor.page_move_up") {
+		@SuppressWarnings("synthetic-access")
+		protected void doActionPerformed(ActionEvent e)
+				throws Exception
+		{
+			// TODO need to keep track of what page we're on somewhere
+			System.out.println("Moving page up...");
+		}
+	};
+	
+	/** Page Move Down action */
+	private final IAction pageMoveDown = new IAction("wysiwyg.editor.page_move_down") {
+		@SuppressWarnings("synthetic-access")
+		protected void doActionPerformed(ActionEvent e)
+				throws Exception
+		{
+			// TODO need to keep track of what page we're on somewhere
+			System.out.println("Moving page down...");
+		}
+	};
 	
 	/** Preview action */
 	private final IAction preview = new IAction("wysiwyg.editor.preview") {
---
+++ src/us/mn/state/dot/tms/client/wysiwyg/editor/WMsgSignPageListRenderer.java
@@ -0,0 +1,161 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2018-2019  SRF Consulting Group
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.client.wysiwyg.editor;
+
+import static us.mn.state.dot.tms.client.widget.Widgets.UI;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.ListCellRenderer;
+
+import us.mn.state.dot.tms.DMS;
+import us.mn.state.dot.tms.DMSHelper;
+import us.mn.state.dot.tms.GeoLocHelper;
+import us.mn.state.dot.tms.client.dms.SignPixelPanel;
+import us.mn.state.dot.tms.client.widget.ILabel;
+
+/**
+ * Renderer for a DMS message page in a list.
+ *
+ * @author Gordon Parikh - SRF Consulting
+ */
+public class WMsgSignPageListRenderer extends JPanel implements ListCellRenderer<WMsgSignPage> {
+
+	/** List cell renderer (needed for colors) */
+	private final DefaultListCellRenderer cell =
+		new DefaultListCellRenderer();
+
+	/** Title panel */
+	private final JPanel title_pnl = new JPanel();
+
+	/** Sign ID label */
+	private final JLabel pgnum_lbl = new JLabel();
+
+	/** Sign pixel panel to display sign message */
+	private final SignPixelPanel pixel_pnl = new SignPixelPanel(50, 200);
+	
+	/** Location panel */
+	private final JPanel info_pnl = new JPanel();
+
+	/** Sign location label */
+	private final JLabel info_lbl = new JLabel();
+	
+	public WMsgSignPageListRenderer() {
+		setOpaque(true);
+		initialize();
+	}
+
+	/** Initialize a large size DMS cell renderer
+	 *  TODO alignment isn't doing what I want it to..... 
+	 *  */
+	private void initialize() {
+		setBorder(UI.cellRendererBorder());
+		setPreferredSize();
+		title_pnl.setLayout(new BoxLayout(title_pnl, BoxLayout.X_AXIS));
+		title_pnl.add(Box.createHorizontalGlue());
+		title_pnl.add(pgnum_lbl);
+//		title_pnl.setAlignmentX(Component.LEFT_ALIGNMENT);
+//		pgnum_lbl.setAlignmentX(Component.LEFT_ALIGNMENT);
+		info_pnl.setLayout(new BoxLayout(info_pnl, BoxLayout.X_AXIS));
+		info_pnl.add(Box.createHorizontalGlue());
+		info_pnl.add(info_lbl);
+//		info_pnl.setAlignmentX(Component.LEFT_ALIGNMENT);
+		add(title_pnl, BorderLayout.NORTH);
+		add(pixel_pnl, BorderLayout.CENTER);
+		add(info_pnl, BorderLayout.SOUTH);
+	}
+
+	private void setPreferredSize() {
+		Dimension pix_pnl_size = pixel_pnl.getPreferredSize();
+		int width = pix_pnl_size.width;
+		// set a dummy page number so we can get a preferred height for the 
+		// labels
+		pgnum_lbl.setText("Page 1");
+		int height = 4*pgnum_lbl.getPreferredSize().height + pix_pnl_size.height;
+		System.out.println(String.format("Width = %d, Height = %d", width, height));
+		setPreferredSize(new Dimension(width, height));
+	}
+	
+	/** Get a component configured to render a cell of the list */
+	@Override
+	public Component getListCellRendererComponent(JList<? extends WMsgSignPage> list,
+			WMsgSignPage sp, int index, boolean isSelected, boolean hasFocus)
+	{
+		if (isSelected) {
+			title_pnl.setBackground(list.getSelectionBackground());
+			pixel_pnl.setBackground(list.getSelectionBackground());
+			info_pnl.setBackground(list.getSelectionBackground());
+			setBackground(list.getSelectionBackground());
+//			title_pnl.setForeground(list.getSelectionForeground());
+		} else {
+			title_pnl.setBackground(pgnum_lbl.getBackground());
+			pixel_pnl.setBackground(pgnum_lbl.getBackground());
+			info_pnl.setBackground(pgnum_lbl.getBackground());
+			setBackground(pgnum_lbl.getBackground());
+//			title_pnl.setForeground(pgnum_lbl.getForeground());
+		}
+		renderSignPage(sp);
+		return this;
+	}
+
+	/** Render the Sign Page */
+	private void renderSignPage(WMsgSignPage sp) {
+		pgnum_lbl.setText(sp.getPageNumberLabel());
+		DMS dms = sp.getDMS();
+		pixel_pnl.setFilterColor(SignPixelPanel.filterColor(dms));
+		pixel_pnl.setDimensions(dms.getSignConfig());
+		pixel_pnl.setGraphic(sp.getPageRaster());
+		info_lbl.setText(sp.getPageInfo());
+	}
+	
+//	/** Get the table cell renderer component */
+//	@Override
+//	public Component getListCellRendererComponent(JList list, Object value,
+//		int index, boolean isSelected, boolean hasFocus)
+//	{
+//		JLabel lbl = (JLabel)super.getListCellRendererComponent(list,
+//			value, index, isSelected, hasFocus);
+//		Component c = null;
+//		if (value instanceof WMsgSignPage) {
+//			WMsgSignPage sp = (WMsgSignPage) value;
+////			lbl.setIcon(sp.getPageRaster());
+//			c = sp.getSignFacePanel();
+//		}
+//		
+//		
+////		} else
+////			lbl.setIcon(null);
+////		return lbl;
+//		
+//		if (isSelected) {
+//			setBackground(list.getSelectionBackground());
+//			setForeground(list.getSelectionForeground());
+//		} else {
+//			setBackground(list.getBackground());
+//			setForeground(list.getForeground());			
+//		}
+//		
+//		return lbl;
+//	}
+}
