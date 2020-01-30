d5e391888a31db77141c4d800f643670c3e6f78e
Gordon Parikh <gparikh@srfconsulting.com>
2020-01-30 13:54:03 -0600

    Added tabs to panel

--- src/us/mn/state/dot/tms/client/wysiwyg/editor/WMsgEditorPanel.java
+++ src/us/mn/state/dot/tms/client/wysiwyg/editor/WMsgEditorPanel.java
@@ -15,43 +15,16 @@
 
 package us.mn.state.dot.tms.client.wysiwyg.editor;
 
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 
-import us.mn.state.dot.tms.DMS;
-import us.mn.state.dot.tms.DmsSignGroup;
-import us.mn.state.dot.tms.DmsSignGroupHelper;
-import us.mn.state.dot.tms.QuickMessage;
-import us.mn.state.dot.tms.SignGroup;
-import us.mn.state.dot.tms.client.Session;
 import us.mn.state.dot.tms.client.dms.SignFacePanel;
-import us.mn.state.dot.tms.client.widget.AbstractForm;
-import us.mn.state.dot.tms.client.widget.IAction;
-import us.mn.state.dot.tms.client.widget.Widgets;
 import us.mn.state.dot.tms.utils.I18N;
 
 /**
  * WYSIWYG DMS Message Editor Pane
  *
- * @author Gordon Parikh, John L. Stanley, and Michael Janson - SRF Consulting
+ * @author Gordon Parikh, John L. Stanley - SRF Consulting
  */
 @SuppressWarnings("serial")
 
@@ -60,18 +33,39 @@ public class WMsgEditorPanel extends JPanel {
 
 	// TODO TODO TODO
 	
-	/** Tabbed sign/sign group listing */
+	/* Handle to the main form */
+	private WMsgEditorForm form;
+	
+	/** Tabs for switching between WYSIWYG/MULTI mode and viewing sign config,
+	 * warnings, and errors */
 	private JTabbedPane tab_pane;
+	private WMsgWysiwygPanel wysiwyg_pnl;
+	private JPanel multi_pnl;
+	private JPanel config_pnl;
+	
+	// TODO - warnings and errors (should be dynamic)
 	
 	/** TODO Temporary */
 	private SignFacePanel sfp = new SignFacePanel();
 	
-	public WMsgEditorPanel() {
-		tab_pane = new JTabbedPane(JTabbedPane.TOP);
-//		tab_pane.add(I18N.get("wysiwyg.epanel.wysiwyg_tab"), dms_pn);
-//		tab_pane.add(I18N.get("wysiwyg.epanel.multi_tab"), sgrp_pn);
-//		tab_pane.add(I18N.get("wysiwyg.epanel.config_tab"), sgrp_pn);
+	public WMsgEditorPanel(WMsgEditorForm f) {
+		form = f;
 		
+		// create the tab pane and the tabs
+		tab_pane = new JTabbedPane(JTabbedPane.TOP);
+		
+		// TODO should we define new classes for the tabs? may make things easier in the long run...
+		wysiwyg_pnl = new WMsgWysiwygPanel(form);
+		multi_pnl = new JPanel();
+		config_pnl = new JPanel();
+		
+		// add the tabs to the pane
+		tab_pane.add(I18N.get("wysiwyg.epanel.wysiwyg_tab"), wysiwyg_pnl);
+		tab_pane.add(I18N.get("wysiwyg.epanel.multi_tab"), multi_pnl);
+		tab_pane.add(I18N.get("wysiwyg.epanel.config_tab"), config_pnl);
+		
+		// add the tab pane to the panel
+		add(tab_pane);
 	}
 	
 	
