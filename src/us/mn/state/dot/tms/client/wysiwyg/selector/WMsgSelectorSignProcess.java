e852af46dbe9f13410772a29b925a0dceb65c160
Gordon Parikh <gparikh@srfconsulting.com>
2020-04-30 20:51:34 -0500

    Fixed copyright years

--- src/us/mn/state/dot/tms/client/wysiwyg/selector/WMsgSelectorSignProcess.java
+++ src/us/mn/state/dot/tms/client/wysiwyg/selector/WMsgSelectorSignProcess.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2017-2018  SRF Consulting Group
+ * Copyright (C) 2019-2020  SRF Consulting Group
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
