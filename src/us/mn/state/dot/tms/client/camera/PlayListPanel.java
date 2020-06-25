2fd350ae1e41baeb225500f18dbb63efe59304cd
Douglas Lau <doug.lau@state.mn.us>
2020-06-25 08:40:08 -0500

    Show camera # instead of name in play list panel.

--- src/us/mn/state/dot/tms/client/camera/PlayListPanel.java
+++ src/us/mn/state/dot/tms/client/camera/PlayListPanel.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2017-2018  Minnesota Department of Transportation
+ * Copyright (C) 2017-2020  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -186,6 +186,7 @@ public class PlayListPanel extends JPanel {
 	public void initialize() {
 		setBorder(UI.border);
 		cam_lst.setVisibleRowCount(12);
+		cam_lst.setCellRenderer(new CameraCellRenderer());
 		layoutPanel();
 		createJobs();
 	}
--- src/us/mn/state/dot/tms/client/incident/IncidentDispatcher.java
+++ src/us/mn/state/dot/tms/client/incident/IncidentDispatcher.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2009-2019  Minnesota Department of Transportation
+ * Copyright (C) 2009-2020  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -56,6 +56,7 @@ import us.mn.state.dot.tms.LCSArray;
 import us.mn.state.dot.tms.SignConfig;
 import us.mn.state.dot.tms.SignMessage;
 import us.mn.state.dot.tms.client.Session;
+import us.mn.state.dot.tms.client.camera.CameraCellRenderer;
 import us.mn.state.dot.tms.client.camera.CameraSelectAction;
 import us.mn.state.dot.tms.client.dms.SignMessageCreator;
 import us.mn.state.dot.tms.client.proxy.ProxyListModel;
