1088f2bc17d9572389ead6063d8aeb4f249902cc
Douglas Lau <doug.lau@state.mn.us>
2020-08-05 14:19:18 -0500

    Fixed warnings + cleaned up naming and indentation and line endings.

--- src/us/mn/state/dot/tms/utils/SubnetChecker.java
+++ src/us/mn/state/dot/tms/utils/SubnetChecker.java
@@ -28,84 +28,86 @@ import java.util.concurrent.TimeUnit;
 import us.mn.state.dot.tms.SystemAttribute;
 import us.mn.state.dot.tms.SystemAttributeHelper;
 
-/** SubnetChecker is a self-launching daemon thread 
+/** SubnetChecker is a self-launching daemon thread
  *  that identifies which subnet the Iris client is
  *  running on using a configurable series of TCP
  *  and/or ICMP pings.
- * 
+ *
  * @author John L. Stanley - SRF Consulting
  */
-
 public class SubnetChecker extends Thread {
 
 	/** A single thread for the checker */
-	static SubnetChecker thread;
-	
+	static SubnetChecker THREAD;
+
+	/** When SubnetChecker is first referenced,
+	 *  this creates an instance and runs it. */
+	static {
+		THREAD = new SubnetChecker();
+		THREAD.setDaemon(true);
+		THREAD.start();
+	}
+
 	/** List of ping targets and subnet names.
 	 * Default list uses google.com to detect
 	 * if we have generic internet access.
 	 * This is replaced by system-attribute
 	 * subnet_target_# entries, if any exist.
 	 */
-	private static String[] targets = {
+	private static String[] TARGETS = {
 		"www.google.com=internet",    // icmp ping
 		"www.google.com:80=internet", // tcp ping
 	};
-	
-	/** When SubnetChecker is first referenced,
-	 *  this creates an instance and runs it. */
-	static {
-		thread = new SubnetChecker();
-		thread.setDaemon(true);
-		thread.start();
-	}
-
-	//-------------------------------------------
 
 	/** Maximum wait time for failed ping.
 	 * Must be less than the default timeout
 	 * for both a socket connection and a
 	 * command-line ping command. {3 seconds}
 	 */
-	private final static int timeoutMillisec = 3*1000;
+	private final static int TIMEOUT_MS = 3 * 1000;
 
 	/** Constant for unknown subnet name */
 	public final static String UNKNOWN = "unknown";
-	
+
 	/** Current subnet */
-	private static String subnetName = UNKNOWN;
+	private static String SUBNET_NAME = UNKNOWN;
 
 	/** Flag to request a recheck */
-	private static boolean updateReq = false;
-	
+	private static boolean UPDATE_REQ = false;
+
 	/** Time from end of previous update to start
 	 *  of next automatic update. {1 minute} */
-	private final static long autoRetryMillisec = 60*1000;
+	private final static long AUTO_RETRY_MS = 60*1000;
 
 	/** Is this machine running Windows? */
-    private static boolean isWindows = 
-    		System.getProperty("os.name").toLowerCase().contains("win");
-    
-    /** Operating system dependent ping retry-count command-line-prefix */
-    private static String pingRetryArg = isWindows ? "-n" : "-c";
-    
-    /** List of current addresses on this machine */
-    private static String ipAddressList;
+	private static boolean IS_WINDOWS = System.getProperty("os.name")
+		.toLowerCase().contains("win");
 
-	//-------------------------------------------
+	/** Operating system dependent ping retry-count command-line-prefix */
+	private static String PING_RETRY_ARG = IS_WINDOWS ? "-n" : "-c";
 
-	//-------------------------------------------
+	/** Get current subnet name */
+	public static String getSubnetName() {
+		return SUBNET_NAME;
+	}
+
+	/** Request an update */
+	public static void reqUpdate() {
+		UPDATE_REQ = true;
+	}
+
+	/** List of current addresses on this machine */
+	private String ipAddressList;
 
 	/** Construct/initialize the subnet checker */
 	private SubnetChecker() {
 		super("SubnetChecker");
 		loadSubnetTargets();
 		ipAddressList = getAllMyAddresses();
-		
 	}
 
 	/** Load subnet target list.
-	 * 
+	 *
 	 * Searches system-attributes for ping target
 	 * attributes named "subnet_target_#" where #
 	 * is a positive int in the range 0..MAX_INT.
@@ -141,19 +143,18 @@ public class SubnetChecker extends Thread {
 		// Only override default targets if we found
 		// at least one valid subnet_target_# entry.
 		if (list.isEmpty() == false)
-			targets = (String[]) list.stream().toArray(String[]::new);
+			TARGETS = list.stream().toArray(String[]::new);
 	}
-		
+
 	/** Try to load subnet-target attribute.
 	 *  Tests target string for basic format rules.
-	 *  
-	 *@return Target string if found.  Null if no
+	 *
+	 * @return Target string if found.  Null if no
 	 *  such attribute.  Zero-length string if
 	 *  target string is malformed.
 	 */
 	private String getTarget(String aName) {
-		SystemAttribute attrib =
-			SystemAttributeHelper.get(aName);
+		SystemAttribute attrib = SystemAttributeHelper.get(aName);
 		if (attrib == null)
 			return null; // missing attribute
 		String target = attrib.getValue();
@@ -166,29 +167,14 @@ public class SubnetChecker extends Thread {
 		return target;
 	}
 
-	//-------------------------------------------
-
-	/** Get current subnet name */
-	public static String getSubnetName() {
-		return subnetName;
-	}
-
-	/** Request an update */
-	public static void reqUpdate() {
-		updateReq = true;
-	}
-	
-	//-------------------------------------------
-
 	/** Main checker loop.
 	 *  Exits when program ends. */
+	@Override
 	public void run() {
 		// the main checker loop
 		while (true) {
 			doPings();
-//			System.out.println("Address list: "+ipAddressList);
-//			System.out.println("Subnet: "+subnetName);
-			updateReq = false;
+			UPDATE_REQ = false;
 			doWait();
 		}
 	}
@@ -201,7 +187,7 @@ public class SubnetChecker extends Thread {
 	 */
 	private void doWait() {
 		long now = System.currentTimeMillis();
-		long end = now + autoRetryMillisec;
+		long end = now + AUTO_RETRY_MS;
 		long prev = now;
 		long delta;
 		String tmp;
@@ -211,7 +197,7 @@ public class SubnetChecker extends Thread {
 			} catch (InterruptedException e) {
 				break;
 			}
-			if (updateReq) {
+			if (UPDATE_REQ) {
 				System.out.println("Update requested");
 				break;
 			}
@@ -238,14 +224,14 @@ public class SubnetChecker extends Thread {
 		}
 	}
 
-	/** Ping hosts and set subnetName */
+	/** Ping hosts and set SUBNET_NAME */
 	private void doPings() {
 		String target, host, port, subnet;
 		String [] strs;
-		int len = targets.length;
+		int len = TARGETS.length;
 		boolean gotPing;
 		for (int i = 0; (i < len); ++i) {
-			target = targets[i];
+			target = TARGETS[i];
 			strs = target.split("=");
 			if (strs.length != 2) {
 				System.err.println(
@@ -266,25 +252,27 @@ public class SubnetChecker extends Thread {
 				gotPing = doIcmpPing(host);
 			}
 			if (gotPing) {
-				subnetName = subnet;
+				SUBNET_NAME = subnet;
 				return;
 			}
 		}
 		// didn't get any responses...
-		subnetName = UNKNOWN;
+		SUBNET_NAME = UNKNOWN;
 	}
 
 	/** Do an ICMP ping */
 	public static boolean doIcmpPing(String host) {
 		// Make sure we can resolve the host name to an IP address
-    	InetSocketAddress sockaddr = new InetSocketAddress(host, 0);
-    	if (sockaddr.isUnresolved())
-    		return false;
+		InetSocketAddress sockaddr = new InetSocketAddress(host, 0);
+		if (sockaddr.isUnresolved())
+			return false;
+
 		// Run command line ping with a timeout of 5 seconds
-	    ProcessBuilder processBuilder = new ProcessBuilder("ping", pingRetryArg, "1", "-w", "5000", host);
+		ProcessBuilder processBuilder = new ProcessBuilder("ping",
+			PING_RETRY_ARG, "1", "-w", "5000", host);
 		try {
 			Process proc = processBuilder.start();
-		    return proc.waitFor(timeoutMillisec, TimeUnit.MILLISECONDS);
+			return proc.waitFor(TIMEOUT_MS, TimeUnit.MILLISECONDS);
 		} catch (IOException e) {
 			return false;
 		} catch (InterruptedException e) {
@@ -294,19 +282,19 @@ public class SubnetChecker extends Thread {
 
 	/** Do a TCP ping (TCP connect and instant disconnect) */
 	public static boolean doTcpPing(String host, int port) {
-	    try {
-	    	InetSocketAddress sockaddr = new InetSocketAddress(host, port);
-	    	if (sockaddr.isUnresolved())
-	    		return false;
-	        try (Socket soc = new Socket()) {
-	            soc.connect(sockaddr, timeoutMillisec);
-	        }
-	        return true;
-	    } catch (IOException ex) {
-	        return false;
-	    }
+		try {
+			InetSocketAddress sockaddr = new InetSocketAddress(host, port);
+			if (sockaddr.isUnresolved())
+				return false;
+			try (Socket soc = new Socket()) {
+				soc.connect(sockaddr, TIMEOUT_MS);
+			}
+			return true;
+		} catch (IOException ex) {
+			return false;
+		}
 	}
-	
+
 	/** Get string containing comma separated
 	 *  list of all current IP addresses for
 	 *  this workstation. */
@@ -315,11 +303,11 @@ public class SubnetChecker extends Thread {
 		Enumeration<NetworkInterface> e;
 		try {
 			e = NetworkInterface.getNetworkInterfaces();
-			while(e.hasMoreElements()) {
-				NetworkInterface n = (NetworkInterface) e.nextElement();
+			while (e.hasMoreElements()) {
+				NetworkInterface n = e.nextElement();
 				Enumeration<InetAddress> ee = n.getInetAddresses();
 				while (ee.hasMoreElements()) {
-					InetAddress i = (InetAddress) ee.nextElement();
+					InetAddress i = ee.nextElement();
 					if (sb.length() > 0)
 						sb.append(',');
 					sb.append(i.getHostAddress());
@@ -330,17 +318,4 @@ public class SubnetChecker extends Thread {
 		}
 		return sb.toString();
 	}
-
-	//-------------------------------------------
-	
-	/** test */
-//	static public void main(String[] args) {
-//		try {
-//			sleep(10*1000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		System.out.println("Exiting");
-//	}	
 }
