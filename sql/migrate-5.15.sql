409b19ad0ecfc01255e0b725d9cade0af88adb78
Douglas Lau <doug.lau@state.mn.us>
2020-08-06 10:14:01 -0500

    Simplified SQL for new video stuff + added to migrate-5.15.sql script.

--- sql/migrate-5.15.sql
+++ sql/migrate-5.15.sql
@@ -17,4 +17,139 @@ CREATE VIEW travel_time_event_view AS
 	ON travel_time_event.event_desc_id = event_description.event_desc_id;
 GRANT SELECT ON travel_time_event_view TO PUBLIC;
 
+-- Add client video system attributes
+INSERT INTO iris.system_attribute (name, value)
+VALUES ('vid_connect_autostart', true),
+	('vid_connect_fail_next_source', true),
+	('vid_connect_fail_sec', 20),
+	('vid_lost_timeout_sec', 10),
+	('vid_reconnect_auto', true),
+	('vid_reconnect_timeout_sec', 10),
+	('gstreamer_version_windows','1.16.2');
+
+-- Drop old views
+DROP VIEW camera_view;
+DROP VIEW iris.camera;
+
+-- Add camera template
+CREATE TABLE iris.camera_template (
+	name VARCHAR(20) PRIMARY KEY,
+	notes text,
+	label text
+);
+
+-- Add video source template
+CREATE TABLE iris.vid_src_template (
+	name VARCHAR(20) PRIMARY KEY,
+	label text,
+	config text,
+	default_port INTEGER,
+	subnets text,
+	latency INTEGER,
+	encoder VARCHAR(64),
+	scheme text,
+	codec text,
+	rez_width INTEGER,
+	rez_height INTEGER,
+	multicast BOOLEAN,
+	notes text
+);
+
+-- Add camera video source ord
+CREATE TABLE iris.cam_vid_src_ord (
+	name VARCHAR(24) PRIMARY KEY,
+	camera_template VARCHAR(20) REFERENCES iris.camera_template,
+	src_order INTEGER,
+	src_template VARCHAR(20) REFERENCES iris.vid_src_template
+);
+
+ALTER TABLE iris._camera ADD COLUMN cam_template VARCHAR(20);
+ALTER TABLE iris._camera ADD CONSTRAINT _camera_cam_template_fkey
+	FOREIGN KEY (cam_template) REFERENCES iris.camera_template;
+
+CREATE VIEW iris.camera AS
+	SELECT c.name, geo_loc, controller, pin, notes, cam_num, cam_template,
+	       encoder_type, enc_address, enc_port, enc_mcast, enc_channel,
+	       publish, streamable, video_loss
+	FROM iris._camera c
+	JOIN iris._device_io d ON c.name = d.name;
+
+CREATE OR REPLACE FUNCTION iris.camera_insert() RETURNS TRIGGER AS
+	$camera_insert$
+BEGIN
+	INSERT INTO iris._device_io (name, controller, pin)
+	     VALUES (NEW.name, NEW.controller, NEW.pin);
+	INSERT INTO iris._camera (name, geo_loc, notes, cam_num, cam_template,
+	            encoder_type, enc_address, enc_port, enc_mcast, enc_channel,
+	            publish, streamable, video_loss)
+	     VALUES (NEW.name, NEW.geo_loc, NEW.notes, NEW.cam_num,
+	             NEW.cam_template, NEW.encoder_type, NEW.enc_address,
+	             NEW.enc_port, NEW.enc_mcast, NEW.enc_channel, NEW.publish,
+	             NEW.streamable, NEW.video_loss);
+	RETURN NEW;
+END;
+$camera_insert$ LANGUAGE plpgsql;
+
+CREATE TRIGGER camera_insert_trig
+    INSTEAD OF INSERT ON iris.camera
+    FOR EACH ROW EXECUTE PROCEDURE iris.camera_insert();
+
+CREATE OR REPLACE FUNCTION iris.camera_update() RETURNS TRIGGER AS
+	$camera_update$
+BEGIN
+	UPDATE iris._device_io
+	   SET controller = NEW.controller,
+	       pin = NEW.pin
+	 WHERE name = OLD.name;
+	UPDATE iris._camera
+	   SET geo_loc = NEW.geo_loc,
+	       notes = NEW.notes,
+	       cam_num = NEW.cam_num,
+	       cam_template = NEW.cam_template,
+	       encoder_type = NEW.encoder_type,
+	       enc_address = NEW.enc_address,
+	       enc_port = NEW.enc_port,
+	       enc_mcast = NEW.enc_mcast,
+	       enc_channel = NEW.enc_channel,
+	       publish = NEW.publish,
+	       streamable = NEW.streamable,
+	       video_loss = NEW.video_loss
+	 WHERE name = OLD.name;
+	RETURN NEW;
+END;
+$camera_update$ LANGUAGE plpgsql;
+
+CREATE TRIGGER camera_update_trig
+    INSTEAD OF UPDATE ON iris.camera
+    FOR EACH ROW EXECUTE PROCEDURE iris.camera_update();
+
+CREATE TRIGGER camera_delete_trig
+    INSTEAD OF DELETE ON iris.camera
+    FOR EACH ROW EXECUTE PROCEDURE iris.device_delete();
+
+CREATE VIEW camera_view AS
+	SELECT c.name, cam_num, c.cam_template, encoder_type, et.make, et.model,
+	       et.config, c.enc_address, c.enc_port, c.enc_mcast, c.enc_channel,
+	       c.publish, c.streamable, c.video_loss, c.geo_loc,
+	       l.roadway, l.road_dir, l.cross_mod, l.cross_street, l.cross_dir,
+	       l.landmark, l.lat, l.lon, l.corridor, l.location,
+	       c.controller, ctr.comm_link, ctr.drop_id, ctr.condition, c.notes
+	FROM iris.camera c
+	LEFT JOIN iris.encoder_type et ON c.encoder_type = et.name
+	LEFT JOIN geo_loc_view l ON c.geo_loc = l.name
+	LEFT JOIN controller_view ctr ON c.controller = ctr.name;
+GRANT SELECT ON camera_view TO PUBLIC;
+
+INSERT INTO iris.sonar_type (name) VALUES ('camera_template');
+INSERT INTO iris.sonar_type (name) VALUES ('vid_src_template');
+INSERT INTO iris.sonar_type (name) VALUES ('cam_vid_src_ord');
+
+INSERT INTO iris.privilege (name, capability, type_n, obj_n, attr_n, group_n, write)
+VALUES ('PRV_003F', 'camera_tab', 'camera_template', '', '', '', false),
+       ('PRV_003G', 'camera_tab', 'cam_vid_src_ord', '', '', '', false),
+       ('PRV_003H', 'camera_tab', 'vid_src_template', '', '', '', false),
+       ('PRV_004A', 'camera_admin', 'camera_template', '', '', '', true),
+       ('PRV_004B', 'camera_admin', 'cam_vid_src_ord', '', '', '', true),
+       ('PRV_004C', 'camera_admin', 'vid_src_template', '', '', '', true);
+
 COMMIT;
--- sql/tms-template.sql
+++ sql/tms-template.sql
@@ -1435,62 +1435,18 @@ CREATE VIEW encoder_stream_view AS
 	LEFT JOIN iris.encoding_quality eq ON es.quality = eq.id;
 GRANT SELECT ON encoder_stream_view TO PUBLIC;
 
-CREATE TABLE iris._camera_template
-(
-	name character varying(20),
+CREATE TABLE iris.camera_template (
+	name VARCHAR(20) PRIMARY KEY,
 	notes text,
-	label text,
-	CONSTRAINT camera_template_pkey PRIMARY KEY (name)
+	label text
 );
-
-CREATE OR REPLACE VIEW iris.camera_template
- AS
- SELECT name,
-		notes,
-		label
-   FROM iris._camera_template;
-
-CREATE TABLE iris._vid_src_template
-(
-	name character varying(20),
-	label text,
-	config text,
-	default_port integer,
-	subnets text,
-	latency integer,
-	encoder character varying(64),
-	scheme text,
-	codec text,
-	rez_width integer,
-	rez_height integer,
-	multicast boolean,
-	notes text,
-	CONSTRAINT vid_src_template_pkey PRIMARY KEY (name)
-);
-
-CREATE OR REPLACE VIEW iris.vid_src_template
- AS
- SELECT name,
-		label,
-		config,
-		default_port,
-		subnets,
-		latency,
-		encoder,
-		scheme,
-		codec,
-		rez_width,
-		rez_height,
-		multicast,
-		notes
-   FROM iris._vid_src_template;
-
 
 CREATE TABLE iris._camera (
 	name VARCHAR(20) PRIMARY KEY,
 	geo_loc VARCHAR(20) REFERENCES iris.geo_loc(name),
 	notes VARCHAR(256) NOT NULL,
 	cam_num INTEGER UNIQUE,
+	cam_template VARCHAR(20) REFERENCES iris.camera_template,
 	encoder_type VARCHAR(8) REFERENCES iris.encoder_type,
 	enc_address INET,
 	enc_port INTEGER CHECK (enc_port > 0 AND enc_port <= 65535),
@@ -1498,17 +1454,11 @@ CREATE TABLE iris._camera (
 	enc_channel INTEGER CHECK (enc_channel > 0 AND enc_channel <= 16),
 	publish BOOLEAN NOT NULL,
 	streamable BOOLEAN NOT NULL,
-	cam_template VARCHAR(20),
 	video_loss BOOLEAN NOT NULL
 );
 
 ALTER TABLE iris._camera ADD CONSTRAINT _camera_fkey
 	FOREIGN KEY (name) REFERENCES iris._device_io(name) ON DELETE CASCADE;
- 
-ALTER TABLE iris._camera ADD CONSTRAINT _camera_cam_template_fkey FOREIGN KEY (cam_template)
-        REFERENCES iris._camera_template (name) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION;
 
 CREATE FUNCTION iris.camera_notify() RETURNS TRIGGER AS
 	$camera_notify$
@@ -1533,9 +1483,9 @@ CREATE TRIGGER camera_table_notify_trig
 	FOR EACH STATEMENT EXECUTE PROCEDURE iris.table_notify();
 
 CREATE VIEW iris.camera AS
-	SELECT c.name, geo_loc, controller, pin, notes, cam_num, encoder_type,
-	       enc_address, enc_port, enc_mcast, enc_channel, publish,
-	       streamable, cam_template, video_loss
+	SELECT c.name, geo_loc, controller, pin, notes, cam_num, cam_template,
+	       encoder_type, enc_address, enc_port, enc_mcast, enc_channel,
+	       publish, streamable, video_loss
 	FROM iris._camera c
 	JOIN iris._device_io d ON c.name = d.name;
 
@@ -1544,13 +1494,13 @@ CREATE FUNCTION iris.camera_insert() RETURNS TRIGGER AS
 BEGIN
 	INSERT INTO iris._device_io (name, controller, pin)
 	     VALUES (NEW.name, NEW.controller, NEW.pin);
-	INSERT INTO iris._camera (name, geo_loc, notes, cam_num, encoder_type,
-	            enc_address, enc_port, enc_mcast, enc_channel, publish,
-	            streamable, cam_template, video_loss)
+	INSERT INTO iris._camera (name, geo_loc, notes, cam_num, cam_template,
+	            encoder_type, enc_address, enc_port, enc_mcast, enc_channel,
+	            publish, streamable, video_loss)
 	     VALUES (NEW.name, NEW.geo_loc, NEW.notes, NEW.cam_num,
-	             NEW.encoder_type, NEW.enc_address, NEW.enc_port,
-	             NEW.enc_mcast, NEW.enc_channel, NEW.publish,
-	             NEW.streamable, NEW.cam_template, NEW.video_loss);
+	             NEW.cam_template, NEW.encoder_type, NEW.enc_address,
+	             NEW.enc_port, NEW.enc_mcast, NEW.enc_channel, NEW.publish,
+	             NEW.streamable, NEW.video_loss);
 	RETURN NEW;
 END;
 $camera_insert$ LANGUAGE plpgsql;
@@ -1570,6 +1520,7 @@ BEGIN
 	   SET geo_loc = NEW.geo_loc,
 	       notes = NEW.notes,
 	       cam_num = NEW.cam_num,
+	       cam_template = NEW.cam_template,
 	       encoder_type = NEW.encoder_type,
 	       enc_address = NEW.enc_address,
 	       enc_port = NEW.enc_port,
@@ -1577,7 +1528,6 @@ BEGIN
 	       enc_channel = NEW.enc_channel,
 	       publish = NEW.publish,
 	       streamable = NEW.streamable,
-	       cam_template = NEW.cam_template,
 	       video_loss = NEW.video_loss
 	 WHERE name = OLD.name;
 	RETURN NEW;
@@ -1593,9 +1543,9 @@ CREATE TRIGGER camera_delete_trig
     FOR EACH ROW EXECUTE PROCEDURE iris.device_delete();
 
 CREATE VIEW camera_view AS
-	SELECT c.name, cam_num, encoder_type, et.make, et.model, et.config,
-	       c.enc_address, c.enc_port, c.enc_mcast, c.enc_channel, c.publish,
-	       c.streamable, c.cam_template, c.video_loss, c.geo_loc,
+	SELECT c.name, cam_num, c.cam_template, encoder_type, et.make, et.model,
+	       et.config, c.enc_address, c.enc_port, c.enc_mcast, c.enc_channel,
+	       c.publish, c.streamable, c.video_loss, c.geo_loc,
 	       l.roadway, l.road_dir, l.cross_mod, l.cross_street, l.cross_dir,
 	       l.landmark, l.lat, l.lon, l.corridor, l.location,
 	       c.controller, ctr.comm_link, ctr.drop_id, ctr.condition, c.notes
@@ -1609,32 +1559,28 @@ CREATE TABLE iris._cam_sequence (
 	seq_num INTEGER PRIMARY KEY
 );
 
-CREATE TABLE iris._cam_vid_src_ord
-(
-	name character varying(24),
-	camera_template character varying(20),
-	src_order integer,
-	src_template character varying(20),
-	CONSTRAINT camera_vid_src_order_pkey PRIMARY KEY (name)
+CREATE TABLE iris.vid_src_template (
+	name VARCHAR(20) PRIMARY KEY,
+	label text,
+	config text,
+	default_port INTEGER,
+	subnets text,
+	latency INTEGER,
+	encoder VARCHAR(64),
+	scheme text,
+	codec text,
+	rez_width INTEGER,
+	rez_height INTEGER,
+	multicast BOOLEAN,
+	notes text
 );
 
-CREATE OR REPLACE VIEW iris.cam_vid_src_ord
- AS
- SELECT name,
-		camera_template,
-		src_order,
-		src_template
-   FROM iris._cam_vid_src_ord;
-
-ALTER TABLE iris._cam_vid_src_ord ADD CONSTRAINT cam_vid_src_ord_camera_template_fkey FOREIGN KEY (camera_template)
-        REFERENCES iris._camera_template (name) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION;
-		
-ALTER TABLE iris._cam_vid_src_ord ADD CONSTRAINT cam_vid_src_ord_src_template_fkey FOREIGN KEY (src_template)
-       REFERENCES iris._vid_src_template (name) MATCH SIMPLE
-       ON UPDATE NO ACTION
-       ON DELETE NO ACTION;
+CREATE TABLE iris.cam_vid_src_ord (
+	name VARCHAR(24) PRIMARY KEY,
+	camera_template VARCHAR(20) REFERENCES iris.camera_template,
+	src_order INTEGER,
+	src_template VARCHAR(20) REFERENCES iris.vid_src_template
+);
 
 CREATE TABLE iris._play_list (
 	name VARCHAR(20) PRIMARY KEY,
