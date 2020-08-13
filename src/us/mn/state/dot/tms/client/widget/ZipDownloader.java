7901d4af0a793014fb7403f9a520eea722e22788
Douglas Lau <doug.lau@state.mn.us>
2020-08-13 12:25:49 -0500

    Whitespace cleanup.

--- src/us/mn/state/dot/tms/client/widget/ZipDownloader.java
+++ src/us/mn/state/dot/tms/client/widget/ZipDownloader.java
@@ -56,35 +56,35 @@ import us.mn.state.dot.tms.utils.I18N;
  * directory (next to the user.properties file), after which the zip file will
  * be deleted. This is view only, the user has no ability to control what is
  * happening.
- * 
+ *
  * This class also contains the code used to perform the actual download.
- * 
+ *
  * @author Gordon Parikh
  */
 @SuppressWarnings("serial")
 public class ZipDownloader extends AbstractForm {
-	
+
 	/** Frame containing this form */
 	private JInternalFrame frame;
-	
+
 	/** Message displayed to the user */
 	private ILabel msgLbl;
-	
+
 	/** Progress bar for tracking download */
 	private JProgressBar progressBar;
 
 	/** URL to the file being downloaded */
 	private URL url;
-	
+
 	/** Directory into which the contents of the zip will be placed */
 	private File destDir;
-	
+
 	/** Size of the file to be downloaded */
 	private int fileSize;
-	
+
 	/** Current number of bytes read from the server */
 	private int bytesRead;
-	
+
 	/** Construct a ZipDownloader to download from the specified URL and
 	 *  unzip into directory d, showing progress in a window with the given
 	 *  I18N'd title and message.
@@ -97,16 +97,16 @@ public class ZipDownloader extends AbstractForm {
 		progressBar = new JProgressBar(0, 100);
 		progressBar.setPreferredSize(new Dimension(200, 20));
 	}
-	
+
 	/** Initialize the form */
 	@Override
 	protected void initialize() {
 		// initialize layout
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		
+
 		/* Message for the user */
 		add(msgLbl);
-		
+
 		/* Progress bar */
 		add(progressBar);
 	}
@@ -119,14 +119,14 @@ public class ZipDownloader extends AbstractForm {
 		if (bytesRead <= progressBar.getMaximum())
 			progressBar.setValue(bytesRead);
 	}
-	
+
 	public void setFrame(JInternalFrame f) {
 		frame = f;
-		
+
 		// disable closing the frame - only let it happen when it's done
 		frame.setDefaultCloseOperation(JInternalFrame.DO_NOTHING_ON_CLOSE);
 	}
-	
+
 	/** Start the background process. If an Action is provided, the action
 	 *  will be performed (from the event dispatch thread) at the end of the
 	 *  background process.
@@ -136,20 +136,20 @@ public class ZipDownloader extends AbstractForm {
 		dlWorker = new DownloadWorker(cb);
 		dlWorker.execute();
 	}
-	
+
 	/** Worker for downloading and unzipping the file. */
 	private DownloadWorker dlWorker;
-	
+
 	private class DownloadWorker extends SwingWorker<File, Integer> {
 
 		/** Action to be executed (in EDT) when we're done */
 		private Action cbAction;
-		
+
 		/** Construct a new DownloadWorker with a callback action. */
 		public DownloadWorker(Action cb) {
 			cbAction = cb;
 		}
-		
+
 		/** Process updates from the background thread */
 		@Override
 		protected void process(List<Integer> byteCounts) {
@@ -157,13 +157,13 @@ public class ZipDownloader extends AbstractForm {
 			// to give some time to unzip)
 			if (fileSize != -1 && progressBar.getMaximum() != 2*fileSize)
 				progressBar.setMaximum(2*fileSize);
-			
+
 			// update the progress bar with whatever chunks we got
 			for (Integer b: byteCounts) {
 				updateProgressBar(b);
 			}
 		}
-		
+
 		/** Finish up. Runs the callback action (if provided) and closes the
 		 *  GUI containing the progress tracker.
 		 */
@@ -173,29 +173,29 @@ public class ZipDownloader extends AbstractForm {
 				cbAction.actionPerformed(null);
 			close(Session.getCurrent().getDesktop());
 		}
-		
+
 		/** Bytes "read" - used when downloading and unzipping */
 		private int totalBytesRead = 0;
-		
+
 		@Override
 		protected File doInBackground() throws Exception {
 			// first check the size of the file for progress tracking
 			System.out.println("Checking download size...");
 			fileSize = getFileSize(url);
-			
+
 			// if this didn't work, we're done
 			if (fileSize == -1) {
 				System.out.println("Download size check failed!");
 				return null;
 			}
-			
+
 			System.out.println("Download size: " + Integer.toString(fileSize));
-			
+
 			// get the $HOME/iris directory and make sure it exists
 			File uIris = UserProperty.getDir();
 			if (!uIris.canWrite())
 				uIris.mkdirs();
-			
+
 			// setup the ReadableByteChannel to handle the download
 			ReadableByteChannel rbc = Channels.newChannel(url.openStream());
 
@@ -204,38 +204,38 @@ public class ZipDownloader extends AbstractForm {
 			String zipPath = Paths.get(
 					uIris.getCanonicalPath(), zipName).toString();
 			FileOutputStream fos = new FileOutputStream(zipPath);
-			
+
 			// download the file
 			System.out.println("Downloading " + Integer.toString(fileSize)
 						+ " bytes from " + url.toString() + " to " + zipPath);
 			ReadableProgressByteChannel rpbc =
 					new ReadableProgressByteChannel(rbc);
 			long r = fos.getChannel().transferFrom(rpbc, 0, Long.MAX_VALUE);
-			
+
 			// close the streams
 			rpbc.close();
 			fos.close();
-			
+
 			// stop if the file couldn't be downloaded
 			if (r == 0)
 				return null;
-			
+
 			// once the file is done, create a directory for the contents and
 			// unzip
 			if (!destDir.canWrite())
 				destDir.mkdirs();
 			unzipFile(zipPath, destDir);
-			
-			// whether or not the file was unzipped successfully, delete the 
+
+			// whether or not the file was unzipped successfully, delete the
 			// zip file
 			System.out.println("Deleting zip file: " + zipPath);
 			File zipFile = new File(Paths.get(zipPath).toString());
 			boolean d = zipFile.delete();
-			
+
 			// return the directory containing the files we just unzipped
 			return destDir;
 		}
-		
+
 		/** Unzip the file at zipName into the directory oDir. */
 		private void unzipFile(String zipName, File destDir) {
 			try {
@@ -246,7 +246,7 @@ public class ZipDownloader extends AbstractForm {
 				ZipEntry e = zis.getNextEntry();
 				while (e != null) {
 					Path p = newFile(destDir, e);
-					
+
 					// check if the entry is a file or directory
 					if (!e.isDirectory()) {
 						// if it's a file, write out the file
@@ -256,7 +256,7 @@ public class ZipDownloader extends AbstractForm {
 						// if it's a directory, create it
 						Files.createDirectories(p);
 					}
-					
+
 					// update the progress tracker using the compressed size
 					// of each entry
 					totalBytesRead += e.getCompressedSize();
@@ -273,11 +273,11 @@ public class ZipDownloader extends AbstractForm {
 				ex.printStackTrace();
 			}
 		}
-		
+
 		/** Create a new file in destDir for unzipping the ZipEntry e. This
 		 *  guards against "Zip Slipping" (see here:
 		 *  https://snyk.io/research/zip-slip-vulnerability).
-		 * @throws IOException 
+		 * @throws IOException
 		 */
 		private Path newFile(File destDir, ZipEntry e) throws IOException {
 			Path destPath = Paths.get(
@@ -291,24 +291,24 @@ public class ZipDownloader extends AbstractForm {
 			}
 			return destPath;
 		}
-		
+
 		/** A wrapper class implementing a ReadableByteChannel for providing
 		 *  progress updates.
 		 */
 		class ReadableProgressByteChannel implements ReadableByteChannel {
-			
+
 			private final ReadableByteChannel rbc;
-			
+
 			public ReadableProgressByteChannel(ReadableByteChannel rbc) {
 				this.rbc = rbc;
 			}
-			
+
 			@Override
 			public void close() throws IOException { rbc.close(); }
-			
+
 			@Override
 			public boolean isOpen() { return rbc.isOpen(); }
-			
+
 			/** Read data from remote and update progress */
 			@Override
 			public int read(ByteBuffer dst) throws IOException {
@@ -317,11 +317,9 @@ public class ZipDownloader extends AbstractForm {
 				publish(totalBytesRead);
 				return nRead;
 			}
-			
 		}
-		
 	};
-	
+
 	/** Query the size of the file to be downloaded from the server using a
 	 *  GET request.
 	 */
@@ -342,19 +340,4 @@ public class ZipDownloader extends AbstractForm {
 	            ((HttpURLConnection)conn).disconnect();
 		}
 	}
-	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
