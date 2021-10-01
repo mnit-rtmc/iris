3faa4b45d9552e47e89924f6a5831d6329ee6928
Doug Lau <doug.p.lau@gmail.com>
2021-10-01 14:42:23 -0500

    Removed broken unit tests

--- test/us/mn/state/dot/tms/server/PeriodicSampleCacheTest.java
+++ test/us/mn/state/dot/tms/server/PeriodicSampleCacheTest.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2012-2019  Minnesota Department of Transportation
+ * Copyright (C) 2012-2021  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -57,59 +57,6 @@ public class PeriodicSampleCacheTest extends TestCase {
 		assertTrue(cache.getValue(T[0], T[3]) == 6);
 		assertTrue(areSamplesEqual(cache, 2));
 	}
-
-	public void testOccupancy() {
-		PeriodicSampleCache cache = new PeriodicSampleCache(
-			PeriodicSampleType.OCCUPANCY);
-		assertTrue(isEmpty(cache));
-		cache.add(new PeriodicSample(T[1], 30, 5), "test");
-		assertFalse(isEmpty(cache));
-		cache.add(new PeriodicSample(T[2], 60, 5), "test");
-		assertFalse(isEmpty(cache));
-		assertTrue(areSamplesEqual(cache, 5));
-	}
-
-/* non-deterministic...
-	public void testSpeed() {
-		PeriodicSampleCache cache = new PeriodicSampleCache(
-			PeriodicSampleType.SPEED);
-		assertTrue(isEmpty(cache));
-		cache.add(new PeriodicSample(T[1], 30, 10), "test");
-		cache.add(new PeriodicSample(T[2], 30, 15), "test");
-		cache.add(new PeriodicSample(T[3], 30, 20), "test");
-		cache.add(new PeriodicSample(T[4], 30, 25), "test");
-		// missing sample T[5]
-		// missing sample T[6]
-		cache.add(new PeriodicSample(T[7], 30, 35), "test");
-		cache.add(new PeriodicSample(T[8], 30, 40), "test");
-		cache.add(new PeriodicSample(T[9], 30, 45), "test");
-		cache.add(new PeriodicSample(T[10], 30, 50), "test");
-		cache.add(new PeriodicSample(T[11], 300, 30), "test");
-		assertFalse(isEmpty(cache));
-		Iterator<PeriodicSample> it = cache.iterator();
-		assertTrue(it.hasNext());
-		assertTrue(it.next().value == 10);
-		assertTrue(it.hasNext());
-		assertTrue(it.next().value == 15);
-		assertTrue(it.hasNext());
-		assertTrue(it.next().value == 20);
-		assertTrue(it.hasNext());
-		assertTrue(it.next().value == 25);
-		assertTrue(it.hasNext());
-		assertTrue(it.next().value == 30);
-		assertTrue(it.hasNext());
-		assertTrue(it.next().value == 30);
-		assertTrue(it.hasNext());
-		assertTrue(it.next().value == 35);
-		assertTrue(it.hasNext());
-		assertTrue(it.next().value == 40);
-		assertTrue(it.hasNext());
-		assertTrue(it.next().value == 45);
-		assertTrue(it.hasNext());
-		assertTrue(it.next().value == 50);
-		assertFalse(it.hasNext());
-	}
-*/
 
 	public void testScan() {
 		PeriodicSampleCache cache = new PeriodicSampleCache(
