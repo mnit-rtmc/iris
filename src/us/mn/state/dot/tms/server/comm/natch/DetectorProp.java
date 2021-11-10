8ec5c2c18441d6bee0ba517039fd45b4c81d5c2c
Doug Lau <doug.p.lau@gmail.com>
2021-11-10 14:37:53 -0600

    natch: Support ramp meter green count detectors.

--- src/us/mn/state/dot/tms/server/comm/natch/DetectorProp.java
+++ src/us/mn/state/dot/tms/server/comm/natch/DetectorProp.java
@@ -14,8 +14,10 @@
  */
 package us.mn.state.dot.tms.server.comm.natch;
 
+import us.mn.state.dot.tms.ControllerIO;
 import us.mn.state.dot.tms.server.ControllerImpl;
 import us.mn.state.dot.tms.server.DetectorImpl;
+import us.mn.state.dot.tms.server.RampMeterImpl;
 
 /**
  * Detector property
@@ -24,8 +26,23 @@ import us.mn.state.dot.tms.server.DetectorImpl;
  */
 abstract public class DetectorProp extends NatchProp {
 
-	/** Maximum detector number */
+	/** Maximum detector number in table.
+	 *
+	 * The last 2 numbers (30 and 31) are reserved for
+	 * ramp meter green count detectors. */
 	static protected final int MAX_DET = 32;
+
+	/** First ramp meter green detector number */
+	static private final int METER_1_GREEN_DET = 30;
+
+	/** Second ramp meter green detector number */
+	static private final int METER_2_GREEN_DET = 31;
+
+	/** I/O pin for first ramp meter */
+	static private final int METER_1_PIN = 2;
+
+	/** I/O pin for second ramp meter */
+	static private final int METER_2_PIN = 3;
 
 	/** Detector number (0-31) */
 	public int detector_num;
@@ -39,9 +56,17 @@ abstract public class DetectorProp extends NatchProp {
 	protected DetectorImpl lookupDet(ControllerImpl ctrl) {
 		if (isValidNum()) {
 			int pin = detector_num + 39;
-			return ctrl.getDetectorAtPin(pin);
-		} else
-			return null;
+			DetectorImpl det = ctrl.getDetectorAtPin(pin);
+			if (det != null)
+				return det;
+			det = getMeter1Green(ctrl);
+			if (det != null)
+				return det;
+			det = getMeter2Green(ctrl);
+			if (det != null)
+				return det;
+		}
+		return null;
 	}
 
 	/** Lookup the input pin for the detector */
@@ -50,8 +75,36 @@ abstract public class DetectorProp extends NatchProp {
 			int pin = detector_num + 39;
 			if (ctrl.getDetectorAtPin(pin) != null)
 				return pin;
+			else if (getMeter1Green(ctrl) != null)
+				return METER_1_PIN;
+			else if (getMeter2Green(ctrl) != null)
+				return METER_2_PIN;
 		}
 		return 0;
+	}
+
+	/** Get the first meter green detector (if det num is correct) */
+	private DetectorImpl getMeter1Green(ControllerImpl ctrl) {
+		if (detector_num == METER_1_GREEN_DET) {
+			ControllerIO cio = ctrl.getIO(METER_1_PIN);
+			if (cio instanceof RampMeterImpl) {
+				RampMeterImpl meter = (RampMeterImpl) cio;
+				return meter.getGreenDet();
+			}
+		}
+		return null;
+	}
+
+	/** Get the second meter green detector (if det num is correct) */
+	private DetectorImpl getMeter2Green(ControllerImpl ctrl) {
+		if (detector_num == METER_2_GREEN_DET) {
+			ControllerIO cio = ctrl.getIO(METER_2_PIN);
+			if (cio instanceof RampMeterImpl) {
+				RampMeterImpl meter = (RampMeterImpl) cio;
+				return meter.getGreenDet();
+			}
+		}
+		return null;
 	}
 
 	/** Create a new detector property */
