d497624b4796e1ea3b42c67c68114577fa0e4a6f
John Stanley <14795456+jlstanley-git@users.noreply.github.com>
2022-01-21 13:37:16 -0600

    Added WYSIWYG standby message-token

---
+++ src/us/mn/state/dot/tms/utils/wysiwyg/token/WtStandby.java
@@ -0,0 +1,69 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2019-2020  SRF Consulting Group
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package us.mn.state.dot.tms.utils.wysiwyg.token;
+
+import us.mn.state.dot.tms.utils.Multi;
+import us.mn.state.dot.tms.utils.wysiwyg.WRenderer;
+import us.mn.state.dot.tms.utils.wysiwyg.WToken;
+import us.mn.state.dot.tms.utils.wysiwyg.WTokenType;
+
+/** Standby-message token for WYSIWYG editor.
+ * 
+ * @author John L. Stanley - SRF Consulting
+ *
+ */
+public class WtStandby extends WToken {
+
+	/**
+	 * @param fid
+	 */
+	public WtStandby() {
+		super(WTokenType.standby, "[standby");
+		updateString();
+	}
+
+	/* (non-Javadoc)
+	 * @see us.mn.state.dot.tms.utils.wysiwyg.WToken#isText()
+	 */
+	@Override
+	public boolean isText() {
+		return false;
+	}
+	
+	/* (non-Javadoc)
+	 * @see us.mn.state.dot.tms.utils.wysiwyg.WToken#doMulti(us.mn.state.dot.tms.utils.Multi)
+	 */
+	@Override
+	public void doMulti(Multi cb) {
+//		cb.addStandby();
+	}
+
+	/* (non-Javadoc)
+	 * @see us.mn.state.dot.tms.utils.wysiwyg.WToken#doRender(us.mn.state.dot.tms.utils.wysiwyg.WRenderer)
+	 */
+	@Override
+	public void doRender(WRenderer wr) {
+//		wr.renderStandby(this);
+	}
+
+	/* (non-Javadoc)
+	 * @see us.mn.state.dot.tms.utils.wysiwyg.WToken#appendParameters(java.lang.StringBuilder)
+	 */
+	@Override
+	public void appendParameters(StringBuilder sb) {
+		// nothing to do
+	}
+}
