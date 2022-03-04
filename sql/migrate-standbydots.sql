3c97ad75697fdd04780e592d96ba52c9326d2aba
Gordon Parikh <gparikh@srfconsulting.com>
2022-03-04 11:20:56 -0600

    Changed standby sign message source ordinal to match code

--- sql/migrate-standbydots.sql
+++ sql/migrate-standbydots.sql
@@ -6,6 +6,6 @@ BEGIN;
 -- Uncomment and change if/as needed
 -- SELECT iris.update_version('5.22.4', '5.22.5');
 
-INSERT INTO iris.sign_msg_source (bit, source) VALUES (14, 'standby');
+INSERT INTO iris.sign_msg_source (bit, source) VALUES (15, 'standby');
 
 COMMIT;
