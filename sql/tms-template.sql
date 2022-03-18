a5a9eec52adb07af2d09e40a4bf1cf1e02fdc776
Doug Lau <doug.p.lau@gmail.com>
2022-03-18 09:09:37 -0500

    sql: Updated database version

--- sql/tms-template.sql
+++ sql/tms-template.sql
@@ -158,7 +158,7 @@ client_event_purge_days	0
 client_units_si	true
 comm_event_enable	true
 comm_event_purge_days	14
-database_version	5.28.0
+database_version	5.29.0
 detector_auto_fail_enable	true
 detector_event_purge_days	90
 detector_occ_spike_secs	60
