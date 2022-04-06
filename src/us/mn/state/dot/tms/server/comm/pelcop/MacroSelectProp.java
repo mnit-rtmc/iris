a22733b8d98f086ccb05639cfa1577533fa87ed3
Doug Lau <doug.p.lau@gmail.com>
2022-04-06 09:01:31 -0500

    pelcop: Remove unused imports

--- src/us/mn/state/dot/tms/server/comm/pelcop/MacroSelectProp.java
+++ src/us/mn/state/dot/tms/server/comm/pelcop/MacroSelectProp.java
@@ -1,6 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2017-2018  Minnesota Department of Transportation
+ * Copyright (C) 2017-2022  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
 package us.mn.state.dot.tms.server.comm.pelcop;
 
 import java.nio.ByteBuffer;
-import us.mn.state.dot.tms.server.CameraImpl;
 import us.mn.state.dot.tms.server.VideoMonitorImpl;
 import us.mn.state.dot.tms.server.comm.Operation;
 import us.mn.state.dot.tms.server.comm.ParsingException;
