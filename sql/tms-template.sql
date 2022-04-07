8f7dc463ca1c8b654ee57187f894edab19ca12ad
Doug Lau <doug.p.lau@gmail.com>
2022-04-07 11:25:08 -0500

    Whitespace cleanup

--- sql/tms-template.sql
+++ sql/tms-template.sql
@@ -2365,11 +2365,11 @@ CREATE VIEW detector_label_view AS
 GRANT SELECT ON detector_label_view TO PUBLIC;
 
 CREATE TABLE event.detector_event (
-	event_id INTEGER DEFAULT nextval('event.event_id_seq') NOT NULL,
-	event_date TIMESTAMP WITH time zone NOT NULL,
-	event_desc_id INTEGER NOT NULL
-		REFERENCES event.event_description(event_desc_id),
-	device_id VARCHAR(20) REFERENCES iris._detector(name) ON DELETE CASCADE
+    event_id INTEGER DEFAULT nextval('event.event_id_seq') NOT NULL,
+    event_date TIMESTAMP WITH time zone NOT NULL,
+    event_desc_id INTEGER NOT NULL
+        REFERENCES event.event_description(event_desc_id),
+    device_id VARCHAR(20) REFERENCES iris._detector(name) ON DELETE CASCADE
 );
 
 CREATE VIEW detector_view AS
@@ -2410,12 +2410,12 @@ GRANT SELECT ON detector_auto_fail_view TO PUBLIC;
 -- GPS
 --
 CREATE TABLE iris._gps (
-	name VARCHAR(20) PRIMARY KEY,
-	notes VARCHAR(32),
-	latest_poll TIMESTAMP WITH time zone,
-	latest_sample TIMESTAMP WITH time zone,
-	lat double precision,
-	lon double precision
+    name VARCHAR(20) PRIMARY KEY,
+    notes VARCHAR(32),
+    latest_poll TIMESTAMP WITH time zone,
+    latest_sample TIMESTAMP WITH time zone,
+    lat double precision,
+    lon double precision
 );
 
 ALTER TABLE iris._gps ADD CONSTRAINT _gps_fkey
@@ -2461,8 +2461,8 @@ END;
 $gps_update$ LANGUAGE plpgsql;
 
 CREATE TRIGGER gps_update_trig
-	INSTEAD OF UPDATE ON iris.gps
-	FOR EACH ROW EXECUTE PROCEDURE iris.gps_update();
+    INSTEAD OF UPDATE ON iris.gps
+    FOR EACH ROW EXECUTE PROCEDURE iris.gps_update();
 
 CREATE TRIGGER gps_delete_trig
     INSTEAD OF DELETE ON iris.gps
@@ -2477,104 +2477,104 @@ GRANT SELECT ON gps_view TO PUBLIC;
 -- DMS, Graphic, Font, Sign Message, Quick Message, Word, Color Scheme
 --
 CREATE TABLE iris.font (
-	name VARCHAR(16) PRIMARY KEY,
-	f_number INTEGER UNIQUE NOT NULL,
-	height INTEGER NOT NULL,
-	width INTEGER NOT NULL,
-	line_spacing INTEGER NOT NULL,
-	char_spacing INTEGER NOT NULL,
-	version_id INTEGER NOT NULL
+    name VARCHAR(16) PRIMARY KEY,
+    f_number INTEGER UNIQUE NOT NULL,
+    height INTEGER NOT NULL,
+    width INTEGER NOT NULL,
+    line_spacing INTEGER NOT NULL,
+    char_spacing INTEGER NOT NULL,
+    version_id INTEGER NOT NULL
 );
 
 ALTER TABLE iris.font
-	ADD CONSTRAINT font_number_ck
-	CHECK (f_number > 0 AND f_number <= 255);
+    ADD CONSTRAINT font_number_ck
+    CHECK (f_number > 0 AND f_number <= 255);
 ALTER TABLE iris.font
-	ADD CONSTRAINT font_height_ck
-	CHECK (height > 0 AND height <= 30);
+    ADD CONSTRAINT font_height_ck
+    CHECK (height > 0 AND height <= 30);
 ALTER TABLE iris.font
-	ADD CONSTRAINT font_width_ck
-	CHECK (width >= 0 AND width <= 12);
+    ADD CONSTRAINT font_width_ck
+    CHECK (width >= 0 AND width <= 12);
 ALTER TABLE iris.font
-	ADD CONSTRAINT font_line_sp_ck
-	CHECK (line_spacing >= 0 AND line_spacing <= 9);
+    ADD CONSTRAINT font_line_sp_ck
+    CHECK (line_spacing >= 0 AND line_spacing <= 9);
 ALTER TABLE iris.font
-	ADD CONSTRAINT font_char_sp_ck
-	CHECK (char_spacing >= 0 AND char_spacing <= 6);
+    ADD CONSTRAINT font_char_sp_ck
+    CHECK (char_spacing >= 0 AND char_spacing <= 6);
 
 CREATE FUNCTION iris.font_ck() RETURNS TRIGGER AS
-	$font_ck$
+    $font_ck$
 DECLARE
-	g_width INTEGER;
+    g_width INTEGER;
 BEGIN
-	IF NEW.width > 0 THEN
-		SELECT width INTO g_width FROM iris.glyph WHERE font = NEW.name;
-		IF FOUND AND NEW.width != g_width THEN
-			RAISE EXCEPTION 'width does not match glyph';
-		END IF;
-	END IF;
-	RETURN NEW;
+    IF NEW.width > 0 THEN
+        SELECT width INTO g_width FROM iris.glyph WHERE font = NEW.name;
+        IF FOUND AND NEW.width != g_width THEN
+            RAISE EXCEPTION 'width does not match glyph';
+        END IF;
+    END IF;
+    RETURN NEW;
 END;
 $font_ck$ LANGUAGE plpgsql;
 
 CREATE TRIGGER font_ck_trig
-	BEFORE UPDATE ON iris.font
-	FOR EACH ROW EXECUTE PROCEDURE iris.font_ck();
+    BEFORE UPDATE ON iris.font
+    FOR EACH ROW EXECUTE PROCEDURE iris.font_ck();
 
 CREATE TRIGGER font_notify_trig
-	AFTER INSERT OR UPDATE OR DELETE ON iris.font
-	FOR EACH STATEMENT EXECUTE PROCEDURE iris.table_notify();
+    AFTER INSERT OR UPDATE OR DELETE ON iris.font
+    FOR EACH STATEMENT EXECUTE PROCEDURE iris.table_notify();
 
 CREATE VIEW font_view AS
-	SELECT name, f_number, height, width, line_spacing, char_spacing,
-	       version_id
-	FROM iris.font;
+    SELECT name, f_number, height, width, line_spacing, char_spacing,
+           version_id
+    FROM iris.font;
 GRANT SELECT ON font_view TO PUBLIC;
 
 CREATE TABLE iris.glyph (
-	name VARCHAR(20) PRIMARY KEY,
-	font VARCHAR(16) NOT NULL REFERENCES iris.font(name),
-	code_point INTEGER NOT NULL,
-	width INTEGER NOT NULL,
-	pixels VARCHAR(128) NOT NULL
+    name VARCHAR(20) PRIMARY KEY,
+    font VARCHAR(16) NOT NULL REFERENCES iris.font(name),
+    code_point INTEGER NOT NULL,
+    width INTEGER NOT NULL,
+    pixels VARCHAR(128) NOT NULL
 );
 
 ALTER TABLE iris.glyph
-	ADD CONSTRAINT glyph_code_point_ck
-	CHECK (code_point > 0 AND code_point < 65536);
+    ADD CONSTRAINT glyph_code_point_ck
+    CHECK (code_point > 0 AND code_point < 65536);
 ALTER TABLE iris.glyph
     ADD CONSTRAINT glyph_width_ck
     CHECK (width >= 0 AND width <= 32);
 
 CREATE FUNCTION iris.glyph_ck() RETURNS TRIGGER AS
-	$glyph_ck$
+    $glyph_ck$
 DECLARE
-	f_width INTEGER;
+    f_width INTEGER;
 BEGIN
-	SELECT width INTO f_width FROM iris.font WHERE name = NEW.font;
-	IF f_width > 0 AND f_width != NEW.width THEN
-		RAISE EXCEPTION 'width does not match font';
-	END IF;
-	RETURN NEW;
+    SELECT width INTO f_width FROM iris.font WHERE name = NEW.font;
+    IF f_width > 0 AND f_width != NEW.width THEN
+        RAISE EXCEPTION 'width does not match font';
+    END IF;
+    RETURN NEW;
 END;
 $glyph_ck$ LANGUAGE plpgsql;
 
 CREATE TRIGGER glyph_ck_trig
-	BEFORE INSERT OR UPDATE ON iris.glyph
-	FOR EACH ROW EXECUTE PROCEDURE iris.glyph_ck();
+    BEFORE INSERT OR UPDATE ON iris.glyph
+    FOR EACH ROW EXECUTE PROCEDURE iris.glyph_ck();
 
 CREATE TRIGGER glyph_notify_trig
-	AFTER INSERT OR UPDATE OR DELETE ON iris.glyph
-	FOR EACH STATEMENT EXECUTE PROCEDURE iris.table_notify();
+    AFTER INSERT OR UPDATE OR DELETE ON iris.glyph
+    FOR EACH STATEMENT EXECUTE PROCEDURE iris.table_notify();
 
 CREATE VIEW glyph_view AS
-	SELECT name, font, code_point, width, pixels
-	FROM iris.glyph;
+    SELECT name, font, code_point, width, pixels
+    FROM iris.glyph;
 GRANT SELECT ON glyph_view TO PUBLIC;
 
 CREATE TABLE iris.dms_type (
-	id INTEGER PRIMARY KEY,
-	description VARCHAR(32) NOT NULL
+    id INTEGER PRIMARY KEY,
+    description VARCHAR(32) NOT NULL
 );
 
 COPY iris.dms_type (id, description) FROM stdin;
@@ -2588,8 +2588,8 @@ COPY iris.dms_type (id, description) FROM stdin;
 \.
 
 CREATE TABLE iris.color_scheme (
-	id INTEGER PRIMARY KEY,
-	description VARCHAR(16) NOT NULL
+    id INTEGER PRIMARY KEY,
+    description VARCHAR(16) NOT NULL
 );
 
 COPY iris.color_scheme (id, description) FROM stdin;
