b6327ae2b2c18e8e32176169df9d03f2ebfa9b79
Doug Lau <doug.p.lau@gmail.com>
2022-09-22 13:32:09 -0500

    ntcip: Store full module table as JSON

--- src/us/mn/state/dot/tms/server/comm/ntcip/OpQueryModules.java
+++ src/us/mn/state/dot/tms/server/comm/ntcip/OpQueryModules.java
@@ -15,8 +15,6 @@
 package us.mn.state.dot.tms.server.comm.ntcip;
 
 import java.io.IOException;
-import org.json.JSONException;
-import org.json.JSONObject;
 import us.mn.state.dot.tms.server.DeviceImpl;
 import us.mn.state.dot.tms.server.comm.CommMessage;
 import us.mn.state.dot.tms.server.comm.PriorityLevel;
@@ -33,24 +31,8 @@ import us.mn.state.dot.tms.server.comm.snmp.ASN1String;
  */
 public class OpQueryModules extends OpNtcip {
 
-	/** Trim and truncate a string, with null checking.
-	 * @param value String to be truncated (may be null).
-	 * @param maxlen Maximum length of string (characters).
-	 * @return Trimmed, truncated string, or null. */
-	static private String trimTruncate(String value, int maxlen) {
-		if (value != null) {
-			String v = value.trim();
-			if (v.length() > 0) {
-				return (v.length() <= maxlen)
-				      ? v
-				      : v.substring(0, maxlen);
-			}
-		}
-		return null;
-	}
-
-	/** Controller setup */
-	private final JSONObject setup = new JSONObject();
+	/** Module table */
+	private final ModuleTable mod_table = new ModuleTable();
 
 	/** Create a new module query operation */
 	public OpQueryModules(DeviceImpl d) {
@@ -69,68 +51,30 @@ public class OpQueryModules extends OpNtcip {
 		/** Query the number of modules */
 		@SuppressWarnings("unchecked")
 		protected Phase poll(CommMessage mess) throws IOException {
-			ASN1Integer modules = globalMaxModules.makeInt();
-			mess.add(modules);
+			mess.add(mod_table.modules);
 			mess.queryProps();
-			logQuery(modules);
-			return new QueryModules(modules.getInteger());
+			logQuery(mod_table.modules);
+			return !mod_table.isDone() ? new QueryModule() : null;
 		}
 	}
 
-	/** Phase to query the module information */
-	protected class QueryModules extends Phase {
-
-		/** Count of rows in the module table */
-		private final int count;
-
-		/** Module row number to query */
-		private int mod = 1;
-
-		/** Create a QueryModules phase */
-		private QueryModules(int c) {
-			count = c;
-		}
+	/** Phase to query a module */
+	protected class QueryModule extends Phase {
 
 		/** Query the module make, model and version */
 		@SuppressWarnings("unchecked")
 		protected Phase poll(CommMessage mess) throws IOException {
-			ASN1String make = moduleMake.makeStr(mod);
-			ASN1String model = moduleModel.makeStr(mod);
-			ASN1String version = moduleVersion.makeStr(mod);
-			ASN1Enum<ModuleType> m_type = new ASN1Enum<ModuleType>(
-				ModuleType.class, moduleType.node, mod);
-			mess.add(make);
-			mess.add(model);
-			mess.add(version);
-			mess.add(m_type);
+			ModuleTable.Row tr = mod_table.addRow();
+			mess.add(tr.make);
+			mess.add(tr.model);
+			mess.add(tr.version);
+			mess.add(tr.m_type);
 			mess.queryProps();
-			logQuery(make);
-			logQuery(model);
-			logQuery(version);
-			logQuery(m_type);
-			if (m_type.getEnum() == ModuleType.hardware) {
-				setSetup("hw_make", make.getValue());
-				setSetup("hw_model", model.getValue());
-				setSetup("hw_version", version.getValue());
-			}
-			if (m_type.getEnum() == ModuleType.software) {
-				setSetup("sw_make", make.getValue());
-				setSetup("sw_model", model.getValue());
-				setSetup("sw_version", version.getValue());
-			}
-			mod += 1;
-			return (mod <= count) ? this : null;
-		}
-	}
-
-	/** Set a JSON setup value */
-	private void setSetup(String key, String value) {
-		try {
-			setup.put(key, trimTruncate(value, 64));
-		}
-		catch (JSONException e) {
-			// malformed JSON
-			e.printStackTrace();
+			logQuery(tr.make);
+			logQuery(tr.model);
+			logQuery(tr.version);
+			logQuery(tr.m_type);
+			return !mod_table.isDone() ? this : null;
 		}
 	}
 
@@ -138,7 +82,7 @@ public class OpQueryModules extends OpNtcip {
 	@Override
 	public void cleanup() {
 		if (isSuccess())
-			controller.setSetupNotify(setup.toString());
+			controller.setSetupNotify(mod_table.toJson());
 		super.cleanup();
 	}
 }
---
+++ src/us/mn/state/dot/tms/server/comm/ntcip/mib1201/ModuleTable.java
@@ -0,0 +1,127 @@
+/*
+ * IRIS -- Intelligent Roadway Information System
+ * Copyright (C) 2022  Minnesota Department of Transportation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package us.mn.state.dot.tms.server.comm.ntcip.mib1201;
+
+import java.util.ArrayList;
+import static us.mn.state.dot.tms.server.comm.ntcip.mib1201.MIB1201.*;
+import us.mn.state.dot.tms.server.comm.snmp.ASN1Enum;
+import us.mn.state.dot.tms.server.comm.snmp.ASN1Integer;
+import us.mn.state.dot.tms.server.comm.snmp.ASN1String;
+import us.mn.state.dot.tms.utils.Json;
+
+/**
+ * Module table, where each table row contains a hardware/software module.
+ *
+ * @author Douglas Lau
+ */
+public class ModuleTable {
+
+	/** Maximum length of string values (characters) */
+	static private final int MAXLEN = 64;
+
+	/** Trim and truncate a string, with null checking.
+	 * @param s String to be truncated.
+	 * @return Trimmed, truncated string, or null. */
+	static private String trimTruncate(ASN1String s) {
+		String value = s.getValue();
+		String v = value.trim();
+		if (v.length() > 0) {
+			return (v.length() <= MAXLEN)
+			      ? v
+			      : v.substring(0, MAXLEN);
+		}
+		return null;
+	}
+
+	/** Module row */
+	static public class Row {
+		public final ASN1String make;
+		public final ASN1String model;
+		public final ASN1String version;
+		public final ASN1Enum<ModuleType> m_type;
+
+		/** Create a table row */
+		private Row(int row) {
+			make = moduleMake.makeStr(row);
+			model = moduleModel.makeStr(row);
+			version = moduleVersion.makeStr(row);
+			m_type = new ASN1Enum<ModuleType>(ModuleType.class,
+				moduleType.node, row);
+		}
+
+		/** Get JSON representation */
+		private String toJson() {
+			StringBuilder sb = new StringBuilder();
+			sb.append('{');
+			sb.append(Json.str("make", trimTruncate(make)));
+			sb.append(Json.str("model", trimTruncate(model)));
+			sb.append(Json.str("version", trimTruncate(version)));
+			// remove trailing comma
+			if (sb.charAt(sb.length() - 1) == ',')
+				sb.setLength(sb.length() - 1);
+			sb.append("},");
+			return sb.toString();
+		}
+	}
+
+	/** Number of modules in table */
+	public final ASN1Integer modules = globalMaxModules.makeInt();
+
+	/** Check if all rows have been read */
+	public boolean isDone() {
+		return table_rows.size() >= size();
+	}
+
+	/** Get number of modules in table */
+	private int size() {
+		return modules.getInteger();
+	}
+
+	/** Rows in table */
+	private final ArrayList<Row> table_rows = new ArrayList<Row>();
+
+	/** Add a row to the table */
+	public Row addRow() {
+		Row tr = new Row(table_rows.size() + 1);
+		table_rows.add(tr);
+		return tr;
+	}
+
+	/** Get JSON representation */
+	public String toJson() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("{\"hw\":");
+		sb.append(typeRowsJson(ModuleType.hardware));
+		sb.append(",\"sw\":");
+		sb.append(typeRowsJson(ModuleType.software));
+		sb.append('}');
+		return sb.toString();
+	}
+
+	/** Get all module rows of the given type as JSON */
+	private String typeRowsJson(ModuleType m_type) {
+		StringBuilder sb = new StringBuilder();
+		sb.append('[');
+		for (Row row : table_rows) {
+			if (row.m_type.getEnum() == m_type)
+				sb.append(row.toJson());
+		}
+		// remove trailing comma
+		if (sb.charAt(sb.length() - 1) == ',')
+			sb.setLength(sb.length() - 1);
+		sb.append(']');
+		return sb.toString();
+	}
+}
