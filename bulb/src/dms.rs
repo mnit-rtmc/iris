46ae1ef4d68c5e8bcd275c0319e45f848d86c0f3
Doug Lau <doug.p.lau@gmail.com>
2023-09-18 13:14:34 -0500

    bulb: Improve composer widget layout

--- bulb/src/dms.rs
+++ bulb/src/dms.rs
@@ -25,6 +25,12 @@ use serde::{Deserialize, Serialize};
 use std::fmt;
 use wasm_bindgen::JsValue;
 
+/// Send button
+const SEND_BUTTON: &str = "<button id='ob_send' type='button'>Send</button>";
+
+/// Clear button
+const CLEAR_BUTTON: &str = "<button id='ob_clear' type='button'>Clear</button>";
+
 /// Photocell status
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct Photocell {
@@ -409,14 +415,8 @@ impl Dms {
             .with_multi_cfg(cfg.multi_cfg())
             .build();
         let mut html = String::new();
-        html.push_str("<div class='fill'>");
-        html.push_str("<select id='pattern'>");
-        for pat in &anc.compose_patterns {
-            html.push_str("<option>");
-            html.push_str(&pat.name);
-            html.push_str("</option>");
-        }
-        html.push_str("</select>");
+        html.push_str("<div class='row'>");
+        html.push_str("<div class='column'>");
         if let Some(pat) = anc.compose_patterns.iter().next() {
             let mut buf = Vec::with_capacity(4096);
             rendzina::render(&mut buf, &dms, &pat.multi, Some(240), Some(60))
@@ -425,6 +425,27 @@ impl Dms {
             b64enc.encode_string(buf, &mut html);
             html.push_str("'/>");
         }
+        html.push_str("<select id='pattern'>");
+        for pat in &anc.compose_patterns {
+            html.push_str("<option>");
+            html.push_str(&pat.name);
+            html.push_str("</option>");
+        }
+        html.push_str("</select>");
+        html.push_str("</div>");
+        html.push_str("<div class='column'>");
+        html.push_str("<select id='line0'>");
+        html.push_str("<option>1</option>");
+        html.push_str("</select>");
+        html.push_str("<select id='line1'>");
+        html.push_str("<option>2</option>");
+        html.push_str("</select>");
+        html.push_str("<select id='line2'>");
+        html.push_str("<option>3</option>");
+        html.push_str("</select>");
+        html.push_str(SEND_BUTTON);
+        html.push_str(CLEAR_BUTTON);
+        html.push_str("</div>");
         html.push_str("</div>");
         Some(html)
     }
