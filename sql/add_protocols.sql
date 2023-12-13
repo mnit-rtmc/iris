beff76d8efc980a0fb423c70b9301b84f6c99624
Doug Lau <doug.p.lau@gmail.com>
2023-12-13 10:18:54 -0600

    Added release notes for 5.49.0

--- sql/add_protocols.sql
+++
@@ -1,9 +0,0 @@
-\set ON_ERROR_STOP
-
-SET SESSION AUTHORIZATION 'tms';
-
--- Reserve protocol values
-INSERT INTO iris.comm_protocol (id, description) VALUES (44, 'GPS Digi WR');
-INSERT INTO iris.comm_protocol (id, description) VALUES (45, 'ONVIF PTZ');
-
-COMMIT;
--- sql/migrate-5.49.sql
+++ sql/migrate-5.49.sql
@@ -5,6 +5,10 @@ BEGIN;
 
 SELECT iris.update_version('5.48.0', '5.49.0');
 
+-- Reserve protocol values
+INSERT INTO iris.comm_protocol (id, description) VALUES (44, 'GPS Digi WR');
+INSERT INTO iris.comm_protocol (id, description) VALUES (45, 'ONVIF PTZ');
+
 -- DELETE camera_preset_store_enable system attribute
 DELETE FROM iris.system_attribute WHERE name = 'camera_preset_store_enable';
 
