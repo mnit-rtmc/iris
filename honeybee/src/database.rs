2bc1e7d27b736f93c83f1e15727bd51e7fc14893
Doug Lau <doug.p.lau@gmail.com>
2024-02-27 12:12:49 -0600

    honeybee: Cleaned up database module

--- honeybee/src/database.rs
+++ honeybee/src/database.rs
@@ -17,7 +17,25 @@ use async_trait::async_trait;
 use bb8::{CustomizeConnection, Pool, PooledConnection};
 use bb8_postgres::PostgresConnectionManager;
 use std::env;
-use tokio_postgres::{Client, NoTls};
+use tokio_postgres::tls::NoTlsStream;
+use tokio_postgres::{Client, Config, Connection, NoTls, Socket};
+
+/// Database state
+#[derive(Clone)]
+pub struct Database {
+    /// Connection configuration
+    config: Config,
+    /// Db connection pool
+    pool: Pool<Manager>,
+}
+
+/// Make database configuration
+fn make_config(db: &str) -> Result<Config> {
+    let username = whoami::username();
+    // Format path for unix domain socket -- not worth using percent_encode
+    let uds = format!("postgres://{username}@%2Frun%2Fpostgresql/{db}");
+    Ok(uds.parse()?)
+}
 
 /// Get time zone from environment variable
 fn time_zone() -> Option<String> {
@@ -31,6 +49,15 @@ fn time_zone() -> Option<String> {
     }
 }
 
+/// The postgres crate sets the session time zone to UTC.
+/// We need to set it back to LOCAL time zone, so that row_to_json
+/// can format properly (for incidents, etc).  Unfortunately,
+/// the LOCAL and DEFAULT zones are also reset to UTC, so the PGTZ
+/// environment variable must be used for this purpose.
+fn sql_init() -> Option<String> {
+    time_zone().map(|tz| format!("SET TIME ZONE '{tz}'"))
+}
+
 /// Postgres connection customizer to fix time zone
 #[derive(Debug)]
 struct TimeZoneCorrecter;
@@ -41,14 +68,8 @@ impl CustomizeConnection<Client, tokio_postgres::Error> for TimeZoneCorrecter {
         &self,
         client: &mut Client,
     ) -> std::result::Result<(), tokio_postgres::Error> {
-        // The postgres crate sets the session time zone to UTC.
-        // We need to set it back to LOCAL time zone, so that row_to_json
-        // can format properly (for incidents, etc).  Unfortunately,
-        // the LOCAL and DEFAULT zones are also reset to UTC, so the PGTZ
-        // environment variable must be used for this purpose.
-        if let Some(tz) = time_zone() {
-            let time_zone = format!("SET TIME ZONE '{tz}'");
-            client.execute(&time_zone[..], &[]).await?;
+        if let Some(sql) = &sql_init() {
+            client.execute(sql, &[]).await?;
         }
         Ok(())
     }
@@ -57,19 +78,8 @@ impl CustomizeConnection<Client, tokio_postgres::Error> for TimeZoneCorrecter {
 /// Postgres connection manager
 type Manager = PostgresConnectionManager<NoTls>;
 
-/// Database state
-#[derive(Clone)]
-pub struct Database {
-    /// Db connection pool
-    pool: Pool<Manager>,
-}
-
 /// Make postgres pool for connecting to a Db
-async fn make_pool(db: &str) -> Result<Pool<Manager>> {
-    let username = whoami::username();
-    // Format path for unix domain socket -- not worth using percent_encode
-    let uds = format!("postgres://{username}@%2Frun%2Fpostgresql/{db}");
-    let config = uds.parse()?;
+async fn make_pool(config: Config) -> Result<Pool<Manager>> {
     let manager = PostgresConnectionManager::new(config, NoTls);
     let correcter = TimeZoneCorrecter;
     let pool = Pool::builder()
@@ -82,12 +92,20 @@ async fn make_pool(db: &str) -> Result<Pool<Manager>> {
 impl Database {
     /// Create database state
     pub async fn new(db: &str) -> Result<Self> {
-        let pool = make_pool(db).await?;
-        Ok(Database { pool })
+        let config = make_config(db)?;
+        let pool = make_pool(config.clone()).await?;
+        Ok(Database { config, pool })
     }
 
-    /// Get database connection
-    pub async fn connection(&self) -> Result<PooledConnection<Manager>> {
+    /// Get database client
+    pub async fn client(&self) -> Result<PooledConnection<Manager>> {
         Ok(self.pool.get().await?)
+    }
+
+    /// Get a dedicated client/connection, no managed by the pool
+    pub async fn dedicated_client(
+        &self,
+    ) -> Result<(Client, Connection<Socket, NoTlsStream>)> {
+        Ok(self.config.connect(NoTls).await?)
     }
 }
