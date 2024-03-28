1ac07bee84cef383be69fe355568cbd8bfdb1a13
Doug Lau <doug.p.lau@gmail.com>
2024-03-28 08:00:48 -0500

    sql: Fix bad sql

--- sql/migrate-5.52.sql
+++ sql/migrate-5.52.sql
@@ -10,7 +10,7 @@ ALTER FUNCTION iris.multi_tags_str(INTEGER)
     SET search_path = pg_catalog, pg_temp;
 
 -- Rename user resource type to i_user
-INSERT INTO iris.resource_type ('name') VALUES ('i_user');
+INSERT INTO iris.resource_type (name) VALUES ('i_user');
 UPDATE iris.permission SET resource_n = 'i_user' WHERE resource_n = 'user';
 UPDATE iris.privilege SET type_n = 'i_user' WHERE type_n = 'user';
 DELETE FROM iris.resource_type WHERE name = 'user';
