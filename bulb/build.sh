72ee6c32b1919d1274cb7286e1690894e97d08f3
Doug Lau <doug.p.lau@gmail.com>
2024-04-23 11:33:26 -0500

    bulb: Improve build.sh script output

--- bulb/build.sh
+++ bulb/build.sh
@@ -2,5 +2,6 @@
 
 set -ex
 cargo build --profile web
-wasm-bindgen --target web ../target/wasm32-unknown-unknown/web/bulb.wasm --out-dir ./pkg
+wasm-bindgen --no-typescript --target web ../target/wasm32-unknown-unknown/web/bulb.wasm --out-dir ./pkg
 # wasm-opt ./pkg/bulb_bg.wasm -Oz -o ./pkg/bulb.wasm
+cp static/* pkg/
