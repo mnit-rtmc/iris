97869399709b5d671cde1f95bdf980c445de72ba
Doug Lau <doug.p.lau@gmail.com>
2024-05-24 15:56:05 -0500

    honeybee: Revert accidental BYPASS_SONAR change

--- honeybee/src/cred.rs
+++ honeybee/src/cred.rs
@@ -18,7 +18,7 @@ use serde::{Deserialize, Serialize};
 use tower_sessions::Session;
 
 /// Switch to bypass sonar connection (debug mode)
-const BYPASS_SONAR: bool = true;
+const BYPASS_SONAR: bool = false;
 
 /// IRIS host name
 const HOST: &str = "localhost.localdomain";
