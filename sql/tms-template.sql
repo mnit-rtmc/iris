9eccf38e557ff86fbcca799adec77cfddc22bd16
Doug Lau <doug.p.lau@gmail.com>
2024-09-09 08:21:13 -0500

    sql: Revert video_monitor_hashtag changes

--- sql/tms-template.sql
+++ sql/tms-template.sql
@@ -5275,27 +5275,6 @@ CREATE TABLE iris._video_monitor (
 ALTER TABLE iris._video_monitor ADD CONSTRAINT _video_monitor_fkey
     FOREIGN KEY (name) REFERENCES iris.controller_io ON DELETE CASCADE;
 
-CREATE FUNCTION iris.video_monitor_hashtag() RETURNS TRIGGER AS
-    $video_monitor_hashtag$
-BEGIN
-    IF (NEW.notes IS DISTINCT FROM OLD.notes) THEN
-        IF (TG_OP != 'INSERT') THEN
-            DELETE FROM iris.hashtag
-            WHERE resource_n = 'video_monitor' AND name = OLD.name;
-        END IF;
-        IF (TG_OP != 'DELETE') THEN
-            INSERT INTO iris.hashtag (resource_n, name, hashtag)
-            SELECT 'video_monitor', NEW.name, iris.parse_tags(NEW.notes);
-        END IF;
-    END IF;
-    RETURN NULL; -- AFTER trigger return is ignored
-END;
-$video_monitor_hashtag$ LANGUAGE plpgsql;
-
-CREATE TRIGGER video_monitor_hashtag_trig
-    AFTER INSERT OR UPDATE OR DELETE ON iris._video_monitor
-    FOR EACH ROW EXECUTE FUNCTION iris.video_monitor_hashtag();
-
 CREATE FUNCTION iris.video_monitor_notify() RETURNS TRIGGER AS
     $video_monitor_notify$
 BEGIN
--- sql/video_monitor_tags.sql
+++ sql/video_monitor_tags.sql
@@ -3,32 +3,6 @@
 SET SESSION AUTHORIZATION 'tms';
 BEGIN;
 
--- Add video monitor hashtag trigger
-CREATE FUNCTION iris.video_monitor_hashtag() RETURNS TRIGGER AS
-    $video_monitor_hashtag$
-BEGIN
-    IF (NEW.notes IS DISTINCT FROM OLD.notes) THEN
-        IF (TG_OP != 'INSERT') THEN
-            DELETE FROM iris.hashtag
-            WHERE resource_n = 'video_monitor' AND name = OLD.name;
-        END IF;
-        IF (TG_OP != 'DELETE') THEN
-            INSERT INTO iris.hashtag (resource_n, name, hashtag)
-            SELECT 'video_monitor', NEW.name, iris.parse_tags(NEW.notes);
-        END IF;
-    END IF;
-    RETURN NULL; -- AFTER trigger return is ignored
-END;
-$video_monitor_hashtag$ LANGUAGE plpgsql;
-
-CREATE TRIGGER video_monitor_hashtag_trig
-    AFTER INSERT OR UPDATE OR DELETE ON iris._video_monitor
-    FOR EACH ROW EXECUTE FUNCTION iris.video_monitor_hashtag();
-
--- Force hashtags to be created by trigger
-UPDATE iris.video_monitor SET notes = notes || ' ';
-UPDATE iris.video_monitor SET notes = trim(notes);
-
 -- Drop group_n from video_monitor
 DROP VIEW video_monitor_view;
 DROP VIEW iris.video_monitor;
