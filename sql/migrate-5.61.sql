37b0a18787810c769ab120e32aad44618d9047dd
Doug Lau <doug.p.lau@gmail.com>
2024-09-09 15:53:15 -0500

    sql: Merged scripts into migrate-5.61.sql

--- sql/migrate-5.61.sql
+++ sql/migrate-5.61.sql
@@ -123,4 +123,93 @@ CREATE VIEW camera_view AS
     LEFT JOIN controller_view ctr ON cio.controller = ctr.name;
 GRANT SELECT ON camera_view TO PUBLIC;
 
+-- Drop group_n from video_monitor
+DROP VIEW video_monitor_view;
+DROP VIEW iris.video_monitor;
+DROP FUNCTION iris.video_monitor_insert();
+DROP FUNCTION iris.video_monitor_update();
+
+ALTER TABLE iris._video_monitor DROP COLUMN group_n;
+
+CREATE VIEW iris.video_monitor AS
+    SELECT m.name, controller, pin, notes, mon_num, restricted, monitor_style,
+           camera
+    FROM iris._video_monitor m
+    JOIN iris.controller_io cio ON m.name = cio.name;
+
+CREATE FUNCTION iris.video_monitor_insert() RETURNS TRIGGER AS
+    $video_monitor_insert$
+BEGIN
+    INSERT INTO iris.controller_io (name, resource_n, controller, pin)
+         VALUES (NEW.name, 'video_monitor', NEW.controller, NEW.pin);
+    INSERT INTO iris._video_monitor (
+        name, notes, mon_num, restricted, monitor_style, camera
+    ) VALUES (
+        NEW.name, NEW.notes, NEW.mon_num, NEW.restricted, NEW.monitor_style,
+        NEW.camera
+    );
+    RETURN NEW;
+END;
+$video_monitor_insert$ LANGUAGE plpgsql;
+
+CREATE TRIGGER video_monitor_insert_trig
+    INSTEAD OF INSERT ON iris.video_monitor
+    FOR EACH ROW EXECUTE FUNCTION iris.video_monitor_insert();
+
+CREATE FUNCTION iris.video_monitor_update() RETURNS TRIGGER AS
+    $video_monitor_update$
+BEGIN
+    UPDATE iris.controller_io
+       SET controller = NEW.controller,
+           pin = NEW.pin
+     WHERE name = OLD.name;
+    UPDATE iris._video_monitor
+       SET notes = NEW.notes,
+           mon_num = NEW.mon_num,
+           restricted = NEW.restricted,
+           monitor_style = NEW.monitor_style,
+           camera = NEW.camera
+     WHERE name = OLD.name;
+    RETURN NEW;
+END;
+$video_monitor_update$ LANGUAGE plpgsql;
+
+CREATE TRIGGER video_monitor_update_trig
+    INSTEAD OF UPDATE ON iris.video_monitor
+    FOR EACH ROW EXECUTE FUNCTION iris.video_monitor_update();
+
+CREATE TRIGGER video_monitor_delete_trig
+    INSTEAD OF DELETE ON iris.video_monitor
+    FOR EACH ROW EXECUTE FUNCTION iris.controller_io_delete();
+
+CREATE VIEW video_monitor_view AS
+    SELECT m.name, m.notes, mon_num, restricted, monitor_style,
+           cio.controller, cio.pin, ctr.condition, ctr.comm_link, camera
+    FROM iris._video_monitor m
+    JOIN iris.controller_io cio ON m.name = cio.name
+    LEFT JOIN controller_view ctr ON cio.controller = ctr.name;
+GRANT SELECT ON video_monitor_view TO PUBLIC;
+
+-- Replace action plan description with notes (+ hashtag_trig)
+DROP VIEW action_plan_view;
+
+ALTER TABLE iris.action_plan ADD COLUMN notes VARCHAR;
+ALTER TABLE iris.action_plan ADD CONSTRAINT action_plan_notes_check
+    CHECK (LENGTH(notes) < 256);
+
+CREATE TRIGGER action_plan_hashtag_trig
+    AFTER INSERT OR UPDATE OR DELETE ON iris.action_plan
+    FOR EACH ROW EXECUTE FUNCTION iris.hashtag_trig('action_plan');
+
+UPDATE iris.action_plan SET notes = description;
+
+ALTER TABLE iris.action_plan DROP COLUMN description;
+ALTER TABLE iris.action_plan DROP COLUMN group_n;
+
+CREATE VIEW action_plan_view AS
+    SELECT name, notes, sync_actions, sticky, ignore_auto_fail, active,
+           default_phase, phase
+    FROM iris.action_plan;
+GRANT SELECT ON action_plan_view TO PUBLIC;
+
 COMMIT;
