ff9a07ea52f58953392cae1f51753d4ad7e5f441
Doug Lau <doug.p.lau@gmail.com>
2025-02-28 10:54:16 -0600

    cap: Convert NWS GeoJSON to "CAP JSON"

--- src/us/mn/state/dot/tms/server/comm/cap/JsonParser.java
+++ src/us/mn/state/dot/tms/server/comm/cap/JsonParser.java
@@ -1,7 +1,6 @@
 /*
  * IRIS -- Intelligent Roadway Information System
- * Copyright (C) 2021-2025  Minnesota Department of Transportation
- * Copyright (C) 2020  SRF Consulting Group, Inc.
+ * Copyright (C) 2025  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,6 +16,7 @@ package us.mn.state.dot.tms.server.comm.cap;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,9 +24,9 @@ import org.json.JSONObject;
 import us.mn.state.dot.tms.server.comm.ParsingException;
 
 /**
- * Common Alerting Protocol (CAP) JSON document parser.
+ * Parser for National Weather Service GeoJSON + CAP hybrid.
  *
- * Parses NWS "CAP" LD-JSON documents and fixes up quirks.
+ * URL: `https://api.weather.gov/alerts/active?area=XX`
  *
  * @author Douglas Lau
  */
@@ -35,33 +35,107 @@ public class JsonParser implements AlertParser {
 	/** Parse alerts */
 	@Override
 	public List<JSONObject> parse(String doc) throws IOException {
+		ArrayList<JSONObject> alerts = new ArrayList<JSONObject>();
 		try {
 			JSONObject jo = new JSONObject(doc);
+			JSONArray features = jo.getJSONArray("features");
+			for (int i = 0; i < features.length(); i++) {
+				JSONObject alert = makeAlert(
+					features.getJSONObject(i));
+				if (alert != null)
+					alerts.add(alert);
+			}
 		}
 		catch (JSONException e) {
 			throw new ParsingException(e);
 		}
-		return new ArrayList<JSONObject>();
+		return alerts;
 	}
-	
-	/*	JSONArray jof = jo.getJSONArray("features");
-		for (int i = 0; i < jof.length(); i++) {
-			JSONObject jf = jof.getJSONObject(i).getJSONObject("properties");
-			PROCESSOR.processAlert(jf);
-		}*/
 
-	/*
-		String id = null;
-		if (ja.has("identifier"))
-			id = ja.getString("identifier");
-		else if (ja.has("id"))
-			id = ja.getString("id");
-			*/
+	/** Convert an NWS GeoJSON feature into a CAP alert */
+	private JSONObject makeAlert(JSONObject feat) {
+		JSONObject alert = new JSONObject();
+		alert.putOnce("identifier", feat.getString("id"));
+		JSONObject props = feat.getJSONObject("properties");
+		alert.putOnce("sender", props.getString("sender"));
+		alert.putOnce("sent", props.getString("sent"));
+		alert.putOnce("status", props.getString("status"));
+		alert.putOnce("msgType", props.getString("messageType"));
+		alert.putOnce("scope", props.optString("scope", "Public"));
+		JSONArray refs = props.optJSONArray("references");
+		if (refs != null)
+			alert.putOnce("references", makeReferences(refs));
+		JSONObject info = new JSONObject();
+		info.append("category", props.getString("category"));
+		info.putOnce("event", props.getString("event"));
+		info.append("responseType", props.getString("response"));
+		info.putOnce("urgency", props.getString("urgency"));
+		info.putOnce("severity", props.getString("severity"));
+		info.putOnce("certainty", props.getString("centainty"));
+		info.putOnce("effective", props.optString("effective"));
+		info.putOnce("onset", props.optString("onset"));
+		info.putOnce("expires", props.optString("expires"));
+		info.putOnce("senderName", props.optString("senderName"));
+		info.putOnce("headline", props.optString("headline"));
+		info.putOnce("description", props.optString("description"));
+		info.putOnce("instruction", props.optString("instruction"));
+		makeParameters(props.optJSONObject("parameters"), info);
+		info.append("area", makeArea(props));
+		alert.append("info", info);
+		return alert;
+	}
 
-	/*
-		String msgType = alert.has("msgType")
-				? alert.getString("msgType")
-				: alert.getString("messageType");
-		CapMsgType msg_type = CapMsgType.fromValue(msgType);
-	*/
+	/** Make CAP references in `sender,identifier,sent` format */
+	private String makeReferences(JSONArray refs) {
+		ArrayList<String> rs = new ArrayList<String>();
+		for (int i = 0; i < refs.length(); i++) {
+			JSONObject ref = refs.getJSONObject(i);
+			String sender = ref.getString("sender");
+			String identifier = ref.getString("identifier");
+			String sent = ref.getString("sent");
+			rs.add(String.join(",", sender, identifier, sent));
+		}
+		return String.join(" ", rs);
+	}
+
+	/** Make parameters */
+	private void makeParameters(JSONObject param, JSONObject info) {
+		if (param != null) {
+			Iterator<String> keys = param.keys();
+			while (keys.hasNext()) {
+				String key = keys.next();
+				JSONArray vals = param.getJSONArray(key);
+				for (int i = 0; i < vals.length(); i++) {
+					String val = vals.getString(i);
+					JSONObject p = new JSONObject();
+					p.putOnce("valueName", key);
+					p.putOnce("value", val);
+					info.append("parameter", p);
+				}
+			}
+		}
+	}
+
+	/** Make area */
+	private JSONObject makeArea(JSONObject props) {
+		JSONObject area = new JSONObject();
+		area.putOnce("areaDesc", props.getString("areaDesc"));
+		JSONObject geocode = props.optJSONObject("geocode");
+		if (geocode != null) {
+			Iterator<String> keys = geocode.keys();
+			while (keys.hasNext()) {
+				String key = keys.next();
+				JSONArray vals = geocode.getJSONArray(key);
+				for (int i = 0; i < vals.length(); i++) {
+					String val = vals.getString(i);
+					JSONObject p = new JSONObject();
+					p.putOnce("valueName", key);
+					p.putOnce("value", val);
+					area.append("geocode", p);
+				}
+			}
+		}
+		// FIXME: convert GeoJSON to CAP polygon
+		return area;
+	}
 }
