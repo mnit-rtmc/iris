73553eb0f9be8cb3fb294657449b13d4a83090c0
Doug Lau <doug.p.lau@gmail.com>
2025-03-24 09:32:14 -0500

    Rearranged client init code

--- src/us/mn/state/dot/tms/client/SonarState.java
+++ src/us/mn/state/dot/tms/client/SonarState.java
@@ -263,6 +263,14 @@ public class SonarState extends Client {
 		return dms_cache;
 	}
 
+	/** Cache of LCS objects */
+	private final LcsCache lcs_cache;
+
+	/** Get the LCS object cache */
+	public LcsCache getLcsCache() {
+		return lcs_cache;
+	}
+
 	/** Cache of incident objects */
 	private final IncCache inc_cache;
 
@@ -293,14 +301,6 @@ public class SonarState extends Client {
 	/** Get the alert info cache */
 	public TypeCache<AlertInfo> getAlertInfos() {
 		return alert_infos;
-	}
-
-	/** Cache of LCS objects */
-	private final LcsCache lcs_cache;
-
-	/** Get the LCS object cache */
-	public LcsCache getLcsCache() {
-		return lcs_cache;
 	}
 
 	/** Cache of weather sensors */
@@ -520,6 +520,7 @@ public class SonarState extends Client {
 		con_cache = new ConCache(this);
 		det_cache = new DetCache(this);
 		dms_cache = new DmsCache(this);
+		lcs_cache = new LcsCache(this);
 		inc_cache = new IncCache(this);
 		alert_configs = new TypeCache<AlertConfig>(AlertConfig.class,
 			this);
@@ -527,7 +528,6 @@ public class SonarState extends Client {
 			this);
 		alert_infos = new TypeCache<AlertInfo>(AlertInfo.class,
 			this);
-		lcs_cache = new LcsCache(this);
 		gate_arm_array_model = new ProxyListModel<GateArmArray>(
 			gate_arm_arrays);
 		gate_arm_array_model.initialize();
@@ -627,8 +627,8 @@ public class SonarState extends Client {
 		}
 		populateReadable(graphics);
 		dms_cache.populate(this);
-		inc_cache.populate(this);
 		lcs_cache.populate(this);
+		inc_cache.populate(this);
 		populateReadable(weather_sensors);
 		if (canRead(WeatherSensor.SONAR_TYPE)) {
 			weather_sensors.ignoreAttribute("operation");
