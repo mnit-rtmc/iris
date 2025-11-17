c98d79c6f6b6659b9f14982344bd026e2c8b3fad
Doug Lau <doug.p.lau@gmail.com>
2025-11-17 09:50:33 -0600

    sql: Add migrate-5.78.sql

--- sql/migrate-5.78.sql
+++ sql/migrate-5.78.sql
@@ -3,6 +3,8 @@
 SET SESSION AUTHORIZATION 'tms';
 BEGIN;
 
+SELECT iris.update_version('5.77.0', '5.78.0');
+
 -- Change comm protocol 31 to RTMS Echo
 UPDATE iris.comm_protocol
     SET description = 'RTMS Echo vlog'
