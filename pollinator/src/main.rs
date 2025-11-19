a615de70f706d29d8e1e51655a90aa3b6ba76f0a
Doug Lau <doug.p.lau@gmail.com>
2025-11-19 13:47:47 -0600

    pollinator: Improve error handling

--- pollinator/src/main.rs
+++ pollinator/src/main.rs
@@ -2,10 +2,11 @@ mod error;
 mod http;
 mod rtms_echo;
 
+use crate::error::Error;
 use crate::rtms_echo::Sensor;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     let mut args = std::env::args();
     let _prog = args.next();
     let host = args.next().unwrap_or(String::from("127.0.0.1"));
@@ -13,17 +14,18 @@ async fn main() {
     let pass = args.next().unwrap_or(String::from("pass"));
 
     let mut sensor = Sensor::new(&host);
-    sensor.login(&user, &pass).await.unwrap();
+    sensor.login(&user, &pass).await?;
 
-    let zones = sensor.poll_zone_identifiers().await.unwrap();
+    let zones = sensor.poll_zone_identifiers().await?;
     for zone in zones {
         println!("  {zone:?}");
     }
 
-    let records = sensor.poll_input_voltage().await.unwrap();
+    let records = sensor.poll_input_voltage().await?;
     for record in records {
         println!("  {record:?}");
     }
 
-    sensor.collect_vehicle_data().await.unwrap();
+    sensor.collect_vehicle_data().await?;
+    Ok(())
 }
