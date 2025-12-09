f9f6d0004b768459d865d390e62e98b48bf1dc57
Doug Lau <doug.p.lau@gmail.com>
2025-12-09 08:17:52 -0600

    mayfly: Fixed counts example program

--- mayfly/examples/counts.rs
+++ mayfly/examples/counts.rs
@@ -1,14 +1,15 @@
-use mayfly::binned::CountData;
-use mayfly::vlog::{VehLogReader, VehicleFilter};
+use mayfly::binned::{BinIter, CountData, VehicleFilter};
+use mayfly::vlog::VehLogReader;
 use std::fs::File;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = File::open("res/test.vlog")?;
     let vlog = VehLogReader::from_reader_blocking(file)?;
+    let events = vlog.events();
     print!("[");
-    for (i, v) in vlog
-        .binned_iter::<CountData>(30, VehicleFilter::default())
-        .enumerate()
+    for (i, v) in
+        BinIter::<CountData>::new(30, &events, VehicleFilter::default())
+            .enumerate()
     {
         if i > 0 {
             print!(",");
