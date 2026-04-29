46a6b73f3f0f1fb350ffe6c75aac36479a0df761
Doug Lau <doug.p.lau@gmail.com>
2026-04-29 14:56:08 -0500

    bulb: Add action plan resource buttons

--- bulb/src/start.rs
+++ bulb/src/start.rs
@@ -361,6 +361,9 @@ async fn handle_resource_change(res: Option<Res>, search: &str) -> Result<()> {
     let sb_list = doc.elem::<Element>("sb_list")?;
     sb_list.set_inner_html("");
     let base = res.map(|r| r.base());
+    if let Some(el) = doc.opt_elem::<Element>("res_plan_row") {
+        el.set_class_name(row_class(base == Some(Res::ActionPlan)));
+    }
     if let Some(el) = doc.opt_elem::<Element>("res_dms_row") {
         el.set_class_name(row_class(base == Some(Res::Dms)));
     }
@@ -476,14 +479,17 @@ fn add_input_listener(el: &Element) -> Result<()> {
 /// Handle an input event
 fn handle_input(id: String) {
     match id.as_str() {
-        "res_dms" | "res_msg_pattern" | "res_sign_config" | "res_word"
-        | "res_lcs" | "res_lcs_state" | "res_video_monitor"
-        | "res_monitor_style" | "res_flow_stream" | "res_comm_config"
-        | "res_comm_link" | "res_controller" | "res_gps" | "res_road"
-        | "res_detector" | "res_map_extent" | "res_r_node"
-        | "res_permission" | "res_user" | "res_role" | "res_domain"
-        | "res_system_attr" | "res_event_config" | "res_alarm"
-        | "res_cabinet_style" | "sb_resource" => handle_res_change(),
+        "res_action_plan" | "res_plan_phase" | "res_day_plan"
+        | "res_device_action" | "res_dms" | "res_msg_pattern"
+        | "res_sign_config" | "res_word" | "res_lcs" | "res_lcs_state"
+        | "res_video_monitor" | "res_monitor_style" | "res_flow_stream"
+        | "res_comm_config" | "res_comm_link" | "res_controller"
+        | "res_gps" | "res_road" | "res_detector" | "res_map_extent"
+        | "res_r_node" | "res_permission" | "res_user" | "res_role"
+        | "res_domain" | "res_system_attr" | "res_event_config"
+        | "res_alarm" | "res_cabinet_style" | "sb_resource" => {
+            handle_res_change()
+        }
         "sb_search" | "sb_state" => spawn_future(handle_search()),
         "ob_view" => handle_ob_view_ev(),
         _ => spawn_future(handle_input_other(id)),
