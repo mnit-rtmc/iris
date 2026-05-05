37027409a97ebb0f279283da2565b83ed9b9da77
Doug Lau <doug.p.lau@gmail.com>
2026-05-05 08:20:11 -0500

    sql: Add migrate-5.84.sql script

--- sql/migrate-5.84.sql
+++ sql/migrate-5.84.sql
@@ -3,6 +3,53 @@
 SET SESSION AUTHORIZATION 'tms';
 BEGIN;
 
+SELECT iris.update_version('5.83.0', '5.84.0');
+
+-- Change alarm base resource to system_attribute
+UPDATE iris.resource_type SET base = 'system_attribute' WHERE name = 'alarm';
+
+-- Remove controller as a base resource; use comm_config instead
+UPDATE iris.resource_type SET base = NULL WHERE name = 'comm_config';
+
+UPDATE iris.permission
+    SET base_resource = 'comm_config'
+    WHERE base_resource = 'controller';
+
+UPDATE iris.resource_type
+    SET base = 'comm_config'
+    WHERE name IN ('comm_link', 'controller', 'geo_loc', 'gps', 'modem');
+
+-- Remove detector as a base resource; use road instead
+UPDATE iris.resource_type SET base = NULL WHERE name = 'road';
+
+UPDATE iris.permission
+    SET base_resource = 'road'
+    WHERE base_resource = 'detector';
+
+UPDATE iris.resource_type
+    SET base = 'road'
+    WHERE name IN ('detector', 'map_extent', 'r_node');
+
+DELETE FROM iris.resource_type WHERE name = 'station';
+
+-- Remove alert_config as a base resource; use weather_sensor instead
+DELETE FROM iris.permission WHERE base_resource = 'alert_config';
+
+UPDATE iris.resource_type
+    SET base = 'weather_sensor'
+    WHERE name IN ('alert_config', 'alert_info', 'alert_message');
+
+-- Remove modem devices
+DROP VIEW modem_view;
+DROP TABLE iris.modem;
+DELETE FROM iris.resource_type WHERE name = 'modem';
+
+-- Add hashtag to map extent
+ALTER TABLE iris.map_extent ADD COLUMN hashtag VARCHAR(16);
+
+ALTER TABLE iris.map_extent ADD
+    CONSTRAINT hashtag_ck CHECK (hashtag ~ '^#[A-Za-z0-9]+$');
+
 -- Add phase actions
 INSERT INTO iris.resource_type (name, base)
     VALUES ('phase_action', 'action_plan');
