20d3f4dc82b063d9f1a90565b63a2dd55ccc97d3
Doug Lau <doug.p.lau@gmail.com>
2026-06-15 09:30:08 -0500

    sql: Add migrate-5.86.sql script

--- sql/migrate-5.86.sql
+++ sql/migrate-5.86.sql
@@ -3,6 +3,14 @@
 SET SESSION AUTHORIZATION 'tms';
 BEGIN;
 
+SELECT iris.update_version('5.85.0', '5.86.0');
+
+-- Add attributes to disable legacy UI
+INSERT INTO iris.system_attribute (name, value) VALUES
+    ('legacy_ui_system_enable', 'true'),
+    ('legacy_ui_users_enable', 'true')
+ON CONFLICT (name) DO NOTHING;
+
 -- Update phase_action_view condition column
 DROP VIEW phase_action_view;
 CREATE VIEW phase_action_view AS
@@ -11,5 +19,9 @@ CREATE VIEW phase_action_view AS
     FROM iris.phase_action pa
     JOIN iris.action_condition c ON pa.condition = c.id;
 GRANT SELECT ON phase_action_view TO PUBLIC;
+
+-- Add max-pressure meter algorithm value
+INSERT INTO iris.meter_algorithm (id, description) VALUES
+    (4, 'Max-Pressure Metering');
 
 COMMIT;
