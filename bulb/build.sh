56752ce8e5b1bebed43e1285d4bdcbcb78e06691
Doug Lau <doug.p.lau@gmail.com>
2026-06-29 08:52:28 -0500

    v5.86.4

--- bulb/build.sh
+++ bulb/build.sh
@@ -5,3 +5,5 @@ cargo build --profile web
 wasm-bindgen --no-typescript --target web ../target/wasm32-unknown-unknown/web/bulb.wasm --out-dir ./pkg
 # wasm-opt ./pkg/bulb_bg.wasm -Oz -o ./pkg/bulb.wasm
 cp static/index.html static/bulb.css static/wyrm.css static/iris.svg static/favicon.png pkg/
+
+# cp -r pkg/* /var/lib/iris/web/bulb/
