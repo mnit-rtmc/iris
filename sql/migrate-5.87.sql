58e5b74fc025177a64550872e5dc28046905e8ee
Doug Lau <doug.p.lau@gmail.com>
2026-07-13 11:23:44 -0500

    sql: Add migrate-5.87.sql script

--- sql/migrate-5.87.sql
+++ sql/migrate-5.87.sql
@@ -3,6 +3,8 @@
 SET SESSION AUTHORIZATION 'tms';
 BEGIN;
 
+SELECT iris.update_version('5.86.0', '5.87.0');
+
 -- Add hashtag to msg_line
 ALTER TABLE iris.msg_line ADD COLUMN hashtag VARCHAR(16);
 ALTER TABLE iris.msg_line ADD
@@ -38,5 +40,25 @@ DELETE FROM iris.msg_pattern
         SELECT DISTINCT msg_pattern
         FROM iris.device_action
         WHERE msg_pattern IS NOT NULL);
+
+-- Add "template_label" to camera_view
+DROP VIEW camera_view;
+CREATE VIEW camera_view AS
+    SELECT c.name, cam_num, c.cam_template, t.label AS template_label,
+           encoder_type, et.make, et.model,
+           et.config, c.enc_address, c.enc_port, c.enc_mcast, c.enc_channel,
+           c.publish, c.video_loss, c.geo_loc,
+           l.roadway, l.road_dir, l.cross_mod, l.cross_street, l.cross_dir,
+           l.landmark, l.lat, l.lon, l.corridor, l.location,
+           cio.controller, ctr.comm_link, ctr.drop_id,
+           cnd.description AS condition, c.notes
+    FROM iris._camera c
+    JOIN iris.controller_io cio ON c.name = cio.name
+    LEFT JOIN iris.camera_template t ON c.cam_template = t.name
+    LEFT JOIN iris.encoder_type et ON c.encoder_type = et.name
+    LEFT JOIN geo_loc_view l ON c.geo_loc = l.name
+    LEFT JOIN iris.controller ctr ON cio.controller = ctr.name
+    LEFT JOIN iris.condition cnd ON ctr.condition = cnd.id;
+GRANT SELECT ON camera_view TO PUBLIC;
 
 COMMIT;
